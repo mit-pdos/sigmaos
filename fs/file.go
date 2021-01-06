c04249936452fded1c2b934f59cc234457535526
Frans Kaashoek <kaashoek@mit.edu>
2021-01-06 16:12:45 -0500

    Support file write

--- fs/file.go
+++ fs/file.go
@@ -17,18 +17,37 @@ func (f *File) Len() np.Tlength {
 	return np.Tlength(len(f.data))
 }
 
+func (f *File) LenOff() np.Toffset {
+	return np.Toffset(len(f.data))
+}
+
 func (f *File) Write(offset np.Toffset, data []byte) (np.Tsize, error) {
-	f.data = data
-	return np.Tsize(len(data)), nil
+	cnt := np.Tsize(len(data))
+	sz := np.Toffset(len(data))
+	if offset > f.LenOff() { // passed end of file?
+		n := f.LenOff() - offset
+		f.data = append(f.data, make([]byte, n)...)
+		f.data = append(f.data, data...)
+		return cnt, nil
+	}
+
+	var d []byte
+	if offset+sz < f.LenOff() { // in the middle of the file?
+		d = f.data[offset+sz:]
+	}
+	f.data = f.data[0:offset]
+	f.data = append(f.data, data...)
+	f.data = append(f.data, d...)
+	return cnt, nil
 }
 
 func (f *File) Read(offset np.Toffset, n np.Tsize) ([]byte, error) {
-	if offset >= np.Toffset(len(f.data)) {
+	if offset >= f.LenOff() {
 		return nil, nil
 	} else {
 		end := offset + np.Toffset(n)
-		if end >= np.Toffset(len(f.data)) {
-			end = np.Toffset(len(f.data))
+		if end >= f.LenOff() {
+			end = f.LenOff()
 		}
 		return f.data[offset:end], nil
 	}

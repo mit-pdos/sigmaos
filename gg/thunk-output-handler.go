97ae93b33462da9ab07236b9cf82ddb10596cea3
ArielSzekely <arielszekely@gmail.com>
2021-02-04 11:26:44 -0500

    [gg] Some cleanup for gg thunk output handlers

--- gg/thunk-output-handler.go
+++ gg/thunk-output-handler.go
@@ -39,13 +39,16 @@ func (toh *ThunkOutputHandler) Exit() {
 
 // XXX Check cache
 func (toh *ThunkOutputHandler) Work() {
-  newThunks, outputNames := toh.getOutputThunksAndFiles()
+  // Read the thunk output file
+  thunkOutput := toh.readThunkOutput()
+  newThunks := toh.getNewThunks(thunkOutput)
+  outputFiles := toh.getOutputFiles(thunkOutput)
   if len(newThunks) == 0 {
     // We have produced a value, and need to propagate it down to functions which
     // depend on us.
     toh.propagateResultUpstream()
   } else {
-    downstreamThunks := toh.spawnDownstreamThunks(newThunks, outputNames)
+    downstreamThunks := toh.spawnDownstreamThunks(newThunks, outputFiles)
     for _, t := range downstreamThunks {
       toh.Wait(t)
     }
@@ -53,12 +56,12 @@ func (toh *ThunkOutputHandler) Work() {
   }
 }
 
-func (toh *ThunkOutputHandler) spawnDownstreamThunks(newThunks map[string][]string, outputNames map[string][]string) []string {
+func (toh *ThunkOutputHandler) spawnDownstreamThunks(newThunks map[string][]string, outputFiles map[string][]string) []string {
   children := []string{}
   for thunkHash, deps := range newThunks {
     db.DPrintf("Handler [%v] spawning [%v], depends on [%v]\n", toh.thunkHash, thunkHash, deps)
     exPid := spawnExecutor(toh, thunkHash, deps)
-    child := spawnThunkOutputHandler(toh, exPid, thunkHash, outputNames[thunkHash])
+    child := spawnThunkOutputHandler(toh, exPid, thunkHash, outputFiles[thunkHash])
     children = append(children, child)
   }
   return children
@@ -76,47 +79,63 @@ func (toh *ThunkOutputHandler) propagateResultUpstream() {
   }
 }
 
+func (toh *ThunkOutputHandler) getOutputFiles(thunkOutput []string) map[string][]string {
+  outputFiles := make(map[string][]string)
+  first := true
+  for _, line := range thunkOutput {
+    // Get output file name this thunk corresponds to
+    result := strings.Split(strings.TrimSpace(line), "=")
+    tag := result[0]
+    // Get output thunk's hash
+    hash := strings.Split(result[1], " ")[0]
+    // XXX remove this sanity check
+    if _, ok := outputFiles[hash]; ok {
+      log.Fatalf("output file was already in map when parsing thunk output")
+    }
+    outputFiles[hash] = []string{ toh.thunkHash + "#" + tag }
+    if first {
+      first = false
+      outputFiles[hash] = append(outputFiles[hash], toh.thunkHash)
+    }
+  }
+  return outputFiles
+}
+
 // TODO: make this more readable
-func (toh *ThunkOutputHandler) getOutputThunksAndFiles() (map[string][]string, map[string][]string) {
-  thunksAndDeps := make(map[string][]string)
-  outputNames := make(map[string][]string)
+func (toh *ThunkOutputHandler) getNewThunks(thunkOutput []string) map[string][]string {
+  // Maps of new thunks to their dependencies
+  newThunks := make(map[string][]string)
+  for _, line := range thunkOutput {
+    thunkLine := strings.Split(strings.TrimSpace(line), "=")[1]
+    // Compute new thunk's dependencies
+    hashes := strings.Split(thunkLine, " ")
+    for i, h := range hashes {
+      // Thunk actually depends on the output handler of its dependency
+      if i > 0 {
+        hashes[i] = h + OUTPUT_HANDLER_SUFFIX
+      }
+    }
+    newThunks[hashes[0]] = hashes[1:]
+  }
+  return newThunks
+}
+
+func (toh *ThunkOutputHandler) readThunkOutput() []string {
   outputThunksPath := path.Join(
     GG_BLOB_DIR,
     toh.thunkHash + THUNK_OUTPUTS_SUFFIX,
   )
-  f, err := toh.ReadFile(outputThunksPath)
+  contents, err := toh.ReadFile(outputThunksPath)
   if err != nil {
     // XXX switch to db
     log.Fatalf("Error reading thunk outputs [%v]: %v\n", toh.thunkHash, err)
   }
-  trimmed_f := strings.TrimSpace(string(f))
-  if len(trimmed_f) > 0 {
-    first := true
-    // Read the thunk output file
-    for _, line := range strings.Split(trimmed_f, "\n") {
-      // Store output file name this thunk corresponds to
-      result := strings.Split(strings.TrimSpace(line), "=")
-      tag := result[0]
-      // Store output thunk & its dependencies
-      hashes := strings.Split(result[1], " ")
-      for i, h := range hashes {
-        if i > 0 {
-          hashes[i] = h + OUTPUT_HANDLER_SUFFIX
-        }
-      }
-      thunksAndDeps[hashes[0]] = hashes[1:]
-      // XXX remove this sanity check
-      if _, ok := outputNames[hashes[0]]; ok {
-        log.Fatalf("output file was already in map when parsing thunk output")
-      }
-      outputNames[hashes[0]] = []string{ toh.thunkHash + "#" + tag }
-      if first {
-        first = false
-        outputNames[hashes[0]] = append(outputNames[hashes[0]], toh.thunkHash)
-      }
-    }
+  trimmedContents := strings.TrimSpace(string(contents))
+  if len(trimmedContents) > 0 {
+    return strings.Split(trimmedContents, "\n")
+  } else {
+    return []string{}
   }
-  return thunksAndDeps, outputNames
 }
 
 func (toh *ThunkOutputHandler) getValue() string {

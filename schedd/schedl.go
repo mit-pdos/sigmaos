dd71ed3008b442574dcbfeed4c32d0b2fe30969f
Frans Kaashoek <kaashoek@mit.edu>
2021-02-13 16:46:26 -0500

    Check error return value at least in test

--- schedd/schedl.go
+++ schedd/schedl.go
@@ -26,7 +26,10 @@ func MakeSchedl(args []string) (*Schedl, error) {
 	s.pid = args[0]
 	s.output = args[1]
 
-	s.Started(s.pid)
+	err := s.Started(s.pid)
+	if err != nil {
+		log.Fatalf("Started: error %v\n", err)
+	}
 
 	return s, nil
 }
@@ -40,5 +43,8 @@ func (s *Schedl) Work() {
 		log.Printf("Makefile error %v\n", err)
 	}
 
-	s.Exiting(s.pid, "OK")
+	err = s.Exiting(s.pid, "OK")
+	if err != nil {
+		log.Fatalf("Exit: error %v\n", err)
+	}
 }

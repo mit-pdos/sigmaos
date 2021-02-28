a642fcc47d821c9d1dd5bba9d757dfdf22e2a0ad
ArielSzekely <arielszekely@gmail.com>
2021-02-28 14:17:57 -0500

    [fslambda] Better error reporting

--- fslambda/downloader.go
+++ fslambda/downloader.go
@@ -33,11 +33,12 @@ func (down *Downloader) Work() {
 	db.DPrintf("Downloading [%v] to [%v]\n", down.src, down.dest)
 	contents, err := down.ReadFile(down.src)
 	if err != nil {
+		log.Printf("Downloader [pid: %v, src: %v, dest: %v] error...\n", down.pid, down.src, down.dest)
 		log.Printf("Read download file error [%v]: %v\n", down.src, err)
 	}
 	err = ioutil.WriteFile(down.dest, contents, 0777)
 	if err != nil {
-		log.Printf("Couldn't download file [%v]: %v\n", down.dest, err)
+		log.Printf("Couldn't write download file [%v]: %v\n", down.dest, err)
 	}
 	// Override umask
 	err = os.Chmod(down.dest, 0777)

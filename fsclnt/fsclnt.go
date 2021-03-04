6620081bdf3b422a7e52535d46c113a250ec0b3e
Frans Kaashoek <kaashoek@mit.edu>
2021-03-04 12:26:38 -0500

    Support union directiories by allowing "queries" in pathname components For example, ~ip to select a directory entry that matches my ip (e.g., an s3npd)

--- fsclnt/fsclnt.go
+++ fsclnt/fsclnt.go
@@ -13,6 +13,7 @@ import (
 	db "ulambda/debug"
 	np "ulambda/ninep"
 	"ulambda/npclnt"
+	"ulambda/npcodec"
 )
 
 const (
@@ -220,6 +221,87 @@ func (fsc *FsClient) clunkFid(fid np.Tfid) {
 	fsc.freeFid(fid)
 }
 
+func (fsc *FsClient) unionMatch(q, name string) bool {
+	db.DPrintf("unionMatch %v %v\n", q, name)
+	switch q {
+	case "~any":
+		return true
+	case "~ip":
+		ip, err := LocalIP()
+		if err != nil {
+			return false
+		}
+		parts := strings.Split(ip, ":")
+		if parts[0] == ip {
+			db.DPrintf("match %v\n", parts[0])
+			return true
+		}
+		return false
+	default:
+		return true
+	}
+	return true
+}
+
+func (fsc *FsClient) unionScan(ch *npclnt.NpChan, fid, fid2 np.Tfid, dirents []*np.Stat, q string) (*np.Rwalk, error) {
+	db.DPrintf("unionScan: %v %v\n", dirents, q)
+	for _, de := range dirents {
+		if fsc.unionMatch(q, de.Name) {
+			reply, err := ch.Walk(fid, fid2, []string{de.Name})
+			if err != nil {
+				return nil, err
+			}
+			return reply, nil
+		}
+	}
+	return nil, nil
+}
+
+func (fsc *FsClient) unionLookup(ch *npclnt.NpChan, fid, fid2 np.Tfid, q string) (*np.Rwalk, error) {
+	db.DPrintf("%v: unionLookup: %v %v %v\n", fsc.uname, fid, fid2, q)
+	_, err := ch.Open(fid, np.OREAD)
+	if err != nil {
+		return nil, err
+	}
+	off := np.Toffset(0)
+	for {
+		reply, err := ch.Read(fid, off, 1024)
+		if err != nil {
+			return nil, err
+		}
+		if len(reply.Data) == 0 {
+			return nil, fmt.Errorf("Not found")
+		}
+		dirents, err := npcodec.Byte2Dir(reply.Data)
+		if err != nil {
+			return nil, err
+		}
+		reply1, err := fsc.unionScan(ch, fid, fid2, dirents, q)
+		if err != nil {
+			return nil, err
+		}
+		if reply1 != nil {
+			return reply1, nil
+		}
+		off += 1024
+	}
+}
+
+func (fsc *FsClient) walkUnion(ch *npclnt.NpChan, fid, fid2 np.Tfid, dir []string, q string) (*np.Rwalk, error) {
+	db.DPrintf("Walk union: %v %v\n", dir, q)
+	fid3 := fsc.allocFid()
+	reply, err := ch.Walk(fid, fid3, dir)
+	if err != nil {
+		return nil, err
+	}
+	reply, err = fsc.unionLookup(fsc.npch(fid), fid3, fid2, q)
+	if err != nil {
+		return nil, err
+	}
+	db.DPrintf("%v: unionLookup -> %v %v", fsc.uname, fid2, reply.Qids)
+	return reply, nil
+}
+
 func (fsc *FsClient) walkOne(path []string) (np.Tfid, int, error) {
 	fid, rest := fsc.mount.resolve(path)
 	db.DPrintf("%v: walkOne: mount -> %v %v\n", fsc.uname, fid, rest)
@@ -232,18 +314,36 @@ func (fsc *FsClient) walkOne(path []string) (np.Tfid, int, error) {
 		return np.NoFid, 0, err
 	}
 	defer fsc.clunkFid(fid1)
-
 	fid2 := fsc.allocFid()
-	reply, err := fsc.npch(fid1).Walk(fid1, fid2, rest)
-	if err != nil {
-		return np.NoFid, 0, err
+	first, union := IsUnion(rest)
+	var reply *np.Rwalk
+	todo := 0
+	if union {
+		reply, err = fsc.walkUnion(fsc.npch(fid1), fid1, fid2,
+			first, rest[len(first)])
+		rest = rest[len(first)+1:]
+		todo = len(rest)
+	} else {
+		reply, err = fsc.npch(fid1).Walk(fid1, fid2, rest)
+		if err != nil {
+			return np.NoFid, 0, err
+		}
+		todo = len(rest) - len(reply.Qids)
+		db.DPrintf("%v: walkOne rest %v -> %v %v", fsc.uname, rest,
+			reply.Qids, todo)
 	}
-	todo := len(rest) - len(reply.Qids)
-	db.DPrintf("%v: walkOne rest %v -> %v %v", fsc.uname, rest, reply.Qids, todo)
-
 	fsc.addFid(fid2, fsc.path(fid1).copyPath())
 	fsc.path(fid2).addn(reply.Qids, rest)
 	return fid2, todo, nil
+}
+
+func IsUnion(path []string) ([]string, bool) {
+	for i, c := range path {
+		if strings.HasPrefix(c, "~") {
+			return path[:i], true
+		}
+	}
+	return nil, false
 }
 
 func IsRemoteTarget(target string) bool {

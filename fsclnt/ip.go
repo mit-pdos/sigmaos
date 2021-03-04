a6c4e94a3996701c16470250090023952f9a3dde
Frans Kaashoek <kaashoek@mit.edu>
2021-03-04 12:35:45 -0500

    Probably should be in a util package

---
+++ fsclnt/ip.go
@@ -0,0 +1,36 @@
+package fsclnt
+
+import (
+	"fmt"
+	"net"
+)
+
+func LocalIP() (string, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+	for _, i := range ifaces {
+		addrs, err := i.Addrs()
+		if err != nil {
+			return "", err
+		}
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			if ip.IsLoopback() {
+				continue
+			}
+			if ip.To4() == nil {
+				continue
+			}
+			return ip.String(), nil
+		}
+	}
+	return "", fmt.Errorf("LocalIP: no IP")
+}

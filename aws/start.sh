3df0dc6add20c292c96443749b56ffbcc59b9a27
Frans Kaashoek <kaashoek@mit.edu>
2021-03-12 08:15:24 -0500

    Fix a few mistakes

--- aws/start.sh
+++ aws/start.sh
@@ -7,29 +7,32 @@ then
 fi
 
 vms=`./lsvpc.py $1 | grep -w VMInstance | cut -d " " -f 5`
-N=0
+
+vma=($vms)
+NAME="${vma[0]}"
+NAMED="${vma[0]}:1111"
+export NAMED="${NAMED}"
+echo "NAME": $NAME
+
 for vm in $vms
 do
-    echo "start $vm"
-    if [ "${N}" -eq 0 ]; then
-	echo "set named"
-	export NAMED=$vm:1111
-    fi
+    echo "START: $vm"
     ssh -i key-$1.pem ubuntu@$vm <<ENDSSH
     ssh-agent bash -c 'ssh-add ~/.ssh/aws-ulambda; (cd ulambda; git pull)'
-    # (cd ulambda; ./make.sh)
-    if [[ "${N}" -eq "0" ]]; then 
-        ./ulambda/bin/named &
+    ./ulambda/stop.sh
+    (cd ulambda; ./make.sh)
+    if [ "${vm}" = "${NAME}" ]; then 
+       echo "START NAMED"
+#       ./ulambda/bin/named &
     fi
     echo "NAMED: " ${NAMED}
     export NAMED="${NAMED}"
     ./ulambda/bin/nps3d &
     # ./ulambda/bin/npuxd &
-    N=1
-    echo "N: " $N
 ENDSSH
 done
-echo "start proxy: $NAMED"
-../bin/proxyd &
-sudo mount -t 9p -o tcp,name=`whoami`,uname=`whoami`,port=1110 127.0.0.1 /mnt/9p
-ls /mnt/9p
+#../umount.sh
+#echo "start proxy: $NAMED"
+#../bin/proxyd &
+#sudo mount -t 9p -o tcp,name=`whoami`,uname=`whoami`,port=1110 127.0.0.1 /mnt/9p
+#ls /mnt/9p

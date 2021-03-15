6a399992c4d9b5a2de01369895dac8d0bac3374c
ArielSzekely <arielszekely@gmail.com>
2021-03-15 10:39:45 -0400

    [gg] Add script to check excamera results

---
+++ check-excamera-results.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+memfs_dir=/mnt/9p/fs/gg
+
+if [ ! -d "/mnt/9p/fs" ]
+then
+  echo "9p not mounted!"
+  exit 1
+fi
+
+exc_dir=$HOME/gg/examples/excamera
+
+cd $exc_dir
+
+echo "1. Copying results to local dir"
+cp -r /mnt/9p/fs/gg/results/* .
+
+ls *-vpxenc.ivf | while read each; do echo "file '$each'" >> mylist.txt; done
+ffmpeg -f concat -i mylist.txt -codec copy output.avi

487bd2e635e4a8395ce626749949ba602257b8bb
Frans Kaashoek <kaashoek@mit.edu>
2021-03-16 11:14:14 -0400

    Repair test

--- memfs/fs_test.go
+++ memfs/fs_test.go
@@ -11,64 +11,54 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	np "ulambda/ninep"
-	"ulambda/npcodec"
+	npo "ulambda/npobjsrv"
 )
 
 type TestState struct {
 	t     *testing.T
-	fs    *Root
+	ctx   *npo.Ctx
 	rooti *Inode
 }
 
 func newTest(t *testing.T) *TestState {
 	ts := &TestState{}
 	ts.t = t
-	ts.fs = MakeRoot()
-	ts.rooti = ts.fs.RootInode()
+	ts.ctx = npo.MkCtx("", nil)
+	ts.rooti = MkRootInode(ts.ctx)
 	return ts
-}
-
-func TestRoot(t *testing.T) {
-	ts := newTest(t)
-	assert.Equal(t, ts.fs.inode.Inum, RootInum)
 }
 
 func (ts *TestState) initfs() {
 	const N = 1000
-	_, err := ts.rooti.Create("", ts.fs, np.DMDIR|07000, "todo")
+	_, err := ts.rooti.Create(ts.ctx, "todo", np.DMDIR|07000, 0)
 	assert.Nil(ts.t, err, "Create todo")
-	is, _, err := ts.rooti.Walk("", []string{"todo"})
+	is, _, err := ts.rooti.Lookup(ts.ctx, []string{"todo"})
 	assert.Nil(ts.t, err, "Walk todo")
 	for i := 0; i < N; i++ {
-		_, err = is[1].Create("", ts.fs, 07000, "job"+strconv.Itoa(i))
+		_, err = is[0].Create(ts.ctx, "job"+strconv.Itoa(i), 07000, 0)
 		assert.Nil(ts.t, err, "Create job")
 	}
 }
 
-func (ts *TestState) testRename(t string) {
-	done := false
-	for !done {
-		is, _, err := ts.rooti.Walk(t, []string{"todo"})
-		assert.Nil(ts.t, err, "Walk todo")
-		assert.Equal(ts.t, len(is), 2, "Walked too few inodes")
-		b, err := is[1].Read(0, 100)
-		if len(b) == 0 { // are we done?
-			done = true
-		} else {
-			assert.Nil(ts.t, err, "Read todo")
-			var st np.Stat
-			err = npcodec.Unmarshal(b, &st)
-			assert.Nil(ts.t, err, "Unmarshal todo")
-			name := st.Name
-			if strings.HasPrefix(name, "job") {
-				err = ts.fs.Rename(t, np.Split("todo/"+name),
-					np.Split("todo/done-"+name))
-				if err != nil {
-					assert.Contains(ts.t, err.Error(), "Unknown name")
-				}
-			} else {
-				assert.Equal(ts.t, err, nil, "Rename todo")
-				_ = ts.rooti.Remove(t, ts.fs, np.Split("todo/"+name))
+func (ts *TestState) testRename(t int) {
+	is, _, err := ts.rooti.Lookup(ts.ctx, []string{"todo"})
+	assert.Nil(ts.t, err, "Lookup todo")
+	assert.Equal(ts.t, 1, len(is), "Walked too few inodes")
+	ino := is[0].(*Inode)
+	sts, err := ino.ReadDir(ts.ctx, 0, 100)
+	assert.Nil(ts.t, err, "ReadDir")
+	for _, st := range sts {
+		is, _, err := ino.Lookup(ts.ctx, []string{st.Name})
+		if len(is) == 0 {
+			continue
+		}
+		assert.Nil(ts.t, err, "Lookup name "+st.Name)
+		assert.Equal(ts.t, 1, len(is), "Walked too few inodes")
+		ino2 := is[0].(*Inode)
+		if strings.HasPrefix(st.Name, "job") {
+			err = ino2.Rename(ts.ctx, st.Name, "done-"+st.Name)
+			if err != nil {
+				assert.Contains(ts.t, err.Error(), "Unknown name")
 			}
 		}
 	}
@@ -83,7 +73,7 @@ func TestConcurRename(t *testing.T) {
 	for i := 0; i < N; i++ {
 		wg.Add(1)
 		go func(t int) {
-			ts.testRename(strconv.Itoa(t))
+			ts.testRename(t)
 			wg.Done()
 		}(i)
 	}

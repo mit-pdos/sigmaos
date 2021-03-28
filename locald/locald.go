dfa2db001a2809dffc50b9505366f4da5b2d9648
ArielSzekely <arielszekely@gmail.com>
2021-03-28 14:03:54 -0400

    [locald] Handle "Unknown file" error on GetLambda

--- locald/locald.go
+++ locald/locald.go
@@ -96,7 +96,7 @@ func (ld *LocalD) Worker() {
 	// XXX pin to a core
 	for !ld.readDone() {
 		b, err := ld.GetLambda()
-		if err != nil && (err.Error() == "EOF" || err.Error() == "file not found schedd") {
+		if err != nil && (err.Error() == "EOF" || err.Error() == "file not found schedd" || err.Error() == "Unknown file") {
 			db.DLPrintf("LOCALD", "EOF on GetLambda %v\n", b)
 			continue
 		}

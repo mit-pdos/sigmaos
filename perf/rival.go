fff30a4ab3f034fb48596748a82658e2887cde8c
ArielSzekely <arielszekely@gmail.com>
2021-04-15 09:53:07 -0400

    [perf] Have rival process monitor CPU util

--- perf/rival.go
+++ perf/rival.go
@@ -2,17 +2,24 @@ package perf
 
 import (
 	"errors"
+	"io/ioutil"
 	"log"
 	"strconv"
+	"strings"
 	"time"
 
 	"ulambda/fslib"
 )
 
+const (
+	MONITOR_HZ = 10
+)
+
 type Rival struct {
 	spawnsPerSec       int
 	secs               float64
-	sleepIntervalMsecs int
+	sleepIntervalUsecs int
+	killed             bool
 	*fslib.FsLib
 }
 
@@ -37,23 +44,14 @@ func MakeRival(args []string) (*Rival, error) {
 		log.Fatalf("Invalid num seconds: %v, %v\n", args[0], err)
 	}
 
-	r.sleepIntervalMsecs = 1000 / r.spawnsPerSec
-	log.Printf("Spawning every %v msec", r.sleepIntervalMsecs)
+	r.sleepIntervalUsecs = 1000000 / r.spawnsPerSec
+	if r.secs >= 0 {
+		log.Printf("Spawning every %v usec(s) for %v secs", r.sleepIntervalUsecs, r.secs)
+	} else {
+		log.Printf("Spawning every %v usec(s) indefinitely", r.sleepIntervalUsecs)
+	}
 
 	return r, nil
-}
-
-func (r *Rival) Work() {
-	start := time.Now()
-	for {
-		// Check if we're done
-		if time.Now().Sub(start).Seconds() >= r.secs {
-			break
-		}
-		pid := fslib.GenPid()
-		r.spawnSpinnerWithPid(pid)
-		time.Sleep(time.Duration(r.sleepIntervalMsecs) * time.Millisecond)
-	}
 }
 
 func (r *Rival) spawnSpinnerWithPid(pid string) {
@@ -64,4 +62,64 @@ func (r *Rival) spawnSpinnerWithPid(pid string) {
 	if err != nil {
 		log.Fatalf("couldn't spawn %v: %v\n", pid, err)
 	}
+}
+
+func getCPUSample() (idle, total uint64) {
+	contents, err := ioutil.ReadFile("/proc/stat")
+	if err != nil {
+		return
+	}
+	lines := strings.Split(string(contents), "\n")
+	for _, line := range lines {
+		fields := strings.Fields(line)
+		if fields[0] == "cpu" {
+			numFields := len(fields)
+			for i := 1; i < numFields; i++ {
+				val, err := strconv.ParseUint(fields[i], 10, 64)
+				if err != nil {
+					log.Printf("Error: %v %v %v", i, fields[i], err)
+				}
+				total += val // tally up all the numbers to get total ticks
+				if i == 4 {  // idle is the 5th field in the cpu line
+					idle = val
+				}
+			}
+			return
+		}
+	}
+	return
+}
+
+func (r *Rival) monitorCpuUtil() {
+	sleepMsecs := 1000 / MONITOR_HZ
+	var idle0 uint64
+	var total0 uint64
+	var idle1 uint64
+	var total1 uint64
+	idle0, total0 = getCPUSample()
+	for !r.killed {
+		time.Sleep(time.Duration(sleepMsecs) * time.Millisecond)
+		idle1, total1 = getCPUSample()
+		idleDelta := float64(idle1 - idle0)
+		totalDelta := float64(total1 - total0)
+		util := 100.0 * (totalDelta - idleDelta) / totalDelta
+		log.Printf("CPU util: %f [busy: %f, total: %f]\n", util, totalDelta-idleDelta, totalDelta)
+		idle0 = idle1
+		total0 = total1
+	}
+}
+
+func (r *Rival) Work() {
+	go r.monitorCpuUtil()
+	start := time.Now()
+	for {
+		// Check if we're done
+		if r.secs >= 0 && time.Now().Sub(start).Seconds() >= r.secs {
+			break
+		}
+		pid := fslib.GenPid()
+		r.spawnSpinnerWithPid(pid)
+		time.Sleep(time.Duration(r.sleepIntervalUsecs) * time.Microsecond)
+	}
+	r.killed = true
 }

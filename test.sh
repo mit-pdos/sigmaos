c93a6e9f2860d0f61b9074449f1891f08cfd9bdd
Frans Kaashoek <kaashoek@mit.edu>
2021-04-19 09:25:47 -0400

    x

--- test.sh
+++ test.sh
@@ -6,6 +6,6 @@ go test $1 ulambda/memfs
 go test $1 ulambda/fsclnt
 go test $1 ulambda/fslib
 (cd nps3; go test $1)
-go test $1 ulambda/schedd
+go test $1 ulambda/locald
 (cd mr; go test $1)
 (cd kv; go test $1)

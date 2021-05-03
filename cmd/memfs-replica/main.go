424f8f83a242f96abc3482b5f3a3bf65404842c5
ArielSzekely <arielszekely@gmail.com>
2021-05-03 11:52:35 -0400

    [memfs-replica] Get union dir from input args

--- cmd/memfs-replica/main.go
+++ cmd/memfs-replica/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"path"
 
 	db "ulambda/debug"
 	"ulambda/fsclnt"
@@ -13,25 +14,25 @@ import (
 	"ulambda/npsrv"
 )
 
-const (
-	CONFIG_PATH = "name/memfs-replica-config.txt"
-)
-
 func main() {
-	if len(os.Args) < 4 {
-		log.Fatalf("Usage: %v pid port config-path", os.Args[0])
+	if len(os.Args) < 5 {
+		log.Fatalf("Usage: %v pid port config-path union-dir-path", os.Args[0])
 	}
 	port := os.Args[2]
 	configPath := os.Args[3]
+	unionDirPath := os.Args[4]
 	fsl := fslib.MakeFsLib("memfs-replica" + port)
 	ip, err := fsclnt.LocalIP()
-	clnt := npclnt.MakeNpClnt()
-	config := npsrv.ReadReplConfig(configPath, ip+port, fsl, clnt)
 	if err != nil {
 		log.Fatalf("%v: no IP %v\n", os.Args[0], err)
 	}
+	clnt := npclnt.MakeNpClnt()
+	config, err := npsrv.ReadReplConfig(configPath, ip+port, fsl, clnt)
+	if err != nil {
+		log.Fatalf("Couldn't rea repl config: %v\n", err)
+	}
 	fsd := memfsd.MakeReplicatedFsd(ip+port, true, config)
-	name := memfsd.MEMFS + "-replicas" + "/" + fsd.GetSrv().MyAddr()
+	name := path.Join(unionDirPath, fsd.GetSrv().MyAddr())
 	db.Name(name)
 	fs, err := fslibsrv.InitFs(name, fsd, nil)
 	if err != nil {

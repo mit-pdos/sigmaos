b719778e37831f04b7193c764b2c4481f46cca0e
ArielSzekely <arielszekely@gmail.com>
2021-07-21 19:27:20 -0400

    x

--- cloudlab/install-sw.sh
+++ cloudlab/install-sw.sh
@@ -88,7 +88,8 @@ then
   htop \
   net-tools \
   libprotoc-dev \
-  libssl-dev
+  libssl-dev \
+  htop
 
   wget 'https://golang.org/dl/go1.16.4.linux-amd64.tar.gz'
   sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.16.4.linux-amd64.tar.gz

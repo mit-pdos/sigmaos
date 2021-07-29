6606f4cdcbec8a8e0dd8071f702280564976685f
ArielSzekely <arielszekely@gmail.com>
2021-07-29 13:28:27 -0400

    [sync/test] More basic tests

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -10,6 +10,13 @@ import (
 	"ulambda/fslib"
 )
 
+const (
+	PID       = "test-PID"
+	COND_PATH = "name/cond"
+	LOCK_DIR  = "name/cond-locks"
+	LOCK_NAME = "test-lock"
+)
+
 type Tstate struct {
 	*fslib.FsLib
 	t *testing.T
@@ -42,28 +49,33 @@ func TestHelloWorld(t *testing.T) {
 	ts.s.Shutdown(ts.FsLib)
 }
 
+func waiter(ts *Tstate, c *Cond, done chan int, id int, signal bool) {
+	err := ts.LockFile(LOCK_DIR, LOCK_NAME)
+	assert.Nil(ts.t, err, "LockFile waiter [%v]: %v", id, err)
+
+	// Wait, and then possibly signal future waiters
+	c.Wait()
+	done <- id
+	if signal {
+		c.Signal()
+	}
+
+	err = ts.UnlockFile(LOCK_DIR, LOCK_NAME)
+	assert.Nil(ts.t, err, "UnlockFile waiter [%v]: %v", id, err)
+}
+
 func TestOneWaiterSignal(t *testing.T) {
 	ts := makeTstate(t)
 
 	Init(ts.FsLib)
 
-	pid := "test-pid"
-	condPath := "name/cond"
-	lockDir := "name/cond-locks"
-	lockName := "test-lock"
-
-	err := ts.Mkdir(lockDir, 0777)
+	err := ts.Mkdir(LOCK_DIR, 0777)
 	assert.Nil(ts.t, err, "Mkdir name/locks: %v", err)
 
-	c := MakeCond(pid, condPath, lockDir, lockName)
+	c := MakeCond(PID, COND_PATH, LOCK_DIR, LOCK_NAME)
 
-	fsl.LockFile(lockDir, lockName)
-
-	done := make(chan bool)
-	go func() {
-		c.Wait()
-		done <- true
-	}()
+	done := make(chan int)
+	go waiter(ts, c, done, 0, false)
 
 	// Make sure we don't miss the signal
 	time.Sleep(250 * time.Millisecond)
@@ -71,7 +83,7 @@ func TestOneWaiterSignal(t *testing.T) {
 	c.Signal()
 
 	res := <-done
-	assert.True(ts.t, res, "Bad done result")
+	assert.Equal(ts.t, 0, res, "Bad done result")
 
 	ts.s.Shutdown(ts.FsLib)
 }
@@ -81,23 +93,13 @@ func TestOneWaiterBroadcast(t *testing.T) {
 
 	Init(ts.FsLib)
 
-	pid := "test-pid"
-	condPath := "name/cond"
-	lockDir := "name/cond-locks"
-	lockName := "test-lock"
-
-	err := ts.Mkdir(lockDir, 0777)
+	err := ts.Mkdir(LOCK_DIR, 0777)
 	assert.Nil(ts.t, err, "Mkdir name/locks: %v", err)
 
-	c := MakeCond(pid, condPath, lockDir, lockName)
+	c := MakeCond(PID, COND_PATH, LOCK_DIR, LOCK_NAME)
 
-	fsl.LockFile(lockDir, lockName)
-
-	done := make(chan bool)
-	go func() {
-		c.Wait()
-		done <- true
-	}()
+	done := make(chan int)
+	go waiter(ts, c, done, 0, false)
 
 	// Make sure we don't miss the signal
 	time.Sleep(250 * time.Millisecond)
@@ -105,7 +107,75 @@ func TestOneWaiterBroadcast(t *testing.T) {
 	c.Broadcast()
 
 	res := <-done
-	assert.True(ts.t, res, "Bad done result")
+	assert.Equal(ts.t, 0, res, "Bad done result")
+
+	ts.s.Shutdown(ts.FsLib)
+}
+
+func TestNWaitersSignal(t *testing.T) {
+	ts := makeTstate(t)
+
+	N := 20
+	_ = N
+
+	Init(ts.FsLib)
+
+	err := ts.Mkdir(LOCK_DIR, 0777)
+	assert.Nil(ts.t, err, "Mkdir name/locks: %v", err)
+
+	c := MakeCond(PID, COND_PATH, LOCK_DIR, LOCK_NAME)
+
+	sum := 0
+
+	done := make(chan int)
+	for i := 0; i < N; i++ {
+		go waiter(ts, c, done, i, true)
+		sum += i
+	}
+
+	// Make sure we don't miss the signal
+	time.Sleep(250 * time.Millisecond)
+
+	c.Signal()
+
+	for i := 0; i < N; i++ {
+		sum -= <-done
+	}
+	assert.Equal(ts.t, 0, sum, "Bad sum")
+
+	ts.s.Shutdown(ts.FsLib)
+}
+
+func TestNWaitersBroadcast(t *testing.T) {
+	ts := makeTstate(t)
+
+	N := 20
+	_ = N
+
+	Init(ts.FsLib)
+
+	err := ts.Mkdir(LOCK_DIR, 0777)
+	assert.Nil(ts.t, err, "Mkdir name/locks: %v", err)
+
+	c := MakeCond(PID, COND_PATH, LOCK_DIR, LOCK_NAME)
+
+	sum := 0
+
+	done := make(chan int)
+	for i := 0; i < N; i++ {
+		go waiter(ts, c, done, i, false)
+		sum += i
+	}
+
+	// Make sure we don't miss the signal
+	time.Sleep(250 * time.Millisecond)
+
+	c.Broadcast()
+
+	for i := 0; i < N; i++ {
+		sum -= <-done
+	}
+	assert.Equal(ts.t, 0, sum, "Bad sum")
 
 	ts.s.Shutdown(ts.FsLib)
 }

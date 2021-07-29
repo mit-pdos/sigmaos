7b6b53c9672b99346207fb0231a1fffa5783686e
ArielSzekely <arielszekely@gmail.com>
2021-07-29 11:47:24 -0400

    Basic tests

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -2,6 +2,7 @@ package sync
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 
@@ -37,6 +38,74 @@ func TestHelloWorld(t *testing.T) {
 	Init(ts.FsLib)
 
 	assert.True(ts.t, true, "test")
+
+	ts.s.Shutdown(ts.FsLib)
+}
+
+func TestOneWaiterSignal(t *testing.T) {
+	ts := makeTstate(t)
+
+	Init(ts.FsLib)
+
+	pid := "test-pid"
+	condPath := "name/cond"
+	lockDir := "name/cond-locks"
+	lockName := "test-lock"
+
+	err := ts.Mkdir(lockDir, 0777)
+	assert.Nil(ts.t, err, "Mkdir name/locks: %v", err)
+
+	c := MakeCond(pid, condPath, lockDir, lockName)
+
+	fsl.LockFile(lockDir, lockName)
+
+	done := make(chan bool)
+	go func() {
+		c.Wait()
+		done <- true
+	}()
+
+	// Make sure we don't miss the signal
+	time.Sleep(250 * time.Millisecond)
+
+	c.Signal()
+
+	res := <-done
+	assert.True(ts.t, res, "Bad done result")
+
+	ts.s.Shutdown(ts.FsLib)
+}
+
+func TestOneWaiterBroadcast(t *testing.T) {
+	ts := makeTstate(t)
+
+	Init(ts.FsLib)
+
+	pid := "test-pid"
+	condPath := "name/cond"
+	lockDir := "name/cond-locks"
+	lockName := "test-lock"
+
+	err := ts.Mkdir(lockDir, 0777)
+	assert.Nil(ts.t, err, "Mkdir name/locks: %v", err)
+
+	c := MakeCond(pid, condPath, lockDir, lockName)
+
+	fsl.LockFile(lockDir, lockName)
+
+	done := make(chan bool)
+	go func() {
+		c.Wait()
+		done <- true
+	}()
+
+	// Make sure we don't miss the signal
+	time.Sleep(250 * time.Millisecond)
+
+	c.Broadcast()
+
+	res := <-done
+	assert.True(ts.t, res, "Bad done result")
 
 	ts.s.Shutdown(ts.FsLib)
 }

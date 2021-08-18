376337e948bdbc9b755b2cc5ea68381354065cec
Ariel Szekely <arielck@mit.edu>
2021-08-18 16:47:47 -0400

    procd/procd_test.go -> proc/proc_test.go

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ go test $1 ulambda/fsclnt
 go test $1 ulambda/fslib_test
 go test $1 ulambda/sync
 (cd nps3; go test $1)
-go test $1 ulambda/procd
+go test $1 ulambda/proc
 go test $1 ulambda/depproc
 (cd kv; go test $1)
 ./test-mr.sh

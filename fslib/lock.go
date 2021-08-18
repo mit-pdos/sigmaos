615763085fb74c7eb8937dd8bf5dbadfbd32b1b5
Ariel Szekely <arielck@mit.edu>
2021-08-18 13:58:40 -0400

    [fslib/lock] Make locks *NOT* ephemeral for now in order for scheduler tests to pass...

--- fslib/lock.go
+++ fslib/lock.go
@@ -19,7 +19,7 @@ func LockName(f string) string {
 // Try to lock a file. If the lock was acquired, return true. Else, return false
 func (fl *FsLib) TryLockFile(lockDir string, f string) bool {
 	lockName := LockName(f)
-	err := fl.MakeFile(path.Join(lockDir, lockName), 0777|np.DMTMP, np.OWRITE, []byte{})
+	err := fl.MakeFile(path.Join(lockDir, lockName), 0777 /*|np.DMTMP*/, np.OWRITE, []byte{})
 	// If name exists, someone already has the lock...
 	if err != nil && err.Error() == "Name exists" {
 		return false
@@ -30,7 +30,7 @@ func (fl *FsLib) TryLockFile(lockDir string, f string) bool {
 // Lock a file
 func (fl *FsLib) LockFile(lockDir string, f string) error {
 	lockName := LockName(f)
-	err := fl.MakeFile(path.Join(lockDir, lockName), 0777|np.DMTMP, np.OWRITE|np.OCEXEC, []byte{})
+	err := fl.MakeFile(path.Join(lockDir, lockName), 0777 /*|np.DMTMP*/, np.OWRITE|np.OCEXEC, []byte{})
 	// Sometimes we get "EOF" on shutdown
 	if err != nil && err.Error() == "EOF" {
 		return nil

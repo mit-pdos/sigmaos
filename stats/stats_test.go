0b2af05a782b27bb95c0f729c93589350bb2706a
Ariel Szekely <arielck@mit.edu>
2021-09-22 10:56:48 -0400

    [stats] Fix stats test

--- stats/stats_test.go
+++ stats/stats_test.go
@@ -45,7 +45,7 @@ func TestStatsd(t *testing.T) {
 	stats := StatInfo{}
 	err := ts.ReadFileJson("name/statsd", &stats)
 	assert.Nil(t, err, "statsd")
-	assert.Equal(t, Tcounter(81), stats.Nread, "Nread")
+	assert.Equal(t, Tcounter(3), stats.Nread, "Nread")
 	for i := 0; i < 1000; i++ {
 		_, err := ts.ReadFile("name/statsd")
 		assert.Nil(t, err, "statsd")

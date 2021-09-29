dce46dccab9c84d3f2ab55afffec1b52f8f21e11
Ariel Szekely <arielck@mit.edu>
2021-09-28 21:06:47 -0400

    [benchmarks] Move rival to benchmarks

--- cmd/user/rival/main.go
+++ cmd/user/rival/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"os"
 
-	"ulambda/perf"
+	"ulambda/benchmarks"
 )
 
 func main() {
@@ -12,7 +12,7 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Usage: %v spawnsPerSecond seconds ninep/native dim its \n", os.Args[0])
 		os.Exit(1)
 	}
-	r, err := perf.MakeRival(os.Args[1:])
+	r, err := benchmarks.MakeRival(os.Args[1:])
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v: error %v", os.Args[0], err)
 		os.Exit(1)

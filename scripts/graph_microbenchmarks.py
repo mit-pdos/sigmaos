ac4059851e9ac93f54d08c85b109dce09d2780a5
Ariel Szekely <arielck@mit.edu>
2021-10-01 17:42:40 -0400

    [scripts] Improve graph script

--- scripts/graph_microbenchmarks.py
+++ scripts/graph_microbenchmarks.py
@@ -26,7 +26,7 @@ def get_y_vals(data):
 
 
 def graph_data(data, normalize, out, units):
-  fig = plt.figure(figsize=(15,10))
+  fig = plt.figure(figsize=(20,15))
   plt.xlabel("Microbenchmark")
   unnormalized_data = data
   if normalize:
@@ -51,7 +51,7 @@ def graph_data(data, normalize, out, units):
         label = "{:.2f} {}".format(y_vals[n][i], units)
       plt.text(bar[i].get_x() + bar[i].get_width() / 2.0, height, label, ha="center", va="bottom", rotation=90)
     offset += width
-  plt.xticks([ i + width for i in ind ], experiments, rotation=45)
+  plt.xticks([ i + width for i in ind ], experiments, rotation=25)
   plt.legend()
   plt.savefig(out)
 

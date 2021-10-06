58575c9b8878d5a68f3c656d82a22ffb36b310c2
Frans Kaashoek <kaashoek@mit.edu>
2021-10-06 08:51:22 -0400

    Split ux obj off

--- cmd/kernel/fss3d/main.go
+++ cmd/kernel/fss3d/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	if len(os.Args) != 2 {
-		log.Fatalf("NPS3 incorrect number of args")
+		log.Fatalf("%v: incorrect number of args", os.Args[0])
 	}
 	fss3 := fss3.MakeFss3(os.Args[1])
 	fss3.Serve()
--- ux/fsux.go
+++ ux/fsux.go
@@ -1,19 +1,14 @@
 package fsux
 
 import (
-	"fmt"
 	"log"
-	"os"
 	"path"
 	"sync"
-	"syscall"
 
-	db "ulambda/debug"
 	"ulambda/fs"
 	"ulambda/fsclnt"
 	"ulambda/fslibsrv"
 	"ulambda/fssrv"
-	"ulambda/inode"
 	"ulambda/named"
 	np "ulambda/ninep"
 	"ulambda/repl"
@@ -50,106 +45,4 @@ func MakeReplicatedFsUx(mount string, addr string, pid string, config repl.Confi
 		fsuxStartCond.Destroy()
 	}
 	return fsux
-}
-
-type Obj struct {
-	*inode.Inode
-	mu   sync.Mutex
-	path []string
-	ino  uint64 // Unix inode
-	sz   np.Tlength
-	init bool
-}
-
-func makeObj(path []string, t np.Tperm, d *Dir) *Obj {
-	o := &Obj{}
-	o.Inode = inode.MakeInode("", t, d)
-	o.path = path
-	return o
-}
-
-func (o *Obj) Path() string {
-	p := np.Join(o.path)
-	if len(o.path) == 0 {
-		p = "."
-	}
-	return p
-}
-
-func uxFlags(m np.Tmode) int {
-	f := 0
-	switch m & 3 {
-	case np.OREAD:
-		f = os.O_RDONLY
-	case np.ORDWR:
-		f = os.O_RDWR
-	case np.OWRITE:
-		f = os.O_WRONLY
-	case np.OEXEC:
-		f = os.O_RDONLY
-	}
-	if m&np.OTRUNC == np.OTRUNC {
-		f |= os.O_TRUNC
-	}
-	return f
-}
-
-func (o *Obj) Inum() uint64 {
-	if !o.init {
-		o.stat()
-	}
-	return o.ino
-}
-
-func (o *Obj) Qid() np.Tqid {
-	if !o.init {
-		o.stat()
-	}
-	return np.MakeQid(np.Qtype(o.Perm()>>np.QTYPESHIFT),
-		np.TQversion(0), np.Tpath(o.ino))
-}
-
-func (o *Obj) Size() np.Tlength {
-	if !o.init {
-		o.stat()
-	}
-	return o.sz
-}
-
-func (o *Obj) stat() (*np.Stat, error) {
-	o.mu.Lock()
-	defer o.mu.Unlock()
-
-	fileinfo, err := os.Stat(o.Path())
-	if err != nil {
-		return nil, err
-	}
-	ustat, ok := fileinfo.Sys().(*syscall.Stat_t)
-	if !ok {
-		return nil, fmt.Errorf("Not a syscall.Stat_t")
-	}
-	o.ino = ustat.Ino
-	o.sz = np.Tlength(ustat.Size)
-	o.init = true
-
-	st := &np.Stat{}
-	if len(o.path) > 0 {
-		st.Name = o.path[len(o.path)-1]
-	} else {
-		st.Name = "" // root
-	}
-	st.Mode = o.Inode.Perm() | np.Tperm(0777)
-	st.Qid = o.Qid()
-	st.Uid = ""
-	st.Gid = ""
-	st.Length = o.sz
-	s, _ := ustat.Mtim.Unix()
-	st.Mtime = uint32(s)
-
-	return st, nil
-}
-
-func (o *Obj) Stat(ctx fs.CtxI) (*np.Stat, error) {
-	db.DLPrintf("UXD", "%v: Stat %v\n", ctx, o)
-	return o.stat()
 }

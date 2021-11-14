09e809de861845670b194efa876cdf3e01851e7f
Frans Kaashoek <kaashoek@mit.edu>
2021-11-14 09:50:53 -0500

    Oops, forgot to commit delay

---
+++ delay/delay.go
@@ -0,0 +1,41 @@
+package delay
+
+import (
+	crand "crypto/rand"
+	"log"
+	"math/big"
+	"time"
+
+	db "ulambda/debug"
+	"ulambda/proc"
+)
+
+//
+// Delay testing
+//
+
+var maxmsDelay int64
+var totalDelay int64
+var lastTotal int64
+
+func SetDelay(maxms int64) {
+	maxmsDelay = maxms
+}
+
+func MaybeDelay() {
+	if maxmsDelay != 0 {
+		max := big.NewInt(1000)
+		rr, _ := crand.Int(crand.Reader, max)
+		if rr.Int64() < 660 {
+			// delay for a while.
+			max := big.NewInt(maxmsDelay)
+			ms, _ := crand.Int(crand.Reader, max)
+			totalDelay += ms.Int64()
+			if totalDelay-lastTotal > 1000 {
+				lastTotal = totalDelay
+				log.Printf("%v: DELAY %v tot %vms\n", db.GetName(), proc.GetPid(), totalDelay)
+			}
+			time.Sleep(time.Duration(ms.Int64()) * time.Millisecond)
+		}
+	}
+}

66c72fc38d5e3601353cf9e9d04d14a2f48e9ab3
Frans Kaashoek <kaashoek@mit.edu>
2021-11-16 07:44:46 -0500

    Generate cryptographically-random pids

--- proc/pid.go
+++ proc/pid.go
@@ -3,12 +3,11 @@ package proc
 import (
 	"os"
 
-	"math/rand"
-	"strconv"
+	"github.com/thanhpk/randstr"
 )
 
 func GenPid() string {
-	return strconv.Itoa(rand.Intn(100000))
+	return randstr.Hex(8)
 }
 
 // Can return "" for test programs that make a procclnt

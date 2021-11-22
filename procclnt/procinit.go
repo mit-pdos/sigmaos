cdaaf02dd3a19dba577d3f5b5d8d7b7961d93a00
Ariel Szekely <arielck@mit.edu>
2021-11-21 21:41:02 -0500

    [procclnt] Add more debug info in case of error

--- procclnt/procinit.go
+++ procclnt/procinit.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"strings"
 
+	"runtime/debug"
+
 	db "ulambda/debug"
 	"ulambda/fslib"
 	"ulambda/named"
@@ -19,12 +21,15 @@ func MakeProcClnt(fsl *fslib.FsLib) *ProcClnt {
 	tree := strings.TrimPrefix(piddir, "name/")
 
 	if err := fsl.MountTree(fslib.Named(), tree, "pids"); err != nil {
+		debug.PrintStack()
 		log.Fatalf("%v: Fatal error mounting %v as %v err %v\n", db.GetName(), tree, "pids", err)
 	}
 	if err := fsl.MountTree(fslib.Named(), "locks", "name/locks"); err != nil {
+		debug.PrintStack()
 		log.Fatalf("%v: Fatal error mounting locks err %v\n", db.GetName(), err)
 	}
 	if err := fsl.MountTree(fslib.Named(), named.PROCDDIR, named.PROCDDIR); err != nil {
+		debug.PrintStack()
 		log.Fatalf("%v: Fatal error mounting procd err %v\n", db.GetName(), err)
 	}
 	return makeProcClnt(fsl, piddir, proc.GetPid())
@@ -38,20 +43,24 @@ func MakeProcClntInit(fsl *fslib.FsLib, NamedAddr []string) *ProcClnt {
 	os.Setenv("SIGMAPID", pid)
 
 	if err := fsl.MountTree(NamedAddr, named.PROCDDIR, named.PROCDDIR); err != nil {
+		debug.PrintStack()
 		log.Fatalf("%v: Fatal error mounting procd err %v\n", db.GetName(), err)
 	}
 
 	// Make a pid directory for this initial proc
 	if err := fsl.MountTree(NamedAddr, PIDS, PIDS); err != nil {
+		debug.PrintStack()
 		log.Fatalf("%v: Fatal error mounting %v as %v err %v\n", db.GetName(), "pids", "pids", err)
 	}
 	d := PIDS + "/" + proc.GetPid()
 	if err := fsl.Mkdir(d, 0777); err != nil {
+		debug.PrintStack()
 		log.Fatalf("%v: Spawn mkdir pid %v err %v\n", db.GetName(), d, err)
 		return nil
 	}
 	d = PIDS + "/" + proc.GetPid() + "/" + CHILD
 	if err := fsl.Mkdir(d, 0777); err != nil {
+		debug.PrintStack()
 		log.Fatalf("%v: MakeProcClntInit childs %v err %v\n", db.GetName(), d, err)
 		return nil
 	}

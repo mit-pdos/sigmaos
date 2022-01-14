608adaafcf94126c757d328bddbb62e9a148a7f7
Ariel Szekely <arielck@mit.edu>
2022-01-14 14:48:55 -0500

    More detailed proc directory strucutre diagram, including kernel procs

--- procclnt/procdir.go
+++ procclnt/procdir.go
@@ -1,22 +1,39 @@
 package procclnt
 
 /*
- * Directory structure:
+ * Proc Directory structure:
  *
- * /procd
+ * /
+ * |- procd
+ * |  |
+ * |  |- x.x.x.x
+ * |  |  |
+ * |  |  |- pids
+ * |  |     |
+ * |  |     |- 1000
+ * |  |         |
+ * |  |         |- start-sem
+ * |  |         |- evict-sem
+ * |  |         |- status-pipe
+ * |  |         |- children
+ * |  |            |- 1001 // Child mounts this directory as procdir/parent
+ * |  |               |- run-sem
+ * |  |               |- procdir -> /procd/y.y.y.y/pids/1001 // Symlink to child's procdir.
+ * |  |                  |- ...
+ * |  |
+ * |  |- y.y.y.y
+ * |     |
+ * |     |- pids
+ * |        |
+ * |        |- 1001
+ * |            |
+ * |            |- parent -> /procd/x.x.x.x/pids/1000/children/1001 // Mount of subdir of parent proc.
+ * |            |- ...
  * |
- * |- x.x.x.x
+ * |- pids // Only for kernel procs such as s3, ux, procd, ...
  *    |
- *    |- pids
+ *    |- fsux-2000
  *       |
- *       |- 1000
- *           |
- *           |- start-sem
- *           |- evict-sem
- *           |- status-pipe
- *           |- kernel-proc // Only present if this is a kernel proc.
- *           |- children
- *              |- 1001
- *                 |- run-sem
- *                 |- procdir -> /procd/y.y.y.y/pids/1001 // This is a symbolic link
+ *       |- kernel-proc // Only present if this is a kernel proc.
+ *       |- ... // Same directory structure as regular procs
  */

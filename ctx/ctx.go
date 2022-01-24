ff983f419accb7c4d566c68ab314f8477477d7d6
Frans Kaashoek <kaashoek@mit.edu>
2022-01-24 10:25:24 -0500

    Oops, add ctx

---
+++ ctx/ctx.go
@@ -0,0 +1,28 @@
+package ctx
+
+import (
+	np "ulambda/ninep"
+	"ulambda/sesscond"
+)
+
+type Ctx struct {
+	uname  string
+	sessid np.Tsession
+	sct    *sesscond.SessCondTable
+}
+
+func MkCtx(uname string, sessid np.Tsession, sct *sesscond.SessCondTable) *Ctx {
+	return &Ctx{uname, sessid, sct}
+}
+
+func (ctx *Ctx) Uname() string {
+	return ctx.uname
+}
+
+func (ctx *Ctx) SessionId() np.Tsession {
+	return ctx.sessid
+}
+
+func (ctx *Ctx) SessCondTable() *sesscond.SessCondTable {
+	return ctx.sct
+}

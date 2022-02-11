cebd4a67162af03cbfa51e98a799f3c3a03880d8
Ariel Szekely <arielck@mit.edu>
2022-02-11 07:26:38 -0500

    Mapper symlink fail is only ok if it's a file not found. THis makes it more robust to a more diverse set of partition errors

--- mr/mapper.go
+++ mr/mapper.go
@@ -159,14 +159,13 @@ func (m *Mapper) doMap() error {
 		target := "name/ux/" + st.Name + "/m-" + m.file + "/r-" + strconv.Itoa(r) + "/"
 		err = m.Symlink([]byte(target), name, 0777)
 		if err != nil {
-			// May be due to partition
-			if np.IsErrEOF(err) {
-				log.Fatalf("%v: FATA/L symlink %v err %v\n", proc.GetProgram(), name, err)
+			if np.IsErrNotfound(err) {
+				// If the reducer successfully completed, the reducer dir won't be found.
+				// In that case, we don't want to mark the mapper as "failed", since this
+				// will loop infinitely.
+				log.Printf("%v: symlink %v err %v\n", proc.GetProgram(), name, err)
 			}
-			// If the reducer successfully completed, the reducer dir won't be found.
-			// In that case, we don't want to mark the mapper as "failed", since this
-			// will loop infinitely.
-			log.Printf("%v: symlink %v err %v\n", proc.GetProgram(), name, err)
+			log.Fatalf("%v: FATA/L symlink %v err %v\n", proc.GetProgram(), name, err)
 		}
 	}
 	return nil

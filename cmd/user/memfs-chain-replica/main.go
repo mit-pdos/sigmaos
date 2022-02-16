55692e158ac8bfec679aaab7851ea511f7fcf4a6
Ariel Szekely <arielck@mit.edu>
2022-02-16 16:44:01 -0500

    x

--- cmd/user/memfs-chain-replica/main.go
+++ cmd/user/memfs-chain-replica/main.go
@@ -10,35 +10,36 @@ import (
 )
 
 func main() {
-//	if len(os.Args) < 6 {
-//		log.Fatalf("Usage: %v pid port config-path union-dir-path symlink-path", os.Args[0])
-//	}
-//	args := os.Args[1:]
-//
-//	relayPort := args[1]
-//	portNum, err := strconv.Atoi(relayPort)
-//	if err != nil {
-//		log.Fatalf("Relay port must be an integer")
-//	}
-//
-//	// Server port is relay port + 100
-//	srvPort := strconv.Itoa(100 + portNum)
-//	configPath := args[2]
-//	unionDirPath := args[3]
-//	symlinkPath := args[4]
-//	ip, err := fsclnt.LocalIP()
-//	if err != nil {
-//		log.Fatalf("%v: no IP %v\n", args, err)
-//	}
-//	relayAddr := ip + ":" + relayPort
-//	srvAddr := ip + ":" + srvPort
-//
-//	// Get config
-//	config := replica.GetChainReplConfig("memfsd", relayPort, configPath, relayAddr, unionDirPath, symlinkPath)
-//	if len(args) == 6 && args[5] == "log-ops" {
-//		config.LogOps = true
-//	}
-//
-//	// Start the replica server
-//	replica.RunMemfsdReplica(args, srvAddr, unionDirPath, config)
-//}
+	//	if len(os.Args) < 6 {
+	//		log.Fatalf("Usage: %v pid port config-path union-dir-path symlink-path", os.Args[0])
+	//	}
+	//	args := os.Args[1:]
+	//
+	//	relayPort := args[1]
+	//	portNum, err := strconv.Atoi(relayPort)
+	//	if err != nil {
+	//		log.Fatalf("Relay port must be an integer")
+	//	}
+	//
+	//	// Server port is relay port + 100
+	//	srvPort := strconv.Itoa(100 + portNum)
+	//	configPath := args[2]
+	//	unionDirPath := args[3]
+	//	symlinkPath := args[4]
+	//	ip, err := fsclnt.LocalIP()
+	//	if err != nil {
+	//		log.Fatalf("%v: no IP %v\n", args, err)
+	//	}
+	//	relayAddr := ip + ":" + relayPort
+	//	srvAddr := ip + ":" + srvPort
+	//
+	//	// Get config
+	//	config := replica.GetChainReplConfig("memfsd", relayPort, configPath, relayAddr, unionDirPath, symlinkPath)
+	//	if len(args) == 6 && args[5] == "log-ops" {
+	//		config.LogOps = true
+	//	}
+	//
+	//	// Start the replica server
+	//	replica.RunMemfsdReplica(args, srvAddr, unionDirPath, config)
+	//}
+}

b901d891c0d2ecbda09810b3aaf6abc9be1c71ab
Ariel Szekely <arielck@mit.edu>
2022-02-18 16:56:50 -0500

    x

--- replraft/raft.go
+++ replraft/raft.go
@@ -14,6 +14,7 @@ import (
 	"go.etcd.io/etcd/client/pkg/v3/types"
 	raft "go.etcd.io/etcd/raft/v3"
 	"go.etcd.io/etcd/raft/v3/raftpb"
+	//	etcstorage "go.etcd.io/etcd/server/v3/storage"
 	"go.etcd.io/etcd/server/v3/etcdserver/api/rafthttp"
 	stats "go.etcd.io/etcd/server/v3/etcdserver/api/v2stats"
 	"go.uber.org/zap"

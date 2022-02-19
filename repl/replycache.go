2ba9bd9fb99d3993921f88c727fd957c0748857e
Ariel Szekely <arielck@mit.edu>
2022-02-19 14:00:05 -0500

    Add reply cache to fssrv

--- repl/replycache.go
+++ repl/replycache.go
@@ -1,34 +1,29 @@
-package replchain
+package repl
 
 import (
 	np "ulambda/ninep"
 )
 
-type Reply struct {
-	fcall *np.Fcall
-	frame []byte
-}
-
 type ReplyCache struct {
-	entries map[np.Tsession]map[np.Tseqno]*Reply
+	entries map[np.Tsession]map[np.Tseqno]np.Tmsg
 }
 
 func MakeReplyCache() *ReplyCache {
 	rc := &ReplyCache{}
-	rc.entries = map[np.Tsession]map[np.Tseqno]*Reply{}
+	rc.entries = map[np.Tsession]map[np.Tseqno]np.Tmsg{}
 	return rc
 }
 
-func (rc *ReplyCache) Put(request *np.Fcall, reply *np.Fcall, replyFrame []byte) {
+func (rc *ReplyCache) Put(request *np.Fcall, reply np.Tmsg) {
 	if _, ok := rc.entries[request.Session]; !ok {
-		rc.entries[request.Session] = map[np.Tseqno]*Reply{}
+		rc.entries[request.Session] = map[np.Tseqno]np.Tmsg{}
 	}
-	rc.entries[request.Session][request.Seqno] = &Reply{reply, replyFrame}
+	rc.entries[request.Session][request.Seqno] = reply
 }
 
 // XXX Will need to handle entries which are "too old" eventually once we
 // start evicting entries from the cache.
-func (rc *ReplyCache) Get(request *np.Fcall) (*Reply, bool) {
+func (rc *ReplyCache) Get(request *np.Fcall) (np.Tmsg, bool) {
 	if sessionMap, ok := rc.entries[request.Session]; !ok {
 		return nil, false
 	} else {

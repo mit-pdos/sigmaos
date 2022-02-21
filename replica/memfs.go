e946991c7ff5a3693726531ff810650b594b5dfa
Ariel Szekely <arielck@mit.edu>
2022-02-20 22:21:43 -0500

    Checkpoint WIP replicating kvds

--- replica/memfs.go
+++ replica/memfs.go
@@ -2,15 +2,13 @@ package replica
 
 import (
 	"log"
-	"path"
 
 	"ulambda/fslibsrv"
 	"ulambda/repl"
 )
 
-func RunMemfsdReplica(args []string, srvAddr string, unionDirPath string, config repl.Config) {
-	name := path.Join(unionDirPath, config.ReplAddr())
-	fss, _, _, err := fslibsrv.MakeReplMemfs(name, srvAddr, name, config)
+func RunMemfsdReplica(name string, config repl.Config) {
+	fss, _, _, err := fslibsrv.MakeReplMemfs(config.ReplAddr(), "", name, config)
 	if err != nil {
 		log.Fatalf("RunMemfdReplica: err %v\n", err)
 	}

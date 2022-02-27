6bbdf0553c10671f5cace335979b751dfe95136e
Frans Kaashoek <kaashoek@mit.edu>
2022-02-27 13:22:07 -0500

    Support multiple labels (e.g., SIGMADEBUG="RPC;PATHCLNT")

--- debug/debug.go
+++ debug/debug.go
@@ -4,23 +4,35 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"ulambda/proc"
 )
 
 // XXX maybe a list of levels?
-func debug() string {
-	return os.Getenv("SIGMADEBUG")
+func debugLabels() map[string]bool {
+	m := make(map[string]bool)
+	s := os.Getenv("SIGMADEBUG")
+	if s == "" {
+		return m
+	}
+	labels := strings.Split(s, ";")
+	for _, l := range labels {
+		m[l] = true
+	}
+	return m
 }
 
 func DPrintf(format string, v ...interface{}) {
-	if debug() != "" {
+	m := debugLabels()
+	if len(m) != 0 {
 		log.Printf("%v: %v", proc.GetName(), fmt.Sprintf(format, v...))
 	}
 }
 
 func DLPrintf(label string, format string, v ...interface{}) {
-	if debug() == label {
+	m := debugLabels()
+	if _, ok := m[label]; ok {
 		log.Printf("%v %v %v", proc.GetName(), label, fmt.Sprintf(format, v...))
 	}
 }

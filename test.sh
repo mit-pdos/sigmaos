0c5bf3592fcab1d4e25e7ce5f2c390cba8123f9c
Frans Kaashoek <kaashoek@mit.edu>
2022-03-01 15:33:37 -0500

    Run procclnt before ux and s3, since they rely on proc

--- test.sh
+++ test.sh
@@ -38,10 +38,10 @@ go test $1 ulambda/semclnt
 # tests kernel (without realms)
 #
 
+go test $1 ulambda/procclnt
 go test $1 ulambda/ux
 go test $1 ulambda/s3
 go test $1 ulambda/kernel
-go test $1 ulambda/procclnt
 # run this procclnt test again from a script to grep the output from
 # fencers for correctness
 ./fences/test.sh

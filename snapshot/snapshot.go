0a1956543f8b14ee2a33e9311a1c1be74e653262
Ariel Szekely <arielck@mit.edu>
2022-03-04 17:42:12 -0500

    Snapshot restore

--- snapshot/snapshot.go
+++ snapshot/snapshot.go
@@ -84,7 +84,7 @@ func Restore(rps protsrv.RestoreProtServer, pfn threadmgr.ProcessFn, b []byte) (
 	// Restore the recent fence table.
 	rft := fences.RestoreRecentTable(s.Rft)
 	// Restore the session table.
-	st := session.RestoreTable(nil /* TODO: Get mkps */, nil, nil /* TODO: get fssrv */, rft, tmt, s.St)
+	st := session.RestoreTable(nil /* TODO: Get mkps */, rps, nil /* TODO: get fssrv */, rft, tmt, s.St)
 	// Restore the reply cache.
 	rc := repl.Restore(s.Rc)
 	return root, st, tmt, rft, rc

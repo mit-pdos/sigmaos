e92106c1220a7ecfa0d79fdfcc809fa7eaae6740
Frans Kaashoek <kaashoek@mit.edu>
2022-03-11 20:34:11 -0500

    Add delay to test more orderings

--- semclnt/semclnt_test.go
+++ semclnt/semclnt_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
+	"ulambda/delay"
 	"ulambda/fslib"
 	"ulambda/semclnt"
 	"ulambda/test"
@@ -48,7 +49,7 @@ func TestSemClntSimple(t *testing.T) {
 	ts.Shutdown()
 }
 
-func TestSemClnt(t *testing.T) {
+func TestSemClntConcur(t *testing.T) {
 	ts := test.MakeTstate(t)
 
 	err := ts.MkDir(WAIT_PATH, 0777)
@@ -56,18 +57,20 @@ func TestSemClnt(t *testing.T) {
 	fsl0 := fslib.MakeFsLibAddr("sem0", fslib.Named())
 	fsl1 := fslib.MakeFsLibAddr("semd1", fslib.Named())
 
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 1000; i++ {
 		sem := semclnt.MakeSemClnt(ts.FsLib, WAIT_PATH+"/x")
 		sem.Init(0)
 
 		ch := make(chan bool)
 
 		go func(ch chan bool) {
+			delay.Delay(10)
 			sem := semclnt.MakeSemClnt(fsl0, WAIT_PATH+"/x")
 			sem.Down()
 			ch <- true
 		}(ch)
 		go func(ch chan bool) {
+			delay.Delay(10)
 			sem := semclnt.MakeSemClnt(fsl1, WAIT_PATH+"/x")
 			sem.Up()
 			ch <- true

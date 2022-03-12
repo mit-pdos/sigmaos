68bb92ad963ef590955a0f01a827a18aae883cd7
Frans Kaashoek <kaashoek@mit.edu>
2022-03-11 19:16:28 -0500

    Retry on version mismatch

--- semclnt/semclnt.go
+++ semclnt/semclnt.go
@@ -33,30 +33,34 @@ func (c *SemClnt) Init(perm np.Tperm) error {
 // Down semaphore. If not upped yet (i.e., if file exists), block
 func (c *SemClnt) Down() error {
 	signal := make(chan error)
-	err := c.SetRemoveWatch(c.path, func(p string, err error) {
-		if err != nil {
-			db.DLPrintf("SEMCLNT", "watch %v err %v\n", c.path, err)
+	for {
+		err := c.SetRemoveWatch(c.path, func(p string, err1 error) {
+			if err1 != nil {
+				db.DLPrintf("SEMCLNT", "watch %v err %v\n", c.path, err1)
+			}
+			signal <- err1
+		})
+		// If err is because file has been removed, then no error: the
+		// semaphore has been "upped".
+		if err != nil && np.IsErrNotfound(err) {
+			db.DLPrintf("SEMCLNT", "down %v ok err %v\n", c.path, err)
+			break
 		}
-		signal <- err
-	})
-	if err == nil {
-		db.DLPrintf("SEMCLNT", "semaphore wait %v\n", c.path)
-		err = <-signal
-	}
-	// If err is because file has been removed, then no error: the
-	// semaphore has been "upped". A file is removed when it isn't
-	// found or SetRemoveWatch erred with version mismatch; the
-	// latter case happens when removed has unliked the file,
-	// increasing version #, but remove hasn't removed the
-	// underlying inode (e.g., because SetRemoveWatch has fid for
-	// the unlinked file)
-	if err != nil && (np.IsErrNotfound(err) || np.IsErrVersion(err)) {
-		db.DLPrintf("SEMCLNT", "down %v ok err %v\n", c.path, err)
-		return nil
-	}
-	if err != nil {
-		db.DLPrintf("SEMCLNT", "down %v err %v\n", c.path, err)
-		return err
+		if err == nil {
+			db.DLPrintf("SEMCLNT", "semaphore wait %v\n", c.path)
+			err = <-signal
+		} else {
+			db.DLPrintf("SEMCLNT_ERR", "down %v err %v\n", c.path, err)
+			return err
+		}
+		if err != nil && np.IsErrVersion(err) {
+			db.DLPrintf("SEMCLNT_ERR", "down %v retry err %v\n", c.path, err)
+			continue
+		}
+		if err != nil {
+			db.DLPrintf("SEMCLNT_ERR", "down %v watch err %v\n", c.path, err)
+		}
+		break
 	}
 	return nil
 }

37bf43221556d413ef6e5b29fdbd7030e59c9251
Ariel Szekely <arielck@mit.edu>
2022-03-24 11:37:45 -0400

    Move procd work steal frequency hyperparameter to ninep/hyperparams.go

--- ninep/hyperparams.go
+++ ninep/hyperparams.go
@@ -2,6 +2,11 @@ package ninep
 
 // Session constants
 const (
-	SESSHEARTBEATMS = 50                  // Hearbeat every 50 msec.
+	SESSHEARTBEATMS = 50                  // Client heartbeat frequency.
 	SESSTIMEOUTMS   = SESSHEARTBEATMS * 4 // Kill a session after 4 missed heartbeats.
+)
+
+// Procd constants
+const (
+	WORK_STEAL_TIMEOUT_MS = 100 // Procd work steal frequency.
 )

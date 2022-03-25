4c098d5664a676bfc34110342f28d696fb1207ca
Frans Kaashoek <kaashoek@mit.edu>
2022-03-25 09:59:38 -0400

    Fix one more Tfence1

--- npcodec/npcode_test.go
+++ npcodec/npcode_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestPutfile(t *testing.T) {
 	b := []byte("hello")
-	fence := np.Tfence1{np.Tfenceid1{36, 2}, 7}
+	fence := np.Tfence{np.Tfenceid{36, 2}, 7}
 	msg := np.Tputfile{1, np.OWRITE, 0777, 101, []string{"f"}, b}
 	fcall := np.MakeFcall(msg, 13, nil, fence)
 	frame, error := marshal(fcall)

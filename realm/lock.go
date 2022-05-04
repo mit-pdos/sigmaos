21004bfa7c33ee70b4d200606d9de827d608bf21
Ariel Szekely <arielck@mit.edu>
2022-05-04 11:13:19 -0400

    realm/realm.go -> realm/lock.go

--- realm/lock.go
+++ realm/lock.go
@@ -8,15 +8,15 @@ import (
 )
 
 func lockRealm(ec *electclnt.ElectClnt, realmId string) {
-	db.DPrintf(db.ALWAYS, "try lock realm %v", realmId)
+	db.DPrintf("REALMLOCK", "try lock realm %v", realmId)
 	if err := ec.AcquireLeadership([]byte("sigmamgr")); err != nil {
 		db.DFatalf("%v error SigmaResourceMgr acquire leadership: %v", string(debug.Stack()), err)
 	}
-	db.DPrintf(db.ALWAYS, "acquire lock realm %v", realmId)
+	db.DPrintf("REALMLOCK", "acquire lock realm %v", realmId)
 }
 
 func unlockRealm(ec *electclnt.ElectClnt, realmId string) {
-	db.DPrintf(db.ALWAYS, "unlock realm %v", realmId)
+	db.DPrintf("REALMLOCK", "unlock realm %v", realmId)
 	if err := ec.ReleaseLeadership(); err != nil {
 		db.DFatalf("%v error SigmaResourceMgr release leadership: %v", string(debug.Stack()), err)
 	}

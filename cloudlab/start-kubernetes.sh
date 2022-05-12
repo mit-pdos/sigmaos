33e5645e5355d29197c913a05544087a4dd27b66
Ariel Szekely <arielck@mit.edu>
2022-05-12 11:52:49 -0400

    Update start-kubernetes to adapt to Calico changes, and properly config docker & containerd to point to the local drive

--- cloudlab/start-kubernetes.sh
+++ cloudlab/start-kubernetes.sh
@@ -29,22 +29,25 @@ done < $DIR/$SERVERS
 
 if [[ $is_leader == 1 ]]; then
 
-#ssh -i $DIR/keys/cloudlab-sigmaos $1 <<"ENDSSH" > $DIR/log/$LEADER
-#    sudo kubeadm init --apiserver-advertise-address=10.10.1.1 --pod-network-cidr=10.10.1.0/24
-# mkdir -p $HOME/.kube
-# yes | sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-# sudo chown $(id -u):$(id -g) $HOME/.kube/config
-# kubectl apply -f https://docs.projectcalico.org/v3.20/manifests/calico.yaml
-# kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.4.0/aio/deploy/recommended.yaml
-# kubectl create serviceaccount --namespace kube-system tiller
-# kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-# kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
-#ENDSSH
+ssh -i $DIR/keys/cloudlab-sigmaos $1 <<"ENDSSH" > $DIR/log/$LEADER
+ sudo kubeadm init --apiserver-advertise-address=10.10.1.1 --pod-network-cidr=10.10.1.0/24
+ mkdir -p $HOME/.kube
+ yes | sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+ sudo chown $(id -u):$(id -g) $HOME/.kube/config
+ # Install CNI
+ kubectl create -f https://projectcalico.docs.tigera.io/manifests/tigera-operator.yaml
+ kubectl create -f ~/ulambda/cloudlab/k8s/calico.yaml
+ kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.4.0/aio/deploy/recommended.yaml
+ kubectl create serviceaccount --namespace kube-system tiller
+ kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+ kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
+ENDSSH
+
 echo "leader"
 
 else
 
-cmd="sudo $(tail -n 2 $DIR/log/$LEADER)"
+cmd="sudo $(grep -v 'created'  $DIR/log/$LEADER | tail -n 2)"
 
 ssh -i $DIR/keys/cloudlab-sigmaos $1 <<ENDSSH
   eval $cmd

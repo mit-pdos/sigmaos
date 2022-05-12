9a3382b283caaec6fd6a92661f69bfe6911a642b
Frans Kaashoek <kaashoek@mit.edu>
2022-05-12 14:39:06 -0400

    x

--- test.sh
+++ test.sh
@@ -47,7 +47,7 @@ do
   go test -v ulambda/fslib -path "name/ux/~ip/fslibtest/" -run Perf
   
   go test $1 ulambda/s3
-  go test -v ulambda/fslib -path "name/ux/~ip/fslibtest/" -run Perf
+  go test -v ulambda/fslib -path "name/s3/~ip/fslibtest/" -run Perf
   
   go test $1 ulambda/kernel
   go test $1 ulambda/leaderclnt

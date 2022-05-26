b74f291c15968226c9bb5b5921149491566da8c2
Ariel Szekely <arielck@mit.edu>
2022-05-26 06:36:41 -0400

    x

--- benchmarks/burst.go
+++ benchmarks/burst.go
@@ -103,7 +103,7 @@ func (b *BurstBenchmark) burst(N int, pidOffset int) *RawResults {
 	throughput := float64(N) / elapsed
 	rs.Data[0].set(throughput, elapsed/float64(N), nRPC)
 
-	log.Printf("BurstBenchmark(%v) Done", N)
+	log.Printf("BurstBenchmark(%v) Done in %vms", N, elapsed/1000.0)
 
 	return rs
 }

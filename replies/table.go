12690cf9b040ba0cb266c0072933630ca702b122
Frans Kaashoek <kaashoek@mit.edu>
2022-05-27 10:32:05 -0400

    Print condensed version of reply table

--- replies/table.go
+++ replies/table.go
@@ -1,9 +1,10 @@
 package replies
 
 import (
+	"fmt"
+	"sort"
 	"sync"
 
-	db "ulambda/debug"
 	np "ulambda/ninep"
 )
 
@@ -20,6 +21,33 @@ func MakeReplyTable() *ReplyTable {
 	return rt
 }
 
+func (rt *ReplyTable) String() string {
+	s := fmt.Sprintf("RT %d: ", len(rt.entries))
+	keys := make([]np.Tseqno, 0, len(rt.entries))
+	for k, _ := range rt.entries {
+		keys = append(keys, k)
+	}
+	if len(keys) == 0 {
+		return s + "\n"
+	}
+	sort.Slice(keys, func(i, j int) bool { return keys[i] < keys[j] })
+	start := keys[0]
+	end := keys[0]
+	for i := 1; i < len(keys); i++ {
+		k := keys[i]
+		if k != end+1 {
+			s += fmt.Sprintf("[%d,%d) ", start, end)
+			start = k
+			end = k
+		} else {
+			end += 1
+		}
+
+	}
+	s += fmt.Sprintf("[%d,%d)\n", start, end+1)
+	return s
+}
+
 func (rt *ReplyTable) Register(request *np.Fcall) {
 	rt.Lock()
 	defer rt.Unlock()
@@ -30,7 +58,6 @@ func (rt *ReplyTable) Register(request *np.Fcall) {
 	for s := request.Received.Start; s < request.Received.End; s++ {
 		delete(rt.entries, np.Tseqno(s))
 	}
-	db.DPrintf("RTABLE", "len rt %d\n", len(rt.entries))
 	rt.entries[request.Seqno] = MakeReplyFuture()
 }
 

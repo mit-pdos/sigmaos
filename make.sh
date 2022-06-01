c00c89c6320bff2bbf78dd84b3ae7d26c1459dcc
Ariel Szekely <arielck@mit.edu>
2022-06-01 14:51:14 -0400

    Clean up line spacing in make.sh

--- make.sh
+++ make.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
 
-RACE="-race"
-
 usage() {
     echo "Usage: $0 [-norace] [-vet] [-target <name>]" 1>&2
 }
 
+RACE="-race"
 CMD="build"
 TARGET="local"
-
 while [[ "$#" -gt 0 ]]; do
-    case "$1" in
-    -norace)
-        shift
-        RACE=""
-	;;
-    -vet)
-	shift
-	CMD="vet"
-	;;
-    -target)
-	TARGET="$2"
-	shift
-	shift
-	;;
-    *)
-	error "unexpected argument $1"
-	usage
-	exit 1
-	;;
-    esac
+  case "$1" in
+  -norace)
+    shift
+    RACE=""
+    ;;
+  -vet)
+    shift
+    CMD="vet"
+    ;;
+  -target)
+    shift
+    TARGET="$1"
+    shift
+    ;;
+  *)
+    echo "unexpected argument $1"
+    usage
+    exit 1
+    ;;
+  esac
 done
 
 mkdir -p bin/kernel
@@ -40,18 +38,14 @@ do
   echo "Building $k components"
   for f in `ls cmd/$k`
   do
-      if [ $CMD == "vet" ]; then
-	  echo "go vet cmd/$k/$f/main.go"
-	  go vet cmd/$k/$f/main.go
-      else 
-      	  echo "go build -ldflags='-X ulambda/ninep.Target=$TARGET' $RACE -o bin/$k/$f cmd/$k/$f/main.go"
-	  go build -ldflags="-X ulambda/ninep.Target=$TARGET" $RACE -o bin/$k/$f cmd/$k/$f/main.go
-      fi
+    if [ $CMD == "vet" ]; then
+      echo "go vet cmd/$k/$f/main.go"
+      go vet cmd/$k/$f/main.go
+    else 
+      echo "go build -ldflags='-X ulambda/ninep.Target=$TARGET' $RACE -o bin/$k/$f cmd/$k/$f/main.go"
+      go build -ldflags="-X ulambda/ninep.Target=$TARGET" $RACE -o bin/$k/$f cmd/$k/$f/main.go
+    fi
   done
 done
 
-# Copy to Ux
-LOCALBINDIR=/tmp/ulambda/bin
-mkdir -p $LOCALBINDIR
-rm -rf $LOCALBINDIR/*
-cp -r bin/* $LOCALBINDIR
+./install.sh

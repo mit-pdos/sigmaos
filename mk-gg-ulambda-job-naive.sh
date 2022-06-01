75381c1ba97c4de12466a38c3cb2759f9ccaee87
Ariel Szekely <arielck@mit.edu>
2022-06-01 12:01:50 -0400

    Remove some old gg scripts

--- mk-gg-ulambda-job-naive.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# ./mk-gg-ulambda-job.sh | ./bin/user/submit
-
-targets=$@
-
-SPID=$((1 + $RANDOM % 1000000))
-echo $SPID,"./bin/user/gg-naive-orchestrator","[name/fs/gg ${targets}]","[]","[]",""

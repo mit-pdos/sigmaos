764f83d57616be1d7062bf68447919086d9f1b40
Frans Kaashoek <kaashoek@mit.edu>
2022-06-01 20:35:21 -0400

    Add seqgrep package

---
+++ seqgrep/grep.go
@@ -0,0 +1,51 @@
+package seqgrep
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"regexp"
+	"strings"
+
+	"ulambda/mr"
+)
+
+func grepline1(n int, line string) {
+	re := regexp.MustCompile("[^a-zA-Z0-9_\\s]+")
+	sanitized := strings.ToLower(re.ReplaceAllString(line, " "))
+	for _, word := range strings.Fields(sanitized) {
+		if word == "scala" {
+			fmt.Printf("%d:%s\n", n, word)
+		}
+	}
+}
+
+func grepline(n int, line string) int {
+	scanner := bufio.NewScanner(strings.NewReader(line))
+	scanner.Split(mr.ScanWords)
+	cnt := 0
+	for scanner.Scan() {
+		w := scanner.Text()
+		if w == "Scala" {
+			// fmt.Printf("%d:%s\n", n, w)
+			cnt += 1
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("scanner err %v\n", err)
+	}
+	return cnt
+}
+
+func Grep(rdr io.Reader) int {
+	scanner := bufio.NewScanner(rdr)
+	n := 1
+	cnt := 0
+	for scanner.Scan() {
+		l := scanner.Text()
+		cnt += grepline(n, l)
+		n += 1
+	}
+	return cnt
+}

df2684df93337c0593ca9106f2098739a500a598
Ariel Szekely <arielck@mit.edu>
2022-06-03 11:14:32 -0400

    Fix sigmaos aws install script.

---
+++ aws/install-sigmaos.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+usage() {
+  echo "Usage: $0 -vpc <VPC_ID>" 1>&2
+}
+
+VPC=""
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  case $key in
+  -vpc)
+    shift
+    VPC=$1
+    shift
+    ;;
+  *)
+    usage
+    exit 1
+    ;;
+  esac
+done
+
+if [ -z "$VPC" ]; then
+    usage
+    exit 1
+fi
+
+vms=`./lsvpc.py $VPC | grep -w VMInstance | cut -d " " -f 5`
+
+for vm in $vms; do
+  echo "UPDATE: $vm"
+  ssh -i key-$VPC.pem ubuntu@$vm /bin/bash <<ENDSSH
+  (cd ulambda; ./stop.sh; ./install.sh -from s3)
+ENDSSH
+done

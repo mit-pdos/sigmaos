ab61d976ab12ff6071658a19e3635bf53c0fad57
Ariel Szekely <arielck@mit.edu>
2022-06-03 14:48:00 -0400

    Have stop look for proc names in cmd instead of bin, since remote machines may not have build the binaries (they may have downloaded them from s3 instead

--- stop.sh
+++ stop.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-./umount.sh
+DIR=$(dirname $0)
+. $DIR/.env
+
+$DIR/umount.sh
 for d in "realm" "kernel" "user"; do
-  for p in `ls bin/$d`; do
+  for p in `ls cmd/$d`; do
+    echo $p
     pgrep -x $p > /dev/null && killall $p
   done
 done

c7835ef56152776f061523934efe2c20066944f2
Ariel Szekely <arielck@mit.edu>
2022-06-03 15:01:21 -0400

    remove ./stop.sh output

--- stop.sh
+++ stop.sh
@@ -5,8 +5,7 @@ DIR=$(dirname $0)
 
 $DIR/umount.sh
 for d in "realm" "kernel" "user"; do
-  for p in `ls cmd/$d`; do
-    echo $p
+  for p in `ls "cmd/$d"`; do
     pgrep -x $p > /dev/null && killall $p
   done
 done

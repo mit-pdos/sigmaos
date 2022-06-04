7fda859ef25f6313a61edf740410266de352a93d
Ariel Szekely <arielck@mit.edu>
2022-06-03 20:16:29 -0400

    Add profile to upload.sh

--- upload.sh
+++ upload.sh
@@ -1,7 +1,39 @@
 #!/bin/bash
 
+usage() {
+  echo "Usage: $0 [--profile PROFILE]" 1>&2
+}
+
+PROFILE=""
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  case $key in
+  --profile)
+    shift
+    PROFILE="--profile $1"
+    shift
+    ;;
+  -help)
+    usage
+    exit 0
+    ;;
+  *)
+    echo "Error: unexpected argument '$1'"
+    usage
+    exit 1
+    ;;
+  esac
+done
+
+if [ $# -gt 0 ]; then
+    usage
+    exit 1
+fi
+
+
+
 DIR=$(dirname $0)
 . $DIR/.env
 
 # Copy to S3
-aws s3 cp --recursive bin s3://9ps3/bin
+aws s3 cp --recursive bin s3://9ps3/bin $PROFILE

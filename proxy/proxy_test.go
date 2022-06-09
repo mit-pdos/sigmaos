e1ddaa3b39c4a26affa9b6f4c083a97b964a9bfe
Frans Kaashoek <kaashoek@mit.edu>
2022-06-09 02:16:17 -0400

    Clean up a bit

--- proxy/proxy_test.go
+++ proxy/proxy_test.go
@@ -14,72 +14,81 @@ import (
 	"ulambda/test"
 )
 
-func TestBasic(t *testing.T) {
-	ts := test.MakeTstatePath(t, "", np.NAMED)
+type Tstate struct {
+	*test.Tstate
+	cmd *exec.Cmd
+}
+
+func initTest(t *testing.T) *Tstate {
+	ts := &Tstate{}
+
+	// start named
+	ts.Tstate = test.MakeTstatePath(t, "", np.NAMED)
 	sts, err := ts.GetDir(np.NAMED)
 	assert.Equal(t, nil, err)
 	assert.True(t, fslib.Present(sts, named.InitDir))
 
-	cmd := exec.Command("../bin/kernel/proxyd")
-	cmd.Stdout = os.Stdout
-	err = cmd.Start()
+	// start proxy
+	ts.cmd = exec.Command("../bin/kernel/proxyd")
+	ts.cmd.Stdout = os.Stdout
+	err = ts.cmd.Start()
+	assert.Equal(t, nil, err)
+	return ts
+}
+
+func (ts *Tstate) cleanup() {
+	shcmd := "sudo umount /mnt/9p"
+	err := exec.Command("bash", "-c", shcmd).Run()
+	assert.Equal(ts.T, nil, err)
+
+	err = ts.cmd.Process.Kill()
+	assert.Equal(ts.T, nil, err)
+
+	ts.Shutdown()
+}
+
+func run(cmd string) ([]byte, error) {
+	return exec.Command("bash", "-c", cmd).Output()
+}
+
+func TestBasic(t *testing.T) {
+	ts := initTest(t)
+
+	out, err := run("sudo mount -t 9p -o trans=tcp,aname=`whoami`,uname=`whoami`,port=1110 127.0.0.1 /mnt/9p")
 	assert.Equal(t, nil, err)
 
-	shcmd := "sudo mount -t 9p -o trans=tcp,aname=`whoami`,uname=`whoami`,port=1110 127.0.0.1 /mnt/9p"
-	out, err := exec.Command("bash", "-c", shcmd).Output()
-	assert.Equal(t, nil, err)
-
-	shcmd = "ls -a /mnt/9p/ | grep '.statsd'"
-	out, err = exec.Command("bash", "-c", shcmd).Output()
+	out, err = run("ls -a /mnt/9p/ | grep '.statsd'")
 	assert.Equal(t, nil, err)
 	assert.Equal(t, ".statsd\n", string(out))
 
-	shcmd = "cat /mnt/9p/.statsd | grep Nwalk"
-	out, err = exec.Command("bash", "-c", shcmd).Output()
+	out, err = run("cat /mnt/9p/.statsd | grep Nwalk")
 	assert.Equal(t, nil, err)
 	assert.True(t, strings.Contains(string(out), "Nwalk"))
 
-	shcmd = "echo hello > /mnt/9p/xxx"
-	err = exec.Command("bash", "-c", shcmd).Run()
+	out, err = run("echo hello > /mnt/9p/xxx")
 	assert.Equal(t, nil, err)
 
-	shcmd = "rm /mnt/9p/xxx"
-	err = exec.Command("bash", "-c", shcmd).Run()
+	out, err = run("rm /mnt/9p/xxx")
 	assert.Equal(t, nil, err)
 
-	shcmd = "mkdir /mnt/9p/d"
-	err = exec.Command("bash", "-c", shcmd).Run()
+	out, err = run("mkdir /mnt/9p/d")
 	assert.Equal(t, nil, err)
 
-	shcmd = "echo hello > /mnt/9p/d/xxx"
-	err = exec.Command("bash", "-c", shcmd).Run()
+	out, err = run("echo hello > /mnt/9p/d/xxx")
 	assert.Equal(t, nil, err)
 
-	shcmd = "ls /mnt/9p/d | grep 'xxx'"
-	out, err = exec.Command("bash", "-c", shcmd).Output()
+	out, err = run("ls /mnt/9p/d | grep 'xxx'")
 	assert.Equal(t, nil, err)
 	assert.Equal(t, "xxx\n", string(out))
 
-	shcmd = "rm /mnt/9p/d/xxx"
-	err = exec.Command("bash", "-c", shcmd).Run()
+	out, err = run("rm /mnt/9p/d/xxx")
 	assert.Equal(t, nil, err)
 
-	shcmd = "rmdir /mnt/9p/d"
-	err = exec.Command("bash", "-c", shcmd).Run()
+	out, err = run("rmdir /mnt/9p/d")
 	assert.Equal(t, nil, err)
 
-	shcmd = "ls /mnt/9p/xxx"
-	err = exec.Command("bash", "-c", shcmd).Run()
+	out, err = run("ls /mnt/9p/xxx")
 	assert.NotNil(t, err)
 
-	// Clean up
-
-	shcmd = "sudo umount /mnt/9p"
-	err = exec.Command("bash", "-c", shcmd).Run()
-	assert.Equal(t, nil, err)
-
-	err = cmd.Process.Kill()
-	assert.Equal(t, nil, err)
-
-	ts.Shutdown()
+	ts.cleanup()
 }

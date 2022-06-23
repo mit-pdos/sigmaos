1f191f3dc6bd890d802f6de8a11c477c71c26dc3
Frans Kaashoek <kaashoek@mit.edu>
2022-06-23 15:51:13 -0400

    Don't use sortedmap because it sorts by value instead of key

--- sorteddir/sorteddir.go
+++ sorteddir/sorteddir.go
@@ -1,34 +1,39 @@
 package sorteddir
 
 import (
+	"sort"
 	"sync"
-
-	"github.com/umpc/go-sortedmap"
-	// np "ulambda/ninep"
 )
 
 type SortedDir struct {
 	sync.Mutex
-	dents *sortedmap.SortedMap
-}
-
-func cmp(a, b interface{}) bool {
-	if a == b {
-		return true
-	}
-	return false
+	dents  map[string]interface{}
+	sorted []string
 }
 
 func MkSortedDir() *SortedDir {
 	sd := &SortedDir{}
-	sd.dents = sortedmap.New(100, cmp)
+	sd.dents = make(map[string]interface{})
+	sd.sorted = make([]string, 0)
 	return sd
+}
+
+func (sd *SortedDir) Iter(f func(string, interface{}) bool) {
+	sd.Lock()
+	defer sd.Unlock()
+
+	for _, k := range sd.sorted {
+		b := f(k, sd.dents[k])
+		if !b {
+			return
+		}
+	}
 }
 
 func (sd *SortedDir) String() string {
 	s := "["
-	sd.dents.IterFunc(false, func(rec sortedmap.Record) bool {
-		s += rec.Key.(string) + ", "
+	sd.Iter(func(k string, v interface{}) bool {
+		s += k + ", "
 		return true
 	})
 	return s + "]"
@@ -37,37 +42,46 @@ func (sd *SortedDir) String() string {
 func (sd *SortedDir) Len() int {
 	sd.Lock()
 	defer sd.Unlock()
-
-	return sd.dents.Len()
+	return len(sd.dents)
 }
 
 func (sd *SortedDir) Lookup(n string) (interface{}, bool) {
 	sd.Lock()
 	defer sd.Unlock()
+	e, ok := sd.dents[n]
+	return e, ok
+}
 
-	return sd.dents.Get(n)
+func (sd *SortedDir) insertSort(name string) {
+	i := sort.SearchStrings(sd.sorted, name)
+	sd.sorted = append(sd.sorted, "")
+	copy(sd.sorted[i+1:], sd.sorted[i:])
+	sd.sorted[i] = name
+}
+
+func (sd *SortedDir) delSort(name string) {
+	i := sort.SearchStrings(sd.sorted, name)
+	sd.sorted = append(sd.sorted[:i], sd.sorted[i+1:]...)
 }
 
 func (sd *SortedDir) Insert(name string, e interface{}) bool {
 	sd.Lock()
 	defer sd.Unlock()
-	return sd.dents.Insert(name, e)
+	if _, ok := sd.dents[name]; !ok {
+		sd.dents[name] = e
+		sd.insertSort(name)
+		return true
+	}
+	return false
 }
 
 func (sd *SortedDir) Delete(name string) {
 	sd.Lock()
 	defer sd.Unlock()
 
-	sd.dents.Delete(name)
-}
-
-func (sd *SortedDir) Iter(f func(string, interface{}) bool) {
-	sd.Lock()
-	defer sd.Unlock()
-
-	sd.dents.IterFunc(false, func(rec sortedmap.Record) bool {
-		b := f(rec.Key.(string), rec.Val)
-		return b
-	})
-
+	if _, ok := sd.dents[name]; !ok {
+		return
+	}
+	delete(sd.dents, name)
+	sd.delSort(name)
 }

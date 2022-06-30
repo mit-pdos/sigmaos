7c5cf181a538cf603bc8af3628ab96a9c7a22ae2
Ariel Szekely <arielck@mit.edu>
2022-06-30 09:33:04 -0400

    Update microbenchmark script

--- run-microbenchmarks.sh
+++ run-microbenchmarks.sh
@@ -2,12 +2,8 @@
 
 DIR=$(dirname $0)
 
-./start.sh
-
 # Make results dirs
 mkdir -p $DIR/benchmarks/results/microbenchmarks
 mkdir -p $DIR/benchmarks/results/pprof
 
 GOGC=off ./bin/user/microbenchmarks $DIR/benchmarks/results
-
-./stop.sh

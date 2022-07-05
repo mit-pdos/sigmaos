eadc2bca10a737406f21154a05ceef5744dd7bed
Frans Kaashoek <kaashoek@mit.edu>
2022-07-05 16:36:47 -0400

    x

---
+++ memfs/pipe.go
@@ -0,0 +1,50 @@
+package memfs
+
+import (
+	db "ulambda/debug"
+	"ulambda/fs"
+	np "ulambda/ninep"
+	"ulambda/pipe"
+)
+
+type Pipe struct {
+	fs.Inode
+	*pipe.Pipe
+}
+
+func MakePipe(ctx fs.CtxI, i fs.Inode) *Pipe {
+	p := Pipe{}
+	p.Pipe = pipe.MakePipe(ctx)
+	p.Inode = i
+	return &p
+}
+
+func (p *Pipe) Size() (np.Tlength, *np.Err) {
+	return p.Pipe.Size(), nil
+}
+
+func (p *Pipe) Close(ctx fs.CtxI, m np.Tmode) *np.Err {
+	return p.Pipe.Close(ctx, m)
+}
+
+func (p *Pipe) Open(ctx fs.CtxI, mode np.Tmode) (fs.FsObj, *np.Err) {
+	return p.Pipe.Open(ctx, mode)
+}
+
+func (p *Pipe) Unlink() {
+	p.Pipe.Unlink()
+}
+
+func (p *Pipe) Snapshot(fn fs.SnapshotF) []byte {
+	db.DFatalf("tried to snapshot pipe")
+	return nil
+}
+
+func (p *Pipe) Stat(ctx fs.CtxI) (*np.Stat, *np.Err) {
+	st, err := p.Inode.Stat(ctx)
+	if err != nil {
+		return nil, err
+	}
+	st.Length = p.Pipe.Size()
+	return nil, nil
+}

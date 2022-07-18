15d4bfd84cd81cf1d93b4370d4b7b4242784f1dc
Ariel Szekely <arielck@mit.edu>
2022-07-17 22:50:46 -0400

    Matrix multiplication lambda

---
+++ cmd/user/matmul/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"os"
+	"strconv"
+	"time"
+
+	"gonum.org/v1/gonum/mat"
+
+	db "ulambda/debug"
+	"ulambda/fslib"
+	"ulambda/proc"
+	"ulambda/procclnt"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		db.DFatalf("Usage: %v N\n", os.Args[0])
+	}
+	m, err := MakeMatrixMult(os.Args[1:])
+	if err != nil {
+		db.DFatalf("%v: error %v", os.Args[0], err)
+	}
+	m.Work()
+}
+
+type MatrixMult struct {
+	*fslib.FsLib
+	*procclnt.ProcClnt
+	n  int
+	m1 *mat.Dense
+	m2 *mat.Dense
+	m3 *mat.Dense
+}
+
+func MakeMatrixMult(args []string) (*MatrixMult, error) {
+	var err error
+	db.DPrintf("MATMUL", "MakeMatrixMul: %v %v", proc.GetPid(), args)
+	m := &MatrixMult{}
+	m.FsLib = fslib.MakeFsLib("spinner")
+	m.ProcClnt = procclnt.MakeProcClnt(m.FsLib)
+	m.n, err = strconv.Atoi(args[0])
+	if err != nil {
+		db.DFatalf("Error parsing N: %v", err)
+	}
+	m.m1 = matrix(m.n)
+	m.m2 = matrix(m.n)
+	m.m3 = matrix(m.n)
+	return m, nil
+}
+
+// Create an n x n matrix.
+func matrix(n int) *mat.Dense {
+	s := make([]float64, n*n)
+	for i := 0; i < n*n; i++ {
+		s[i] = float64(i)
+	}
+	return mat.NewDense(n, n, s)
+}
+
+func (m *MatrixMult) doMM() {
+	// Multiply m.m1 and m.m2, and place the result in m.m3
+	m.m3.Mul(m.m1, m.m2)
+}
+
+func (m *MatrixMult) Work() {
+	start := time.Now()
+	err := m.Started()
+	if err != nil {
+		db.DFatalf("Started: error %v\n", err)
+	}
+	db.DPrintf("MATMUL", "doMM %v", proc.GetPid())
+	m.doMM()
+	db.DPrintf("MATMUL", "doMM done %v", proc.GetPid())
+	m.Exited(proc.MakeStatusInfo(proc.StatusOK, "Latency (us)", time.Since(start).Microseconds()))
+	db.DPrintf("MATMUL", "Exited %v", proc.GetPid())
+}

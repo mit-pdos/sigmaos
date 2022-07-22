908511462071ac87b81a1e4ae4f38f2f5c1f95c2
Ariel Szekely <arielck@mit.edu>
2022-07-22 13:54:12 -0400

    Tune down procd BE task oversubscription rate

--- ninep/hyperparams.go
+++ ninep/hyperparams.go
@@ -35,7 +35,7 @@ procd:
   stealable_proc_timeout: 100ms
   work_steal_scan_timeout: 100ms
   be_proc_claim_cpu_threshold: 90.0
-  be_proc_oversubscription_rate: 2.0
+  be_proc_oversubscription_rate: 1.0
 
 raft:
   tick_interval: 25ms
@@ -68,7 +68,7 @@ procd:
   stealable_proc_timeout: 1000ms
   work_steal_scan_timeout: 1000ms
   be_proc_claim_cpu_threshold: 90.0
-  be_proc_oversubscription_rate: 2.0
+  be_proc_oversubscription_rate: 1.0
 
 raft:
   tick_interval: 500ms

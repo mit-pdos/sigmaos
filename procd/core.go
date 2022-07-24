984c3f8e7db9b260bf7052059421f74fc65cac47
Ariel Szekely <arielck@mit.edu>
2022-07-24 13:51:59 -0400

    Get mroe info about what procs are running when a proc is claimed

--- procd/core.go
+++ procd/core.go
@@ -175,7 +175,7 @@ func (pd *Procd) overloaded(util float64, cload stats.Tload) bool {
 	}
 	t := np.Conf.Procd.BE_PROC_CLAIM_CPU_THRESHOLD
 	// If we have a history of high utilization...
-	if cload[0] >= t && cload[1] >= t && cload[2] >= t {
+	if cload[0] >= t && cload[1] >= t {
 		return true
 	}
 	// If there is a sudden drop in CPU utilization...
@@ -203,7 +203,11 @@ func (pd *Procd) hasEnoughCores(p *proc.Proc) bool {
 		cload := pd.GetStats().GetCustomLoad()
 		rlc := pd.procClaimRateLimitCheck(util, p)
 		if util < np.Conf.Procd.BE_PROC_CLAIM_CPU_THRESHOLD && !pd.overloaded(util, cload) && rlc {
-			db.DPrintf(db.ALWAYS, "Claimed BE proc: util %v Linux load %v Custom load %v rate-limit check %v proc %v", util, load, cload, rlc, p.Program)
+			progs := make([]string, 0, len(pd.runningProcs))
+			for _, p := range pd.runningProcs {
+				progs = append(progs, p.attr.Program)
+			}
+			db.DPrintf(db.ALWAYS, "Claimed BE proc: util %v Linux load %v Custom load %v rate-limit check %v proc %v, running %v", util, load, cload, rlc, p.Program, progs)
 			return true
 		}
 		db.DPrintf("PROCD", "Couldn't claim BE proc: util %v rate-limit check %v proc %v", util, rlc, p)

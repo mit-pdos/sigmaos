983f02e0cc4ec44844c209ffc9b89ab7b75ad392
Ariel Szekely <arielck@mit.edu>
2022-07-29 15:40:47 -0400

    Adjust graphing script to include procd monitoring information

--- benchmarks/scripts/graph/realmbalance.py
+++ benchmarks/scripts/graph/realmbalance.py
@@ -32,6 +32,22 @@ def get_time_range(tpts):
     end = max(end, max_t)
   return (start, end)
 
+def extend_tpts_to_range(tpts, r):
+  if len(tpts) == 0:
+    return
+  assert(len(tpts) == 1)
+  last_tick = tpts[0][len(tpts[0]) - 1]
+  if last_tick[0] < r[1]:
+    tpts[0].append((r[1], last_tick[1]))
+
+def get_overall_time_range(ranges):
+  start = sys.maxsize
+  end = 0
+  for r in ranges:
+    start = min(start, r[0])
+    end = max(end, r[1])
+  return (start, end)
+
 # Fit times to the data collection range, and convert us -> ms
 def fit_times_to_range(tpts, time_range):
   for tpt in tpts:
@@ -74,15 +90,19 @@ def finalize_graph(out):
   plt.savefig(out)
 
 def graph_data(input_dir, out):
-  # TODO: change to mr below
+  procd_tpts = read_tpts(input_dir, "test")
+  procd_range = get_time_range(procd_tpts)
   mr_tpts = read_tpts(input_dir, "mr")
   mr_range = get_time_range(mr_tpts)
   kv_tpts = read_tpts(input_dir, "kv")
   kv_range = get_time_range(kv_tpts)
   # Time range for graph
-  time_range = (min(kv_range[0], mr_range[0]), max(kv_range[1], mr_range[1]))
+  time_range = get_overall_time_range([procd_range, mr_range, kv_range])
+  extend_tpts_to_range(procd_tpts, time_range)
   mr_tpts = fit_times_to_range(mr_tpts, time_range)
   kv_tpts = fit_times_to_range(kv_tpts, time_range)
+  procd_tpts = fit_times_to_range(procd_tpts, time_range)
+  # Convert range ms -> sec
   time_range = ((time_range[0] - time_range[0]) / 1000.0, (time_range[1] - time_range[0]) / 1000.0)
   mr_buckets = bucketize(mr_tpts, time_range)
   if len(mr_tpts) > 0:
@@ -90,6 +110,9 @@ def graph_data(input_dir, out):
   kv_buckets = bucketize(kv_tpts, time_range)
   if len(kv_tpts) > 0:
     add_data_to_graph(kv_buckets, "KV Throughput")
+  if len(procd_tpts) > 0:
+    print(procd_tpts)
+    add_data_to_graph(dict(procd_tpts[0]), "Procds Assigned")
   finalize_graph(out)
 
 if __name__ == "__main__":

49b56900fff4fc09ccefd0185b001d1dfa1ed839
Ariel Szekely <arielck@mit.edu>
2022-08-04 11:11:55 -0400

    Graph moving-average of aggregate tpt instead of bucketized instantaneous tpt. Also, graph a "Cores Assigned" line for each realm in the realmBalance benchmark

--- benchmarks/scripts/graph/aggregate-tpt.py
+++ benchmarks/scripts/graph/aggregate-tpt.py
@@ -58,10 +58,9 @@ def fit_times_to_range(tpts, time_range):
 def find_bucket(time, step_size):
   return int(time - time % step_size)
 
-# Fit into 100ms buckets.
+# Fit into 10ms buckets.
 def bucketize(tpts, time_range):
-  # TODO: bucket size?
-  step_size = 100
+  step_size = 10
   buckets = {}
   for i in range(0, find_bucket(time_range[1], step_size) + step_size * 2, step_size):
     buckets[i] = 0.0
@@ -70,9 +69,27 @@ def bucketize(tpts, time_range):
       buckets[find_bucket(t[0], step_size)] += t[1]
   return buckets
 
-def add_data_to_graph(ax, buckets, label, color, linestyle, normalize=True):
+def buckets_to_lists(buckets):
   x = np.array(sorted(list(buckets.keys())))
   y = np.array([ buckets[x1] for x1 in x ])
+  return (x, y)
+
+def moving_avg(y):
+  # to get ms, multiply by step_size in bucketize
+  window_size = 30
+  moving_avgs = []
+  for i in range(len(y) - window_size + 1):
+    window = y[ i : i + window_size ]
+    window_avg = sum(window) / window_size
+    moving_avgs.append(window_avg)
+  # Fill in the last few slots.
+  for i in range(len(y) - len(moving_avgs)):
+    window = y[len(moving_avgs):]
+    window_avg = sum(window) / len(window)
+    moving_avgs.append(window_avg)
+  return np.array(moving_avgs)
+
+def add_data_to_graph(ax, x, y, label, color, linestyle, normalize=True):
   if normalize:
     n = max(y)
     y = y / n
@@ -98,8 +115,14 @@ def setup_graph():
   ax2.set_ylabel("Cores Assigned")
   return fig, ax, ax2
 
-def graph_data(input_dir, out):
-  procd_tpts = read_tpts(input_dir, "test")
+def graph_data(input_dir, out, kv_realm, mr_realm):
+  if kv_realm is None and mr_realm is None:
+    procd_tpts = read_tpts(input_dir, "test")
+    assert(len(procd_tpts) <= 1)
+  else:
+    procd_tpts = read_tpts(input_dir, kv_realm)
+    procd_tpts.append(read_tpts(input_dir, mr_realm)[0])
+    assert(len(procd_tpts) == 2)
   procd_range = get_time_range(procd_tpts)
   mr_tpts = read_tpts(input_dir, "mr")
   mr_range = get_time_range(mr_tpts)
@@ -117,21 +140,37 @@ def graph_data(input_dir, out):
   fig, ax, ax2 = setup_graph()
   plots = []
   if len(kv_tpts) > 0:
-    p = add_data_to_graph(ax, kv_buckets, "KV Throughput", "blue", "-", normalize=True)
+    x, y = buckets_to_lists(kv_buckets)
+    y = moving_avg(y)
+    p = add_data_to_graph(ax, x, y, "KV Throughput", "blue", "-", normalize=True)
     plots.append(p)
   mr_buckets = bucketize(mr_tpts, time_range)
   if len(mr_tpts) > 0:
-    p = add_data_to_graph(ax, mr_buckets, "MR Throughput", "orange", "-", normalize=True)
+    x, y = buckets_to_lists(mr_buckets)
+    y = moving_avg(y)
+    p = add_data_to_graph(ax, x, y, "MR Throughput", "orange", "-", normalize=True)
     plots.append(p)
   if len(procd_tpts) > 0:
-    p = add_data_to_graph(ax2, dict(procd_tpts[0]), "Cores Assigned", "green", "--", normalize=False)
-    plots.append(p)
+    # If we are dealing with multiple realms...
+    if len(procd_tpts) > 1:
+      x, y = buckets_to_lists(dict(procd_tpts[0]))
+      p = add_data_to_graph(ax2, x, y, "KV Realm Cores Assigned", "blue", "--", normalize=False)
+      plots.append(p)
+      x, y = buckets_to_lists(dict(procd_tpts[1]))
+      p = add_data_to_graph(ax2, x, y, "MR Realm Cores Assigned", "orange", "--", normalize=False)
+      plots.append(p)
+    else:
+      x, y = buckets_to_lists(dict(procd_tpts[0]))
+      p = add_data_to_graph(ax2, x, y, "Cores Assigned", "green", "--", normalize=False)
+      plots.append(p)
   finalize_graph(fig, ax, plots, out)
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument("--measurement_dir", type=str, required=True)
+  parser.add_argument("--kv_realm", type=str, default=None)
+  parser.add_argument("--mr_realm", type=str, default=None)
   parser.add_argument("--out", type=str, required=True)
 
   args = parser.parse_args()
-  graph_data(args.measurement_dir, args.out)
+  graph_data(args.measurement_dir, args.out, args.kv_realm, args.mr_realm)

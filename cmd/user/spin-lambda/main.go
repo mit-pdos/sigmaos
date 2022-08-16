10f8a862e553e106699db11d18277726266d8a4b
Ariel Szekely <arielck@mit.edu>
2022-08-16 11:33:13 -0400

    Have lambda read name/

--- cmd/user/spin-lambda/main.go
+++ cmd/user/spin-lambda/main.go
@@ -3,29 +3,31 @@ package main
 import (
 	"log"
 	"path"
-	//	"time"
+	"time"
 
 	"github.com/aws/aws-lambda-go/lambda"
-	//	"ulambda/fslib"
-	//	"ulambda/semclnt"
+	"ulambda/fslib"
+	//		"ulambda/semclnt"
 )
 
 func spin(args []string) error {
 	addr := args[0]
 	sempath := args[1]
 	log.Printf("Addr %v sem %v", addr, path.Base(sempath))
-	//fsl := fslib.MakeFsLibAddr("spin-"+path.Base(sempath), []string{addr})
-	//	sts, err := fsl.GetDir("name/")
-	//	if err != nil {
-	//		return err
-	//	}
-	//	log.Printf("Got dir: %v", sts)
+	fsl := fslib.MakeFsLibAddr("spin-"+path.Base(sempath), []string{addr})
+	sts, err := fsl.GetDir("name/")
+	if err != nil {
+		return err
+	}
+	log.Printf("Got dir: %v", sts)
 	//	sem := semclnt.MakeSemClnt(fsl, sempath)
 	//	err := sem.Up()
 	//	if err != nil {
 	//		return err
 	//	}
-	//	time.Sleep(4 * time.Minute)
+	//	time.Sleep(4 * time.Second)
+	//	//	time.Sleep(4 * time.Minute)
+	//	log.Printf("Done sleep sem %v", path.Base(sempath))
 	return nil
 }
 

41dfc00283b82743628609c900c1f58e5f01ca09
Frans Kaashoek <kaashoek@mit.edu>
2022-09-09 10:01:28 -0400

    Set split sz actual to 10MB, which is what we use for corral word_count

--- mr/mr.go
+++ mr/mr.go
@@ -97,7 +97,8 @@ func MkBins(fsl *fslib.FsLib, dir string, maxbinsz np.Tlength) ([]Bin, error) {
 	binsz := np.Tlength(0)
 	bin := Bin{}
 	// Set split size to 10MB
-	splitsz := maxbinsz >> 3 //np.Tlength(10 * 1024 * 1024)
+	splitsz := np.Tlength(10 * 1024 * 1024)
+	// splitsz := maxbinsz >> 3 //np.Tlength(10 * 1024 * 1024)
 
 	sts, err := fsl.GetDir(dir)
 	if err != nil {

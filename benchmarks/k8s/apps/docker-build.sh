996a90f44c52ac81df5cf314a6206742c56b9c53
Ariel Szekely <arielck@mit.edu>
2022-10-04 20:16:32 -0400

    Remove some redundancy by using a single Dockerfile

--- benchmarks/k8s/apps/docker-build.sh
+++ benchmarks/k8s/apps/docker-build.sh
@@ -3,5 +3,5 @@
 app=$(basename $(pwd))
 
 cd ..
-docker build -t arielszekely/$app -f $app/Dockerfile .
+docker build -t arielszekely/$app --build-arg APP_NAME=$app -f Dockerfile .
 docker push arielszekely/$app

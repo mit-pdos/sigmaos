7cf65f4f96b74bff9a709143894ebf8a4ec447cb
Ariel Szekely <arielck@mit.edu>
2022-10-06 10:56:48 -0400

    Make sure docker builds bins from scratch, and add module dependencies for websrv

--- benchmarks/k8s/apps/docker-build.sh
+++ benchmarks/k8s/apps/docker-build.sh
@@ -2,6 +2,8 @@
 
 app=$(basename $(pwd))
 
+rm ./bin/*
+
 cd ..
 docker build -t arielszekely/$app --build-arg APP_NAME=$app -f Dockerfile .
 docker push arielszekely/$app
---
+++ benchmarks/k8s/apps/websrv/go.mod
@@ -0,0 +1,5 @@
+module websrv
+
+go 1.19
+
+require gonum.org/v1/gonum v0.12.0

bf10ced3746f0a414e6577f25a2860f976723ebe
Ariel Szekely <arielck@mit.edu>
2022-10-06 12:00:22 -0400

    Make webcli more configurable for performance testing, and clean up its yaml files. It should run as a Pod, not a Deployment, so it isn't restarted automatically

--- benchmarks/k8s/apps/webcli/cmd/webcli/main.go
+++ benchmarks/k8s/apps/webcli/cmd/webcli/main.go
@@ -5,9 +5,26 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
+
+func runCli(url, path, query string, done chan bool) {
+	start := time.Now()
+	resp, err := http.Get(url + "/" + path + query)
+	if err != nil {
+		log.Fatalf("Error GET: %v", err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Error ReadAll: %v", err)
+	}
+
+	log.Printf("%v sec got response:\n\"%v\"", time.Since(start).Seconds(), strings.TrimSpace(string(body)))
+	done <- true
+}
 
 func main() {
 	url := os.Getenv("WEBSRV_URL")
@@ -27,16 +44,15 @@ func main() {
 		}
 	}
 
-	start := time.Now()
-	resp, err := http.Get(url + "/" + path + query)
+	nclnt, err := strconv.Atoi(os.Getenv("N_CLNT"))
 	if err != nil {
-		log.Fatalf("Error GET: %v", err)
+		log.Fatalf("Error Invalid N_CLINT (%v): %v", os.Getenv("N_CLNT"), err)
 	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalf("Error ReadAll: %v", err)
+	done := make(chan bool)
+	for i := 0; i < nclnt; i++ {
+		go runCli(url, path, query, done)
 	}
-
-	log.Printf("%v sec got response:\n\"%v\"", time.Since(start).Seconds(), strings.TrimSpace(string(body)))
+	for i := 0; i < nclnt; i++ {
+		<-done
+	}
 }

26cc0a767cc6fc54ddaaa1700bc95aab500d7dc0
Ariel Szekely <arielck@mit.edu>
2022-10-10 09:57:34 -0400

    x

--- benchmarks/benchmarks_test.go
+++ benchmarks/benchmarks_test.go
@@ -231,7 +231,7 @@ func TestMicroSpawnWaitExit5msSleeper(t *testing.T) {
 func TestMicroSpawnBurstTpt(t *testing.T) {
 	ts := test.MakeTstateAll(t)
 	rs := benchmarks.MakeRawResults(N_TRIALS)
-	db.DPrintf(db.ALWAYS, "SpawnBursting %v procs", N_PROC)
+	db.DPrintf(db.ALWAYS, "SpawnBursting %v procs with max parallelism %v", N_PROC, MAX_PARALLEL)
 	ps, _ := makeNProcs(N_PROC, "user/sleeper", []string{"0s", ""}, []string{}, 1)
 	runOps(ts, []interface{}{ps}, spawnBurstWaitStartProcs, rs)
 	printResults(rs)

adf5903f7fd547ee9a33e67d41a3303f6c4cb572
Ariel Szekely <arielck@mit.edu>
2022-11-18 13:21:12 -0500

    Revert protdevsrv non-blocking changes

--- protdevsrv/stream.go
+++ protdevsrv/stream.go
@@ -6,27 +6,20 @@ import (
 	"log"
 	"reflect"
 	"strings"
-	"sync"
 
 	// db "sigmaos/debug"
-	db "sigmaos/debug"
 	"sigmaos/fs"
 	np "sigmaos/ninep"
-	"sigmaos/sesscond"
 )
 
 type Stream struct {
-	sync.Mutex
-	svc      *service
-	sct      *sesscond.SessCondTable
-	inflight bool
-	repl     []byte
+	svc  *service
+	repl []byte
 }
 
-func mkStream(ctx fs.CtxI, svc *service) (fs.File, *np.Err) {
+func mkStream(svc *service) (fs.File, *np.Err) {
 	st := &Stream{}
 	st.svc = svc
-	st.sct = ctx.SessCondTable()
 	return st, nil
 }
 
@@ -42,40 +35,8 @@ func (st *Stream) Write(ctx fs.CtxI, off np.Toffset, b []byte, v np.TQversion) (
 		return 0, np.MkErrError(err)
 	}
 
-	// Lock the stream.
-	st.Lock()
-	defer st.Unlock()
-
-	// Sanity-check that there aren't concurrent RPCs on a single stream. These
-	// should (currently) be synchronized on the client side.
-	if st.inflight {
-		db.DFatalf("Tried to perform an RPC on an already-inflight stream.")
-	}
-
-	// Mark that an RPC is in-flight.
-	st.inflight = true
-
-	// Create a sesscond to allow concurrent RPCs.
-	cond := st.sct.MakeSessCond(&st.Mutex)
-
 	// Dispatch RPC in a separate thread & store reply.
-	go func() {
-		rep = st.svc.dispatch(req.Method, req)
-		// Wake up the writer thread.
-		st.Lock()
-		defer st.Unlock()
-
-		// Mark that the in-flight RPC has terminated..
-		st.inflight = false
-		cond.Signal()
-	}()
-
-	// Wait for the RPC to complete. This allows other sessions to make
-	// concurrent  RPCs to this server.
-	for st.inflight {
-		cond.Wait(ctx.SessionId())
-	}
-	st.sct.FreeSessCond(cond)
+	rep = st.svc.dispatch(req.Method, req)
 
 	rb := new(bytes.Buffer)
 	re := gob.NewEncoder(rb)

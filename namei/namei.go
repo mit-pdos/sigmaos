af48a42ed92b5c735dba1ac9e4ece4fa05b634c5
Frans Kaashoek <kaashoek@mit.edu>
2022-11-25 09:11:13 -0500

    x

--- namei/namei.go
+++ namei/namei.go
@@ -27,13 +27,13 @@ func Walk(plt *lockmap.PathLockTable, ctx fs.CtxI, o fs.FsObj, dlk *lockmap.Path
 	d := o.(fs.Dir)
 	nos, e, rest, err := d.LookupPath(ctx, target)
 	if err != nil { // an error or perhaps a ~
-		db.DPrintf("PROTSRV", "%v: dir %v: file not found %v", ctx.Uname(), d, target[0])
+		db.DPrintf("NAMEI", "%v: dir %v: file not found %v", ctx.Uname(), d, target[0])
 		releaseLk(plt, ctx, plk)
 		return os, d, dlk, target, err
 	}
 	os = append(os, nos...)
 	if len(rest) == 0 { // done?
-		db.DPrintf("PROTSRV", "%v: namei %v e %v os %v", ctx.Uname(), fn, e, os)
+		db.DPrintf("NAMEI", "%v: namei %v e %v os %v", ctx.Uname(), fn, e, os)
 		flk := plt.Acquire(ctx, fn)
 		plt.Release(ctx, dlk)
 		releaseLk(plt, ctx, plk)
@@ -45,7 +45,7 @@ func Walk(plt *lockmap.PathLockTable, ctx fs.CtxI, o fs.FsObj, dlk *lockmap.Path
 		dlk = plt.HandOverLock(ctx, dlk, target[0])
 		return Walk(plt, ctx, e, dlk, dn.Append(target[0]), target[1:], os)
 	default: // an error or perhaps a symlink
-		db.DPrintf("PROTSRV", "%v: error not dir namei %T %v %v %v %v", ctx.Uname(), e, target, d, os, target[1:])
+		db.DPrintf("NAMEI", "%v: error not dir namei %T %v %v %v %v", ctx.Uname(), e, target, d, os, target[1:])
 		return os, e, dlk, target, np.MkErr(np.TErrNotDir, target[0])
 	}
 }

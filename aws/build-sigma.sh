2312e52965795ee8488e437c5304b036775c90f4
Ariel Szekely <arielck@mit.edu>
2022-11-29 08:54:38 -0500

    Actually checkout desired branch in aws build script

--- aws/build-sigma.sh
+++ aws/build-sigma.sh
@@ -59,7 +59,7 @@ MAIN="${vms[0]}"
 
 echo "UPDATE: $MAIN"
 ssh -i key-$VPC.pem ubuntu@$MAIN /bin/bash <<ENDSSH
-ssh-agent bash -c 'ssh-add ~/.ssh/aws-ulambda; ( cd ulambda; git fetch --all; git checkout master; )'
+ssh-agent bash -c 'ssh-add ~/.ssh/aws-ulambda; ( cd ulambda; git fetch --all; git checkout $BRANCH; )'
 ssh-agent bash -c 'ssh-add ~/.ssh/aws-ulambda; (cd ulambda; git pull > /tmp/git.out 2>&1 )'
 (cd ulambda; ./stop.sh)
 # Make sure we build the first time sigmaos is installed.

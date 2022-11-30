26eae450ff6eff0a7c798b266f5e967ebf22cbcc
Frans Kaashoek <kaashoek@mit.edu>
2022-11-30 13:56:30 -0500

    Make proxy work again; go test sigmaos/proxy passes

--- npcodec/frame.go
+++ npcodec/frame.go
@@ -67,11 +67,53 @@ func WriteFrameAndBuf(wr io.Writer, frame []byte, buf []byte) *fcall.Err {
 }
 
 func MarshalFcallMsg(fc fcall.Fcall, b *bufio.Writer) *fcall.Err {
-	frame, error := marshal1(true, fc)
+	fcm := fc.(*np.FcallMsg)
+	frame, error := marshal1(true, fcm.ToWireCompatible())
 	if error != nil {
 		return fcall.MkErr(fcall.TErrBadFcall, error.Error())
 	}
-	return WriteFrame(b, frame)
+	dataBuf := false
+	var data []byte
+	switch fcm.Type() {
+	case fcall.TTwrite:
+		msg := fcm.Msg.(*np.Twrite)
+		data = msg.Data
+		dataBuf = true
+	case fcall.TTwriteV:
+		msg := fcm.Msg.(*np.TwriteV)
+		data = msg.Data
+		dataBuf = true
+	case fcall.TRread:
+		msg := fcm.Msg.(*np.Rread)
+		data = msg.Data
+		dataBuf = true
+	case fcall.TRgetfile:
+		msg := fcm.Msg.(*np.Rgetfile)
+		data = msg.Data
+		dataBuf = true
+	case fcall.TTsetfile:
+		msg := fcm.Msg.(*np.Tsetfile)
+		data = msg.Data
+		dataBuf = true
+	case fcall.TTputfile:
+		msg := fcm.Msg.(*np.Tputfile)
+		data = msg.Data
+		dataBuf = true
+	case fcall.TTwriteread:
+		msg := fcm.Msg.(*np.Twriteread)
+		data = msg.Data
+		dataBuf = true
+	case fcall.TRwriteread:
+		msg := fcm.Msg.(*np.Rwriteread)
+		data = msg.Data
+		dataBuf = true
+	default:
+	}
+	if dataBuf {
+		return WriteFrameAndBuf(b, frame, data)
+	} else {
+		return WriteFrame(b, frame)
+	}
 }
 
 func MarshalFcallMsgByte(fcm *np.FcallMsg) ([]byte, *fcall.Err) {

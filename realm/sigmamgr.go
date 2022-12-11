cc6c193dc5713e1c77f221aaac85a6dd37b1bfe3
Ariel Szekely <arielck@mit.edu>
2022-12-11 12:58:31 -0500

    x

--- realm/sigmamgr.go
+++ realm/sigmamgr.go
@@ -111,7 +111,7 @@ func (m *SigmaResourceMgr) DestroyRealm(req proto.SigmaMgrRequest, res *proto.Si
 func (m *SigmaResourceMgr) RequestCores(req proto.SigmaMgrRequest, res *proto.SigmaMgrResponse) error {
 	m.Lock()
 	defer m.Unlock()
-	db.DPrintf("SIGMAMGR", "Request cores %v", req.RealmId)
+	db.DPrintf("SIGMAMGR", "[%v] Request cores", req.RealmId)
 
 	// If realm still exists, try to grow it.
 	if _, ok := m.realmLocks[req.RealmId]; ok {

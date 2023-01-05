1c3ba3af0653111f8d262157c45125c2b1110a28
Ariel Szekely <arielck@mit.edu>
2023-01-04 21:38:23 -0500

    Configure iptables script to usedefault interface

--- iptables.sh
+++ iptables.sh
@@ -6,5 +6,7 @@ sudo iptables -nvL FORWARD | grep -c sigmab > /dev/null && exit 1
 # enable ip forwarding
 echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward
 
+iface=$(route | grep "default" | tr -s ' ' | cut -d ' ' -f8)
+
 # do this once; routing rules for bridge are inserted by scnet
-sudo iptables --append POSTROUTING --table nat --out-interface wlp2s0 --jump MASQUERADE
+sudo iptables --append POSTROUTING --table nat --out-interface $iface --jump MASQUERADE

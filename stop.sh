6511bcd73e6cac07b3d6a140358395b82a1d7f15
Frans Kaashoek <kaashoek@mit.edu>
2023-01-08 11:20:54 -0500

    Kill just bootkernel

--- stop.sh
+++ stop.sh
@@ -4,11 +4,7 @@ DIR=$(dirname $0)
 . $DIR/env/env.sh
 
 $DIR/umount.sh
-for d in "realm" "kernel" "user"; do
-  for p in `ls "cmd/$d"`; do
-    pgrep -x $p > /dev/null && killall $p
-  done
-done
+pgrep -x bootkernel > /dev/null && killall bootkernel
 
 sudo ip link del `ip link list | grep -o "sb[a-z]*" | head -1`
 sudo ip link del `ip link list | grep -o "sp[0-9]+" | head -1`

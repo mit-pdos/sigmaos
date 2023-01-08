244250135e9f5e95daa385d5b053ee11e0c0d3b2
Frans Kaashoek <kaashoek@mit.edu>
2023-01-08 07:53:55 -0500

    Use ip route to determine iface

--- iptables.sh
+++ iptables.sh
@@ -6,7 +6,8 @@ sudo iptables -nvL FORWARD | grep -c sigmab > /dev/null && exit 1
 # enable ip forwarding
 echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward
 
-iface=$(route | grep "default" | tr -s ' ' | cut -d ' ' -f8)
+
+iface=$(ip route get 8.8.8.8 | awk -- '{printf $5}')
 
 # do this once; routing rules for bridge are inserted by scnet
 sudo iptables --append POSTROUTING --table nat --out-interface $iface --jump MASQUERADE

18e5f9dd266e5c44e6937c7ad14ec8d546357749
Frans Kaashoek <kaashoek@mit.edu>
2023-01-14 10:16:03 -0500

    Checkpoint experiments with setupFs()

--- container/pcontainer.go
+++ container/pcontainer.go
@@ -6,13 +6,13 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"path"
+	//"path"
 	"syscall"
 
 	db "sigmaos/debug"
 	"sigmaos/rand"
 	// "sigmaos/seccomp"
-	// sp "sigmaos/sigmap"
+	//sp "sigmaos/sigmap"
 )
 
 const (
@@ -97,7 +97,7 @@ func ls(dir string) error {
 func setupFs(newRoot string) error {
 	oldRootMnt := "old_root" + rand.String(8)
 
-	xnewRoot := newRoot
+	// xnewRoot := newRoot
 	newRoot = newRoot + "/rootrealm"
 
 	log.Printf("new root %v\n", newRoot)
@@ -108,13 +108,19 @@ func setupFs(newRoot string) error {
 		return err
 	}
 
-	// Mount old file system
-	if err := syscall.Mkdir(path.Join(newRoot, oldRootMnt), 0700); err != nil {
+	// Chdir to new root
+	if err := syscall.Chdir(newRoot); err != nil {
+		log.Printf("failed to chdir to /: %v", err)
+		return err
+	}
+
+	// Make dir for oldMount
+	if err := syscall.Mkdir(oldRootMnt, 0700); err != nil {
 		log.Printf("failed to mkdir: %v", err)
 		return err
 	}
 
-	ls(newRoot)
+	ls(".")
 
 	// // Mount /sys for /sys/devices/system/cpu/online; XXX exclude
 	// // /sys/firmware; others?
@@ -124,43 +130,43 @@ func setupFs(newRoot string) error {
 	// }
 
 	// // Mount /dev/urandom
-	// if err := syscall.Mount("/dev/urandom", path.Join(newRoot, "dev/urandom"), "none", syscall.MS_BIND, ""); err != nil {
-	// 	log.Printf("failed to mount /dev/urandom: %v", err)
+	// if err := syscall.Mount("/dev", "dev", "none", syscall.MS_BIND|syscall.MS_RDONLY, ""); err != nil {
+	// 	log.Printf("failed to mount /dev: %v", err)
 	// 	return err
 	// }
 
 	// Mount /usr
-	if err := syscall.Mount("/usr", path.Join(newRoot, "usr"), "none", syscall.MS_BIND, ""); err != nil {
+	if err := syscall.Mount("/usr", "usr", "none", syscall.MS_BIND|syscall.MS_RDONLY, ""); err != nil {
 		log.Printf("failed to mount /dev/usr: %v", err)
 		return err
 	}
 
 	// Mount /lib
-	if err := syscall.Mount("/lib", path.Join(newRoot, "lib"), "none", syscall.MS_BIND, ""); err != nil {
+	if err := syscall.Mount("/lib", "lib", "none", syscall.MS_BIND, ""); err != nil {
 		log.Printf("failed to mount /dev/lib: %v", err)
 		return err
 	}
 
 	// Mount /lib
-	if err := syscall.Mount("/lib64", path.Join(newRoot, "lib64"), "none", syscall.MS_BIND, ""); err != nil {
+	if err := syscall.Mount("/lib64", "lib64", "none", syscall.MS_BIND, ""); err != nil {
 		log.Printf("failed to mount /dev/lib64: %v", err)
 		return err
 	}
 
-	// // Mount /etc
-	// if err := syscall.Mount("/etc", path.Join(newRoot, "etc"), "none", syscall.MS_BIND, ""); err != nil {
-	// 	log.Printf("failed to mount /etc: %v", err)
-	// 	return err
-	// }
-
-	// Mount bin/user on /bin so that user procs can run only programs from /bin/user
-	if err := syscall.Mount(path.Join(xnewRoot)+"/bin/user", path.Join(newRoot, UBIN), "none", syscall.MS_BIND, ""); err != nil {
-		log.Printf("failed to mount /bin: %v", err)
+	// Mount /etc
+	if err := syscall.Mount("/etc", "etc", "none", syscall.MS_BIND, ""); err != nil {
+		log.Printf("failed to mount /etc: %v", err)
 		return err
 	}
 
+	// // Mount bin/user on /bin so that user procs can run only programs from /bin/user
+	// if err := syscall.Mount(path.Join(xnewRoot)+"/bin/user", path.Join(newRoot, UBIN), "none", syscall.MS_BIND, ""); err != nil {
+	// 	log.Printf("failed to mount /bin: %v", err)
+	// 	return err
+	// }
+
 	// pivot_root
-	if err := syscall.PivotRoot(newRoot, path.Join(newRoot, oldRootMnt)); err != nil {
+	if err := syscall.PivotRoot(".", oldRootMnt); err != nil {
 		log.Printf("failed to pivot root: %v", err)
 		return err
 	}

c04c627b4cb3428b257133082352432a6573339d
Ariel Szekely <arielck@mit.edu>
2023-01-16 13:11:36 -0500

    Add mem package and have procd call into it

--- procd/mem.go
+++ procd/mem.go
@@ -1,34 +1,10 @@
 package procd
 
 import (
-	"io/ioutil"
-	"strconv"
-	"strings"
-
 	db "sigmaos/debug"
+	"sigmaos/mem"
 	"sigmaos/proc"
 )
-
-func getMemTotal() proc.Tmem {
-	b, err := ioutil.ReadFile("/proc/meminfo")
-	if err != nil {
-		db.DFatalf("Can't read /proc/meminfo: %v", err)
-	}
-	lines := strings.Split(string(b), "\n")
-	for _, l := range lines {
-		if strings.Contains(l, "MemTotal") {
-			s := strings.Split(l, " ")
-			kbStr := s[len(s)-2]
-			kb, err := strconv.Atoi(kbStr)
-			if err != nil {
-				db.DFatalf("Couldn't convert MemTotal: %v", err)
-			}
-			return proc.Tmem(kb / 1000)
-		}
-	}
-	db.DFatalf("Couldn't find total mem")
-	return 0
-}
 
 func (pd *Procd) hasEnoughMemL(p *proc.Proc) bool {
 	return pd.memAvail >= p.GetMem()
@@ -36,6 +12,7 @@ func (pd *Procd) hasEnoughMemL(p *proc.Proc) bool {
 
 func (pd *Procd) allocMemL(p *proc.Proc) {
 	pd.memAvail -= p.GetMem()
+	pd.sanityCheckMemL()
 }
 
 func (pd *Procd) freeMem(p *proc.Proc) {
@@ -43,4 +20,11 @@ func (pd *Procd) freeMem(p *proc.Proc) {
 	defer pd.mu.Unlock()
 
 	pd.memAvail += p.GetMem()
+	pd.sanityCheckMemL()
+}
+
+func (pd *Procd) sanityCheckMemL() {
+	if pd.memAvail < 0 || pd.memAvail > mem.GetTotalMem() {
+		db.DFatalf("Memory sanity check failed %v", pd.memAvail)
+	}
 }

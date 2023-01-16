f6b6776264e7151a0079e6c7ea7b97bb105bf38a
Ariel Szekely <arielck@mit.edu>
2023-01-15 20:29:50 -0500

    Tear down network interfaces with stop.sh

--- stop.sh
+++ stop.sh
@@ -10,6 +10,7 @@ fi
 
 pgrep -x bootkernel > /dev/null && killall bootkernel
 pgrep -x bootsys > /dev/null && killall bootsys
+pgrep -x proxyd > /dev/null && killall bootsys
 
 while read -r line; do
     if [ ! -z "$line" ]; then
@@ -18,5 +19,16 @@ while read -r line; do
         sudo ip link del $b
     fi
 done <<< $(ip link list | grep -o "[0-9]*: sb[a-z]*")
+
+while read -r line; do
+    if [ ! -z "$line" ]; then
+        links=$(echo $line | cut -f 2 -d ' ')
+        l1=$(echo $links | cut -f 1 -d '@')
+        l2=$(echo $links | cut -f 2 -d '@')
+        echo "delete ifaces $l1 $l2"
+        sudo ip link del $l1
+        sudo ip link del $l2
+    fi
+done <<< $(ip link list | grep -o "[0-9]*: sp[0-9]*@sp[0-9]*")
 
 sudo iptables -S | ./delroute.sh

901ae67c0dbd8c5ed5fb968792f36ba3907f129e
Frans Kaashoek <kaashoek@mit.edu>
2023-01-22 15:17:36 -0500

    Restructure realm tests

--- realmclnt/realmclnt_test.go
+++ realmclnt/realmclnt_test.go
@@ -23,61 +23,68 @@ const (
 	REALM       sp.Trealm = "testrealm"
 )
 
-func TestBasic(t *testing.T) {
-	ts := test.MakeTstateAll(t)
+type Tstate struct {
+	*test.Tstate
+	rc *realmclnt.RealmClnt
+	sc *sigmaclnt.SigmaClnt
+}
 
-	sts1, err := ts.GetDir(sp.SCHEDD)
-	assert.Nil(t, err)
-
-	log.Printf("names sched %v\n", sp.Names(sts1))
+func mkTstate(t *testing.T) *Tstate {
+	ts := &Tstate{Tstate: test.MakeTstateAll(t)}
 
 	rc, err := realmclnt.MakeRealmClnt(ts.FsLib)
 	assert.Nil(t, err)
+	ts.rc = rc
 
 	err = rc.MakeRealm(REALM)
 	assert.Nil(t, err)
 
 	sc, err := sigmaclnt.MkSigmaClntRealm(ts.FsLib, "testrealm", REALM)
 	assert.Nil(t, err)
+	ts.sc = sc
 
-	sts, err := sc.GetDir(sp.NAMED)
+	return ts
+}
+
+func TestBasic(t *testing.T) {
+	ts := mkTstate(t)
+
+	sts1, err := ts.GetDir(sp.SCHEDD)
+	assert.Nil(t, err)
+
+	log.Printf("names sched %v\n", sp.Names(sts1))
+
+	sts, err := ts.sc.GetDir(sp.NAMED)
 	assert.Nil(t, err)
 
 	log.Printf("realm named root %v\n", sp.Names(sts))
 
 	assert.True(t, fslib.Present(sts, named.InitDir), "initfs")
 
-	sts, err = sc.GetDir(sp.SCHEDD)
+	sts, err = ts.sc.GetDir(sp.SCHEDD)
 	assert.Nil(t, err)
 
 	log.Printf("realm names sched %v\n", sp.Names(sts))
 
 	assert.True(t, sts1[0].Name == sts[0].Name)
+
 	ts.Shutdown()
 }
 
 func TestWaitExitSimpleSingle(t *testing.T) {
-	ts := test.MakeTstateAll(t)
-
-	rc, err := realmclnt.MakeRealmClnt(ts.FsLib)
-	assert.Nil(t, err)
-
-	err = rc.MakeRealm(REALM)
-	assert.Nil(t, err)
-
-	sc, err := sigmaclnt.MkSigmaClntRealmProc(ts.FsLib, "testrealm", REALM)
-	assert.Nil(t, err)
+	ts := mkTstate(t)
 
 	a := proc.MakeProc("sleeper", []string{fmt.Sprintf("%dms", SLEEP_MSECS), "name/"})
 	db.DPrintf(db.TEST, "Pre spawn")
-	err = sc.Spawn(a)
+	err := ts.sc.Spawn(a)
 	assert.Nil(t, err, "Spawn")
 	db.DPrintf(db.TEST, "Post spawn")
 
 	db.DPrintf(db.TEST, "Pre waitexit")
-	status, err := sc.WaitExit(a.GetPid())
+	status, err := ts.sc.WaitExit(a.GetPid())
 	db.DPrintf(db.TEST, "Post waitexit")
 	assert.Nil(t, err, "WaitExit error")
 	assert.True(t, status.IsStatusOK(), "Exit status wrong")
+
 	ts.Shutdown()
 }

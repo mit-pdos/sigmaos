0f264cc3b67bfa06ffdf98336316a1d67f4cba12
Frans Kaashoek <kaashoek@mit.edu>
2023-01-23 16:26:46 -0500

    Kill hanging-around proxyd's with more force

--- stop.sh
+++ stop.sh
@@ -5,7 +5,7 @@ if mount | grep -q 9p; then
     $DIR/umount.sh
 fi
 
-pgrep -x proxyd > /dev/null && killall proxyd
+pgrep -x proxyd > /dev/null && killall -9 proxyd
 
 docker stop $(docker ps -a | grep 'sigma' | cut -d ' ' -f1)
 docker rm $(docker ps -a | grep 'sigma' | cut -d ' ' -f1)

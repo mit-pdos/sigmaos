efb73f21c338c9997186004bc629c2941b98d6b2
Frans Kaashoek <kaashoek@mit.edu>
2023-01-23 18:43:15 -0500

    x

--- stop.sh
+++ stop.sh
@@ -7,7 +7,7 @@ fi
 
 pgrep -x proxyd > /dev/null && killall -9 proxyd
 
-if docker ps -a | grep 'sigma'; then
+if docker ps -a | grep -q 'sigma'; then
     docker stop $(docker ps -a | grep 'sigma' | cut -d ' ' -f1)
     docker rm $(docker ps -a | grep 'sigma' | cut -d ' ' -f1)
 fi

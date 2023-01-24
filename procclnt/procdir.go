22a004c695ad1520d044cf37d14476b78aff2726
ArielSzekely <arielszekely@gmail.com>
2023-01-23 20:08:38 -0500

    Move stack trace print into if statement for procclnt.MakeProcDir

--- procclnt/procdir.go
+++ procclnt/procdir.go
@@ -16,8 +16,8 @@ import (
 
 func (clnt *ProcClnt) MakeProcDir(pid proc.Tpid, procdir string, isKernelProc bool) error {
 	if err := clnt.MkDir(procdir, 0777); err != nil {
-		debug.PrintStack()
 		if serr.IsErrUnreachable(err) {
+			debug.PrintStack()
 			db.DFatalf("MakeProcDir mkdir pid %v procdir %v err %v\n", pid, procdir, err)
 		}
 		db.DPrintf(db.PROCCLNT_ERR, "MakeProcDir mkdir pid %v procdir %v err %v\n", pid, procdir, err)

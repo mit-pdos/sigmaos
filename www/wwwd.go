f65c7ff2d31b14299b42f7c2f391383c5a168931
Frans Kaashoek <kaashoek@mit.edu>
2023-01-23 20:09:06 -0500

    Build all user binaries and fix a few compile errors

--- www/wwwd.go
+++ www/wwwd.go
@@ -86,7 +86,7 @@ func MakeWwwd(job, tree string) *Wwwd {
 
 	//	www.FsLib = fslib.MakeFsLibBase("www") // don't mount Named()
 	// In order to automount children, we need to at least mount /pids.
-	if err := procclnt.MountPids(www.FsLib, fslib.Named()); err != nil {
+	if err := procclnt.MountPids(www.FsLib, www.NamedAddr()); err != nil {
 		db.DFatalf("wwwd err mount pids %v", err)
 	}
 

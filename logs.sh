92a205c57fd4942148a604e656fcf1552633d7cf
Ariel Szekely <arielck@mit.edu>
2023-01-24 08:32:18 -0500

    Add script to print docker logs

---
+++ logs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if docker ps -a | grep -qE 'sigma|uprocd|bootkerne'; then
+  for containerid in $(docker ps -a | grep -E 'sigma|uprocd|bootkerne' | cut -d ' ' -f1); do
+    imageid=$(docker ps -a | grep $containerid | tr -s " " | cut -d ' ' -f2)
+    echo "========== Logs for image $imageid =========="
+    docker logs $containerid
+  done
+fi

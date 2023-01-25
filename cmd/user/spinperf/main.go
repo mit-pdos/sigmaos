0a2cb9a1d57db9ef7909056d031d839a1f5b4cf3
Ariel Szekely <arielck@mit.edu>
2023-01-25 15:59:58 -0500

    Make spinperf runnable as a proc *or* as a linux process

--- cmd/user/spinperf/main.go
+++ cmd/user/spinperf/main.go
@@ -7,24 +7,47 @@ import (
 	"time"
 
 	db "sigmaos/debug"
+	"sigmaos/proc"
+	"sigmaos/sigmaclnt"
 )
 
 func main() {
-	if len(os.Args) != 4 {
-		db.DFatalf("Usage: %v nthread niter id", os.Args[0])
+	if len(os.Args) != 5 {
+		db.DFatalf("Usage: %v sigmaproc nthread niter id\nArgs: %v", os.Args[0], os.Args)
 	}
-	nthread, err := strconv.Atoi(os.Args[1])
+	isSigmaProc, err := strconv.ParseBool(os.Args[1])
 	if err != nil {
 		db.DFatalf("Error strconv: %v", err)
 	}
-	niter, err := strconv.Atoi(os.Args[2])
+	nthread, err := strconv.Atoi(os.Args[2])
 	if err != nil {
 		db.DFatalf("Error strconv: %v", err)
 	}
-	id := os.Args[3]
+	niter, err := strconv.Atoi(os.Args[3])
+	if err != nil {
+		db.DFatalf("Error strconv: %v", err)
+	}
+	id := os.Args[4]
+	var sc *sigmaclnt.SigmaClnt
+	if isSigmaProc {
+		sc, err = sigmaclnt.MkSigmaClnt("spinperf-" + proc.GetPid().String())
+		if err != nil {
+			db.DFatalf("Error mkSigmaClnt: %v", err)
+		}
+		err = sc.Started()
+		if err != nil {
+			db.DFatalf("Error started: %v", err)
+		}
+	}
 	start := time.Now()
 	spinPerf(nthread, niter)
 	db.DPrintf(db.ALWAYS, "%v:  %v", id, time.Since(start))
+	if isSigmaProc {
+		sc.Exited(proc.MakeStatusInfo(proc.StatusOK, "elapsed time", time.Since(start)))
+		if err != nil {
+			db.DFatalf("Error exited: %v", err)
+		}
+	}
 }
 
 func spinWorker(niter int, wg *sync.WaitGroup) {

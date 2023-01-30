f0053437aae8e5da08ddfacc8e8c77012f9c497a
Frans Kaashoek <kaashoek@mit.edu>
2023-01-30 15:42:53 -0500

    x

--- aws/update-repo.sh
+++ aws/update-repo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 usage() {
-  echo "Usage: $0 [-n N] --vpc VPC --realm REALM [--parallel]" 1>&2
+  echo "Usage: $0 [-n N] --vpc VPC [--parallel]" 1>&2
 }
 
 VPC=""
@@ -21,11 +21,6 @@ while [[ $# -gt 0 ]]; do
     N_VM=$1
     shift
     ;;
-  --realm)
-    shift
-    REALM=$1
-    shift
-    ;;
   --parallel)
     shift
     PARALLEL="--parallel"
@@ -42,7 +37,7 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-if [ -z "$VPC" ] || [ -z "$REALM" ] || [ $# -gt 0 ]; then
+if [ -z "$VPC" ] || [ $# -gt 0 ]; then
     usage
     exit 1
 fi
@@ -55,11 +50,10 @@ if ! [ -z "$N_VM" ]; then
 fi
 
 for vm in $vms; do
-  echo "INSTALL: $vm"
+  echo "UPDATE: $vm"
   install="
     ssh -i key-$VPC.pem ubuntu@$vm /bin/bash <<ENDSSH
       ssh-agent bash -c 'ssh-add ~/.ssh/aws-ulambda; (cd ulambda; git pull > /tmp/git.out 2>&1 )'
-      (cd ulambda; ./stop.sh; ./install.sh --from s3 --realm $REALM)
 ENDSSH"
   if [ -z "$PARALLEL" ]; then
     eval "$install"

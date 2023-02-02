9ab8255b13353233206d2a68f5c01156b734d15c
Ariel Szekely <arielck@mit.edu>
2023-02-02 09:42:00 -0500

    Sleep for 2 secs between core utilization calculations

--- benchmarks/performance_test.go
+++ benchmarks/performance_test.go
@@ -64,7 +64,7 @@ func monitorCoresAssigned(ts *test.RealmTstate) *perf.Perf {
 			ncores := perc / 100.0
 			// Total CPU utilized by this realm (in cores).
 			p.TptTick(ncores)
-			time.Sleep(1 * time.Second)
+			time.Sleep(2 * time.Second)
 		}
 	}()
 

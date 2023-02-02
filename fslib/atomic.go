f64c9d1dc81a7bfae099ad5fd5d857607692327a
Ariel Szekely <arielck@mit.edu>
2023-02-02 10:37:06 -0500

    x

--- fslib/atomic.go
+++ fslib/atomic.go
@@ -13,7 +13,7 @@ func (fsl *FsLib) PutFileAtomic(fname string, perm sp.Tperm, data []byte) error 
 	tmpName := fname + rand.String(16)
 	_, err := fsl.PutFile(tmpName, perm, sp.OWRITE, data)
 	if err != nil {
-		db.DFatalf("MakeFileAtomic %v: %v", tmpName, err)
+		db.DFatalf("MakeFileAtomic %v %v: %v", fname, tmpName, err)
 		return err
 	}
 	err = fsl.Rename(tmpName, fname)

cf2f55e1d09ce50309fba2127e453e80c1b0a70b
Ariel Szekely <arielck@mit.edu>
2023-02-08 10:49:35 -0500

    Comment some unused code from container package

--- container/kcontainer.go
+++ container/kcontainer.go
@@ -2,14 +2,14 @@ package container
 
 import (
 	"context"
-	"os"
+	//	"os"
 
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/container"
+	//	"github.com/docker/docker/api/types"
+	//	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 
 	db "sigmaos/debug"
-	sp "sigmaos/sigmap"
+	// sp "sigmaos/sigmap"
 )
 
 //
@@ -22,41 +22,41 @@ const (
 	HOSTTMP     = "/tmp/sigmaos"
 )
 
-func StartKContainer(nameds sp.Taddrs, conf string, env []string) (*Container, error) {
-	ctx := context.Background()
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-	if err != nil {
-		return nil, err
-	}
-	db.DPrintf(db.CONTAINER, "start container %v %v\n", nameds, env)
-	resp, err := cli.ContainerCreate(ctx, &container.Config{
-		Image: SIGMAKIMAGE,
-		Cmd:   []string{"bin/linux/bootkernel", nameds.String()},
-		Tty:   false,
-		Env:   env,
-	}, &container.HostConfig{
-		// This is bad idea in general because it requires to give rw
-		// permission on host to privileged daemon.  But maybe ok in
-		// our case where kernel is trusted as is. XXX Use different
-		// image for user procs.
-		Binds: []string{
-			"/var/run/docker.sock:/var/run/docker.sock",
-			os.Getenv("HOME") + "/.aws" + ":/home/sigmaos/.aws",
-			HOSTTMP + ":" + HOSTTMP,
-		},
-		// Network: "host",
-	}, nil, nil, "")
-	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
-		return nil, err
-	}
-	json, err1 := cli.ContainerInspect(ctx, resp.ID)
-	if err1 != nil {
-		return nil, err
-	}
-	ip := json.NetworkSettings.IPAddress
-	db.DPrintf(db.CONTAINER, "Booting %s %s at %s...\n", SIGMAKIMAGE, resp.ID[:10], ip)
-	return &Container{ctx, cli, resp.ID, ip, nil}, nil
-}
+//func StartKContainer(nameds sp.Taddrs, conf string, env []string) (*Container, error) {
+//	ctx := context.Background()
+//	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+//	if err != nil {
+//		return nil, err
+//	}
+//	db.DPrintf(db.CONTAINER, "start container %v %v\n", nameds, env)
+//	resp, err := cli.ContainerCreate(ctx, &container.Config{
+//		Image: SIGMAKIMAGE,
+//		Cmd:   []string{"bin/linux/bootkernel", nameds.String()},
+//		Tty:   false,
+//		Env:   env,
+//	}, &container.HostConfig{
+//		// This is bad idea in general because it requires to give rw
+//		// permission on host to privileged daemon.  But maybe ok in
+//		// our case where kernel is trusted as is. XXX Use different
+//		// image for user procs.
+//		Binds: []string{
+//			"/var/run/docker.sock:/var/run/docker.sock",
+//			os.Getenv("HOME") + "/.aws" + ":/home/sigmaos/.aws",
+//			HOSTTMP + ":" + HOSTTMP,
+//		},
+//		// Network: "host",
+//	}, nil, nil, "")
+//	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+//		return nil, err
+//	}
+//	json, err1 := cli.ContainerInspect(ctx, resp.ID)
+//	if err1 != nil {
+//		return nil, err
+//	}
+//	ip := json.NetworkSettings.IPAddress
+//	db.DPrintf(db.CONTAINER, "Booting %s %s at %s...\n", SIGMAKIMAGE, resp.ID[:10], ip)
+//	return &Container{ctx, cli, resp.ID, ip, nil}, nil
+//}
 
 func ContainerIP(container string) (string, error) {
 	cli, err := client.NewClientWithOpts(client.FromEnv)

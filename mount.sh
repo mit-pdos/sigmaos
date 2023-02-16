6d00284e05d389f0f56b38d491bc7da2059d2b87
Frans Kaashoek <kaashoek@mit.edu>
2023-02-16 11:07:52 -0500

    x

--- mount.sh
+++ mount.sh
@@ -16,4 +16,4 @@ fi
 
 ./bin/linux/proxyd $1 $1:1111 &
 
-sudo mount -t 9p -o trans=tcp,aname=`whoami`,uname=`whoami`,port=1110 127.0.0.1 /mnt/9p
+sudo mount -t 9p -o trans=tcp,aname=`whoami`,uname=`whoami`,port=1110 $1 /mnt/9p

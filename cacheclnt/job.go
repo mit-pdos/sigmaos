404b5d9a50855b515ea9f1f8a2c0cb622d85d1f3
Ariel Szekely <arielck@mit.edu>
2023-02-27 06:08:09 -0500

    Force shardsvcmgr callers to specify ncore, and package up cacheclnt for use in benchmarks package

---
+++ cacheclnt/job.go
@@ -0,0 +1,36 @@
+package cacheclnt
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	"sigmaos/proc"
+	"sigmaos/sigmaclnt"
+)
+
+func StartClerk(sc *sigmaclnt.SigmaClnt, job string, nkeys int, dur time.Duration, keyOffset int, sempn string, ncore proc.Tcore) (proc.Tpid, error) {
+	p := proc.MakeProc("cache-clerk", []string{job, strconv.Itoa(nkeys), dur.String(), strconv.Itoa(keyOffset), sempn})
+	p.SetNcore(ncore)
+	// SpawnBurst to spread clerks across procds.
+	_, errs := sc.SpawnBurst([]*proc.Proc{p})
+	if len(errs) > 0 {
+		return "", errs[0]
+	}
+	err := sc.WaitStart(p.GetPid())
+	if err != nil {
+		return "", err
+	}
+	return p.GetPid(), nil
+}
+
+func WaitClerk(sc *sigmaclnt.SigmaClnt, pid proc.Tpid) (float64, error) {
+	status, err := sc.WaitExit(pid)
+	if err != nil {
+		return 0.0, err
+	}
+	if !status.IsStatusOK() {
+		return 0.0, fmt.Errorf("Bad status %v", status)
+	}
+	return status.Data().(float64), nil
+}

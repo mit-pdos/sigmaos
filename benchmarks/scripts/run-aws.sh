996e9a84465d4d30dbae66b8bde16e9f614df036
Ariel Szekely <arielck@mit.edu>
2023-02-28 15:11:12 -0500

    Add String function for semclnts

--- benchmarks/scripts/run-aws.sh
+++ benchmarks/scripts/run-aws.sh
@@ -139,7 +139,7 @@ run_hotel() {
     export SIGMADEBUG=\"TEST;\"; \
     go clean -testcache; \
     ulimit -n 100000; \
-    go test -v sigmaos/benchmarks -timeout 0 --run Hotel${sys}Search --k8saddr $k8saddr --hotel_dur 60s --hotel_max_rps $rps --prewarm_realm > /tmp/bench.out 2>&1
+    go test -v sigmaos/benchmarks -timeout 0 --run Hotel${sys}Search  --rootNamedIP $LEADER_IP --k8saddr $k8saddr --hotel_dur 60s --hotel_max_rps $rps --prewarm_realm > /tmp/bench.out 2>&1
   "
   if [ "$sys" = "Sigmaos" ]; then
     vpc=$VPC

42683611056b844eca53e9e6e9d0669d9db2bb47
Ariel Szekely <arielck@mit.edu>
2023-03-03 23:46:40 -0500

    Up searchd ncores again:

--- hotel/job.go
+++ hotel/job.go
@@ -59,7 +59,7 @@ func MkHotelSvc(public bool) []Srv {
 	return []Srv{
 		Srv{"hotel-userd", public, 0}, Srv{"hotel-rated", public, 2},
 		Srv{"hotel-geod", public, 2}, Srv{"hotel-profd", public, 2},
-		Srv{"hotel-searchd", public, 2}, Srv{"hotel-reserved", public, 3},
+		Srv{"hotel-searchd", public, 3}, Srv{"hotel-reserved", public, 3},
 		Srv{"hotel-recd", public, 0}, Srv{"hotel-wwwd", public, 3},
 	}
 }

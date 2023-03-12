efc28728f286dd93a89283b10481deb50f094f8e
Ariel Szekely <arielck@mit.edu>
2023-03-12 09:07:07 -0400

    x

--- start-kernel.sh
+++ start-kernel.sh
@@ -94,10 +94,10 @@ if docker ps | grep -q sigmadb; then
     DBIP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' sigmadb)
 fi
 
-if docker ps | grep -q sigmajaeger; then
-#    JAEGERIP="localhost"
-    JAEGERIP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' sigmajaeger)
-fi
+#if docker ps | grep -q sigmajaeger; then
+##    JAEGERIP="localhost"
+#    JAEGERIP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' sigmajaeger)
+#fi
 
 # Mounting docker.sock is bad idea in general because it requires to
 # give rw permission on host to privileged daemon.  But maybe ok in

1d807dbcffe1658acf148a6831af94049883fe83
Ariel Szekely <arielck@mit.edu>
2023-03-15 08:45:56 -0400

    Resurrect intervals tests

--- intervals/intervals_test.go
+++ intervals/intervals_test.go
@@ -1,26 +1,26 @@
-package intervals
+package intervals_test
 
 import (
-	"log"
-
 	"github.com/stretchr/testify/assert"
 	"testing"
 
-	sp "sigmaos/sigmap"
+	db "sigmaos/debug"
+	"sigmaos/intervals"
+	"sigmaos/sessp"
 )
 
 func TestSimple(t *testing.T) {
-	ivs := MkIntervals()
-	ivs.Insert(&sessp.Tinterval{1, 2})
-	ivs.Insert(&sessp.Tinterval{2, 3})
-	ivs.Delete(&sessp.Tinterval{1, 2})
-	assert.Equal(t, 1, len(ivs.ivs))
+	ivs := intervals.MkIntervals()
+	ivs.Insert(sessp.MkInterval(1, 2))
+	ivs.Insert(sessp.MkInterval(2, 3))
+	ivs.Delete(sessp.MkInterval(1, 2))
+	assert.Equal(t, 1, ivs.Size())
 }
 
 func TestContains(t *testing.T) {
-	ivs := MkIntervals()
-	ivs.Insert(&sessp.Tinterval{0, 10})
-	ivs.Insert(&sessp.Tinterval{90, 100})
+	ivs := intervals.MkIntervals()
+	ivs.Insert(sessp.MkInterval(0, 10))
+	ivs.Insert(sessp.MkInterval(90, 100))
 	assert.True(t, ivs.Contains(0))
 	assert.False(t, ivs.Contains(10))
 	assert.False(t, ivs.Contains(11))
@@ -30,56 +30,56 @@ func TestContains(t *testing.T) {
 }
 
 func TestInsert(t *testing.T) {
-	ivs := MkIntervals()
-	ivs.Insert(&sessp.Tinterval{0, 10})
-	ivs.Insert(&sessp.Tinterval{10, 20})
-	assert.Equal(t, 1, len(ivs.ivs))
-	ivs.Insert(&sessp.Tinterval{15, 20})
-	assert.Equal(t, 1, len(ivs.ivs))
-	ivs.Insert(&sessp.Tinterval{30, 40})
-	assert.Equal(t, 2, len(ivs.ivs))
-	ivs.Insert(&sessp.Tinterval{20, 25})
-	assert.Equal(t, 2, len(ivs.ivs))
-	ivs.Insert(&sessp.Tinterval{50, 60})
-	assert.Equal(t, 3, len(ivs.ivs))
-	ivs.Insert(&sessp.Tinterval{70, 80})
-	assert.Equal(t, 4, len(ivs.ivs))
-	ivs.Insert(&sessp.Tinterval{40, 50})
-	assert.Equal(t, 3, len(ivs.ivs))
-	ivs.Insert(&sessp.Tinterval{25, 30})
-	assert.Equal(t, 2, len(ivs.ivs))
-	ivs.Insert(&sessp.Tinterval{60, 70})
-	assert.Equal(t, 1, len(ivs.ivs))
+	ivs := intervals.MkIntervals()
+	ivs.Insert(sessp.MkInterval(0, 10))
+	ivs.Insert(sessp.MkInterval(10, 20))
+	assert.Equal(t, 1, ivs.Size())
+	ivs.Insert(sessp.MkInterval(15, 20))
+	assert.Equal(t, 1, ivs.Size())
+	ivs.Insert(sessp.MkInterval(30, 40))
+	assert.Equal(t, 2, ivs.Size())
+	ivs.Insert(sessp.MkInterval(20, 25))
+	assert.Equal(t, 2, ivs.Size())
+	ivs.Insert(sessp.MkInterval(50, 60))
+	assert.Equal(t, 3, ivs.Size())
+	ivs.Insert(sessp.MkInterval(70, 80))
+	assert.Equal(t, 4, ivs.Size())
+	ivs.Insert(sessp.MkInterval(40, 50))
+	assert.Equal(t, 3, ivs.Size())
+	ivs.Insert(sessp.MkInterval(25, 30))
+	assert.Equal(t, 2, ivs.Size())
+	ivs.Insert(sessp.MkInterval(60, 70))
+	assert.Equal(t, 1, ivs.Size())
 }
 
 func TestDelete(t *testing.T) {
-	ivs := MkIntervals()
-	ivs.Insert(&sessp.Tinterval{0, 100})
-	log.Printf("ivs %v\n", ivs.ivs)
-	ivs.Delete(&sessp.Tinterval{5, 10})
-	assert.Equal(t, 2, len(ivs.ivs))
-	log.Printf("ivs %v\n", ivs.ivs)
-	ivs.Delete(&sessp.Tinterval{30, 50})
-	log.Printf("ivs %v\n", ivs.ivs)
-	assert.Equal(t, 3, len(ivs.ivs))
-	ivs.Delete(&sessp.Tinterval{50, 100})
-	log.Printf("ivs %v\n", ivs.ivs)
-	assert.Equal(t, 2, len(ivs.ivs))
-	ivs.Delete(&sessp.Tinterval{20, 30})
-	assert.Equal(t, 2, len(ivs.ivs))
-	log.Printf("ivs %v\n", ivs.ivs)
-	ivs.Delete(&sessp.Tinterval{0, 5})
-	log.Printf("ivs %v\n", ivs.ivs)
-	assert.Equal(t, 1, len(ivs.ivs))
-	ivs.Delete(&sessp.Tinterval{10, 20})
-	assert.Equal(t, 0, len(ivs.ivs))
+	ivs := intervals.MkIntervals()
+	ivs.Insert(sessp.MkInterval(0, 100))
+	db.DPrintf(db.ALWAYS, "ivs %v\n", ivs)
+	ivs.Delete(sessp.MkInterval(5, 10))
+	assert.Equal(t, 2, ivs.Size())
+	db.DPrintf(db.ALWAYS, "ivs %v\n", ivs)
+	ivs.Delete(sessp.MkInterval(30, 50))
+	db.DPrintf(db.ALWAYS, "ivs %v\n", ivs)
+	assert.Equal(t, 3, ivs.Size())
+	ivs.Delete(sessp.MkInterval(50, 100))
+	db.DPrintf(db.ALWAYS, "ivs %v\n", ivs)
+	assert.Equal(t, 2, ivs.Size())
+	ivs.Delete(sessp.MkInterval(20, 30))
+	assert.Equal(t, 2, ivs.Size())
+	db.DPrintf(db.ALWAYS, "ivs %v\n", ivs)
+	ivs.Delete(sessp.MkInterval(0, 5))
+	db.DPrintf(db.ALWAYS, "ivs %v\n", ivs)
+	assert.Equal(t, 1, ivs.Size())
+	ivs.Delete(sessp.MkInterval(10, 20))
+	assert.Equal(t, 0, ivs.Size())
 
-	ivs.Insert(&sessp.Tinterval{0, 100})
-	log.Printf("ivs %v\n", ivs.ivs)
-	ivs.Delete(&sessp.Tinterval{5, 10})
-	assert.Equal(t, 2, len(ivs.ivs))
-	log.Printf("ivs %v\n", ivs.ivs)
-	ivs.Delete(&sessp.Tinterval{0, 100})
-	log.Printf("ivs %v\n", ivs.ivs)
-	assert.Equal(t, 0, len(ivs.ivs))
+	ivs.Insert(sessp.MkInterval(0, 100))
+	db.DPrintf(db.ALWAYS, "ivs %v\n", ivs)
+	ivs.Delete(sessp.MkInterval(5, 10))
+	assert.Equal(t, 2, ivs.Size())
+	db.DPrintf(db.ALWAYS, "ivs %v\n", ivs)
+	ivs.Delete(sessp.MkInterval(0, 100))
+	db.DPrintf(db.ALWAYS, "ivs %v\n", ivs)
+	assert.Equal(t, 0, ivs.Size())
 }

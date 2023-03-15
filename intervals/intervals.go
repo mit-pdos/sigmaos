f6d1f4ce3cfe9252467f3932283837e189cc42d3
Ariel Szekely <arielck@mit.edu>
2023-03-14 22:52:52 -0400

    Clean up intervals a bit

--- intervals/intervals.go
+++ intervals/intervals.go
@@ -13,16 +13,17 @@ import (
 
 type Intervals struct {
 	sync.Mutex
-	ivs []*sessp.Tinterval
+	entries []*sessp.Tinterval
+	idx     int
 }
 
 func (ivs *Intervals) String() string {
-	return fmt.Sprintf("%v", ivs.ivs)
+	return fmt.Sprintf("%v", ivs.entries)
 }
 
 func MkIntervals() *Intervals {
 	ivs := &Intervals{}
-	ivs.ivs = make([]*sessp.Tinterval, 0)
+	ivs.entries = make([]*sessp.Tinterval, 0)
 	return ivs
 }
 
@@ -30,82 +31,61 @@ func (ivs *Intervals) First() *sessp.Tinterval {
 	ivs.Lock()
 	defer ivs.Unlock()
 
-	if len(ivs.ivs) == 0 {
+	if len(ivs.entries) == 0 {
 		return nil
 	}
-	return sessp.MkInterval(ivs.ivs[0].Start, ivs.ivs[0].End)
+	return sessp.MkInterval(ivs.entries[0].Start, ivs.entries[0].End)
 }
 
-func (ivs *Intervals) Len() int {
-	return len(ivs.ivs)
-}
+func (ivs *Intervals) Next() *sessp.Tinterval {
+	ivs.Lock()
+	defer ivs.Unlock()
 
-// maybe merge with wi with wi+1
-func (ivs *Intervals) merge(i int) {
-	iv := ivs.ivs[i]
-	if len(ivs.ivs) > i+1 { // is there a next iv
-		iv1 := ivs.ivs[i+1]
-		if iv.End >= iv1.Start { // merge iv1 into iv
-			if iv1.End > iv.End {
-				iv.End = iv1.End
-			}
-			if i+2 == len(ivs.ivs) { // trim i+1
-				ivs.ivs = ivs.ivs[:i+1]
-			} else {
-				ivs.ivs = append(ivs.ivs[:i+1], ivs.ivs[i+2:]...)
-			}
-		}
+	if len(ivs.entries) == 0 {
+		return nil
 	}
+	iv := sessp.MkInterval(ivs.entries[ivs.idx%len(ivs.entries)].Start, ivs.entries[ivs.idx%len(ivs.entries)].End)
+	ivs.idx++
+	return iv
+}
+
+func (ivs *Intervals) ResetRetrieved() {
+	ivs.Lock()
+	defer ivs.Unlock()
+
+	ivs.idx = 0
 }
 
 func (ivs *Intervals) Insert(n *sessp.Tinterval) {
 	ivs.Lock()
 	defer ivs.Unlock()
 
-	for i, iv := range ivs.ivs {
-		if n.Start > iv.End { // n is beyond iv
-			continue
-		}
-		if n.End < iv.Start { // n preceeds iv
-			ivs.ivs = append(ivs.ivs[:i+1], ivs.ivs[i:]...)
-			ivs.ivs[i] = n
-			return
-		}
-		// n overlaps iv
-		if n.Start < iv.Start {
-			iv.Start = n.Start
-		}
-		if n.End > iv.End {
-			iv.End = n.End
-			ivs.merge(i)
-			return
-		}
-		return
-	}
-	ivs.ivs = append(ivs.ivs, n)
-}
-
-// Caller received [start, end), which may increase lower bound of
-// what the caller has seen sofar.  XXX split insert from prune
-// and use a better name for Prune
-func (ivs *Intervals) Prune(lb, start, end uint64) uint64 {
-	ivs.Insert(sessp.MkInterval(start, end))
-	iv0 := ivs.ivs[0]
-	if iv0.Start > lb { // out of order
-		return 0
-	}
-	if iv0.Start < lb { // new data may have straggle off
-		iv0.Start = lb
-	}
-	ivs.ivs = ivs.ivs[1:]
-	return iv0.End - iv0.Start
+	insert(&ivs.entries, n)
 }
 
 func (ivs *Intervals) Contains(e uint64) bool {
 	ivs.Lock()
 	defer ivs.Unlock()
 
-	for _, iv := range ivs.ivs {
+	return contains(ivs.entries, e)
+}
+
+func (ivs *Intervals) Delete(ivd *sessp.Tinterval) {
+	ivs.Lock()
+	defer ivs.Unlock()
+
+	del(&ivs.entries, ivd)
+}
+
+func (ivs *Intervals) Size() int {
+	ivs.Lock()
+	defer ivs.Unlock()
+
+	return len(ivs.entries)
+}
+
+func contains(entries []*sessp.Tinterval, e uint64) bool {
+	for _, iv := range entries {
 		if e < iv.Start {
 			return false
 		}
@@ -116,12 +96,9 @@ func (ivs *Intervals) Contains(e uint64) bool {
 	return false
 }
 
-func (ivs *Intervals) Delete(ivd *sessp.Tinterval) {
-	ivs.Lock()
-	defer ivs.Unlock()
-
-	for i := 0; i < len(ivs.ivs); {
-		iv := ivs.ivs[i]
+func del(entries *[]*sessp.Tinterval, ivd *sessp.Tinterval) {
+	for i := 0; i < len(*entries); {
+		iv := (*entries)[i]
 		if ivd.Start > iv.End { // ivd is beyond iv
 			i++
 			continue
@@ -134,7 +111,7 @@ func (ivs *Intervals) Delete(ivd *sessp.Tinterval) {
 			ivd.Start = iv.Start
 		}
 		if ivd.Start <= iv.Start && ivd.End >= iv.End { // delete i?
-			ivs.ivs = append(ivs.ivs[:i], ivs.ivs[i+1:]...)
+			*entries = append((*entries)[:i], (*entries)[i+1:]...)
 		} else if ivd.Start > iv.Start && ivd.End >= iv.End {
 			iv.End = ivd.Start
 			i++
@@ -142,17 +119,52 @@ func (ivs *Intervals) Delete(ivd *sessp.Tinterval) {
 			iv.Start = ivd.End
 			i++
 		} else { // split iv
-			ivs.ivs = append(ivs.ivs[:i+1], ivs.ivs[i:]...)
-			ivs.ivs[i] = sessp.MkInterval(iv.Start, ivd.Start)
-			ivs.ivs[i+1].Start = ivd.End
+			*entries = append((*entries)[:i+1], (*entries)[i:]...)
+			(*entries)[i] = sessp.MkInterval(iv.Start, ivd.Start)
+			(*entries)[i+1].Start = ivd.End
 			i += 2
 		}
 	}
 }
 
-func (ivs *Intervals) Size() int {
-	ivs.Lock()
-	defer ivs.Unlock()
+// maybe merge with wi with wi+1
+func merge(entries *[]*sessp.Tinterval, i int) {
+	iv := (*entries)[i]
+	if len(*entries) > i+1 { // is there a next iv
+		iv1 := (*entries)[i+1]
+		if iv.End >= iv1.Start { // merge iv1 into iv
+			if iv1.End > iv.End {
+				iv.End = iv1.End
+			}
+			if i+2 == len(*entries) { // trim i+1
+				*entries = (*entries)[:i+1]
+			} else {
+				*entries = append((*entries)[:i+1], (*entries)[i+2:]...)
+			}
+		}
+	}
+}
 
-	return len(ivs.ivs)
+func insert(entries *[]*sessp.Tinterval, n *sessp.Tinterval) {
+	for i, iv := range *entries {
+		if n.Start > iv.End { // n is beyond iv
+			continue
+		}
+		if n.End < iv.Start { // n preceeds iv
+			*entries = append((*entries)[:i+1], (*entries)[i:]...)
+			(*entries)[i] = n
+			return
+		}
+		// n overlaps iv
+		if n.Start < iv.Start {
+			iv.Start = n.Start
+		}
+		if n.End > iv.End {
+			iv.End = n.End
+			merge(entries, i)
+			return
+		}
+		return
+	}
+	*entries = append(*entries, n)
 }

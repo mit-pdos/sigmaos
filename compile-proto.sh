6bfdf819ff19a37f0b7fb8d62c1d78f446eea9d0
Ariel Szekely <arielck@mit.edu>
2023-03-20 10:45:18 -0400

    Add sessp to proto compilation script

--- compile-proto.sh
+++ compile-proto.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # Note: order is important.
-for P in tracing ; do
+
+for P in sessp ; do
   echo "protoc $P"
-  protoc -I=. --go_out=../ $P/proto/$P.proto
+  protoc -I=. --go_out=../ $P/$P.proto
 done
 
-for PP in cache kv hotel rpcbench ; do
+for PP in tracing cache kv hotel rpcbench ; do
   for P in $PP/proto/*.proto ; do
     echo "protoc $P"
     protoc -I=. --go_out=../ $P

0413ecdd5e6eb5d4265ecf798df0234a69e968dc
Ariel Szekely <arielck@mit.edu>
2023-04-02 18:10:55 -0400

    Increase root's open-file ulimit for k8s instances

--- aws/setup-instance.sh
+++ aws/setup-instance.sh
@@ -189,6 +189,9 @@ ENDSSH
 if [ $VPC == $K8S_VPC ]; then
   echo "Installing kubernetes components"
   ssh -i key-$VPC.pem $LOGIN@$VM <<'ENDSSH'
+    # Increase root's open file ulimits.
+    bash -c "echo \"root hard nofile 20000\" | sudo tee -a /etc/security/limits.conf"
+    bash -c "echo \"root soft nofile 20000\" | sudo tee -a /etc/security/limits.conf"
     bash -c "sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg"
     bash -c "echo \"deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main\" | sudo tee /etc/apt/sources.list.d/kubernetes.list"
     bash -c "sudo apt update"

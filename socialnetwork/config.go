673380c3cfc959e6c1284d6167fafa7abdd8bfd6
Yizheng He <yizhengh@mit.edu>
2023-04-06 23:12:30 -0400

    updated test skeleton

--- socialnetwork/config.go
+++ socialnetwork/config.go
@@ -2,12 +2,7 @@ package socialnetwork
 
 import (
 	"sigmaos/proc"
-	"sigmaos/sigmap"
-	"sigmaos/kernel"
-	"sigmaos/bootkernelclnt"
-	"sigmaos/container"
 	"sigmaos/sigmaclnt"
-	"sigmaos/rand"
 	"fmt"
 	"path"
 	"strconv"
@@ -36,66 +31,8 @@ func MakeMoLSrvs(public bool) []Srv {
 	}
 }
 
-type SigmaConfig struct {
-	*sigmaclnt.SigmaClnt
-	kclnts  []*bootkernelclnt.Kernel
-	killidx int
-}
-
-// Inspired by test.makeSysClnt in test/test.go
-func MakeSigmaConfig(start, overlays bool, tag, rootNamedIP string) (*SigmaConfig, error) {
-	namedport := sigmap.MkTaddrs([]string{NAMEDPORT})
-	kernelid := ""
-	// configure IP
-	var containerIP string
-	var err error
-	if rootNamedIP == "" {
-		containerIP, err = container.LocalIP()
-		if err != nil {
-			return nil, err
-		}
-	} else {
-		containerIP = rootNamedIP
-	}
-	// start kernel
-	if start {
-		kernelid = bootkernelclnt.GenKernelId()
-		ip, err := bootkernelclnt.Start(kernelid, tag, BOOT_ALL, namedport, overlays)
-		if err != nil {
-			return nil, err
-		}
-		containerIP = ip
-	}
-	// retrieve kernel clients
-	proc.SetPid(proc.Tpid("socialnetowrk-" + proc.GenPid().String()))
-	namedAddr, err := kernel.SetNamedIP(containerIP, namedport)
-	if err != nil {
-		fmt.Printf("Failed to set kernel IP!")
-		return nil, err
-	}
-	k, err := bootkernelclnt.MkKernelClnt(kernelid, "socialnetwork", containerIP, namedAddr)
-	if err != nil {
-		fmt.Printf("Failed to make kernel client!")
-		return nil, err
-	}
-	return &SigmaConfig {
-		SigmaClnt: k.SigmaClnt,
-		kclnts:    []*bootkernelclnt.Kernel{k},
-		killidx:   0,
-	}, nil
-}
-
-func (sigmaCfg *SigmaConfig) Shutdown() error {
-	for i := len(sigmaCfg.kclnts) - 1; i >= 0; i-- {
-		if err := sigmaCfg.kclnts[i].Shutdown(); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
 type SocialNetworkConfig struct {
-	*SigmaConfig
+	*sigmaclnt.SigmaClnt
 	srvs  []Srv
 	pids  []proc.Tpid
 }
@@ -104,8 +41,8 @@ func JobDir(job string) string {
 	return path.Join(SOCIAL_NETWORK_ROOT, job)
 }
 
-func MakeSocialNetworkConfig(sigmaCfg *SigmaConfig, jobname string, srvs []Srv) (*SocialNetworkConfig, error) {
-	fsl := sigmaCfg.FsLib
+func MakeConfig(sc *sigmaclnt.SigmaClnt, jobname string, srvs []Srv) (*SocialNetworkConfig, error) {
+	fsl := sc.FsLib
 	fsl.MkDir(SOCIAL_NETWORK_ROOT, 0777)
 	if err := fsl.MkDir(JobDir(jobname), 0777); err != nil {
 		fmt.Printf("Mkdir %v err %v\n", JobDir(jobname), err)
@@ -117,27 +54,17 @@ func MakeSocialNetworkConfig(sigmaCfg *SigmaConfig, jobname string, srvs []Srv) 
 	for _, srv := range srvs {
 		p := proc.MakeProc(srv.Name, []string{strconv.FormatBool(srv.Public)})
 		p.SetNcore(srv.Ncore)
-		if _, errs := sigmaCfg.SpawnBurst([]*proc.Proc{p}, 2); len(errs) > 0 {
+		if _, errs := sc.SpawnBurst([]*proc.Proc{p}, 2); len(errs) > 0 {
 			dbg.DFatalf("Error burst-spawnn proc %v: %v", p, errs)
 			return nil, err
 		}
-		if err = sigmaCfg.WaitStart(p.GetPid()); err != nil {
+		if err = sc.WaitStart(p.GetPid()); err != nil {
 			dbg.DFatalf("Error spawn proc %v: %v", p, err)
 			return nil, err
 		}
 		pids = append(pids, p.GetPid())
 	}
-	return &SocialNetworkConfig{sigmaCfg, srvs, pids}, nil
-}
-
-func MakeDefaultSocialNetworkConfig() (*SocialNetworkConfig, error) {
-	overlays := false
-	sigmaCfg, err := MakeSigmaConfig(true, overlays, "", "")
-	if err != nil {
-		return nil, err
-	}
-	srvs := MakeMoLSrvs(overlays)
-	return MakeSocialNetworkConfig(sigmaCfg, rand.String(8), srvs)
+	return &SocialNetworkConfig{sc, srvs, pids}, nil
 }
 
 func (molCfg *SocialNetworkConfig) Stop() error {
--- socialnetwork/socialnetwork_test.go
+++ socialnetwork/socialnetwork_test.go
@@ -2,15 +2,34 @@ package socialnetwork_test
 
 import (
 	"testing"
-	"sigmaos/socialnetwork"
+	"sigmaos/test"
+	"sigmaos/rand"
+	sn "sigmaos/socialnetwork"
 	"sigmaos/socialnetwork/proto"
 	"sigmaos/protdevclnt"
 	"github.com/stretchr/testify/assert"
 	"time"
 )
 
+type TstateSN struct {
+	*test.Tstate
+	jobname string
+	snCfg   *sn.SocialNetworkConfig
+}
+
+func makeTstateSN(t *testing.T, srvs []sn.Srv) *TstateSN {
+	var err error
+	tssn := &TstateSN{}
+	tssn.jobname = rand.String(8)
+	tssn.Tstate = test.MakeTstateAll(t)
+	assert.Nil(tssn.T, err, "test kernel should start properly.")
+	tssn.snCfg, err = sn.MakeConfig(tssn.SigmaClnt, tssn.jobname, srvs)
+	assert.Nil(tssn.T, err, "config should be created properly.")
+	return tssn
+}
+
 func TestFindMeaningLocal(t *testing.T) {
-	mol := socialnetwork.MeaningOfLife{}
+	mol := sn.MeaningOfLife{}
 	arg := proto.MoLRequest{
 		Name: "test",
 	} 
@@ -21,11 +40,11 @@ func TestFindMeaningLocal(t *testing.T) {
 
 func TestFindMeanlingServer(t *testing.T) {
 	// start server
-	snCfg, startErr := socialnetwork.MakeDefaultSocialNetworkConfig()
-	assert.Nil(t, startErr, "config should be created properly")
+	tssn := makeTstateSN(t, []sn.Srv{sn.Srv{"socialnetwork-mol", test.Overlays, 1}})
+	snCfg := tssn.snCfg
 
 	// create a RPC client and query
-	pdc, err := protdevclnt.MkProtDevClnt(snCfg.FsLib, socialnetwork.MOL_SERVICE_NAME)
+	pdc, err := protdevclnt.MkProtDevClnt(snCfg.FsLib, sn.MOL_SERVICE_NAME)
 	assert.Nil(t, err, "RPC client should be created properly")
 	arg := proto.MoLRequest{
 		Name: "test",
@@ -39,7 +58,5 @@ func TestFindMeanlingServer(t *testing.T) {
 	time.Sleep(10 * time.Second)
 	stopErr := snCfg.Stop()
 	assert.Nil(t, stopErr, "Procs should stop properly")
-	shutdownErr := snCfg.Shutdown()
-	assert.Nil(t, shutdownErr, "Kernel should shut down properly")
-
+	tssn.Shutdown()
 }

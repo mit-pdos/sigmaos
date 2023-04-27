73c49d47133ee791c1900c0f18cefb76ee78a283
Ariel Szekely <arielck@mit.edu>
2023-04-27 12:52:18 -0400

    Make N_HOTEL configurable in perf benchmarks

--- benchmarks/benchmarks_test.go
+++ benchmarks/benchmarks_test.go
@@ -46,7 +46,8 @@ var WWWD_REQ_TYPE string
 var WWWD_REQ_DELAY time.Duration
 var HOTEL_NCACHE int
 var HOTEL_CACHE_NCORE int
-var HOTEL_IMG_SZ_MB string
+var N_HOTEL int
+var HOTEL_IMG_SZ_MB int
 var CACHE_TYPE string
 var CACHE_GC bool
 
@@ -90,6 +91,7 @@ func init() {
 	flag.IntVar(&HOTEL_NCACHE, "hotel_ncache", 1, "Hotel ncache")
 	flag.IntVar(&HOTEL_CACHE_NCORE, "hotel_cache_ncore", 2, "Hotel cache ncore")
 	flag.IntVar(&HOTEL_IMG_SZ_MB, "hotel_img_sz_mb", 0, "Hotel image data size in megabytes.")
+	flag.IntVar(&N_HOTEL, "nhotel", 80, "Number of hotels in the dataset.")
 	flag.StringVar(&CACHE_TYPE, "cache_type", "cached", "Hotel cache type (kvd or cached).")
 	flag.BoolVar(&CACHE_GC, "cache_gc", false, "Turn hotel cache GC on (true) or off (false).")
 	flag.StringVar(&MEMCACHED_ADDRS, "memcached", "", "memcached server addresses (comma-separated).")

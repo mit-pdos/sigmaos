f5702eb5996c4c87f0646408cf08ab5299b6424f
Ariel Szekely <arielck@mit.edu>
2023-04-29 15:57:14 -0400

    Pass docker swarm advertise addr to start-network.sh

--- start-network.sh
+++ start-network.sh
@@ -1,12 +1,38 @@
 #!/bin/bash
 
 usage() {
-  echo "Usage: $0" 1>&2
+  echo "Usage: $0 [--addr ADVERTISE_ADDR]" 1>&2
 }
+
+ADDR=""
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  case $key in
+  --addr)
+    shift
+    ADDR="--advertise-addr $1"
+    shift
+    ;;
+  -help)
+    usage
+    exit 0
+    ;;
+  *)
+    echo "Error: unexpected argument '$1'"
+    usage
+    exit 1
+    ;;
+  esac
+done
+
+if [ $# -gt 0 ]; then
+    usage
+    exit 1
+fi
 
 # enter swarm mode so that we can make network
 if ! docker node ls | grep -q 'Leader'; then
-    docker swarm init
+    docker swarm init $ADDR
 fi 
 
 # one network for tests

25b412b7e7cd70990a5049f5fa2f86ac3152a871
Ariel Szekely <arielck@mit.edu>
2023-04-30 07:16:58 -0400

    x

--- cloudlab/stop-k8s-app.sh
+++ cloudlab/stop-k8s-app.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 usage() {
-  echo "Usage: $0 --path APP_PATH --nrunning N_RUNNING" 1>&2
+  echo "Usage: $0 --vpc VPC --path APP_PATH" 1>&2
 }
 
 APP_PATH=""
-N_RUNNING=""
 while [[ $# -gt 0 ]]; do
   key="$1"
   case $key in
@@ -16,11 +15,6 @@ while [[ $# -gt 0 ]]; do
   --path)
     shift
     APP_PATH=$1
-    shift
-    ;;
-  --nrunning)
-    shift
-    N_RUNNING=$1
     shift
     ;;
   -help)
@@ -35,7 +29,7 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-if [ -z "$APP_PATH" ] || [ -z "$N_RUNNING" ] || [ $# -gt 0 ]; then
+if [ -z "$APP_PATH" ] || [ $# -gt 0 ]; then
     usage
     exit 1
 fi
@@ -48,20 +42,6 @@ vms=`cat servers.txt | cut -d " " -f2`
 vma=($vms)
 MAIN="${vma[0]}"
 
-export APP_PATH=$APP_PATH
-export N_RUNNING=$N_RUNNING
-
-CMD=$(
-envsubst '$APP_PATH:$N_RUNNING' <<'ENDSSH'
-  kubectl apply -Rf $APP_PATH > /dev/null 2>&1
-  until [ $(kubectl get pods | grep -w "Running" | wc -l ) == "$N_RUNNING" ]; do
-    echo "Missing pods" > /dev/null 2>&1
-    sleep 2s
-  done
-  echo "$N_RUNNING pods running"
-ENDSSH
-)
-
 ssh -i $DIR/keys/cloudlab-sigmaos $LOGIN@$MAIN <<ENDSSH
-  $CMD
+  kubectl delete -Rf $APP_PATH > /dev/null 2>&1
 ENDSSH

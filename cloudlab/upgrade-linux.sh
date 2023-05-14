24e7907e2230f14f6e78c646c37af59e63a14a85
Ariel Szekely <arielck@mit.edu>
2023-05-14 18:11:12 -0400

    Add cloudlab install instructions

--- cloudlab/upgrade-linux.sh
+++ cloudlab/upgrade-linux.sh
@@ -45,15 +45,18 @@ mkdir kbuild-$KERNEL
 wget https://cdn.kernel.org/pub/linux/kernel/v$(printf %.1s "$KERNEL").x/linux-$KERNEL.tar.xz
 tar -xvf linux-$KERNEL.tar.xz
 
-cd kbuild-$KERNEL
+cd /var/local/$USER/kernel/kbuild-$KERNEL
 yes "" | make -C ../linux-$KERNEL O=/var/local/$USER/kernel/kbuild-$KERNEL config
 sed -ri '/CONFIG_SYSTEM_TRUSTED_KEYS/s/=.+/=""/g' .config
 sed -ri 's/CONFIG_SATA_AHCI=m/CONFIG_SATA_AHCI=y/g' .config
 sed -ri 's/CONFIG_SYSTEM_REVOCATION_LIST=y/CONFIG_SYSTEM_REVOCATION_LIST=n/g' .config
 sudo apt install -y dwarves
 sudo make -j$(nproc) 
-INSTALL_MOD_STRIP=1 sudo make -j$(nproc) modules_install
-INSTALL_MOD_STRIP=1 sudo make -j$(nproc) install
+INSTALL_MOD_STRIP=1 sudo make modules_install -j$(nproc)
+INSTALL_MOD_STRIP=1 sudo make install -j$(nproc)
+sudo make -j$(nproc) 
+INSTALL_MOD_STRIP=1 sudo make modules_install -j$(nproc)
+INSTALL_MOD_STRIP=1 sudo make install -j$(nproc)
 sudo reboot
 
 ENDSSH

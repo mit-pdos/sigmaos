57b5468f46680ccd4706f908fc19959954df9784
Frans Kaashoek <kaashoek@mit.edu>
2023-07-02 17:17:59 -0400

    Pass sessclnt_test

--- sessclnt/sessclnt_test.go
+++ sessclnt/sessclnt_test.go
@@ -21,56 +21,70 @@ import (
 )
 
 const (
-	GRP0      = "GRP0"
 	CRASH     = 1000
 	PARTITION = 200
 	NETFAIL   = 200
 	NTRIALS   = "3001"
 	JOBDIR    = "name/"
+	GRP       = "grp-0"
 )
 
-var DIRGRP0 = group.GrpPath(JOBDIR, GRP0) + "/"
+type Tstate struct {
+	*test.Tstate
+	grp string
+	gm  *groupmgr.GroupMgr
+}
+
+func makeTstate(t *testing.T, ncrash, crash, partition, netfail int) *Tstate {
+	ts := &Tstate{grp: GRP}
+	ts.Tstate = test.MakeTstateAll(t)
+	ts.RmDir(group.JobDir(JOBDIR))
+	ts.MkDir(group.JobDir(JOBDIR), 0777)
+	ts.gm = groupmgr.Start(ts.SigmaClnt, 0, "kvd", []string{ts.grp, strconv.FormatBool(test.Overlays)}, JOBDIR, 0, ncrash, crash, partition, netfail)
+	cfg, err := group.WaitStarted(ts.SigmaClnt.FsLib, JOBDIR, ts.grp)
+	assert.Nil(t, err)
+	db.DPrintf(db.TEST, "cfg %v\n", cfg)
+	return ts
+}
 
 func TestWaitClosed(t *testing.T) {
-	ts := test.MakeTstateAll(t)
+	ts := makeTstate(t, 1, CRASH, 0, 0)
 
-	grp := groupmgr.Start(ts.SigmaClnt, 0, "kvd", []string{GRP0, strconv.FormatBool(test.Overlays)}, JOBDIR, 0, 1, CRASH, 0, 0)
-	_, err := ts.Stat(DIRGRP0)
+	st, err := ts.Stat(group.GrpPath(JOBDIR, ts.grp) + "/")
 	assert.Nil(t, err, "stat")
 
-	grp.Stop()
+	db.DPrintf(db.TEST, "stat %v\n", st)
+	ts.gm.Stop()
 
 	// XXX should hang around until client closes sessions; once WaitClosed() is
 	// propertly implemented.
 
-	ts.Detach(DIRGRP0)
+	ts.Detach(group.GrpPath(JOBDIR, ts.grp) + "/")
 	ts.Shutdown()
 }
 
 func TestServerCrash(t *testing.T) {
-	ts := test.MakeTstateAll(t)
+	ts := makeTstate(t, 1, CRASH, 0, 0)
 
-	grp := groupmgr.Start(ts.SigmaClnt, 0, "kvd", []string{GRP0, strconv.FormatBool(test.Overlays)}, JOBDIR, 0, 1, CRASH, 0, 0)
-
-	sem := semclnt.MakeSemClnt(ts.FsLib, DIRGRP0+"sem")
+	sem := semclnt.MakeSemClnt(ts.FsLib, group.GrpPath(JOBDIR, ts.grp)+"/sem")
 	err := sem.Init(0)
 	assert.Nil(t, err)
 
-	db.DPrintf(db.TEST, "Sem %v", DIRGRP0+"sem")
+	db.DPrintf(db.TEST, "Sem %v", group.GrpPath(JOBDIR, ts.grp)+"/sem")
 
 	ch := make(chan error)
 	go func() {
 		fsl, err := fslib.MakeFsLibAddr("fslibtest-1", sp.ROOTREALM, ts.GetLocalIP(), ts.NamedAddr())
 		assert.Nil(t, err)
-		sem := semclnt.MakeSemClnt(fsl, DIRGRP0+"sem")
+		sem := semclnt.MakeSemClnt(fsl, group.GrpPath(JOBDIR, ts.grp)+"/sem")
 		err = sem.Down()
 		ch <- err
 	}()
 
 	err = <-ch
-	assert.NotNil(ts.T, err, "down")
+	assert.Nil(ts.T, err, "down")
 
-	grp.Stop()
+	ts.gm.Stop()
 
 	ts.Shutdown()
 }
@@ -133,14 +147,14 @@ func TestProcPartitionMany(t *testing.T) {
 
 func TestReconnectSimple(t *testing.T) {
 	const N = 1000
-	ts := test.MakeTstateAll(t)
-	grp := groupmgr.Start(ts.SigmaClnt, 0, "kvd", []string{GRP0, strconv.FormatBool(test.Overlays)}, JOBDIR, 0, 0, 0, 0, NETFAIL)
+	ts := makeTstate(t, 0, 0, 0, NETFAIL)
+
 	ch := make(chan error)
 	go func() {
 		fsl, err := fslib.MakeFsLibAddr("fslibtest-1", sp.ROOTREALM, ts.GetLocalIP(), ts.NamedAddr())
 		assert.Nil(t, err)
 		for i := 0; i < N; i++ {
-			_, err := fsl.Stat(DIRGRP0)
+			_, err := fsl.Stat(group.GrpPath(JOBDIR, ts.grp) + "/")
 			if err != nil {
 				ch <- err
 				return
@@ -153,15 +167,14 @@ func TestReconnectSimple(t *testing.T) {
 	err := <-ch
 	assert.Nil(ts.T, err, "fsl1")
 
-	grp.Stop()
+	ts.gm.Stop()
 	ts.Shutdown()
 }
 
 func TestServerPartitionNonBlocking(t *testing.T) {
 	const N = 50
 
-	ts := test.MakeTstateAll(t)
-	grp := groupmgr.Start(ts.SigmaClnt, 0, "kvd", []string{GRP0, strconv.FormatBool(test.Overlays)}, JOBDIR, 0, 0, 0, PARTITION, 0)
+	ts := makeTstate(t, 0, 0, PARTITION, 0)
 
 	for i := 0; i < N; i++ {
 		ch := make(chan error)
@@ -169,7 +182,7 @@ func TestServerPartitionNonBlocking(t *testing.T) {
 			fsl, err := fslib.MakeFsLibAddr(sp.Tuname(fmt.Sprintf("test-fsl-%v", i)), sp.ROOTREALM, ts.GetLocalIP(), ts.NamedAddr())
 			assert.Nil(t, err)
 			for true {
-				_, err := fsl.Stat(DIRGRP0)
+				_, err := fsl.Stat(group.GrpPath(JOBDIR, ts.grp) + "/")
 				if err != nil {
 					ch <- err
 					break
@@ -183,22 +196,21 @@ func TestServerPartitionNonBlocking(t *testing.T) {
 		assert.NotNil(ts.T, err, "stat")
 	}
 	db.DPrintf(db.TEST, "Stopping group")
-	grp.Stop()
+	ts.gm.Stop()
 	ts.Shutdown()
 }
 
 func TestServerPartitionBlocking(t *testing.T) {
 	const N = 50
 
-	ts := test.MakeTstateAll(t)
-	grp := groupmgr.Start(ts.SigmaClnt, 0, "kvd", []string{GRP0, strconv.FormatBool(test.Overlays)}, JOBDIR, 0, 0, 0, PARTITION, 0)
+	ts := makeTstate(t, 0, 0, PARTITION, 0)
 
 	for i := 0; i < N; i++ {
 		ch := make(chan error)
 		go func() {
 			fsl, err := fslib.MakeFsLibAddr("fsl", sp.ROOTREALM, ts.GetLocalIP(), ts.NamedAddr())
 			assert.Nil(t, err)
-			sem := semclnt.MakeSemClnt(fsl, DIRGRP0+"sem")
+			sem := semclnt.MakeSemClnt(fsl, group.GrpPath(JOBDIR, ts.grp)+"/sem")
 			sem.Init(0)
 			err = sem.Down()
 			ch <- err
@@ -209,7 +221,7 @@ func TestServerPartitionBlocking(t *testing.T) {
 		err := <-ch
 		assert.NotNil(ts.T, err, "down")
 	}
-	grp.Stop()
+	ts.gm.Stop()
 	ts.Shutdown()
 }
 

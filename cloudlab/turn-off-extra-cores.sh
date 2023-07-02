9dd440c9051fba6798f407e1166848ca8584d5c8
Ariel Szekely <arielck@mit.edu>
2023-07-02 14:02:27 -0400

    Cloudlab script to turn off extra cores

---
+++ cloudlab/turn-off-extra-cores.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+DIR=$(dirname $0)
+source $DIR/env.sh
+
+vms=`cat servers.txt | cut -d " " -f2` 
+
+vma=($vms)
+MAIN="${vma[0]}"
+MAIN_PRIVADDR=$(./leader-ip.sh) 
+#export SIGMANAMED="${SIGMANAMED}"
+
+if ! [ -z "$N_VM" ]; then
+  vms=${vma[@]:0:$N_VM}
+fi
+
+if [ ! -z "$TAG" ]; then
+  ./update-repo.sh --parallel --branch jaeger # docker-dev
+fi
+
+vm_ncores=$(ssh -i $DIR/keys/cloudlab-sigmaos $LOGIN@$MAIN nproc)
+
+for vm in $vms; do
+  echo $vm
+  ssh -i $DIR/keys/cloudlab-sigmaos $LOGIN@$vm <<ENDSSH
+  cd ulambda
+  ./set-cores.sh --set 0 --start 4 --end $(($vm_ncores - 1))
+ENDSSH
+done

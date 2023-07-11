b00140556090fba608ab5b524b25e2e5681dfdc5
Frans Kaashoek <kaashoek@mit.edu>
2023-07-11 14:12:08 -0400

    x

--- leasemgrsrv/leasesrv.go
+++ leasemgrsrv/leasesrv.go
@@ -21,3 +21,13 @@ func (ls *LeaseSrv) AskLease(ctx fs.CtxI, req leaseproto.AskRequest, rep *leasep
 	db.DPrintf(db.LEASESRV, "%v: AskLease %v req\n", ctx, req)
 	return nil
 }
+
+func (ls *LeaseSrv) Extend(ctx fs.CtxI, req leaseproto.ExtendRequest, rep *leaseproto.ExtendResult) error {
+	db.DPrintf(db.LEASESRV, "%v: Extend %v\n", ctx.ClntId(), req.LeaseId)
+	return nil
+}
+
+func (ls *LeaseSrv) End(ctx fs.CtxI, req leaseproto.ExtendRequest, rep *leaseproto.ExtendResult) error {
+	db.DPrintf(db.LEASESRV, "%v: End %v\n", ctx.ClntId(), req.LeaseId)
+	return nil
+}

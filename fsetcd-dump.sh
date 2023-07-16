cfd7c32fdb0422086ddae3a8330d1b60a66ecf93
Frans Kaashoek <kaashoek@mit.edu>
2023-07-16 08:51:28 -0400

    Support dumping a specific realm

--- fsetcd-dump.sh
+++ fsetcd-dump.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
 
-go clean -testcache && go test -v sigmaos/fsetcd -run Dump
+usage() {
+  echo "Usage: $0 [--realm realm] " 1>&2
+}
+
+REALM=""
+
+while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+        --realm)
+            shift
+            REALM="$1"
+            shift
+            ;;
+        *)
+            usage
+            exit 1
+    esac
+done
+
+if [ $# -gt 0 ]; then
+    usage
+    exit 1
+fi
+
+if [[ "$REALM" != "" ]] ; then
+    go clean -testcache && go test -v sigmaos/fsetcd -run Dump --realm "$REALM"
+else
+    go clean -testcache && go test -v sigmaos/fsetcd -run Dump
+fi
+
+
--- fsetcd/fsetcd_test.go
+++ fsetcd/fsetcd_test.go
@@ -1,6 +1,7 @@
 package fsetcd_test
 
 import (
+	"flag"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,8 +11,14 @@ import (
 	sp "sigmaos/sigmap"
 )
 
+var realm string
+
+func init() {
+	flag.StringVar(&realm, "realm", string(sp.ROOTREALM), "realm")
+}
+
 func TestDump(t *testing.T) {
-	fs, err := fsetcd.MkFsEtcd(sp.ROOTREALM)
+	fs, err := fsetcd.MkFsEtcd(sp.Trealm(realm))
 	assert.Nil(t, err)
 	nd, err := fs.ReadDir(fsetcd.ROOT)
 	assert.Nil(t, err)

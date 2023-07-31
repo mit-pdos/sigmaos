d98312dbdb6a11062d429049659eba6b0aab1d9a
Frans Kaashoek <kaashoek@mit.edu>
2023-07-31 19:39:51 -0400

    x

--- named/named_test.go
+++ named/named_test.go
@@ -2,21 +2,15 @@ package named_test
 
 import (
 	"log"
-	"path"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
-	// "sigmaos/groupmgr"
-	// "sigmaos/sigmaclnt"
 	sp "sigmaos/sigmap"
 	"sigmaos/test"
 )
 
 func TestBootNamed(t *testing.T) {
-	// crash := 1
-	// crashinterval := 0
-
 	ts := test.MakeTstateAll(t)
 
 	sts, err1 := ts.GetDir(sp.NAMED + "/")
@@ -25,66 +19,3 @@ func TestBootNamed(t *testing.T) {
 
 	ts.Shutdown()
 }
-
-func TestXXX(t *testing.T) {
-	log.Printf("%v\n", path.Join("", "xxx"))
-}
-
-// type Tstate struct {
-// 	*test.Tstate
-// }
-
-// func makeTstate(t *testing.T) *Tstate {
-// 	ts := &Tstate{}
-// 	ts.Tstate = test.MakeTstateAll(t)
-// 	return ts
-// }
-
-// func TestNamedWalk(t *testing.T) {
-// 	crash := 1
-// 	crashinterval := 200
-// 	// crashinterval := 0
-
-// 	ts := makeTstate(t)
-
-// 	pn := sp.NAMED + "/"
-
-// 	d := []byte("hello")
-// 	_, err := ts.PutFile(path.Join(pn, "testf"), 0777, sp.OWRITE, d)
-// 	assert.Nil(t, err)
-
-// 	ndg := startNamed(ts.SigmaClnt, "rootrealm", crash, crashinterval)
-
-// 	// wait until kernel-started named exited and its lease expired
-// 	time.Sleep((fsetcd.SessionTTL + 2) * time.Second)
-
-// 	start := time.Now()
-// 	i := 0
-// 	for time.Since(start) < 10*time.Second {
-// 		d1, err := ts.GetFile(path.Join(pn, "testf"))
-// 		if err != nil {
-// 			log.Printf("err %v\n", err)
-// 			assert.Nil(t, err)
-// 			break
-// 		}
-// 		assert.Equal(t, d, d1)
-// 		i += 1
-// 	}
-
-// 	log.Printf("#getfile %d\n", i)
-
-// 	for {
-// 		err := ts.Remove(path.Join(pn, "testf"))
-// 		if err == nil {
-// 			break
-// 		}
-// 		log.Printf("remove f retry\n")
-// 		time.Sleep(100 * time.Millisecond)
-// 	}
-
-// 	ndg.Stop()
-
-// 	log.Printf("named stopped\n")
-
-// 	ts.Shutdown()
-// }

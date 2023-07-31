3e3e0671e09bcb0fb968a4dcdaa8b467b23936a2
Frans Kaashoek <kaashoek@mit.edu>
2023-07-31 08:44:45 -0400

    x

--- hotel/job.go
+++ hotel/job.go
@@ -172,7 +172,7 @@ func MakeHotelJob(sc *sigmaclnt.SigmaClnt, job string, srvs []Srv, nhotel int, c
 			ca = cacheclnt.MakeAutoscaler(cm, cc)
 		case "kvd":
 			db.DPrintf(db.ALWAYS, "Hotel running with kvd")
-			kvf, err = kv.MakeKvdFleet(sc, job, nsrv, 0, cacheMcpu, "0", "manual")
+			kvf, err = kv.MakeKvdFleet(sc, job, 0, nsrv, 0, cacheMcpu, "0", "manual")
 			if err != nil {
 				return nil, err
 			}

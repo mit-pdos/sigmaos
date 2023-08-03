a98b94c561b7384ae65c84c817eac15d9a0ea1f0
Frans Kaashoek <kaashoek@mit.edu>
2023-08-03 08:20:11 -0400

    Add nosort argument

--- logs.sh
+++ logs.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
+usage() {
+  echo "Usage: $0 [--nosort]" 1>&2
+}
+
+SORT="sort"
+while [[ "$#" -gt 0 ]]; do
+  case "$1" in
+  --nosort)
+    shift
+    SORT=""
+    ;;
+   *)
+   echo "unexpected argument $1"
+   usage
+   exit 1
+ esac
+done
+
 for containerid in $(docker ps -a --format "{{.Names}}"); do
     if [[ $containerid == sigma-* ]] ; then
         echo "========== Logs for $containerid =========="
-        docker logs $containerid | sort -k 1
-        # docker logs $containerid
+        if [ $SORT == "sort" ]; then 
+            docker logs $containerid | sort -k 1
+        else 
+            docker logs $containerid
+        fi
     fi
 done

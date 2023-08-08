79c27f5720da215f61f5e9df71f6ce8ef69d027e
Frans Kaashoek <kaashoek@mit.edu>
2023-08-08 14:30:27 -0400

    x

--- benchmarks/cached_test.go
+++ benchmarks/cached_test.go
@@ -5,7 +5,8 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"sigmaos/cacheclnt"
+	"sigmaos/cachedsvc"
+	"sigmaos/cachedsvcclnt"
 	db "sigmaos/debug"
 	"sigmaos/proc"
 	"sigmaos/rand"
@@ -23,7 +24,7 @@ type CachedJobInstance struct {
 	nkeys     int
 	ready     chan bool
 	clerks    []proc.Tpid
-	cm        *cacheclnt.CacheMgr
+	cm        *cachedsvc.CacheMgr
 	sempn     string
 	sem       *semclnt.SemClnt
 	*test.RealmTstate
@@ -46,7 +47,7 @@ func MakeCachedJob(ts *test.RealmTstate, nkeys, ncache, nclerks int, dur time.Du
 }
 
 func (ji *CachedJobInstance) RunCachedJob() {
-	cm, err := cacheclnt.MkCacheMgr(ji.SigmaClnt, ji.job, ji.ncache, ji.cachemcpu, CACHE_GC, test.Overlays)
+	cm, err := cachedsvc.MkCacheMgr(ji.SigmaClnt, ji.job, ji.ncache, ji.cachemcpu, CACHE_GC, test.Overlays)
 	assert.Nil(ji.T, err, "Error MkCacheMgr: %v", err)
 	ji.cm = cm
 	ji.sempn = ji.cm.SvcDir() + "-cacheclerk-sem"
@@ -56,7 +57,7 @@ func (ji *CachedJobInstance) RunCachedJob() {
 
 	// Start clerks
 	for i := 0; i < ji.nclerks; i++ {
-		ck, err := cacheclnt.StartClerk(ji.SigmaClnt, ji.job, ji.nkeys, ji.dur, i*ji.nkeys, ji.sempn, ji.ckmcpu)
+		ck, err := cachedsvcclnt.StartClerk(ji.SigmaClnt, ji.job, ji.nkeys, ji.dur, i*ji.nkeys, ji.sempn, ji.ckmcpu)
 		assert.Nil(ji.T, err, "Err StartClerk: %v", err)
 		ji.clerks = append(ji.clerks, ck)
 	}
@@ -64,7 +65,7 @@ func (ji *CachedJobInstance) RunCachedJob() {
 	// Stop clerks
 	aggTpt := float64(0)
 	for _, ck := range ji.clerks {
-		tpt, err := cacheclnt.WaitClerk(ji.SigmaClnt, ck)
+		tpt, err := cachedsvcclnt.WaitClerk(ji.SigmaClnt, ck)
 		db.DPrintf(db.ALWAYS, "Clerk throughput: %v ops/sec", tpt)
 		assert.Nil(ji.T, err, "Err waitclerk %v", err)
 		aggTpt += tpt

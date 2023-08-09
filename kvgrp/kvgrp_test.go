465fa115f6a5e6c465b213e933fa3d3f2c749717
Frans Kaashoek <kaashoek@mit.edu>
2023-08-08 20:54:48 -0400

    Convert test to use cacheclnt instead of fslib for Put/Get

--- kvgrp/kvgrp_test.go
+++ kvgrp/kvgrp_test.go
@@ -7,7 +7,12 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
+	"sigmaos/cache"
+	"sigmaos/cache/proto"
+	"sigmaos/cacheclnt"
+	"sigmaos/cachesrv"
 	db "sigmaos/debug"
+	"sigmaos/fslib"
 	"sigmaos/groupmgr"
 	"sigmaos/kvgrp"
 	sp "sigmaos/sigmap"
@@ -26,6 +31,7 @@ type Tstate struct {
 	*test.Tstate
 	grp string
 	gm  *groupmgr.GroupMgr
+	cc  *cacheclnt.CacheClnt
 }
 
 func makeTstate(t *testing.T, nrepl, ncrash int) *Tstate {
@@ -36,6 +42,7 @@ func makeTstate(t *testing.T, nrepl, ncrash int) *Tstate {
 	ts.gm = groupmgr.Start(ts.SigmaClnt, nrepl, "kvd", []string{ts.grp, strconv.FormatBool(test.Overlays)}, JOBDIR, 0, ncrash, CRASH_KVD, 0, 0)
 	cfg, err := kvgrp.WaitStarted(ts.SigmaClnt.FsLib, JOBDIR, ts.grp)
 	assert.Nil(t, err)
+	ts.cc = cacheclnt.NewCacheClnt([]*fslib.FsLib{ts.SigmaClnt.FsLib}, JOBDIR, 1)
 	db.DPrintf(db.TEST, "cfg %v\n", cfg)
 	return ts
 }
@@ -46,27 +53,28 @@ func (ts *Tstate) Shutdown() {
 
 func (ts *Tstate) setupKeys(nkeys int) {
 	db.DPrintf(db.TEST, "setupKeys")
+	srv := path.Join(kvgrp.GrpPath(JOBDIR, ts.grp))
+	err := ts.cc.CreateShard(srv, cache.Tshard(0), sp.NullFence(), make(cachesrv.Tcache))
+	assert.Nil(ts.T, err, "CreateShard %v", err)
 	for i := 0; i < nkeys; i++ {
 		i_str := strconv.Itoa(i)
-		fname := path.Join(kvgrp.GrpPath(JOBDIR, ts.grp), i_str)
-		_, err := ts.PutFile(fname, 0777, sp.OWRITE|sp.OREAD, []byte(i_str))
+		err := ts.cc.PutSrv(srv, i_str, &proto.CacheString{Val: i_str})
 		assert.Nil(ts.T, err, "Put %v", err)
 	}
 	db.DPrintf(db.TEST, "done setupKeys")
 }
 
-func (ts *Tstate) testGetPutSet(nkeys int) {
+func (ts *Tstate) testGetPut(nkeys int) {
 	db.DPrintf(db.TEST, "testGetPutSet")
 	for i := 0; i < nkeys; i++ {
 		i_str := strconv.Itoa(i)
-		fname := path.Join(kvgrp.GrpPath(JOBDIR, ts.grp), i_str)
-		b, err := ts.GetFile(fname)
-		assert.Nil(ts.T, err, "Get %v", err)
-		assert.Equal(ts.T, i_str, string(b), "Didn't read expected")
-		_, err = ts.PutFile(fname, 0777, sp.OWRITE|sp.OREAD|sp.OEXCL, []byte(i_str))
-		assert.NotNil(ts.T, err, "Put nil")
-		_, err = ts.SetFile(fname, []byte(i_str+i_str), sp.OWRITE|sp.OREAD, 0)
-		assert.Nil(ts.T, err, "Set %v", err)
+		srv := path.Join(kvgrp.GrpPath(JOBDIR, ts.grp))
+		res := &proto.CacheString{}
+		err := ts.cc.GetSrv(srv, i_str, res)
+		assert.Nil(ts.T, err, "GetSrv %v", err)
+		assert.Equal(ts.T, i_str, res.Val, "Didn't read expected")
+		err = ts.cc.PutSrv(srv, i_str, &proto.CacheString{Val: i_str + i_str})
+		assert.Nil(ts.T, err, "PutSrv")
 	}
 	db.DPrintf(db.TEST, "done testGetPutSet")
 }
@@ -109,7 +117,7 @@ func TestStartStopReplN(t *testing.T) {
 func TestGetPutSetReplOK(t *testing.T) {
 	ts := makeTstate(t, N_REPL, 0)
 	ts.setupKeys(N_KEYS)
-	ts.testGetPutSet(N_KEYS)
+	ts.testGetPut(N_KEYS)
 	err := ts.gm.Stop()
 	assert.Nil(ts.T, err, "Stop")
 	ts.Shutdown()
@@ -118,7 +126,7 @@ func TestGetPutSetReplOK(t *testing.T) {
 func TestGetPutSetFail1(t *testing.T) {
 	ts := makeTstate(t, N_REPL, 1)
 	ts.setupKeys(N_KEYS)
-	ts.testGetPutSet(N_KEYS)
+	ts.testGetPut(N_KEYS)
 	db.DPrintf(db.TEST, "Pre stop")
 	err := ts.gm.Stop()
 	assert.Nil(ts.T, err, "Stop")

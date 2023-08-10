32b537f3fe55f1d9833bf1e065b1e79f85e01b43
Frans Kaashoek <kaashoek@mit.edu>
2023-08-10 07:56:17 -0400

    Oops, forgot to commit

---
+++ cache/replproto/types.go
@@ -0,0 +1,13 @@
+package replproto
+
+import (
+	sp "sigmaos/sigmap"
+)
+
+func (rr *ReplOpRequest) Tseqno() sp.Tseqno {
+	return sp.Tseqno(rr.Seqno)
+}
+
+func (rr *ReplOpRequest) TclntId() sp.TclntId {
+	return sp.TclntId(rr.ClntId)
+}
---
+++ cachereplclnt/cachereplclnt.go
@@ -0,0 +1,46 @@
+package cachereplclnt
+
+import (
+	"google.golang.org/protobuf/proto"
+
+	replproto "sigmaos/cache/replproto"
+	"sigmaos/cacheclnt"
+	// db "sigmaos/debug"
+	"sigmaos/fslib"
+	sp "sigmaos/sigmap"
+	// tproto "sigmaos/tracing/proto"
+)
+
+type CacheReplClnt struct {
+	*cacheclnt.CacheClnt
+}
+
+func NewCacheReplClnt(fsls []*fslib.FsLib, job string, nshard int) *CacheReplClnt {
+	cc := &CacheReplClnt{CacheClnt: cacheclnt.NewCacheClnt(fsls, job, nshard)}
+	return cc
+}
+
+func NewReplOp(method, key string, cid sp.TclntId, seqno sp.Tseqno, val proto.Message) (*replproto.ReplOpRequest, error) {
+	b, err := proto.Marshal(val)
+	if err != nil {
+		return nil, err
+	}
+	return &replproto.ReplOpRequest{
+		Method: method,
+		ClntId: uint32(cid),
+		Seqno:  uint64(seqno),
+		Msg:    b,
+	}, nil
+}
+
+func (cc *CacheReplClnt) ReplOpSrv(srv, method, key string, cid sp.TclntId, seqno sp.Tseqno, val proto.Message) error {
+	req, err := NewReplOp(method, key, cid, seqno, val)
+	if err != nil {
+		return err
+	}
+	var res replproto.ReplOpReply
+	if err := cc.RPC(srv, "CacheSrvRepl.SubmitOp", req, &res); err != nil {
+		return err
+	}
+	return nil
+}

0c1f7273e79de9ab3e9c9142f148ed62933e8df0
Frans Kaashoek <kaashoek@mit.edu>
2023-08-25 12:12:02 -0400

    Add test for groupmgr restarting imgresized after system shutdown and finishing its job.

--- imgresized/job.go
+++ imgresized/job.go
@@ -8,7 +8,10 @@ import (
 	"sigmaos/sigmaclnt"
 )
 
-func StartImgd(sc *sigmaclnt.SigmaClnt, job string, workerMcpu proc.Tmcpu) *groupmgr.GroupMgr {
+func StartImgd(sc *sigmaclnt.SigmaClnt, job string, workerMcpu proc.Tmcpu, persist bool) *groupmgr.GroupMgr {
 	cfg := groupmgr.NewGroupConfig(1, "imgresized", []string{strconv.Itoa(0), strconv.Itoa(int(workerMcpu))}, 0, job)
+	if persist {
+		cfg.Persist(sc.FsLib)
+	}
 	return cfg.StartGrpMgr(sc, 0)
 }

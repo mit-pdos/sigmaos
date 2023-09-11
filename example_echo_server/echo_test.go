0e0153062c0346fe96430e5d4eeba6171a40c5b4
Ariel Szekely <arielck@mit.edu>
2023-09-11 11:36:39 -0400

    Fix fslib creation in echo test

--- example_echo_server/echo_test.go
+++ example_echo_server/echo_test.go
@@ -115,7 +115,7 @@ func TestEchoLoad(t *testing.T) {
 	// create a RPC client and query server
 	fsls := make([]*fslib.FsLib, 0, N_RPC_SESSIONS)
 	for i := 0; i < N_RPC_SESSIONS; i++ {
-		fsl, err := fslib.MakeFsLib(sp.Tuname(tse.jobname + "-" + strconv.Itoa(i)))
+		fsl, err := fslib.MakeFsLibAddr(sp.Tuname(tse.jobname+"-"+strconv.Itoa(i)), sp.ROOTREALM, tse.GetLocalIP(), tse.NamedAddr())
 		if err != nil {
 			dbg.DFatalf("Error mkfsl: %v", err)
 		}

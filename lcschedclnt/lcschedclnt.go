65db03906e9557d0898ab4bad24ff502cf38bd0c
Ariel Szekely <arielck@mit.edu>
2023-09-20 17:08:41 -0400

    x

--- lcschedclnt/lcschedclnt.go
+++ lcschedclnt/lcschedclnt.go
@@ -38,8 +38,8 @@ func NewLCSchedClnt(fsl *fslib.FsLib) *LCSchedClnt {
 	}
 }
 
-func (pqc *LCSchedClnt) Nlcsched() (int, error) {
-	sds, err := pqc.getLCScheds()
+func (lcs *LCSchedClnt) Nlcsched() (int, error) {
+	sds, err := lcs.getLCScheds()
 	if err != nil {
 		return 0, err
 	}
@@ -48,13 +48,13 @@ func (pqc *LCSchedClnt) Nlcsched() (int, error) {
 
 // Enqueue a proc on the lcsched. Returns the ID of the kernel that is running
 // the proc.
-func (pqc *LCSchedClnt) Enqueue(p *proc.Proc) (string, error) {
-	pqc.UpdateLCScheds()
-	pqID, err := pqc.NextLCSched()
+func (lcs *LCSchedClnt) Enqueue(p *proc.Proc) (string, error) {
+	lcs.UpdateLCScheds()
+	pqID, err := lcs.NextLCSched()
 	if err != nil {
 		return NOT_ENQ, errors.New("No lcscheds available")
 	}
-	rpcc, err := pqc.GetLCSchedClnt(pqID)
+	rpcc, err := lcs.GetLCSchedClnt(pqID)
 	if err != nil {
 		db.DFatalf("Error: Can't get lcsched clnt: %v", err)
 		return NOT_ENQ, err
@@ -67,7 +67,7 @@ func (pqc *LCSchedClnt) Enqueue(p *proc.Proc) (string, error) {
 		db.DPrintf(db.ALWAYS, "LCSched.Enqueue err %v", err)
 		if serr.IsErrCode(err, serr.TErrUnreachable) {
 			db.DPrintf(db.ALWAYS, "Force lookup %v", pqID)
-			pqc.UnregisterClnt(pqID)
+			lcs.UnregisterClnt(pqID)
 		}
 		return NOT_ENQ, err
 	}
@@ -75,59 +75,59 @@ func (pqc *LCSchedClnt) Enqueue(p *proc.Proc) (string, error) {
 	return res.KernelID, nil
 }
 
-func (pqc *LCSchedClnt) GetLCSchedClnt(lcschedID string) (*rpcclnt.RPCClnt, error) {
-	pqc.Lock()
-	defer pqc.Unlock()
+func (lcs *LCSchedClnt) GetLCSchedClnt(lcschedID string) (*rpcclnt.RPCClnt, error) {
+	lcs.Lock()
+	defer lcs.Unlock()
 
 	var rpcc *rpcclnt.RPCClnt
 	var ok bool
-	if rpcc, ok = pqc.lcscheds[lcschedID]; !ok {
+	if rpcc, ok = lcs.lcscheds[lcschedID]; !ok {
 		var err error
-		rpcc, err = rpcclnt.NewRPCClnt([]*fslib.FsLib{pqc.FsLib}, path.Join(sp.LCSCHED, lcschedID))
+		rpcc, err = rpcclnt.NewRPCClnt([]*fslib.FsLib{lcs.FsLib}, path.Join(sp.LCSCHED, lcschedID))
 		if err != nil {
 			db.DPrintf(db.LCSCHEDCLNT_ERR, "Error newRPCClnt[lcsched:%v]: %v", lcschedID, err)
 			return nil, err
 		}
-		pqc.lcscheds[lcschedID] = rpcc
+		lcs.lcscheds[lcschedID] = rpcc
 	}
 	return rpcc, nil
 }
 
 // Update the list of active procds.
-func (pqc *LCSchedClnt) UpdateLCScheds() {
-	pqc.Lock()
-	defer pqc.Unlock()
+func (lcs *LCSchedClnt) UpdateLCScheds() {
+	lcs.Lock()
+	defer lcs.Unlock()
 
 	// If we updated the list of active procds recently, return immediately. The
 	// list will change at most as quickly as the realm resizes.
-	if time.Since(pqc.lastUpdate) < sp.Conf.Realm.RESIZE_INTERVAL && len(pqc.lcschedIDs) > 0 {
+	if time.Since(lcs.lastUpdate) < sp.Conf.Realm.RESIZE_INTERVAL && len(lcs.lcschedIDs) > 0 {
 		db.DPrintf(db.LCSCHEDCLNT, "Update lcscheds too soon")
 		return
 	}
 	// Read the procd union dir.
-	lcscheds, err := pqc.getLCScheds()
+	lcscheds, err := lcs.getLCScheds()
 	if err != nil {
 		db.DPrintf(db.ALWAYS, "Error ReadDir procd: %v", err)
 		return
 	}
-	pqc.lastUpdate = time.Now()
+	lcs.lastUpdate = time.Now()
 	db.DPrintf(db.LCSCHEDCLNT, "Got lcscheds %v", lcscheds)
 	// Alloc enough space for the list of lcscheds.
-	pqc.lcschedIDs = make([]string, 0, len(lcscheds))
+	lcs.lcschedIDs = make([]string, 0, len(lcscheds))
 	for _, lcsched := range lcscheds {
-		pqc.lcschedIDs = append(pqc.lcschedIDs, lcsched)
+		lcs.lcschedIDs = append(lcs.lcschedIDs, lcsched)
 	}
 }
 
-func (pqc *LCSchedClnt) UnregisterClnt(id string) {
-	pqc.Lock()
-	defer pqc.Unlock()
+func (lcs *LCSchedClnt) UnregisterClnt(id string) {
+	lcs.Lock()
+	defer lcs.Unlock()
 
-	delete(pqc.lcscheds, id)
+	delete(lcs.lcscheds, id)
 }
 
-func (pqc *LCSchedClnt) getLCScheds() ([]string, error) {
-	sts, err := pqc.GetDir(sp.LCSCHED)
+func (lcs *LCSchedClnt) getLCScheds() ([]string, error) {
+	sts, err := lcs.GetDir(sp.LCSCHED)
 	if err != nil {
 		return nil, err
 	}
@@ -135,15 +135,15 @@ func (pqc *LCSchedClnt) getLCScheds() ([]string, error) {
 }
 
 // Get the next procd to burst on.
-func (pqc *LCSchedClnt) NextLCSched() (string, error) {
-	pqc.Lock()
-	defer pqc.Unlock()
+func (lcs *LCSchedClnt) NextLCSched() (string, error) {
+	lcs.Lock()
+	defer lcs.Unlock()
 
-	if len(pqc.lcschedIDs) == 0 {
+	if len(lcs.lcschedIDs) == 0 {
 		return "", serr.NewErr(serr.TErrNotfound, "no lcscheds to spawn on")
 	}
 
-	sdip := pqc.lcschedIDs[pqc.burstOffset%len(pqc.lcschedIDs)]
-	pqc.burstOffset++
+	sdip := lcs.lcschedIDs[lcs.burstOffset%len(lcs.lcschedIDs)]
+	lcs.burstOffset++
 	return sdip, nil
 }

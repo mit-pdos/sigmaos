defd4303e451c0fa1857695a50e1466e8f9ed3a1
Ariel Szekely <arielck@mit.edu>
2023-09-25 09:27:24 -0400

    Add fork test to sleeper proc

--- cmd/user/sleeper/main.go
+++ cmd/user/sleeper/main.go
@@ -8,6 +8,8 @@ import (
 	"strconv"
 	"time"
 
+	"os/exec"
+
 	db "sigmaos/debug"
 	"sigmaos/proc"
 	"sigmaos/sessp"
@@ -16,10 +18,35 @@ import (
 )
 
 func main() {
+	if os.Getenv("IS_FORKTEST") == "true" {
+		if len(os.Args) == 1 {
+			db.DPrintf(db.ALWAYS, "In parent")
+			cmd := exec.Command("./bin", []string{strconv.FormatInt(time.Now().UnixMicro(), 10)}...)
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			if err := cmd.Start(); err != nil {
+				db.DFatalf("Error start %v %v", cmd, err)
+			}
+			if err := cmd.Wait(); err != nil {
+				db.DFatalf("Error wait %v %v", cmd, err)
+			}
+			return
+		} else {
+			execTimeStr := os.Args[1]
+			execTimeMicro, err := strconv.ParseInt(execTimeStr, 10, 64)
+			if err != nil {
+				db.DFatalf("Error parsing exec time 1: %v", err)
+			}
+			execTime := time.UnixMicro(execTimeMicro)
+			db.DPrintf(db.ALWAYS, "Trampoline exec latency: %v", time.Since(execTime))
+			db.DPrintf(db.ALWAYS, "In child")
+			return
+		}
+	}
 	execTimeStr := os.Getenv("SIGMA_EXEC_TIME")
 	execTimeMicro, err := strconv.ParseInt(execTimeStr, 10, 64)
 	if err != nil {
-		db.DFatalf("Error parsing exec time: %v", err)
+		db.DFatalf("Error parsing exec time 2: %v", err)
 	}
 	execTime := time.UnixMicro(execTimeMicro)
 	db.DPrintf(db.SPAWN_LAT, "[%v] Proc exec latency: %v", proc.GetSigmaDebugPid(), time.Since(execTime))

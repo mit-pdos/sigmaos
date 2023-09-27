e67ddf25894aa08a32fa7a419043b55962027f25
Frans Kaashoek <kaashoek@mit.edu>
2023-09-27 13:02:20 -0400

    Checkpoint unmarshalling SIGMACONFIG

--- exec-uproc-rs/src/main.rs
+++ exec-uproc-rs/src/main.rs
@@ -3,10 +3,13 @@ use std::process::Command;
 use std::os::unix::process::CommandExt;
 use std::time::{SystemTime, UNIX_EPOCH};
 
+use json;
+
 fn main() {
     let exec_time = env::var("SIGMA_EXEC_TIME").unwrap_or("".to_string());
     let exec_time_micro: u64 = exec_time.parse().unwrap_or(0);
-
+    let cfg = env::var("SIGMACONFIG").unwrap_or("".to_string());
+    
     eprintln!("exec_uproc SIGMA_EXEC_TIME {}", exec_time_micro);
 
     let pn = env::args().nth(1).expect("no program");
@@ -18,6 +21,11 @@ fn main() {
         .expect("Time went backwards");
      
     env::set_var("SIGMA_EXEC_TIME", now.as_micros().to_string());
+
+    if cfg != "" {
+        let parsed = json::parse(&cfg).unwrap();
+        println!("{}", parsed);
+    }
 
     cmd.args(new_args).exec();
 }

e3720d1a9a41753d2b62c9e4c42bcd8b5f1599f1
Ariel Szekely <arielck@mit.edu>
2023-10-07 11:42:24 -0400

    Fix procclnt test compilation issue with new linuxsched.GetNCores()

--- procclnt/procclnt_test.go
+++ procclnt/procclnt_test.go
@@ -559,7 +559,7 @@ func TestEvict(t *testing.T) {
 func TestEvictN(t *testing.T) {
 	ts := test.NewTstateAll(t)
 
-	N := int(linuxsched.NCores)
+	N := int(linuxsched.GetNCores())
 
 	pids := []sp.Tpid{}
 	for i := 0; i < N; i++ {
@@ -581,7 +581,7 @@ func TestBurstSpawn(t *testing.T) {
 	ts := test.NewTstateAll(t)
 
 	// Number of spinners to burst-spawn
-	N := (linuxsched.NCores) * 3
+	N := (linuxsched.GetNCores()) * 3
 
 	// Start a couple new procds.
 	err := ts.BootNode(1)
@@ -617,7 +617,7 @@ func TestReserveCores(t *testing.T) {
 
 	start := time.Now()
 	pid := sp.Tpid("sleeper-aaaaaaa")
-	majorityCpu := 1000 * (linuxsched.NCores/2 + 1)
+	majorityCpu := 1000 * (linuxsched.GetNCores()/2 + 1)
 	spawnSleeperMcpu(t, ts, pid, proc.Tmcpu(majorityCpu), SLEEP_MSECS)
 
 	time.Sleep(SLEEP_MSECS / 2)
@@ -659,7 +659,7 @@ func TestSpawnCrashSchedd(t *testing.T) {
 	ts := test.NewTstateAll(t)
 
 	// Spawn a proc which can't possibly be run by any schedd.
-	pid := spawnSpinnerMcpu(ts, proc.Tmcpu(1000*linuxsched.NCores*2))
+	pid := spawnSpinnerMcpu(ts, proc.Tmcpu(1000*linuxsched.GetNCores()*2))
 
 	err := ts.KillOne(sp.SCHEDDREL)
 	assert.Nil(t, err, "KillOne: %v", err)

412dad85a2b34613aa595192b2de5ed3f0ba186c
Ariel Szekely <arielck@mit.edu>
2023-10-21 22:15:23 -0400

    Improve stat scraping script

--- scripts/scrape-stats.py
+++ scripts/scrape-stats.py
@@ -15,15 +15,19 @@ def str_dur_to_ms(dstr):
       return float(dstr.removesuffix(suffixes[i])) * mults[i]
   raise ValueError("Unexpected suffix for duration string {}".format(dstr))
 
-def scrape_stats(path, regex, pos, verbose):
+def scrape_stats(path, regex, pos):
   p = re.compile(regex)
   with open(path, "r") as f:
     x = f.read()
   lines = [ l.strip() for l in x.split("\n") if p.match(l) ]
   if len(lines) == 0:
-    raise ValueError("No matches for regex [{}]".format(regex))
+    print("No matches for regex [{}] in file {}".format(regex, path))
+    return []
   lat = [ str_dur_to_ms(l.split(" ")[pos]) for l in lines ]
   lat = [ l for l in lat if l is not None ]
+  return lat
+
+def print_stats(path, regex, lat, verbose):
   if verbose:
     for l in lat:
       print("{:.3f}ms".format(l))
@@ -35,8 +39,8 @@ def scrape_stats(path, regex, pos, verbose):
     min(lat),
     max(lat),
     np.mean(lat),
-    np.median(lat),
     np.std(lat),
+    np.median(lat),
     np.percentile(lat, 90),
     np.percentile(lat, 99),
   ))
@@ -45,7 +49,18 @@ if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument("--path", type=str, required=True)
   parser.add_argument("--regex", type=str, required=True)
+  parser.add_argument("--file_suffix", type=str, default=".out")
   parser.add_argument("--pos", type=int, default=-1)
+  parser.add_argument("--is_dir", action="store_true", default=False)
   parser.add_argument("--v", action="store_true", default=False)
   args = parser.parse_args()
-  scrape_stats(path=args.path, regex=args.regex, pos=args.pos, verbose=args.v)
+
+  paths = [ args.path ]
+  if args.is_dir:
+    paths = [ os.path.join(args.path, fn) for fn in os.listdir(args.path) if fn.endswith(args.file_suffix) ]
+
+  lats = [ scrape_stats(path=pn, regex=args.regex, pos=args.pos) for pn in paths ]
+  lat = []
+  for l in lats:
+    lat = lat + l
+  print_stats(path=args.path, regex=args.regex, lat=lat, verbose=args.v)

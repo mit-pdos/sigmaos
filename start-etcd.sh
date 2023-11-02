e103decf7030750fec22af1f361b161337c6a944
Frans Kaashoek <kaashoek@mit.edu>
2023-11-01 20:55:08 -0400

    Fix start-etcd.sh

--- start-etcd.sh
+++ start-etcd.sh
@@ -17,13 +17,12 @@ fi
 
 docker run -d \
     --name etcd-server \
+    --env ALLOW_NONE_AUTHENTICATION=yes \
     --publish 2379:2379 \
     --publish 2380:2380 \
     --publish 2381:2381 \
     --publish 2382:2382 \
     --publish 2383:2383 \
-    -listen-client-urls http://localhost:2379,http://localhost:2380,http://localhost:2381,http://localhost:2382, http://localhost:2383 \
-    --env ALLOW_NONE_AUTHENTICATION=yes \
     bitnami/etcd:latest
 
 

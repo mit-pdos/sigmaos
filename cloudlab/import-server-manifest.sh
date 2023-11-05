175a2106a3663296309f8f0e9dbe1c5eb45eb13d
Ariel Szekely <arielck@mit.edu>
2023-11-05 09:34:01 -0500

    Script to import cloudlab server dns names from manifest

---
+++ cloudlab/import-server-manifest.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+usage() {
+  echo "Usage: $0 --manifest PATH" 1>&2
+}
+
+MANIFEST=""
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  case $key in
+  --manifest)
+    shift
+    MANIFEST=$1
+    shift
+    ;;
+  -help)
+    usage
+    exit 0
+    ;;
+  *)
+    echo "Error: unexpected argument '$1'"
+    usage
+    exit 1
+    ;;
+  esac
+done
+
+if [ -z "$MANIFEST" ] || [ $# -gt 0 ]; then
+    usage
+    exit 1
+fi
+
+DIR=$(dirname $0)
+source $DIR/env.sh
+
+servers=$(grep "username=\"arielck" $MANIFEST | cut -d "\"" -f4)
+
+rm $DIR/servers.txt
+
+i=0
+for s in $servers ; do
+  echo "node$i $s" | tee -a $DIR/servers.txt
+  i=$(($i + 1))
+done
+

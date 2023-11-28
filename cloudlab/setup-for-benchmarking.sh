562a4c0d0f27ef04cba19df315d07fdfc3ec036e
Ariel Szekely <arielck@mit.edu>
2023-11-28 17:10:11 -0500

    In cloudlab benchmarking setup script, optionally enable turbo-boost

--- cloudlab/setup-for-benchmarking.sh
+++ cloudlab/setup-for-benchmarking.sh
@@ -1,18 +1,50 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]
+usage() {
+  echo "Usage: $0 [--turbo] address" 1>&2
+}
+
+NOTURBO="true"
+ADDR="NONE"
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  case $key in
+  --turbo)
+    shift
+    NOTURBO="false"
+    ;;
+  -help)
+    usage
+    exit 0
+    ;;
+  *)
+    ADDR=$1
+    shift
+    ;;
+  esac
+done
+
+if [ "$#" -ne 0 ] || [ "$ADDR" == "NONE" ];
 then
-  echo "Usage: $0 address"
+  usage
   exit 1
 fi
 
 DIR=$(dirname $0)
 source $DIR/env.sh
 
-ssh -i $DIR/keys/cloudlab-sigmaos $LOGIN@$1 <<'ENDSSH'
-# Turn off turbo boost.
-echo 1 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
+export NOTURBO=$NOTURBO
 
+CMD=$(
+envsubst '$NOTURBO' <<'ENDSSH'
+# Turn off turbo boost.
+if [ "${NOTURBO}" == "true" ]; then
+  echo "Turning off turbo boost"
+  echo 1 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
+else
+  echo "Turning on turbo boost"
+  echo 0 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
+fi
 # Disable CPU frequency scaling.
 # sudo cpupower frequency-set -g performance
 np=$(nproc)
@@ -23,7 +55,13 @@ do
   sudo cpufreq-set -g performance -c $i
 done
 ENDSSH
+)
+
+#ssh -i $DIR/keys/cloudlab-sigmaos $LOGIN@$ADDR <<'ENDSSH'
+ssh -i $DIR/keys/cloudlab-sigmaos $LOGIN@$ADDR <<ENDSSH
+  $CMD
+ENDSSH
 
 echo "== TO LOGIN TO VM INSTANCE USE: =="
-echo "ssh $1"
+echo "ssh $ADDR"
 echo "============================="

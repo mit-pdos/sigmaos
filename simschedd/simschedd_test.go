7578ba5f39ea499e38ab4c4b8cea18aace8835a0
Frans Kaashoek <kaashoek@mit.edu>
2023-12-02 20:47:45 -0500

    Add tests for 2 realms

--- simschedd/simschedd_test.go
+++ simschedd/simschedd_test.go
@@ -4,9 +4,39 @@ import (
 	"testing"
 )
 
-func TestRun(t *testing.T) {
-	w := newWorld(1, 1)
-	for i := 0; i < 1000; i++ {
+const N = 1000
+
+func TestRunOneRealmSimple(t *testing.T) {
+	w := newWorld(1, 1, 1)
+	for i := 0; i < N; i++ {
+		w.Tick()
+	}
+}
+
+func TestRunOneRealmTwoSchedd(t *testing.T) {
+	w := newWorld(1, 2, 1)
+	for i := 0; i < N; i++ {
+		w.Tick()
+	}
+}
+
+func TestRunTwoRealmOneSchedd(t *testing.T) {
+	w := newWorld(1, 1, 2)
+	for i := 0; i < N; i++ {
+		w.Tick()
+	}
+}
+
+func TestRunTwoRealmTwoProcq(t *testing.T) {
+	w := newWorld(2, 1, 2)
+	for i := 0; i < N; i++ {
+		w.Tick()
+	}
+}
+
+func TestRunTwoAll(t *testing.T) {
+	w := newWorld(2, 2, 2)
+	for i := 0; i < N; i++ {
 		w.Tick()
 	}
 }

49702121b5d280a7b11aaaa4c4cc8320e3979001
Ariel Szekely <arielck@mit.edu>
2023-12-05 11:43:46 -0500

    Check in script to generate proc start latency breakdown graph

--- benchmarks/scripts/graph/start-latency-breakdown.py
+++ benchmarks/scripts/graph/start-latency-breakdown.py
@@ -12,29 +12,27 @@ import durationpy
 def graph_data(out):
   fig, ax = plt.subplots(figsize=(6.4, 2.4))
 #  ax.set_ylim(bottom=0)
-  ax.set_yscale("log")
   ax.set_ylabel("Start Latency (ms)")
 
-  sys = [ "σOS", "σOS-ux", "AWS λ", "Docker", "K8s", "Mitosis", "FAASM", "Ray", ]
-  cold = [  223,     41.5,  1289.6,   2671.4,  1143,       3.1,     8.8,  25.5, ]
-  warm = [  1.9,      1.9,    45.9,    469.1,   217,       3.1,     0.3,   0.6, ]
+  config   = [ "σOS-Docker", "σOS-gvisor" ]
+  sched    = np.array([        0.395,        0.395 ])
+  ns_exec  = np.array([        0.269,        1.727 ])
+  fs_jail  = np.array([        0.402,        1.735 ])
+  seccomp  = np.array([        0.811,        2.200 ])
+  apparmor = np.array([        0.023,        0.009 ])
+#  bin_dl   = [      169.298,      169.298 ]
 
-  assert(len(sys) == len(cold))
-  assert(len(sys) == len(warm))
+  x = np.arange(len(config))
 
-  width = 0.35
-  xticks = np.arange(len(sys))
-  coldx = [ x for x in xticks ]
-  warmx = [ x + width for x in xticks ]
-  coldplot = plt.bar(coldx, cold, width=width, label="Cold-start")
-  for i, v in enumerate(cold):
-    plt.text(xticks[i], v + .25, str(v), ha="center")
-  warmplot = plt.bar(warmx, warm, width=width, label="Warm-start")
-  for i, v in enumerate(warm):
-    plt.text(xticks[i] + width, v + .25, str(v), ha="center")
-  plt.xticks(xticks + width / 2.0, sys)
+  sched_plt    = plt.bar(x,    sched, label="σOS scheduling")
+  ns_exec_plt  = plt.bar(x,  ns_exec, bottom=sched, label="Linux NS + exec")
+  fs_jail_plt  = plt.bar(x,  fs_jail, bottom=sched+ns_exec, label="FS jail")
+  seccomp_plt  = plt.bar(x,  seccomp, bottom=sched+ns_exec+fs_jail, label="Seccomp")
+  apparmor_plt = plt.bar(x, apparmor, bottom=sched+ns_exec+fs_jail+seccomp, label="AppArmor")
 
-  ax.legend(loc="upper right")
+  plt.xticks(x, config)
+
+  ax.legend(loc="upper left")
   fig.savefig(out)
 
 if __name__ == "__main__":

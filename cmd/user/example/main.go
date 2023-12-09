9283690c4d2764e93b6f00c26375d47a5c84496f
Frans Kaashoek <kaashoek@mit.edu>
2023-12-09 08:17:18 -0500

    Make sure example proc works without ckpt

--- cmd/user/example/main.go
+++ cmd/user/example/main.go
@@ -1,54 +1,49 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"strconv"
 	"time"
+
+	db "sigmaos/debug"
+	"sigmaos/proc"
+	"sigmaos/sigmaclnt"
 )
 
 func main() {
+	if len(os.Args) < 3 {
+		fmt.Fprintf(os.Stderr, "Usage: %v <n> <sleep_length>\n", os.Args[0])
+		os.Exit(1)
+	}
 
-	// sigmaclnt.NewSigmaClntTestVersion(proc.GetProcEnv())
-	// if err != nil {
-	// sc.ClntExit(proc.NewStatus(proc.StatusErr))
-	// }
-	// err = sc.Started()
-	// if err != nil {
-	// sc.ClntExit(proc.NewStatus(proc.StatusErr))
-	// }
+	n, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		db.DFatalf("strconv err %v", err)
+	}
 
-	timer := time.NewTicker(5 * time.Second)
+	d, err := time.ParseDuration(os.Args[2])
+	if err != nil {
+		db.DFatalf("Error parsing duration: %v", err)
+	}
 
-	<-timer.C
+	db.DPrintf(db.ALWAYS, "Running %v", os.Args)
 
-	// sc.ClntExit(proc.NewStatus(proc.StatusOK))
+	sc, err := sigmaclnt.NewSigmaClnt(proc.GetProcEnv())
+	if err != nil {
+		db.DFatalf("example %v", err)
+	}
 
-	// ---------------
+	db.DPrintf(db.ALWAYS, "Set started")
 
-	// sc, err := sigmaclnt.NewSigmaClnt(proc.GetProcEnv())
-	// if err != nil {
-	// 	db.DFatalf("NewSigmaClnt: error %v\n", err)
-	// }
-	// err = sc.Started()
-	// if err != nil {
-	// 	db.DFatalf("Started: error %v\n", err)
-	// }
+	if err := sc.Started(); err != nil {
+		db.DFatalf("Started err %v", err)
+	}
 
-	// timer := time.NewTicker(5 * time.Second)
+	for i := 1; i < n; i++ {
+		db.DPrintf(db.ALWAYS, "Running ..")
+		time.Sleep(d)
+	}
 
-	// testDir := sp.S3 + "~any/hmngtestbucket/"
-	// filePath := testDir + "example-out.txt"
-	// dstFd, err := sc.Create(filePath, 0777, sp.OWRITE)
-
-	// for {
-	// 	select {
-	// 	case <-timer.C:
-	// 		sc.Write(dstFd, []byte("exiting"))
-	// 		err = sc.Close(dstFd)
-	// 		sc.ClntExitOK()
-	// 		return
-	// 	default:
-	// 		fmt.Println("here sleep")
-	// 		sc.Write(dstFd, []byte("here sleep"))
-	// 		time.Sleep(2 * time.Second)
-	// 	}
-	// }
+	sc.ClntExitOK()
 }
--- example/example_test.go
+++ example/example_test.go
@@ -66,7 +66,7 @@ func TestExerciseS3(t *testing.T) {
 	dir := sp.S3
 	ts := test.NewTstatePath(t, dir)
 
-	testDir := sp.S3 + "~any/hmngtestbucket/"
+	testDir := sp.S3 + "~local/fkaashoek/"
 
 	ts.MkDir(testDir, 0777)
 	log.Printf("created dir: %v\n", testDir)
@@ -110,13 +110,32 @@ func TestExerciseProc(t *testing.T) {
 	ts := test.NewTstateAll(t)
 
 	log.Printf("starting")
-	chkptProc := proc.NewProc("example", []string{})
-
+	chkptProc := proc.NewProc("example", []string{"1", "1s"})
 	err := ts.Spawn(chkptProc)
 	assert.Nil(t, err)
-	// err = ts.WaitStart(chkptProc.GetPid())
-	// log.Printf("started")
-	// assert.Nil(t, err)
+	err = ts.WaitStart(chkptProc.GetPid())
+	assert.Nil(t, err)
+
+	log.Printf("started")
+
+	status, err := ts.WaitExit(chkptProc.GetPid())
+	assert.Nil(t, err)
+	assert.True(t, status.IsStatusOK())
+
+	ts.Shutdown()
+}
+
+func TestExerciseProcCkpt(t *testing.T) {
+	ts := test.NewTstateAll(t)
+
+	log.Printf("starting")
+	chkptProc := proc.NewProc("example", []string{"10", "1s"})
+	err := ts.Spawn(chkptProc)
+	assert.Nil(t, err)
+	err = ts.WaitStart(chkptProc.GetPid())
+	assert.Nil(t, err)
+
+	log.Printf("started")
 
 	// let her run for a sec
 	time.Sleep(3 * time.Second)
@@ -134,6 +153,7 @@ func TestExerciseProc(t *testing.T) {
 	time.Sleep(5 * time.Second)
 
 	log.Printf("restoring")
+
 	chkptLocList := strings.Split(chkptLoc, "/")
 	sigmaPid := chkptLocList[len(chkptLocList)-2]
 	restProc := proc.MakeRestoreProc(chkptLoc, osPid, sigmaPid)
@@ -141,9 +161,6 @@ func TestExerciseProc(t *testing.T) {
 	// spawn and run it
 	err = ts.Spawn(restProc)
 	assert.Nil(t, err)
-	// err = ts.WaitStart(restProc.GetPid())
-	// log.Printf("started")
-	// assert.Nil(t, err)
 
 	status, err := ts.WaitExit(restProc.GetPid())
 	assert.Nil(t, err)
@@ -158,7 +175,7 @@ func TestExerciseRestore(t *testing.T) {
 	log.Printf("starting")
 	// gotten from returned values from checkpointing
 	osPid := 18
-	chkptLoc := "name/s3/~any/sigmaoscheckpoint/example-3592d035307de6d7/"
+	chkptLoc := "name/s3/~any/fkaashoek/example-3592d035307de6d7/"
 
 	// make restore proc
 	// TODO make this be perf?
@@ -189,20 +206,20 @@ func TestExerciseOut(t *testing.T) {
 	// testDir := sp.S3 + "~any/hmngtestbucket/"
 	// fileName := testDir + "example-out.txt"
 
-	// fileName := "name/s3/~any/sigmaoscheckpoint/example-558436b294fad070/mountpoints-12.img"
+	// fileName := "name/s3/~any/fkaashoek/example-558436b294fad070/mountpoints-12.img"
 	// w/ everything + tramp:
-	// fileName := "name/s3/~any/sigmaoscheckpoint/example-3d4aaf938bf06900/fdinfo-2.img"
+	// fileName := "name/s3/~any/fkaashoek/example-3d4aaf938bf06900/fdinfo-2.img"
 	// w/ everything, no tramp:
-	// fileName := "name/s3/~any/sigmaoscheckpoint/example-8cbcc31036d18cfc/fdinfo-2.img"
+	// fileName := "name/s3/~any/fkaashoek/example-8cbcc31036d18cfc/fdinfo-2.img"
 	// w/ fslib, but no pathclnt
-	// fileName := "name/s3/~any/sigmaoscheckpoint/example-3dbdf5ba54df88ab/fdinfo-2.img"
+	// fileName := "name/s3/~any/fkaashoek/example-3dbdf5ba54df88ab/fdinfo-2.img"
 
 	// w/ nilled out sc
-	// fileName := "name/s3/~any/sigmaoscheckpoint/example-cd3bca6a83315115/mm-18.img"
+	// fileName := "name/s3/~any/fkaashoek/example-cd3bca6a83315115/mm-18.img"
 
-	fileName := "name/s3/~any/sigmaoscheckpoint/example-cc3e923ed91b0af5/mm-18.img"
+	fileName := "name/s3/~any/fkaashoek/example-cc3e923ed91b0af5/mm-18.img"
 
-	// fileName := "name/s3/~any/sigmaoscheckpoint/example-800a8b07b7aad4df/restore.log"
+	// fileName := "name/s3/~any/fkaashoek/example-800a8b07b7aad4df/restore.log"
 
 	// chkptDir := "name/s3/~any/hmngtestbucket/"
 

53045f031a1ff52d95b631b13d68418f4b2ab6a5
Frans Kaashoek <kaashoek@mit.edu>
2023-12-17 10:21:31 -0500

    Document API better by including names for arguments

--- sigmaos/sigmaos.go
+++ sigmaos/sigmaos.go
@@ -10,45 +10,45 @@ type Watch func(string, error)
 type SigmaOS interface {
 	// Core interface
 
-	Close(int) error
-	Stat(string) (*sp.Stat, error)
-	Create(string, sp.Tperm, sp.Tmode) (int, error)
-	Open(string, sp.Tmode) (int, error)
-	Rename(string, string) error
-	Remove(string) error
-	GetFile(string) ([]byte, error)
-	PutFile(string, sp.Tperm, sp.Tmode, []byte, sp.Toffset, sp.TleaseId) (sp.Tsize, error)
-	Read(int, sp.Tsize) ([]byte, error)
-	Write(int, []byte) (sp.Tsize, error)
-	Seek(int, sp.Toffset) error
+	Close(fd int) error
+	Stat(path string) (*sp.Stat, error)
+	Create(path string, p sp.Tperm, m sp.Tmode) (int, error)
+	Open(path string, m sp.Tmode) (int, error)
+	Rename(srcpath string, dstpath string) error
+	Remove(path string) error
+	GetFile(path string) ([]byte, error)
+	PutFile(path string, p sp.Tperm, m sp.Tmode, d []byte, o sp.Toffset, l sp.TleaseId) (sp.Tsize, error)
+	Read(fd int, sz sp.Tsize) ([]byte, error)
+	Write(fd int, d []byte) (sp.Tsize, error)
+	Seek(fd int, o sp.Toffset) error
 
 	// Ephemeral
-	CreateEphemeral(string, sp.Tperm, sp.Tmode, sp.TleaseId, sp.Tfence) (int, error)
+	CreateEphemeral(path string, p sp.Tperm, m sp.Tmode, l sp.TleaseId, f sp.Tfence) (int, error)
 	ClntId() sp.TclntId
 
 	// Fences
-	FenceDir(string, sp.Tfence) error
-	WriteFence(int, []byte, sp.Tfence) (sp.Tsize, error)
+	FenceDir(path string, f sp.Tfence) error
+	WriteFence(fd int, d []byte, f sp.Tfence) (sp.Tsize, error)
 
 	// RPC
-	WriteRead(int, []byte) ([]byte, error)
+	WriteRead(fd int, d []byte) ([]byte, error)
 
 	// Watches
-	OpenWatch(string, sp.Tmode, Watch) (int, error)
-	SetDirWatch(int, string, Watch) error
-	SetRemoveWatch(string, Watch) error
+	OpenWatch(path string, m sp.Tmode, w Watch) (int, error)
+	SetDirWatch(fd int, dir string, w Watch) error
+	SetRemoveWatch(path string, w Watch) error
 
 	// Mounting
-	MountTree(sp.Taddrs, string, string) error
-	IsLocalMount(sp.Tmount) bool
-	SetLocalMount(*sp.Tmount, string)
-	PathLastMount(string) (path.Path, path.Path, error)
+	MountTree(addrs sp.Taddrs, tree, mount string) error
+	IsLocalMount(mnt sp.Tmount) bool
+	SetLocalMount(mnt *sp.Tmount, port string)
+	PathLastMount(path string) (path.Path, path.Path, error)
 	GetNamedMount() sp.Tmount
-	NewRootMount(string, string) error
+	NewRootMount(path string, mntname string) error
 	Mounts() []string
 
 	// Debugging
 	DetachAll() error
-	Detach(string) error
-	Disconnect(string) error
+	Detach(path string) error
+	Disconnect(path string) error
 }

37a6afd65c3517f8d9ca1e554f4fb55cac6bcf96
Frans Kaashoek <kaashoek@mit.edu>
2024-01-08 11:07:24 -0500

    x

--- protsrv/protsrv.go
+++ protsrv/protsrv.go
@@ -69,7 +69,7 @@ func (ps *ProtSrv) Auth(args *sp.Tauth, rets *sp.Rauth) *sp.Rerror {
 }
 
 func (ps *ProtSrv) Attach(args *sp.Tattach, rets *sp.Rattach, attach sps.AttachClntF) (sp.TclntId, *sp.Rerror) {
-	db.DPrintf(db.PROTSRV, "Attach %v sid %v", args, ps.sid)
+	db.DPrintf(db.PROTSRV, "Attach %v cid %v sid %v", args, args.TclntId(), ps.sid)
 	p := path.Split(args.Aname)
 	root, ctx := ps.ssrv.GetRootCtx(args.Tuname(), args.Aname, ps.sid, args.TclntId())
 	tree := root.(fs.FsObj)

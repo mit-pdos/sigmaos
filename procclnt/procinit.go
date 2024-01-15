da0eb07a62c30d0695870b38b62ae07552aed7ff
Frans Kaashoek <kaashoek@mit.edu>
2024-01-14 19:47:05 -0500

    x

--- procclnt/procinit.go
+++ procclnt/procinit.go
@@ -2,7 +2,6 @@ package procclnt
 
 import (
 	"path"
-	"runtime/debug"
 	"time"
 
 	db "sigmaos/debug"
@@ -36,21 +35,28 @@ func NewProcClnt(fsl *fslib.FsLib) *ProcClnt {
 // Fake an initial process for, for example, tests.
 // XXX deduplicate with Spawn()
 // XXX deduplicate with NewProcClnt()
-func NewProcClntInit(pid sp.Tpid, fsl *fslib.FsLib, program string) *ProcClnt {
+func NewProcClntInit(pid sp.Tpid, fsl *fslib.FsLib, program string) (*ProcClnt, error) {
 	MountPids(fsl)
 	// XXX needed?
 	db.DPrintf(db.PROCCLNT, "Mount %v as %v", sp.SCHEDDREL, sp.SCHEDDREL)
 	if err := fsl.NewRootMount(sp.SCHEDDREL, sp.SCHEDDREL); err != nil {
-		debug.PrintStack()
-		db.DFatalf("error mounting schedd err %v\n", err)
+		db.DPrintf(db.ALWAYS, "Error mounting schedd err %v\n", err)
+		return nil, err
 	}
 	db.DPrintf(db.PROCCLNT, "Mount %v as %v", fsl.ProcEnv().ProcDir, proc.PROCDIR)
 	if err := fsl.NewRootMount(fsl.ProcEnv().ProcDir, proc.PROCDIR); err != nil {
-		db.DFatalf("Error mounting procdir: %v", err)
+		db.DPrintf(db.ALWAYS, "Error mounting procdir: %v", err)
+		return nil, err
 	}
 	clnt := newProcClnt(fsl, pid, true)
-	clnt.MakeProcDir(pid, fsl.ProcEnv().ProcDir, false, proc.HSCHEDD)
-	return clnt
+	if err := clnt.MakeProcDir(pid, fsl.ProcEnv().ProcDir, false, proc.HSCHEDD); err != nil {
+		db.DPrintf(db.ALWAYS, "NewProcClntInit: MakeProcDir err %v", err)
+		// ignore error; the initial process may make several fslibs,
+		// which each need to add mount point, but already has created
+		// ProcDir.
+		return clnt, nil
+	}
+	return clnt, nil
 }
 
 func MountPids(fsl *fslib.FsLib) error {

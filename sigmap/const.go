8e93bfdaf36dc3686febba89fc3c7f30038f5f93
Ariel Szekely <arielck@mit.edu>
2024-01-15 08:20:45 -0500

    Make local build tag a sigmap const

--- sigmap/const.go
+++ sigmap/const.go
@@ -1,8 +1,9 @@
 package sigmap
 
 const (
-	KBYTE = 1 << 10
-	MBYTE = 1 << 20
-	GBYTE = 1 << 30
-	BUFSZ = 4 * MBYTE
+	KBYTE       = 1 << 10
+	MBYTE       = 1 << 20
+	GBYTE       = 1 << 30
+	BUFSZ       = 4 * MBYTE
+	LOCAL_BUILD = "local-build"
 )
--- test/test.go
+++ test/test.go
@@ -41,7 +41,7 @@ var useSigmaclntd bool
 
 func init() {
 	flag.StringVar(&EtcdIP, "etcdIP", "127.0.0.1", "Etcd IP")
-	flag.StringVar(&tag, "tag", "local-build", "Docker image tag")
+	flag.StringVar(&tag, "tag", sp.LOCAL_BUILD, "Docker image tag")
 	flag.BoolVar(&Start, "start", false, "Start system")
 	flag.BoolVar(&noShutdown, "no-shutdown", false, "Don't shut down the system")
 	flag.BoolVar(&Overlays, "overlays", false, "Overlays")

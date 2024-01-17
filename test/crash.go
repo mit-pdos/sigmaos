209dfc945a7eab700e063174d640b0f7f5930bad
Ariel Szekely <arielck@mit.edu>
2024-01-16 20:46:43 -0500

    MOre debug aroundcrashes for test

--- test/crash.go
+++ test/crash.go
@@ -17,7 +17,9 @@ func (ts *Tstate) CrashServer(srv string, randMax int, l *sync.Mutex, crashchan 
 	// Make sure not too many crashes happen at once by taking a lock (we always
 	// want >= 1 server to be up).
 	l.Lock()
+	db.DPrintf(db.ALWAYS, "Booting a node Before crashing a %v.", srv)
 	err := ts.BootNode(1)
+	db.DPrintf(db.ALWAYS, "Done booting a node before crashing a %v.", srv)
 	if err != nil {
 		db.DFatalf("Error BootNode %v", srv)
 	}
@@ -26,6 +28,7 @@ func (ts *Tstate) CrashServer(srv string, randMax int, l *sync.Mutex, crashchan 
 	if err != nil {
 		db.DFatalf("Error non-nil kill %v: %v", srv, err)
 	}
+	db.DPrintf(db.ALWAYS, "Done Kill one %v", srv)
 	l.Unlock()
 	crashchan <- true
 }

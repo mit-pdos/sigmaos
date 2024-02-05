772e22128613184ca7163b4e1bdaade052b9c3c3
Frans Kaashoek <kaashoek@mit.edu>
2024-02-05 08:07:00 -0500

    Delete queue, since dmxclnt has this info too (if needed again)

--- sessclnt/sessclnt.go
+++ sessclnt/sessclnt.go
@@ -1,6 +1,7 @@
 // The sessclnt package establishes a session with a server using a
 // [netclnt], which sets up a TCP connection.  If [netclnt] fails,
-// sessclnt may try to re-establish the connection.
+// sessclnt could re-establish a new [netclnt] (but no longer
+// supported for now).
 package sessclnt
 
 import (
@@ -26,7 +27,6 @@ type SessClnt struct {
 	closed  bool
 	addrs   sp.Taddrs
 	nc      *netclnt.NetClnt
-	queue   *RequestQueue
 	clntnet string
 }
 
@@ -34,7 +34,6 @@ func newSessClnt(clntnet string, addrs sp.Taddrs, rf demux.ReadCallF, wf demux.W
 	c := &SessClnt{sid: sessp.Tsession(rand.Uint64()),
 		clntnet: clntnet,
 		addrs:   addrs,
-		queue:   NewRequestQueue(addrs),
 	}
 	db.DPrintf(db.SESSCLNT, "Make session %v to srvs %v", c.sid, addrs)
 	nc, err := netclnt.NewNetClnt(clntnet, addrs, spcodec.ReadCall, spcodec.WriteCall, c)
@@ -71,7 +70,8 @@ func (c *SessClnt) IsConnected() bool {
 }
 
 // XXX if unreachable, nothing to be done (netconn is closed), but if
-// marshaling error, close conn?
+// marshaling error, close conn?  If we want to support reconnect, we
+// can get outstanding requests from dmxclnt.
 func (c *SessClnt) ReportError(err error) {
 	db.DPrintf(db.SESSCLNT, "Netclnt sess %v reports err %v\n", c.sid, err)
 }
@@ -90,27 +90,12 @@ func (c *SessClnt) RPC(req sessp.Tmsg, data []byte) (*sessp.FcallMsg, *serr.Err)
 	return rep.(*sessp.FcallMsg), nil
 }
 
+// For supporting reconnect
 func (c *SessClnt) sendHeartbeat() {
 	_, err := c.RPC(sp.NewTheartbeat(map[uint64]bool{uint64(c.sid): true}), nil)
 	if err != nil {
 		db.DPrintf(db.SESSCLNT_ERR, "%v heartbeat %v err %v", c.sid, c.addrs, err)
 	}
-}
-
-// Clear the connection, reset the request queue, and enqueue a heartbeat to
-// re-establish a connection with the replica group if possible.
-func (c *SessClnt) Reset() {
-	c.Lock()
-	defer c.Unlock()
-
-	if c.nc != nil {
-		c.nc = nil
-	}
-	// Reset outstanding request queue.
-	db.DPrintf(db.SESSCLNT, "%v Reset outstanding request queue to %v", c.sid, c.addrs)
-	c.queue.Reset()
-	// Try to send a heartbeat to force a reconnect to the replica group.
-	go c.sendHeartbeat()
 }
 
 // Get a connection to the server. If it isn't possible to make a connection,
@@ -147,13 +132,8 @@ func (c *SessClnt) ownClosed() bool {
 	return true
 }
 
-// Creator of sessclnt closes session
-func (c *SessClnt) Close() error {
-	return c.close()
-}
-
 // Close the session permanently
-func (c *SessClnt) close() error {
+func (c *SessClnt) Close() error {
 	db.DPrintf(db.SESSCLNT, "%v Close session to %v %v\n", c.sid, c.addrs, c.closed)
 	if !c.ownClosed() {
 		return nil
@@ -163,11 +143,4 @@ func (c *SessClnt) close() error {
 		return nil
 	}
 	return nc.Close()
-
-	//outstanding := c.queue.Close()
-	// Kill outstanding requests.
-	//for _, rpc := range outstanding {
-	//	rpc.Abort()
-	//}
-	//return nil
 }

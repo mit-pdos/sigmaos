f63e49cb4efb63f48707d8db34b1832acf58eec1
Ariel Szekely <arielck@mit.edu>
2024-02-05 20:03:31 -0500

    Add perf selector for sigmaclntsrv

--- perf/selector.go
+++ perf/selector.go
@@ -24,15 +24,16 @@ const (
 
 // kernel procs
 const (
-	NAMED   Tselector = "NAMED"
-	KNAMED            = "KNAMED"
-	PROCD             = "PROCD"
-	S3                = "S3"
-	UX                = "UX"
-	SCHEDD            = "SCHEDD"
-	UPROCD            = "UPROCD"
-	PROCQ             = "PROCQ"
-	LCSCHED           = "LCSCHED"
+	NAMED        Tselector = "NAMED"
+	KNAMED                 = "KNAMED"
+	PROCD                  = "PROCD"
+	S3                     = "S3"
+	UX                     = "UX"
+	SCHEDD                 = "SCHEDD"
+	SIGMACLNTSRV           = "SIGMACLNTSRV"
+	UPROCD                 = "UPROCD"
+	PROCQ                  = "PROCQ"
+	LCSCHED                = "LCSCHED"
 )
 
 // libs

f5e8e6f96112cde7007ac490add81a6b724e190b
Ariel Szekely <arielck@mit.edu>
2024-02-22 15:16:14 -0500

    Add some debug to authsrv to see the principal who is asking for token verification

--- auth/auth.go
+++ auth/auth.go
@@ -16,7 +16,7 @@ type AuthSrv interface {
 	SetDelegatedProcToken(p *proc.Proc) error
 	MintToken(pc *ProcClaims) (*sp.Ttoken, error)
 	MintAndSetToken(pe *proc.ProcEnv) error
-	VerifyTokenGetClaims(signedToken *sp.Ttoken) (*ProcClaims, error)
+	VerifyTokenGetClaims(principalID sp.TprincipalID, signedToken *sp.Ttoken) (*ProcClaims, error)
 	IsAuthorized(principal *sp.Tprincipal, attachPath string) (*ProcClaims, bool, error)
 	KeyMgr
 }

d6cd56e93737db708450828e4b6fc47600be717d
Frans Kaashoek <kaashoek@mit.edu>
2024-02-24 20:50:26 -0500

    oops, add fslib/stats.go

---
+++ fslib/stats.go
@@ -0,0 +1,26 @@
+package fslib
+
+import (
+	gopath "path"
+
+	"sigmaos/rpc"
+	sp "sigmaos/sigmap"
+	"sigmaos/stats"
+)
+
+func (fsl *FsLib) ReadStats(pn string) (*stats.StatsSnapshot, error) {
+	st := &stats.StatsSnapshot{}
+	err := fsl.GetFileJson(gopath.Join("name", sp.STATSD), &st)
+	if err != nil {
+		return nil, err
+	}
+	return st, nil
+}
+
+func (fsl *FsLib) ReadRPCStats(pn string) (*rpc.RPCStatsSnapshot, error) {
+	st := &rpc.RPCStatsSnapshot{}
+	if err := fsl.GetFileJson(gopath.Join(pn, rpc.RPC, rpc.STATS), st); err != nil {
+		return nil, err
+	}
+	return st, nil
+}

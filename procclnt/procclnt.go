ca1215012aba46a455aa8e5fdb03db43a3faf9b4
Frans Kaashoek <kaashoek@mit.edu>
2024-03-30 19:48:41 -0400

    x

--- procclnt/procclnt.go
+++ procclnt/procclnt.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"os/exec"
 	"path"
-	"path/filepath"
 	"runtime/debug"
 	"sync"
 	"time"
@@ -87,15 +86,14 @@ func (clnt *ProcClnt) spawn(kernelId string, how proc.Thow, p *proc.Proc) error 
 
 	p.SetHow(how)
 
-	pn := filepath.Join(sp.CHUNKD, kernelId)
-	if kid, ok := clnt.cs.BinKernelID(p.GetProgram()); ok {
-		pn = filepath.Join(sp.CHUNKD, kid)
-		db.DPrintf(db.ALWAYS, "spawn: BinKernelID %v %v\n", p.GetProgram(), kid)
-	} else {
-		db.DPrintf(db.ALWAYS, "spawn: no BinKernelID %v; use %q\n", p.GetProgram(), pn)
-	}
+	//if kid, ok := clnt.cs.BinKernelID(p.GetProgram()); ok {
+	//	pn = filepath.Join(sp.CHUNKD, kid)
+	//	db.DPrintf(db.ALWAYS, "spawn: BinKernelID %v %v\n", p.GetProgram(), kid)
+	//} else {
+	//	db.DPrintf(db.ALWAYS, "spawn: no BinKernelID %v; use %q\n", p.GetProgram(), pn)
+	//}
 
-	p.PrependSigmaPath(pn)
+	//p.PrependSigmaPath(pn)
 
 	p.InheritParentProcEnv(clnt.ProcEnv())
 

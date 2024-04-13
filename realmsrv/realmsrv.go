d79b39750e86a746d72b2c2055645e35ef7ce0b4
Ariel Szekely <arielck@mit.edu>
2024-04-13 11:52:20 -0400

    Improve realmsrv debug

--- realmsrv/realmsrv.go
+++ realmsrv/realmsrv.go
@@ -162,7 +162,7 @@ func (rm *RealmSrv) Make(ctx fs.CtxI, req proto.MakeRequest, res *proto.MakeResu
 	defer rm.mu.Unlock()
 
 	db.DPrintf(db.REALMD, "RealmSrv.Make %v %v", req.Realm, req.Network)
-	db.DPrintf(db.REALMD, "RealmSrv.Make done %v %v", req.Realm, req.Network)
+	defer db.DPrintf(db.REALMD, "RealmSrv.Make done %v %v", req.Realm, req.Network)
 	rid := sp.Trealm(req.Realm)
 	// If realm already exists
 	if _, ok := rm.realms[rid]; ok {
@@ -301,7 +301,7 @@ func (rm *RealmSrv) Remove(ctx fs.CtxI, req proto.RemoveRequest, res *proto.Remo
 
 func (rm *RealmSrv) bootPerRealmKernelSubsystems(realm sp.Trealm, ss string, n int64) error {
 	db.DPrintf(db.REALMD, "[%v] boot per-kernel subsystems [%v] n %v", realm, ss, n)
-	db.DPrintf(db.REALMD, "[%v] boot per-kernel subsystems done [%v] n %v", realm, ss, n)
+	defer db.DPrintf(db.REALMD, "[%v] boot per-kernel subsystems done [%v] n %v", realm, ss, n)
 	kernels, err := rm.mkc.GetKernelSrvs()
 	if err != nil {
 		return err

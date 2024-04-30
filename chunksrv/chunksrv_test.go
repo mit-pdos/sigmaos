e9c0d831242bff0c629c154618c28c03ac22fd3c
Frans Kaashoek <kaashoek@mit.edu>
2024-04-30 11:46:52 -0400

    Checkpoint some scaffolding

--- chunksrv/chunksrv_test.go
+++ chunksrv/chunksrv_test.go
@@ -19,32 +19,67 @@ const (
 	PATH = "name/ux/~local/bin/user/common/"
 )
 
-func TestFetch(t *testing.T) {
-	ts, err := test.NewTstateAll(t)
-	assert.Nil(t, err)
+type Tstate struct {
+	*test.Tstate
+	ckclnt *chunkclnt.ChunkClnt
+	srvs   []string
+	paths  []string
+}
 
-	pid := ts.ProcEnv().GetPID()
+func newTstate(t *testing.T, n int) *Tstate {
+	ts := &Tstate{paths: []string{PATH}}
+	s, err := test.NewTstateAll(t)
+	assert.Nil(t, err)
+	ts.Tstate = s
+
+	ts.ckclnt = chunkclnt.NewChunkClnt(s.FsLib)
+	ts.ckclnt.UpdateChunkds()
+
+	err = s.BootNode(n)
+	assert.Nil(t, err, "Boot node: %v", err)
+	db.DPrintf(db.TEST, "Done boot node %d", n)
 
 	ckclnt := chunkclnt.NewChunkClnt(ts.FsLib)
 	ckclnt.UpdateChunkds()
-
-	srv, err := ckclnt.RandomSrv()
+	srvs, err := ckclnt.GetSrvs()
 	assert.Nil(t, err)
 
-	pn := chunksrv.PathHostKernelRealm(srv, sp.ROOTREALM)
-	os.Mkdir(pn, 0700)
+	for _, srv := range srvs {
+		pn := chunksrv.PathHostKernelRealm(srv, sp.ROOTREALM)
+		os.Mkdir(pn, 0700)
+	}
+	return ts
+}
 
-	st, err := ckclnt.GetFileStat(srv, PROG, pid, sp.ROOTREALM, []string{PATH})
+func (ts *Tstate) check(srv string, st *sp.Stat) {
+	pn := chunksrv.PathHostKernelRealm(srv, sp.ROOTREALM)
+	pn = path.Join(pn, PROG)
+	fi, err := os.Stat(pn)
+	assert.Nil(ts.T, err)
+	assert.Equal(ts.T, st.Length, uint64(fi.Size()))
+}
+
+func TestFetchOne(t *testing.T) {
+	ts := newTstate(t, 0)
+	pid := ts.ProcEnv().GetPID()
+
+	srv, err := ts.ckclnt.RandomSrv()
+	assert.Nil(t, err)
+
+	st, err := ts.ckclnt.GetFileStat(srv, PROG, pid, sp.ROOTREALM, ts.paths)
 	assert.Nil(t, err)
 	db.DPrintf(db.TEST, "st %v\n", st)
 
-	err = ckclnt.FetchBinary(srv, PROG, pid, sp.ROOTREALM, sp.Tsize(st.Length), []string{PATH})
+	err = ts.ckclnt.FetchBinary(srv, PROG, pid, sp.ROOTREALM, sp.Tsize(st.Length), ts.paths)
 	assert.Nil(t, err, "err %v", err)
 
-	pn = path.Join(pn, PROG)
-	fi, err := os.Stat(pn)
-	assert.Nil(t, err)
-	assert.Equal(t, st.Length, uint64(fi.Size()))
+	ts.check(srv, st)
+
+	ts.Shutdown()
+}
+
+func TestFetchMulti(t *testing.T) {
+	ts := newTstate(t, 2)
 
 	ts.Shutdown()
 }

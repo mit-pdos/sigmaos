8f140623601198336525d4f0ce93a5d3bc43502e
Ariel Szekely <arielck@mit.edu>
2024-05-09 15:49:33 -0400

    Fix panic in netrpoxy/net.go

--- netproxy/net.go
+++ netproxy/net.go
@@ -29,8 +29,9 @@ func ListenDirect(addr *sp.Taddr) (net.Listener, error) {
 	l, err := net.Listen("tcp", addr.IPPort())
 	if err != nil {
 		db.DPrintf(db.NETSIGMA_ERR, "Listen on addr %v: err %v", addr, err)
+	} else {
+		db.DPrintf(db.NETSIGMA, "Listen on addr %v res addr %v", addr, l.Addr())
 	}
-	db.DPrintf(db.NETSIGMA, "Listen on addr %v res addr %v", addr, l.Addr())
 	return l, err
 }
 

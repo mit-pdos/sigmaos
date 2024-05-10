fa7740f427e12e55d3546cc6023513545146b735
Frans Kaashoek <kaashoek@mit.edu>
2024-05-10 08:40:21 -0400

    Tweak prints

--- mntclnt/named.go
+++ mntclnt/named.go
@@ -3,6 +3,7 @@ package mntclnt
 import (
 	"fmt"
 	gpath "path"
+	"time"
 
 	db "sigmaos/debug"
 	"sigmaos/fsetcd"
@@ -12,22 +13,24 @@ import (
 )
 
 func (mc *MntClnt) GetNamedEndpointRealm(realm sp.Trealm) (*sp.Tendpoint, error) {
+	s := time.Now()
 	if ep, err := mc.getNamedEndpointRealm(realm); err != nil {
 		return ep, err
 	} else {
+		db.DPrintf(db.SPAWN_LAT, "GetNamedEndpointRealm %v %v %v", mc.cid, realm, time.Since(s))
 		return ep, nil
 	}
 }
 
 // Get named enpoint via netproxy or directly
 func (mc *MntClnt) getNamedEndpointRealm(realm sp.Trealm) (*sp.Tendpoint, *serr.Err) {
-	// If this mount was passed via the proc env, return it immediately.
 	if ep, ok := mc.ndMntCache.Get(realm); ok {
-		db.DPrintf(db.MOUNT, "getNamedEndpoint cached %v %v", mc.pe.GetRealm(), ep)
+		db.DPrintf(db.MOUNT, "getNamedEndpointRealm cached %v %v", realm, ep)
 		return ep, nil
 	}
 	var ep *sp.Tendpoint
 	if mc.pe.UseNetProxy {
+		// If this mount was passed via the proc env, return it immediately.
 		ep0, err := mc.npc.GetNamedEndpoint(realm)
 		if err != nil {
 			if sr, ok := serr.IsErr(err); ok {
@@ -37,7 +40,7 @@ func (mc *MntClnt) getNamedEndpointRealm(realm sp.Trealm) (*sp.Tendpoint, *serr.
 		}
 		ep = ep0
 	} else {
-		ep0, err := mc.getRootNamedEndpoint(realm)
+		ep0, err := mc.getNamedEndpointDirect(realm)
 		if err != nil {
 			return &sp.Tendpoint{}, err
 		}
@@ -45,44 +48,50 @@ func (mc *MntClnt) getNamedEndpointRealm(realm sp.Trealm) (*sp.Tendpoint, *serr.
 	}
 	// Cache the newly resolved mount
 	mc.ndMntCache.Put(realm, ep)
-	db.DPrintf(db.MOUNT, "GetNamedEndpointRealm [%v] %v", realm, ep)
+	db.DPrintf(db.MOUNT, "getNamedEndpointRealm [%v] %v", realm, ep)
 	return ep, nil
+
 }
 
 // Get named enpoint directly
-func (mc *MntClnt) getRootNamedEndpoint(realm sp.Trealm) (*sp.Tendpoint, *serr.Err) {
-	var ep *sp.Tendpoint
+func (mc *MntClnt) getNamedEndpointDirect(realm sp.Trealm) (*sp.Tendpoint, *serr.Err) {
 	// If this is the root realm, then get the root named.
 	if realm == sp.ROOTREALM {
-		ep0, err := fsetcd.GetRootNamed(mc.npc.Dial, mc.pe.GetEtcdEndpoints(), realm)
+		s := time.Now()
+		ep, err := fsetcd.GetRootNamed(mc.npc.Dial, mc.pe.GetEtcdEndpoints(), realm)
 		if err != nil {
-			db.DPrintf(db.MOUNT_ERR, "getNamedEndpoint [%v] err GetRootNamed %v", realm, ep)
+			db.DPrintf(db.MOUNT_ERR, "getNamedEndpointDirect [%v] err GetRootNamed %v", realm, ep)
 			if sr, ok := serr.IsErr(err); ok {
 				return &sp.Tendpoint{}, sr
 			}
 			return &sp.Tendpoint{}, serr.NewErrError(err)
 		}
-		ep = ep0
+		db.DPrintf(db.SPAWN_LAT, "getNamedEndpointDirect %v %v %v", mc.cid, sp.ROOTREALM, time.Since(s))
+		return ep, nil
 	} else {
 		// Otherwise, walk through the root named to find this named's mount.
+		s := time.Now()
 		if _, rest, err := mc.mnt.resolveMnt(path.Path{"root"}, true); err != nil && len(rest) >= 1 {
 			if err := mc.mountNamed(sp.ROOTREALM, "root"); err != nil {
-				db.DPrintf(db.MOUNT_ERR, "getNamedEndpoint [%v] err mounting root named %v", realm, err)
+				db.DPrintf(db.MOUNT_ERR, "getNamedEndpointDirect [%v] err mounting root named %v", realm, err)
 				return &sp.Tendpoint{}, err
 			}
 		}
+		db.DPrintf(db.SPAWN_LAT, "getNamedEndpointDirect %v mount %v %v", mc.cid, sp.ROOTREALM, time.Since(s))
+		s = time.Now()
 		pn := gpath.Join("root", sp.REALMREL, sp.REALMDREL, sp.REALMSREL, realm.String())
 		target, err := mc.pathc.GetFile(pn, mc.pe.GetPrincipal(), sp.OREAD, 0, sp.MAXGETSET, sp.NullFence())
 		if err != nil {
-			db.DPrintf(db.MOUNT_ERR, "getNamedEndpoint [%v] GetFile err %v", realm, err)
+			db.DPrintf(db.MOUNT_ERR, "getNamedEndpointDirect [%v] GetFile err %v", realm, err)
 			return &sp.Tendpoint{}, serr.NewErrError(err)
 		}
-		ep, err = sp.NewEndpointFromBytes(target)
+		ep, err := sp.NewEndpointFromBytes(target)
 		if err != nil {
 			return &sp.Tendpoint{}, serr.NewErrError(err)
 		}
+		db.DPrintf(db.SPAWN_LAT, "getNamedEndpointDirect %v getfile %v %v", mc.cid, realm, time.Since(s))
+		return ep, nil
 	}
-	return ep, nil
 }
 
 func (mc *MntClnt) mountNamed(realm sp.Trealm, name string) *serr.Err {

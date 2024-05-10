22ca51d41ec291df0301a5523b4b0c1159ae1fc5
Frans Kaashoek <kaashoek@mit.edu>
2024-05-10 08:41:00 -0400

    Time walksymlink1

--- pathclnt/symlink.go
+++ pathclnt/symlink.go
@@ -1,6 +1,8 @@
 package pathclnt
 
 import (
+	"time"
+
 	db "sigmaos/debug"
 	"sigmaos/path"
 	"sigmaos/serr"
@@ -9,6 +11,8 @@ import (
 
 func (pathc *PathClnt) walkSymlink1(fid sp.Tfid, resolved, left path.Path) (path.Path, *serr.Err) {
 	// XXX change how we readlink; getfile?
+
+	s := time.Now()
 	target, err := pathc.readlink(fid)
 	if err != nil {
 		db.DPrintf(db.WALK, "walksymlink1 %v err %v\n", fid, err)
@@ -17,7 +21,7 @@ func (pathc *PathClnt) walkSymlink1(fid sp.Tfid, resolved, left path.Path) (path
 	var p path.Path
 	ep, error := sp.NewEndpointFromBytes(target)
 	if error == nil {
-		db.DPrintf(db.WALK, "walksymlink1 %v ep %v err %v\n", fid, ep, err)
+		db.DPrintf(db.WALK, "walksymlink1 %v ep %v lat %v\n", fid, ep, time.Since(s))
 		err := pathc.mntclnt.AutoMount(pathc.FidClnt.Lookup(fid).Principal(), ep, resolved)
 		if err != nil {
 			db.DPrintf(db.WALK, "automount %v %v err %v\n", resolved, ep, err)

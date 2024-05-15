59d90f86ec4fcd6c73765db14fabd77b0a160263
Ariel Szekely <arielck@mit.edu>
2024-05-15 11:36:14 -0400

    x

--- demux/dmxclnt.go
+++ demux/dmxclnt.go
@@ -37,7 +37,7 @@ func (dmx *DemuxClnt) reply(tag sessp.Ttag, rep CallI, err *serr.Err) {
 	if ch, ok := dmx.callmap.remove(tag); ok {
 		ch <- reply{rep, err}
 	} else {
-		db.DFatalf("reply %v no matching req %v\n", rep, tag)
+		db.DFatalf("reply %v no matching req %v", rep, tag)
 	}
 }
 
@@ -45,7 +45,7 @@ func (dmx *DemuxClnt) reader() {
 	for {
 		c, err := dmx.trans.ReadCall()
 		if err != nil {
-			db.DPrintf(db.DEMUXCLNT_ERR, "reader rf err %v\n", err)
+			db.DPrintf(db.DEMUXCLNT_ERR, "reader rf err %v", err)
 			dmx.callmap.close()
 			break
 		}
@@ -63,11 +63,11 @@ func (dmx *DemuxClnt) reader() {
 func (dmx *DemuxClnt) SendReceive(req CallI, outiov sessp.IoVec) (CallI, *serr.Err) {
 	ch := make(chan reply)
 	if err := dmx.callmap.put(req.Tag(), ch); err != nil {
-		db.DPrintf(db.DEMUXCLNT_ERR, "SendReceive: enqueue req %v err %v\n", req, err)
+		db.DPrintf(db.DEMUXCLNT_ERR, "SendReceive: enqueue req %v err %v", req, err)
 		return nil, err
 	}
 	if err := dmx.iovm.Put(req.Tag(), outiov); err != nil {
-		db.DPrintf(db.DEMUXCLNT_ERR, "SendReceive: iovm enqueue req %v err %v\n", req, err)
+		db.DPrintf(db.DEMUXCLNT_ERR, "SendReceive: iovm enqueue req %v err %v", req, err)
 		return nil, err
 	}
 	dmx.mu.Lock()

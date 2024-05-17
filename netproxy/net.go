7e4393f447e680c0fd511ec332b3a70afe97678b
Ariel Szekely <arielck@mit.edu>
2024-05-17 19:52:31 -0400

    Handle failed dials/accepts cleanly when sending preamble

--- netproxy/net.go
+++ netproxy/net.go
@@ -2,6 +2,7 @@ package netproxy
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"sync/atomic"
 
@@ -9,6 +10,10 @@ import (
 	db "sigmaos/debug"
 	"sigmaos/netsigma"
 	sp "sigmaos/sigmap"
+)
+
+const (
+	INIT_MSG = "HELLO"
 )
 
 type Tlid uint64
@@ -23,9 +28,14 @@ func DialDirect(ep *sp.Tendpoint) (net.Conn, error) {
 		db.DPrintf(db.NETSIGMA_ERR, "Dial direct addr err %v: err %v", ep.Addrs()[0], err)
 	} else {
 		db.DPrintf(db.NETSIGMA, "Dial direct addr ok %v", ep.Addrs()[0])
-	}
-	if ep.Type() == sp.INTERNAL_EP {
-		// TODO: send principal
+		if ep.Type() == sp.INTERNAL_EP {
+			// TODO: send principal
+			n, err := c.Write([]byte(INIT_MSG))
+			if err != nil || n != len(INIT_MSG) {
+				db.DPrintf(db.NETSIGMA_ERR, "Dial direct addr err %v: err %v", ep.Addrs()[0], err)
+				return nil, fmt.Errorf("Error send principal preamble: n %v err %v", n, err)
+			}
+		}
 	}
 	return c, err
 }
@@ -46,8 +56,19 @@ func AcceptDirect(l net.Listener) (net.Conn, error) {
 		db.DPrintf(db.NETSIGMA_ERR, "Accept on %v err: %v", l.Addr(), err)
 	} else {
 		db.DPrintf(db.NETSIGMA, "Accept on %v ok local addr: %v", l.Addr(), c.LocalAddr())
+		// TODO: optionally, don't read principal
+		if true {
+			// TODO: read principal
+			b := make([]byte, len(INIT_MSG))
+			n, err := io.ReadFull(c, b)
+			if err != nil || n != len(INIT_MSG) {
+				if string(b) != INIT_MSG {
+					db.DFatalf("Unexpected init message: %v", string(b))
+				}
+				return nil, fmt.Errorf("Error read principal preamble: n %v err %v", n, err)
+			}
+		}
 	}
-	// TODO: read principal
 	return c, err
 }
 

ce78d8aad1e39dd13614d7eb132123f413768228
Ariel Szekely <arielck@mit.edu>
2024-05-22 14:36:00 -0400

    Improve protsrv debug

--- protsrv/protsrv.go
+++ protsrv/protsrv.go
@@ -36,7 +36,7 @@ func NewProtServer(pss *ProtSrvState, p *sp.Tprincipal, sid sessp.Tsession, grf 
 		sid:          sid,
 		getRootCtx:   grf,
 	}
-	db.DPrintf(db.PROTSRV, "NewProtSrv -> %v", ps)
+	db.DPrintf(db.PROTSRV, "NewProtSrv[%v] -> %v", p, ps)
 	return ps
 }
 
@@ -591,6 +591,7 @@ func (ps *ProtSrv) lookupPathOpen(f *fid.Fid, dir fs.Dir, name string, mode sp.T
 
 // Create file or open file, and write data to it
 func (ps *ProtSrv) PutFile(args *sp.Tputfile, data []byte, rets *sp.Rwrite) *sp.Rerror {
+	db.DPrintf(db.PROTSRV, "%v: PutFile start args {%v}", ps.sid, args)
 	if sp.Tsize(len(data)) > sp.MAXGETSET {
 		return sp.NewRerrorSerr(serr.NewErr(serr.TErrInval, "too large"))
 	}

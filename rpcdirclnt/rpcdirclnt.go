72614dcf3ecb309cc5108673e14f7df0fa1e6798
Frans Kaashoek <kaashoek@mit.edu>
2024-06-16 02:48:20 -0400

    Rename newEntry to newClnt

--- rpcdirclnt/rpcdirclnt.go
+++ rpcdirclnt/rpcdirclnt.go
@@ -2,6 +2,7 @@ package rpcdirclnt
 
 import (
 	"path/filepath"
+	"time"
 
 	db "sigmaos/debug"
 	"sigmaos/dircache"
@@ -14,25 +15,28 @@ type RPCDirClnt struct {
 	*dircache.DirCache[*rpcclnt.RPCClnt]
 }
 
-func (rpcdc *RPCDirClnt) newEntry(n string) (*rpcclnt.RPCClnt, error) {
-	rpcc, err := sigmarpcchan.NewSigmaRPCClnt([]*fslib.FsLib{rpcdc.FsLib}, filepath.Join(rpcdc.Path, n))
+func (rpcdc *RPCDirClnt) newClnt(n string) (*rpcclnt.RPCClnt, error) {
+	s := time.Now()
+	pn := filepath.Join(rpcdc.Path, n)
+	rpcc, err := sigmarpcchan.NewSigmaRPCClnt([]*fslib.FsLib{rpcdc.FsLib}, pn)
 	if err != nil {
-		db.DPrintf(rpcdc.ESelector, "Error NewSigmaRPCClnt[srvID:%v]: %v", n, err)
+		db.DPrintf(rpcdc.ESelector, "Error NewSigmaRPCClnt[srvID:%v]: %v", pn, err)
 		return nil, err
 	}
-	db.DPrintf(rpcdc.LSelector, "newEntry NewSigmaRPCClnt[srvID:%v]: %v", n, rpcc)
+	db.DPrintf(rpcdc.LSelector, "newClnt NewSigmaRPCClnt[srvID:%v]: %v", pn, rpcc)
+	db.DPrintf(db.SPAWN_LAT, "newClnt %v lat %v", pn, time.Since(s))
 	return rpcc, nil
 }
 
 func NewRPCDirClnt(fsl *fslib.FsLib, path string, lSelector db.Tselector, eSelector db.Tselector) *RPCDirClnt {
 	u := &RPCDirClnt{}
-	u.DirCache = dircache.NewDirCache[*rpcclnt.RPCClnt](fsl, path, u.newEntry, lSelector, eSelector)
+	u.DirCache = dircache.NewDirCache[*rpcclnt.RPCClnt](fsl, path, u.newClnt, lSelector, eSelector)
 	return u
 }
 
 func NewRPCDirClntFilter(fsl *fslib.FsLib, path string, lSelector db.Tselector, eSelector db.Tselector, filter string) *RPCDirClnt {
 	u := &RPCDirClnt{}
-	u.DirCache = dircache.NewDirCacheFilter[*rpcclnt.RPCClnt](fsl, path, u.newEntry, lSelector, eSelector, filter)
+	u.DirCache = dircache.NewDirCacheFilter[*rpcclnt.RPCClnt](fsl, path, u.newClnt, lSelector, eSelector, filter)
 	return u
 }
 

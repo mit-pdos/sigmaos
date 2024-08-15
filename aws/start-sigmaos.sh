b577b3c1210c197edf9c47b464f4265659d1ed43
Ariel Szekely <arielck@mit.edu>
2024-08-14 21:59:14 -0400

    Copy jpg8 to ux for imgresize bench

--- aws/start-sigmaos.sh
+++ aws/start-sigmaos.sh
@@ -185,7 +185,7 @@ for vm in $vms; do
 #    docker cp ~/1.jpg ${KERNELID}:/home/sigmaos/1.jpg
 #    docker cp ~/6.jpg ${KERNELID}:/home/sigmaos/6.jpg
 #    docker cp ~/7.jpg ${KERNELID}:/home/sigmaos/7.jpg
-#    docker cp ~/8.jpg ${KERNELID}:/home/sigmaos/8.jpg
+    docker cp ~/8.jpg ${KERNELID}:/home/sigmaos/8.jpg
   fi
 ENDSSH
  if [ "${vm}" = "${MAIN}" ]; then

93c1684a76960e6f84c0cf07d906efdaad7bb069
Ariel Szekely <arielck@mit.edu>
2024-08-16 17:04:29 -0400

    Make dbsrv accessible from all realms

--- dbsrv/db.go
+++ dbsrv/db.go
@@ -32,6 +32,7 @@ func RunDbd(dbdaddr string) error {
 		db.DFatalf("Error NewSigmaClnt: %v", err)
 		return err
 	}
+	sc.GetNetProxyClnt().AllowConnectionsFromAllRealms()
 	ssrv, err := sigmasrv.NewSigmaSrvClnt(filepath.Join(sp.DB, pe.GetKernelID()), sc, s)
 	if err != nil {
 		return err

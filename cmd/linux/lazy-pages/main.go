e841031f5c8fd423b839168efebb36ff42e56097
Frans Kaashoek <kaashoek@mit.edu>
2024-08-17 15:11:10 -0400

    Checkpoint WIP on lazy-pages daemon in golang

---
+++ cmd/linux/lazy-pages/main.go
@@ -0,0 +1,341 @@
+package main
+
+import (
+	"encoding/binary"
+	// "encoding/json"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"strconv"
+	"syscall"
+	"unsafe"
+
+	"github.com/checkpoint-restore/go-criu/v7/crit"
+	"github.com/checkpoint-restore/go-criu/v7/crit/cli"
+	"github.com/checkpoint-restore/go-criu/v7/crit/images/mm"
+	"github.com/checkpoint-restore/go-criu/v7/crit/images/pagemap"
+	"github.com/loopholelabs/userfaultfd-go/pkg/constants"
+	"golang.org/x/sys/unix"
+
+	db "sigmaos/debug"
+)
+
+const SOCKNAME = "lazy-pages.socket"
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %v <image-dir>\n", os.Args[0])
+		os.Exit(1)
+	}
+	db.DPrintf(db.ALWAYS, "lazy-pages: %v", os.Args[1])
+
+	sn := filepath.Join(os.Args[1], SOCKNAME)
+	socket, err := net.Listen("unix", sn)
+	if err != nil {
+		db.DFatalf("Listen: err %v", err)
+	}
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		os.Remove(sn)
+		os.Exit(1)
+	}()
+
+	for {
+		conn, err := socket.Accept()
+		if err != nil {
+			db.DFatalf("Accept: err %v", err)
+		}
+		go func(conn net.Conn) {
+			defer conn.Close()
+
+			connFd, err := getConnFd(conn.(*net.UnixConn))
+			if err != nil {
+				db.DFatalf("getConnFd err %v", err)
+			}
+
+			var pid int32
+			if err := binary.Read(conn, binary.NativeEndian, &pid); err != nil {
+				db.DFatalf("Read pid err %v", err)
+			}
+
+			db.DPrintf(db.ALWAYS, "pid %d\n", pid)
+
+			b := make([]byte, unix.CmsgSpace(4))
+			_, _, _, _, err = unix.Recvmsg(connFd, nil, b, 0)
+			if err != nil {
+				db.DFatalf("Recvmsg err %v", err)
+			}
+			// parse socket control message
+			cmsgs, err := unix.ParseSocketControlMessage(b)
+			if err != nil {
+				db.DFatalf("ParseSocketControlMessage err %v", err)
+			}
+			fds, err := unix.ParseUnixRights(&cmsgs[0])
+			if err != nil {
+				db.DFatalf("ParseUnixRights err %v", err)
+			}
+			fd := fds[0]
+			db.DPrintf(db.ALWAYS, "Received fd %d\n", fd)
+			if err := handleReqs(int(pid), fd); err != nil {
+				db.DPrintf(db.ALWAYS, "handle fd %v err\n", fd, err)
+			}
+		}(conn)
+	}
+}
+
+func readImg(pid int, magic string) (*crit.CriuImage, error) {
+	// f, err := os.Open(filepath.Join(imgDir, "inventory.img"))
+	pn := filepath.Join(os.Args[1], magic+"-"+strconv.Itoa(int(pid))+".img")
+	f, err := os.Open(pn)
+	if err != nil {
+		return nil, err
+	}
+	c := crit.New(f, nil, "", false, false)
+	entryType, err := cli.GetEntryTypeFromImg(f)
+	if err != nil {
+		return nil, fmt.Errorf("Unknown Entry type %q: %w", pn, err)
+	}
+	img, err := c.Decode(entryType)
+	if err != nil {
+		return nil, err
+	}
+	return img, nil
+}
+
+type TpagemapImg struct {
+	PageMapHead    *crit.CriuEntry
+	PagemapEntries []*crit.CriuEntry
+}
+
+func newTpagemapImg(pid int) (*TpagemapImg, error) {
+	img, err := readImg(pid, "pagemap")
+	if err != nil {
+		return nil, err
+	}
+	return &TpagemapImg{PageMapHead: img.Entries[0], PagemapEntries: img.Entries[1:]}, nil
+}
+
+func (pmi *TpagemapImg) find(addr uint64) int {
+	pi := 0
+	for _, pme := range pmi.PagemapEntries {
+		pm := pme.Message.(*pagemap.PagemapEntry)
+		start := pm.GetVaddr()
+		n := pm.GetNrPages()
+		end := start + uint64(n*uint32(os.Getpagesize()))
+		if addr >= start && addr < end {
+			m := (addr - start) / uint64(os.Getpagesize())
+			pi = pi + int(m)
+			db.DPrintf(db.ALWAYS, "m %d pi %d\n", m, pi)
+			return pi
+		}
+		pi += int(n)
+	}
+	return -1
+}
+
+func (pmi *TpagemapImg) read(pid, pi int) ([]byte, error) {
+	page := make([]byte, os.Getpagesize())
+	ph := pmi.PageMapHead.Message.(*pagemap.PagemapHead)
+	if err := readPage(pid, int(ph.GetPagesId()), pi, page); err != nil {
+		return nil, err
+	}
+	return page, nil
+}
+
+type Tmm struct {
+	*mm.MmEntry
+}
+
+func newTmm(pid int) (*Tmm, error) {
+	img, err := readImg(pid, "mm")
+	if err != nil {
+		return nil, err
+	}
+	return &Tmm{MmEntry: img.Entries[0].Message.(*mm.MmEntry)}, nil
+}
+
+type Iov struct {
+	start     uint64
+	end       uint64
+	img_start uint64
+}
+
+func (iov *Iov) String() string {
+	return fmt.Sprintf("{%x %x %x}", iov.start, iov.end, iov.img_start)
+}
+
+type Iovs struct {
+	iovs []*Iov
+}
+
+func newIovs() *Iovs {
+	return &Iovs{iovs: make([]*Iov, 0)}
+}
+
+func (iovs *Iovs) append(iov *Iov) {
+	iovs.iovs = append(iovs.iovs, iov)
+}
+
+func (iovs *Iovs) find(addr uint64) *Iov {
+	for _, iov := range iovs.iovs {
+		if iov.start <= addr && addr < iov.end {
+			return iov
+		}
+	}
+	return nil
+}
+
+func (mm *Tmm) collectIovs(pmi *TpagemapImg) *Iovs {
+	db.DPrintf(db.TEST, "mmInfo %d\n", len(mm.Vmas))
+
+	iovs := newIovs()
+	end := uint64(os.Getpagesize())
+	start := uint64(0)
+	nPages := uint32(0)
+	max_iov_len := start
+
+	ph := pmi.PageMapHead.Message.(*pagemap.PagemapHead)
+	db.DPrintf(db.TEST, "ph %v", ph)
+
+	for _, pme := range pmi.PagemapEntries[1:] {
+		pm := pme.Message.(*pagemap.PagemapEntry)
+
+		db.DPrintf(db.TEST, "pm %v", pm)
+
+		start = pm.GetVaddr()
+		end = start + uint64(pm.GetNrPages()*uint32(os.Getpagesize()))
+		nPages += pm.GetNrPages()
+
+		for _, vma := range mm.Vmas {
+			if start >= vma.GetStart() {
+				continue
+			}
+			iov := &Iov{}
+			vend := vma.GetEnd()
+			len := end
+			if vend < end {
+				end = vend
+			}
+			len = len - start
+			iov.start = start
+			iov.img_start = start
+			iov.end = iov.start + len
+			iovs.append(iov)
+
+			if len > max_iov_len {
+				max_iov_len = len
+			}
+
+			if end < vend {
+				db.DPrintf(db.TEST, "%d vma %v\n", end, vma)
+				break
+			}
+			start = vend
+		}
+	}
+	// XXX do something with max_iov_len
+	db.DPrintf(db.TEST, "max_iov_len %d\n", max_iov_len)
+	return iovs
+}
+
+func readPage(pid, pageId, pi int, page []byte) error {
+	pn := filepath.Join(os.Args[1], "pages-"+strconv.Itoa(pageId)+".img")
+	f, err := os.Open(pn)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	if _, err := f.Seek(int64(pi*os.Getpagesize()), 0); err != nil {
+		return err
+	}
+	if _, err := f.Read(page); err != nil {
+		return err
+	}
+	return nil
+}
+
+func handleReqs(pid, fd int) error {
+	pmi, err := newTpagemapImg(pid)
+	if err != nil {
+		return err
+	}
+	mm, err := newTmm(pid)
+	if err != nil {
+		return err
+	}
+	iovs := mm.collectIovs(pmi)
+	db.DPrintf(db.TEST, "iovs: %v", iovs)
+	for {
+		if _, err := unix.Poll(
+			[]unix.PollFd{{
+				Fd:     int32(fd),
+				Events: unix.POLLIN,
+			}},
+			-1,
+		); err != nil {
+			return err
+		}
+		buf := make([]byte, unsafe.Sizeof(constants.UffdMsg{}))
+		if _, err := syscall.Read(fd, buf); err != nil {
+			return err
+		}
+		msg := (*(*constants.UffdMsg)(unsafe.Pointer(&buf[0])))
+		if constants.GetMsgEvent(&msg) != constants.UFFD_EVENT_PAGEFAULT {
+			return fmt.Errorf("Unknown event %v", constants.GetMsgEvent(&msg))
+		}
+
+		arg := constants.GetMsgArg(&msg)
+		pagefault := (*(*constants.UffdPagefault)(unsafe.Pointer(&arg[0])))
+
+		addr := uint64(constants.GetPagefaultAddress(&pagefault))
+		mask := uint64(^(os.Getpagesize() - 1))
+		db.DPrintf(db.ALWAYS, "page fault %d %x %x", addr, addr, addr&mask)
+		addr = addr & mask
+		iov := iovs.find(addr)
+		if iov == nil {
+			db.DFatalf("no iov for %x", addr)
+		}
+		pi := pmi.find(addr)
+		if pi == -1 {
+			db.DFatalf("no page for %x", addr)
+		}
+		db.DPrintf(db.ALWAYS, "page fault %d -> %d", addr, pi)
+		page, err := pmi.read(pid, pi)
+		if err != nil {
+			db.DFatalf("no page content for %x", addr)
+		}
+		pagesize := os.Getpagesize()
+		cpy := constants.NewUffdioCopy(
+			page,
+			constants.CULong(addr),
+			constants.CULong(pagesize),
+			0,
+			0,
+		)
+		if _, _, errno := syscall.Syscall(
+			syscall.SYS_IOCTL,
+			uintptr(fd),
+			constants.UFFDIO_COPY,
+			uintptr(unsafe.Pointer(&cpy)),
+		); errno != 0 {
+			db.DFatalf("SYS_IOCL err %v", errno)
+		}
+	}
+}
+
+func getConnFd(conn syscall.Conn) (connFd int, err error) {
+	var rawConn syscall.RawConn
+	rawConn, err = conn.SyscallConn()
+	if err != nil {
+		return
+	}
+	err = rawConn.Control(func(fd uintptr) {
+		connFd = int(fd)
+	})
+	return
+}
--- go.mod
+++ go.mod
@@ -30,7 +30,7 @@ require (
 	github.com/thanhpk/randstr v1.0.6
 	go.etcd.io/etcd/server/v3 v3.5.13
 	go.uber.org/zap v1.27.0
-	golang.org/x/sys v0.20.0
+	golang.org/x/sys v0.21.0
 	gonum.org/v1/gonum v0.12.0
 	google.golang.org/protobuf v1.34.1
 	gopkg.in/yaml.v3 v3.0.1
@@ -91,8 +91,10 @@ require (
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
-	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
+	github.com/loopholelabs/userfaultfd-go v0.1.2 // indirect
 	github.com/moby/term v0.5.0 // indirect
 	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
@@ -103,6 +105,8 @@ require (
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.53.0 // indirect
 	github.com/prometheus/procfs v0.14.0 // indirect
+	github.com/spf13/cobra v1.8.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/tklauser/go-sysconf v0.3.11 // indirect
 	github.com/tklauser/numcpus v0.6.0 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
@@ -115,13 +119,13 @@ require (
 	go.etcd.io/etcd/pkg/v3 v3.5.13 // indirect
 	go.opentelemetry.io/otel/metric v1.20.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	golang.org/x/crypto v0.23.0 // indirect
+	golang.org/x/crypto v0.24.0 // indirect
 	golang.org/x/mod v0.17.0 // indirect
-	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
-	golang.org/x/text v0.15.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
-	golang.org/x/tools v0.20.0 // indirect
+	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20240509183442-62759503f434 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240509183442-62759503f434 // indirect
 	gotest.tools/v3 v3.4.0 // indirect

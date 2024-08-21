5f22597172051d98b794f2f6a449b0e6c8172576
Ariel Szekely <arielck@mit.edu>
2024-08-21 09:35:12 -0400

    Uncomment graph generation invocation for all graphs in graphing script

--- artifact/sosp24/scripts/generate-graphs.sh
+++ artifact/sosp24/scripts/generate-graphs.sh
@@ -7,31 +7,29 @@ RES_OUT_DIR=$ROOT_DIR/benchmarks/results/$VERSION
 GRAPH_SCRIPTS_DIR=$ROOT_DIR/benchmarks/scripts/graph
 GRAPH_OUT_DIR=$ROOT_DIR/benchmarks/results/graphs
 
-## Figure 6
-#echo "Generating Figure 6..."
-#$GRAPH_SCRIPTS_DIR/start-latency.py --out $GRAPH_OUT_DIR/start_latency.pdf --cold_res_dir $RES_OUT_DIR/cold_start --warm_res_dir $RES_OUT_DIR/cold_start
-#echo "Done generating Figure 6..."
-#
-## Figure 8
-#echo "Generating Figure 8..."
-#$GRAPH_SCRIPTS_DIR/schedd-scalability-hockey.py --measurement_dir $RES_OUT_DIR/sched_scalability --out $GRAPH_OUT_DIR/sched_scalability.pdf --prefix "23-vm-" --tpt_v_tpt > /dev/null
-#echo "Done generating Figure 8..."
+# Figure 6
+echo "Generating Figure 6..."
+$GRAPH_SCRIPTS_DIR/start-latency.py --out $GRAPH_OUT_DIR/start_latency.pdf --cold_res_dir $RES_OUT_DIR/cold_start --warm_res_dir $RES_OUT_DIR/cold_start
+echo "Done generating Figure 6..."
+
+# Figure 8
+echo "Generating Figure 8..."
+$GRAPH_SCRIPTS_DIR/schedd-scalability-hockey.py --measurement_dir $RES_OUT_DIR/sched_scalability --out $GRAPH_OUT_DIR/sched_scalability.pdf --prefix "23-vm-" --tpt_v_tpt > /dev/null
+echo "Done generating Figure 8..."
 
 # Figure 10
-#echo "Generating Figure 10..."
-#$GRAPH_SCRIPTS_DIR/mr_vs_corral.py --measurement_dir $RES_OUT_DIR/mr_vs_corral/ --out $GRAPH_OUT_DIR/mr_vs_corral.pdf --datasize=2G
-#echo "Done generating Figure 10..."
+echo "Generating Figure 10..."
+$GRAPH_SCRIPTS_DIR/mr_vs_corral.py --measurement_dir $RES_OUT_DIR/mr_vs_corral/ --out $GRAPH_OUT_DIR/mr_vs_corral.pdf --datasize=2G
+echo "Done generating Figure 10..."
 
 # Figure 11
 echo "Generating Figure 11..."
-echo "FIG 11 TODO"
+$GRAPH_SCRIPTS_DIR/microservices-perf-bar-graph.py --out $GRAPH_OUT_DIR/microservices_perf.pdf --hotel_res_dir $RES_OUT_DIR/hotel_tail_latency --socialnet_res_dir $RES_OUT_DIR/socialnet_tail_latency
 echo "Done generating Figure 11..."
 
 # Figure 12
 echo "Generating Figure 12..."
-#nrealm=4
-#ncores=32
-#$GRAPH_SCRIPTS_DIR/bebe-tpt.py --measurement_dir $RES_OUT_DIR/be_imgresize_multiplexing --out $GRAPH_OUT_DIR/be_imgresize_multiplexing.pdf --nrealm $nrealm --units "MB/sec" --title "Aggregate Throughput Balancing $nrealm Realms' BE Applications" --total_ncore $ncores --prefix "imgresize-"
+$GRAPH_SCRIPTS_DIR/bebe-tpt.py --measurement_dir $RES_OUT_DIR/be_imgresize_multiplexing --out $GRAPH_OUT_DIR/be_imgresize_multiplexing.pdf --nrealm 4 --units "MB/sec" --title "Aggregate Throughput Balancing 4 Realms' BE Applications" --total_ncore 32 --prefix "imgresize-"
 echo "Done generating Figure 12..."
 
 # Figure 13

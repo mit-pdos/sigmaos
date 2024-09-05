0731e8ba2793d143c971b9836efae39543c45e89
Ariel Szekely <arielck@mit.edu>
2024-09-05 14:46:35 -0400

    x

--- mr/mr_test.go
+++ mr/mr_test.go
@@ -186,7 +186,7 @@ func TestMapperAlone(t *testing.T) {
 		db.DFatalf("CloseWrt err %v", err)
 	}
 
-	db.DPrintf(db.ALWAYS, "%s: in %s out %s tot %v %vms (%s)\n", "map", humanize.Bytes(uint64(nin)), humanize.Bytes(uint64(nout)), test.Mbyte(nin+nout), time.Since(start).Milliseconds(), test.TputStr(nin+nout, time.Since(start).Milliseconds()))
+	db.DPrintf(db.ALWAYS, "%s: in %s out %s tot %s %vms (%s)\n", "map", humanize.Bytes(uint64(nin)), humanize.Bytes(uint64(nout)), humanize.Bytes(uint64(test.Mbyte(nin+nout))), time.Since(start).Milliseconds(), test.TputStr(nin+nout, time.Since(start).Milliseconds()))
 
 	// data := make(map[string]int, 0)
 	// rdr, err := ts.OpenAsyncReader(REDUCEIN, 0)

085a5841b75f42b8007ab686a3bc5e7b252f7a3f
Ariel Szekely <arielck@mit.edu>
2024-09-08 20:59:53 -0400

    x

--- cloudlab/scratch.sh
+++ cloudlab/scratch.sh
@@ -30,10 +30,11 @@ for vm in $vms; do
 #    (cd sigmaos; sudo apparmor_parser -r scontainer/sigmaos-uproc )
 #    (cd sigmaos; ./set-cores.sh --set 0 --start 4 --end 39 )
 #    sudo rm -rf /data/volumes/*
-  ls ~
-  go version
-  aa-status | grep sigmaos
+#  ls ~
+#  go version
+#  aa-status | grep sigmaos
 #  docker swarm leave
 #  docker swarm join --token SWMTKN-1-1yub2fsj1bsxn9ikm8xsf4an4fb0sln0881opnoy7ko5muxgz9-1shovmaebf8616ds1mip1rzr1 10.10.1.1:2377
+nproc
 ENDSSH
 done

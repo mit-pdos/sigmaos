0c50077d5c7136c23eaed19ef67eb49c09dce14b
Frans Kaashoek <kaashoek@mit.edu>
2024-10-10 13:34:21 -0400

    Remove TestPerf*, which are subsumed by perf tests in sigmapsrv/sigmapsrv_test.go

--- ux/fsux_test.go
+++ ux/fsux_test.go
@@ -1,15 +1,10 @@
 package fsux
 
 import (
-	"fmt"
-	"os"
 	"strconv"
 	"sync"
-	"syscall"
 	"testing"
-	"time"
 
-	"github.com/dustin/go-humanize"
 	"github.com/stretchr/testify/assert"
 
 	"sigmaos/netproxyclnt"
@@ -90,44 +85,6 @@ func TestDir(t *testing.T) {
 	assert.Equal(t, nil, err)
 
 	ts.Shutdown()
-}
-
-func newfile(t *testing.T, name string) {
-	CNT := 500
-	buf := test.NewBuf(sp.BUFSZ)
-	start := time.Now()
-	fd, err := syscall.Open(name, syscall.O_CREAT|syscall.O_EXCL|syscall.O_WRONLY, 0666)
-	assert.Nil(t, err)
-	for i := 0; i < CNT; i++ {
-		n, err := syscall.Pwrite(fd, buf, int64(i*sp.BUFSZ))
-		assert.Nil(t, err)
-		assert.Equal(t, sp.BUFSZ, n)
-	}
-	syscall.Fsync(fd)
-	syscall.Close(fd)
-	ms := time.Since(start).Milliseconds()
-	sz := uint64(CNT * len(buf))
-	fmt.Printf("%s took %vms (%s)\n", humanize.Bytes(sz), ms, test.TputStr(sp.Tlength(sz), ms))
-	os.Remove(name)
-}
-
-func TestFsPerfSingle(t *testing.T) {
-	newfile(t, "xxx")
-}
-
-func TestFsPerfMulti(t *testing.T) {
-
-	var done sync.WaitGroup
-	done.Add(2)
-	go func() {
-		newfile(t, "xxx")
-		done.Done()
-	}()
-	go func() {
-		newfile(t, "yyy")
-		done.Done()
-	}()
-	done.Wait()
 }
 
 func writer(t *testing.T, ch chan error, pe *proc.ProcEnv, idx int) {

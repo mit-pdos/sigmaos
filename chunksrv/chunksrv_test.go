4113a0781b28f7c127d2ea8cc550645a7b547367
Ariel Szekely <arielck@mit.edu>
2024-11-14 07:27:11 -0500

    Stop chunksrv test from panicking

--- chunksrv/chunksrv_test.go
+++ chunksrv/chunksrv_test.go
@@ -80,7 +80,9 @@ func (ts *Tstate) fetch(srv string, paths []string, expect []string) {
 	pid := ts.ProcEnv().GetPID()
 	secrets := ts.ProcEnv().GetSecrets()["s3"]
 	st, path, err := ts.ckclnt.GetFileStat(srv, PROG, pid, sp.ROOTREALM, secrets, paths, nil)
-	assert.Nil(ts.T, err)
+	if !assert.Nil(ts.T, err) {
+		return
+	}
 	assert.True(ts.T, isExpected(path, expect))
 
 	n := (st.Tlength() / chunk.CHUNKSZ) + 1

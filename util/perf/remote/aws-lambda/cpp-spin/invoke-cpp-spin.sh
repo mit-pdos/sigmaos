6fefde084d62ac37f7edb089b8abd222ad80b2a9
Ariel Szekely <arielck@mit.edu>
2024-11-20 06:37:41 -0500

    Remove some dead Lambda code

--- util/perf/remote/aws-lambda/cpp-spin/invoke-cpp-spin.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#aws apigateway test-invoke-method --
-
-curl -v -X POST \
-  'https://m5ica91644.execute-api.us-east-1.amazonaws.com/default/cpp-spin?dim=64&its=5000' \
-  -H 'content-type: application/json' \
-  -d '{ "baseline": false }'

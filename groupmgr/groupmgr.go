f0a12b191abeb70a92afcd9fe5e6f6c11c2e3550
Frans Kaashoek <kaashoek@mit.edu>
2024-11-19 19:00:54 -0500

    x

--- groupmgr/groupmgr.go
+++ groupmgr/groupmgr.go
@@ -1,7 +1,7 @@
 // package groupmgr keeps n instances of the same proc running. If one
 // instance (a member) of the group of n crashes, the manager starts
 // another one.  Some programs use the n instances to form a Raft
-// group (e.g., kvgrp); others use it in primary-backup configuration
+// group (e.g., kvgrp); others use it in a hot-standby configuration
 // (e.g., kv balancer, imageresized).
 //
 // There are two ways of stopping the group manager: the caller calls

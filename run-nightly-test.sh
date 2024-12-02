320d0ea88f1b1b62f250240288f7e52a47c5c363
Ariel Szekely <arielck@mit.edu>
2024-12-01 20:52:57 -0500

    x

--- run-nightly-test.sh
+++ run-nightly-test.sh
@@ -9,12 +9,18 @@ SIGMAOS_ROOT=$HOME/sigmaos
 for branch in $(cat $SIGMAOS_ROOT/branches-to-test.txt); do
   BRANCH_OUT_DIR="$TEST_OUT_DIR/$branch"
   mkdir $BRANCH_OUT_DIR
+  OUT_FILE="$BRANCH_OUT_DIR/run.out"
+  touch $OUT_FILE
+  echo "=== Checking out to branch $branch" | tee -a $OUT_FILE
+  git checkout $branch
+  ./build.sh 2>&1 | tee -a $OUT_FILE
+  echo "=== Building branch $branch" | tee -a $OUT_FILE
   ./stop.sh --parallel > /dev/null
-  echo "=== Running tests on branch $branch"
+  echo "=== Running tests on branch $branch" | tee -a $OUT_FILE
   # Run tests
-  ./test.sh --cleanup --savelogs
+  ./test.sh --cleanup --savelogs 2>&1 | tee -a $OUT_FILE
   # Copy log output
-  cp -r $LOG_DIR/* $BRANCH_OUT_DIR/
-  echo "=== Done running tests on branch $branch"
+  mv $LOG_DIR/ $BRANCH_OUT_DIR/
+  echo "=== Done running tests on branch $branch" | tee -a $OUT_FILE
   ./stop.sh --parallel > /dev/null
 done

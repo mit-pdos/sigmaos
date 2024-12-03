edf5c5422152038ea495a86151b8f4644b994dc9
Ariel Szekely <arielck@mit.edu>
2024-12-02 21:24:33 -0500

    x

--- test.sh
+++ test.sh
@@ -141,7 +141,7 @@ if [[ $COMPILE == "--compile" ]]; then
     # test if test packages compile
     #
 
-    for T in path serr linuxsched util/perf sigmap dialproxy sessclnt npproxysrv fslib/reader fslib/writer stats fslib semclnt chunk/srv leaderclnt/electclnt dircache memfs namesrv procclnt ux proxy/s3 bootkernelclnt leaderclnt leadertest apps/kv/kvgrp apps/cache/cachegrp/clnt apps/www sigmapsrv realm/clnt apps/mr apps/imgresize apps/kv apps/hotel apps/socialnetwork benchmarks benchmarks/remote example example_echo_server benchmarks/netperf; do
+    for T in path serr linuxsched util/perf sigmap dialproxy sessclnt proxy/ninep fslib/reader fslib/writer stats fslib semclnt chunk/srv leaderclnt/electclnt dircache memfs namesrv procclnt ux proxy/s3 bootkernelclnt leaderclnt leadertest apps/kv/kvgrp apps/cache/cachegrp/clnt apps/www sigmapsrv realm/clnt apps/mr apps/imgresize apps/kv apps/hotel apps/socialnetwork benchmarks benchmarks/remote example example_echo_server benchmarks/netperf; do
         if ! [ -z "$SKIPTO" ]; then
           if [[ "$SKIPTO" == "$T" ]]; then
             # Stop skipping
@@ -232,10 +232,10 @@ if [[ $BASIC == "--basic" ]]; then
     done
 
     #
-    # test npproxy with just named and full kernel
+    # test ninep proxy with just named and full kernel
     #
 
-    for T in npproxysrv; do
+    for T in proxy/ninep; do
         if ! [ -z "$SKIPTO" ]; then
           if [[ "$SKIPTO" == "$T" ]]; then
             # Stop skipping

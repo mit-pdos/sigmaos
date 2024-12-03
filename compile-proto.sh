602d2ebc3674f7b6bf8286b6c36115d5a4f894e4
Ariel Szekely <arielck@mit.edu>
2024-12-02 21:19:07 -0500

    x

--- compile-proto.sh
+++ compile-proto.sh
@@ -7,7 +7,7 @@ for P in sigmap sessp proc example_echo_server ; do
   protoc -I=. --go_out=../ $P/$P.proto
 done
 
-for PP in dialproxy rpc kernel sched/besched sched/lcsched sched/msched sched/msched/proc realm db util/k8s tracing apps/cache apps/kv apps/hotel apps/socialnetwork spproxy chunk apps/imgresize mongo ; do
+for PP in dialproxy rpc kernel sched/besched sched/lcsched sched/msched sched/msched/proc realm proxy/db util/k8s tracing apps/cache apps/kv apps/hotel apps/socialnetwork spproxy chunk apps/imgresize proxy/mongo ; do
   for P in $PP/proto/*.proto ; do
     echo "protoc $P"
     protoc -I=. --go_out=../ $P

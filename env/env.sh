e027ce672d47e1284ee86199a0e2716ce66b13e7
Ariel Szekely <arielck@mit.edu>
2024-12-05 21:41:33 -0500

    x

--- env/env.sh
+++ env/env.sh
@@ -8,3 +8,7 @@
 
 # to find proxyd
 export PATH=$PATH:$PWD/bin/linux/:$PWD/bin/kernel
+ROOT=$(realpath $(dirname $0)/..)
+if [ -f $ROOT/env/user.sh ]; then
+  source $ROOT/env/user.sh
+fi
--- env/user.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-export SIGMAOS_USER=""

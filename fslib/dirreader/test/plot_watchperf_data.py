aa34544d98388e08718db9f2d62596e0a9737801
Ryan Chang <rychang@mit.edu>
2024-12-09 09:31:28 +0000

    Add scripts for generating graphs

--- fslib/dirreader/test/plot_watchperf_data.py
+++ fslib/dirreader/test/plot_watchperf_data.py
@@ -50,7 +50,10 @@ def remove_outliers(data):
     return [x for x in data if lower_bound <= x <= upper_bound]
 
 def plot_histogram(data, bins=10, title="Histogram", xlabel="Value", ylabel="Frequency", label=None):
-    plt.hist(data, bins=bins, edgecolor='black', alpha=0.4, label=label)
+    hist = plt.hist(data, bins=bins, edgecolor='black', alpha=0.4, label=label)
+    hist_color = hist[2][0].get_facecolor()
+    plt.axvline(np.mean(data), linestyle='dashed', linewidth=2, color=hist_color)
+
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
@@ -73,22 +76,143 @@ def save_file(save):
     plt.savefig(save)
     plt.clf()
 
+def plot_histograms(timestamp, bucket):
+    for v in ['V1', 'V2']:
+        for loc in ['named', 'local']:
+            for typ in ['include_op', 'watch_only']:
+                process_file(f"{timestamp}/{v}/1wkrs_0stfi_1fpt_{loc}_{typ}", bucket, label_suffix=" (0 other files)")
+                process_file(f"{timestamp}/{v}/1wkrs_100stfi_1fpt_{loc}_{typ}", bucket, label_suffix=" (100 other files)")
+                process_file(f"{timestamp}/{v}/1wkrs_500stfi_1fpt_{loc}_{typ}", bucket, label_suffix=" (500 other files)")
+                process_file(f"{timestamp}/{v}/1wkrs_1000stfi_1fpt_{loc}_{typ}", bucket, label_suffix=" (1000 other files)")
+                save_file(f"./{timestamp}/{v}/1wkrs_*stfi_1fpt_{loc}_{typ}.png")
+
+                process_file(f"{timestamp}/{v}/1wkrs_0stfi_1fpt_{loc}_{typ}", bucket, label_suffix=" (1 watcher)")
+                process_file(f"{timestamp}/{v}/5wkrs_0stfi_1fpt_{loc}_{typ}", bucket, label_suffix=" (5 watchers)")
+                process_file(f"{timestamp}/{v}/10wkrs_0stfi_1fpt_{loc}_{typ}", bucket, label_suffix=" (10 watchers)")
+                process_file(f"{timestamp}/{v}/15wkrs_0stfi_1fpt_{loc}_{typ}", bucket, label_suffix=" (15 watchers)")
+                save_file(f"./{timestamp}/{v}/*wkrs_0stfi_1fpt_{loc}_{typ}.png")
+
+                process_file(f"{timestamp}/{v}/1wkrs_0stfi_1fpt_{loc}_{typ}", bucket, label_suffix=" (1 file per trial)")
+                process_file(f"{timestamp}/{v}/1wkrs_0stfi_5fpt_{loc}_{typ}", bucket, label_suffix=" (5 files per trial)")
+                process_file(f"{timestamp}/{v}/1wkrs_0stfi_10fpt_{loc}_{typ}", bucket, label_suffix=" (10 files per trial)")
+                process_file(f"{timestamp}/{v}/1wkrs_0stfi_15fpt_{loc}_{typ}", bucket, label_suffix=" (15 files per trial)")
+                save_file(f"./{timestamp}/{v}/1wkrs_0stfi_*fpt_{loc}_{typ}.png")
+
+def compute_speedups(timestamp, bucket):
+    files = []
+    for obj in bucket.objects.filter(Prefix=f"{timestamp}/V1/"):
+        files.append(obj.key)
+
+    speedups_include_op = []
+    speedups_watch_only = []
+    speedups_1000_stfi = []
+
+    for file in files:
+        if file.endswith('_'):
+            continue
+        v1_create, _ = read_data(file, bucket)
+        v1_create = remove_outliers(v1_create)
+
+        v2_create, _ = read_data(file.replace("V1", "V2"), bucket)
+        v2_create = remove_outliers(v2_create)
+
+        create_speedup = np.mean(v1_create) / np.mean(v2_create)
+
+        print(f"{file} create speedup: {create_speedup}")
+
+        if "include_op" in file:
+            speedups_include_op.append(create_speedup)
+        else:
+            speedups_watch_only.append(create_speedup)
+            if "1000stfi" in file:
+                speedups_1000_stfi.append(create_speedup)
+    
+    print("Include Op")
+    print(pd.Series(speedups_include_op).describe())
+    print()
+    print("Watch Only")
+    print(pd.Series(speedups_watch_only).describe())
+    print()
+    print("1000 stfi, watch only")
+    print(pd.Series(speedups_1000_stfi).describe())
+
+def plot_starting_file_graph(timestamp, bucket):
+    data = [[], []]
+    x_values = [0, 100, 500, 1000]
+    for ix, v in enumerate(['V1', 'V2']):
+        for nstfi in x_values:
+            file = f"{timestamp}/{v}/1wkrs_{nstfi}stfi_1fpt_local_watch_only"
+            create_watch_times, _ = read_data(file, bucket)
+            create_watch_times = remove_outliers(create_watch_times)
+
+            data[ix].append(np.mean(create_watch_times) / 1000.0)
+
+    plt.plot(x_values, data[0], label="V1", marker='o')
+    plt.plot(x_values, data[1], label="V2", marker='o')
+    plt.xlabel("Num Starting Files")
+    plt.ylabel("Mean Watch Time (us)")
+    plt.xticks(x_values)
+    plt.grid(axis='x', which='major')
+    plt.grid(axis='y')
+    plt.legend()
+    os.makedirs(timestamp, exist_ok=True)
+    plt.savefig(f"./{timestamp}/mean_watch_time_vs_starting_files.png")
+    plt.clf()
+
+def plot_wkrs_graph(timestamp, bucket):
+    data = [[], []]
+    x_values = [1, 5, 10, 15]
+    for ix, v in enumerate(['V1', 'V2']):
+        for nwkrs in x_values:
+            file = f"{timestamp}/{v}/{nwkrs}wkrs_0stfi_1fpt_local_watch_only"
+            create_watch_times, _ = read_data(file, bucket)
+            create_watch_times = remove_outliers(create_watch_times)
+
+            data[ix].append(np.mean(create_watch_times) / 1000.0)
+
+    plt.plot(x_values, data[0], label="V1", marker='o')
+    plt.plot(x_values, data[1], label="V2", marker='o')
+    plt.xlabel("Num Workers")
+    plt.ylabel("Mean Watch Time (us)")
+    plt.xticks(x_values)
+    plt.grid(axis='x', which='major')
+    plt.grid(axis='y')
+    plt.legend()
+    os.makedirs(timestamp, exist_ok=True)
+    plt.savefig(f"./{timestamp}/mean_watch_time_vs_workers.png")
+    plt.clf()
+
+def plot_fpt_graph(timestamp, bucket):
+    data = [[], []]
+    x_values = [1, 5, 10, 15]
+    for ix, v in enumerate(['V1', 'V2']):
+        for fpt in x_values:
+            file = f"{timestamp}/{v}/1wkrs_0stfi_{fpt}fpt_local_watch_only"
+            create_watch_times, _ = read_data(file, bucket)
+            create_watch_times = remove_outliers(create_watch_times)
+
+            data[ix].append(np.mean(create_watch_times) / 1000.0)
+
+    plt.plot(x_values, data[0], label="V1", marker='o')
+    plt.plot(x_values, data[1], label="V2", marker='o')
+    plt.xlabel("Files per Trial")
+    plt.ylabel("Mean Watch Time (us)")
+    plt.xticks(x_values)
+    plt.grid(axis='x', which='major')
+    plt.grid(axis='y')
+    plt.legend()
+    os.makedirs(timestamp, exist_ok=True)
+    plt.savefig(f"./{timestamp}/mean_watch_time_vs_files_per_trial.png")
+    plt.clf()
+
 if __name__ == "__main__":
-    timestamp = "2024-11-27_03:53:28"
+    timestamp = "2024-12-05_16:27:07"
     session = boto3.Session(profile_name='sigmaos')
     s3_resource = session.resource('s3')
     bucket = s3_resource.Bucket('sigmaos-bucket-ryan')
             
-    for v in ['v1', 'v2']:
-        for loc in ['named', 'local']:
-            for typ in ['include_op', 'watch_only']:
-                process_file(f"{timestamp}/{v}/watchperf_1_workers_0_startfiles_{loc}_{typ}.txt", bucket, label_suffix=" (0 other files in dir)")
-                process_file(f"{timestamp}/{v}/watchperf_1_workers_100_startfiles_{loc}_{typ}.txt", bucket, label_suffix=" (0 other files in dir)")
-                process_file(f"{timestamp}/{v}/watchperf_1_workers_1000_startfiles_{loc}_{typ}.txt", bucket, label_suffix=" (0 other files in dir)")
-                save_file(f"./{timestamp}/{v}/watchperf_single_{loc}_{typ}.png")
-
-                process_file(f"{timestamp}/{v}/watchperf_1_workers_0_startfiles_{loc}_{typ}.txt", bucket, label_suffix=" (1 worker)")
-                process_file(f"{timestamp}/{v}/watchperf_10_workers_0_startfiles_{loc}_{typ}.txt", bucket, label_suffix=" (5 workers)")
-                process_file(f"{timestamp}/{v}/watchperf_25_workers_0_startfiles_{loc}_{typ}.txt", bucket, label_suffix=" (15 workers)")
-                save_file(f"./{timestamp}/{v}/watchperf_multiple_{loc}_{typ}.png")
-
+    # plot_histograms(timestamp, bucket)
+    # compute_speedups(timestamp, bucket)
+    plot_starting_file_graph(timestamp, bucket)
+    plot_wkrs_graph(timestamp, bucket)
+    plot_fpt_graph(timestamp, bucket)
--- test-watch-perf.sh
+++ test-watch-perf.sh
@@ -9,21 +9,10 @@ export S3_BUCKET="sigmaos-bucket-ryan/$(date +%Y-%m-%d_%H:%M:%S)"
 DIRREADER_VERSIONS=("1" "2")
 MEASURE_MODES=("watch_only" "include_op")
 USE_NAMEDS=("0" "1")
-NUM_STARTING_FILES=("0" "100" "1000")
+NUM_STARTING_FILES=("0" "100" "500" "1000")
 
-NUM_WORKERS=("10" "25")
-NUM_FILES_PER_TRIAL=("5" "10")
-
-# ./fsetcd-wipe.sh
-# ./stop.sh
-# DIRREADER_VERSION="2" \
-# MEASURE_MODE="watch_only" \
-# USE_NAMED="0" \
-# NUM_WORKERS="1" \
-# NUM_FILES_PER_TRIAL="1" \
-# NUM_STARTING_FILES="0" \
-# NUM_TRIALS="10" \
-# go test sigmaos/fslib/dirreader -v --start --run "TestPerf" --timeout 5m
+NUM_WORKERS=("1" "5" "10" "15")
+NUM_FILES_PER_TRIAL=("1" "5" "10" "15")
 
 retry_until_success() {
   local log_prefix=$1
@@ -51,35 +40,26 @@ retry_until_success() {
 }
 
 for DIRREADER_VERSION in "${DIRREADER_VERSIONS[@]}"; do
-  for MEASURE_MODE in "${MEASURE_MODES[@]}"; do
-    for USE_NAMED in "${USE_NAMEDS[@]}"; do
-      for STARTING_FILES in "${NUM_STARTING_FILES[@]}"; do
-        DIRREADER_VERSION="$DIRREADER_VERSION" \
-        MEASURE_MODE="$MEASURE_MODE" \
-        USE_NAMED="$USE_NAMED" \
-        NUM_WORKERS="1" \
-        NUM_TRIALS="250" \
-        NUM_FILES_PER_TRIAL="1" \
-        NUM_STARTING_FILES="$STARTING_FILES" \
-        retry_until_success "dirreader_${DIRREADER_VERSION}_${MEASURE_MODE}_${USE_NAMED}_${STARTING_FILES}"
-      done
-    done
-  done
-done
-
-for DIRREADER_VERSION in "${DIRREADER_VERSIONS[@]}"; do
-  for MEASURE_MODE in "${MEASURE_MODES[@]}"; do
-    for USE_NAMED in "${USE_NAMEDS[@]}"; do
-      for WORKERS in "${NUM_WORKERS[@]}"; do
-        for NUM_FILES in "${NUM_FILES_PER_TRIAL[@]}"; do
-          DIRREADER_VERSION="$DIRREADER_VERSION" \
-          MEASURE_MODE="$MEASURE_MODE" \
-          USE_NAMED="$USE_NAMED" \
-          NUM_WORKERS="$WORKERS" \
-          NUM_TRIALS="100" \
-          NUM_FILES_PER_TRIAL="$NUM_FILES" \
-          NUM_STARTING_FILES="0" \
-          retry_until_success "dirreader_${DIRREADER_VERSION}_${MEASURE_MODE}_${USE_NAMED}_${WORKERS}_${NUM_FILES_PER_TRIAL}"
+  for STARTING_FILES in "${NUM_STARTING_FILES[@]}"; do
+    for WORKERS in "${NUM_WORKERS[@]}"; do
+      for FILES_PER_TRIAL in "${NUM_FILES_PER_TRIAL[@]}"; do
+        for MEASURE_MODE in "${MEASURE_MODES[@]}"; do
+          for USE_NAMED in "${USE_NAMEDS[@]}"; do
+            if (( STARTING_FILES > 0 )) && (( WORKERS > 1 || FILES_PER_TRIAL > 1 )); then
+              continue
+            fi
+            
+            NUM_TRIALS=$((150 / FILES_PER_TRIAL))
+            
+            DIRREADER_VERSION="$DIRREADER_VERSION" \
+            MEASURE_MODE="$MEASURE_MODE" \
+            USE_NAMED="$USE_NAMED" \
+            NUM_WORKERS="$WORKERS" \
+            NUM_TRIALS="$NUM_TRIALS" \
+            NUM_FILES_PER_TRIAL="$FILES_PER_TRIAL" \
+            NUM_STARTING_FILES="$STARTING_FILES" \
+            retry_until_success "dirreader_${DIRREADER_VERSION}_${MEASURE_MODE}_${USE_NAMED}_${STARTING_FILES}_${WORKERS}_${FILES_PER_TRIAL}"
+          done
         done
       done
     done

00cf06730c186fc868d87a78854aee7b44a16871
Ariel Szekely <arielck@mit.edu>
2024-12-11 13:53:17 -0500

    Fix compilation of example echo server proto

--- compile-proto.sh
+++ compile-proto.sh
@@ -2,12 +2,12 @@
 
 # Note: order is important.
 
-for P in sigmap proc example_echo_server ; do
+for P in sigmap proc ; do
   echo "protoc $P"
   protoc -I=. --go_out=../ $P/$P.proto
 done
 
-for PP in session dialproxy rpc kernel sched/besched sched/lcsched sched/msched sched/msched/proc realm proxy/db util/k8s util/tracing apps/cache apps/kv apps/hotel apps/socialnetwork proxy/sigmap chunk apps/imgresize proxy/mongo ; do
+for PP in session dialproxy rpc kernel sched/besched sched/lcsched sched/msched sched/msched/proc realm proxy/db util/k8s util/tracing apps/cache apps/kv apps/hotel apps/socialnetwork proxy/sigmap chunk apps/imgresize proxy/mongo example/example_echo_server ; do
   for P in $PP/proto/*.proto ; do
     echo "protoc $P"
     protoc -I=. --go_out=../ $P

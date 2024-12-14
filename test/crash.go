42eb1cf03d046d364774c7bfb6e844678a4558df
Frans Kaashoek <kaashoek@mit.edu>
2024-12-14 13:19:44 -0500

    Delete obsolete function

--- test/crash.go
+++ test/crash.go
@@ -8,22 +8,6 @@ import (
 	"sigmaos/util/crash"
 )
 
-func (ts *Tstate) CrashServer1(e0, e1 crash.Tevent) {
-	err := crash.SetSigmaFail(crash.NewTeventMapOne(e1))
-	assert.Nil(ts.T, err)
-	db.DPrintf(db.ALWAYS, "Booting %v node Before crashing a %v.", e1.Path, e0.Path)
-	err = ts.BootNode(1)
-	db.DPrintf(db.ALWAYS, "Done booting a node before crashing a %v.", e0.Path)
-	if !assert.Nil(ts.T, err) {
-		db.DPrintf(db.ERROR, "Error BootNode %v", e1.Path)
-	}
-	err = crash.SignalFailer(ts.FsLib, e0.Path)
-	if !assert.Nil(ts.T, err) {
-		db.DPrintf(db.ERROR, "Error non-nil kill %v: %v", e0.Path, err)
-	}
-	db.DPrintf(db.ALWAYS, "Done crash one %v", e0.Path)
-}
-
 func (ts *Tstate) CrashServer(e0, e1 crash.Tevent, srv string) {
 	db.DPrintf(db.ALWAYS, "Crash %v srv %v", e0.Path, srv)
 	err := crash.SignalFailer(ts.FsLib, e0.Path)

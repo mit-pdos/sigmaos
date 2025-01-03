42076d0d7281c4168544b56c5a6a92f3f4194d5d
Frans Kaashoek <kaashoek@mit.edu>
2025-01-02 20:40:27 -0500

    x

--- proxy/ux/refobj.go
+++ proxy/ux/refobj.go
@@ -9,6 +9,10 @@ import (
 	"sigmaos/util/refmap"
 )
 
+const (
+	N = 1000
+)
+
 // Objects for which a client has an fid. Several clients may have an
 // fid for the same object, therefore we ref count the object
 // references.
@@ -19,7 +23,7 @@ type ObjTable struct {
 
 func NewObjTable() *ObjTable {
 	ot := &ObjTable{}
-	ot.RefTable = refmap.NewRefTable[sp.Tpath, fs.FsObj](debug.UX)
+	ot.RefTable = refmap.NewRefTable[sp.Tpath, fs.FsObj](N, debug.UX)
 	return ot
 }
 
@@ -28,7 +32,7 @@ func (ot *ObjTable) GetRef(path sp.Tpath) fs.FsObj {
 	defer ot.Unlock()
 
 	if e, ok := ot.RefTable.Lookup(path); ok {
-		return e.(fs.FsObj)
+		return *e
 	}
 	return nil
 }
@@ -37,7 +41,7 @@ func (ot *ObjTable) AllocRef(path sp.Tpath, o fs.FsObj) fs.FsObj {
 	ot.Lock()
 	defer ot.Unlock()
 	e, _ := ot.RefTable.Insert(path, o)
-	return e.(fs.FsObj)
+	return *e
 }
 
 func (ot *ObjTable) Clunk(p sp.Tpath) {

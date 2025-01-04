e29f1a23dd7b5ab4d52497c15031cf717bcbea00
Frans Kaashoek <kaashoek@mit.edu>
2025-01-03 19:17:45 -0500

    Clean up and add CreateRename benchmark

--- spproto/srv/srv_test.go
+++ spproto/srv/srv_test.go
@@ -2,6 +2,7 @@ package srv_test
 
 import (
 	"flag"
+	"path/filepath"
 	"strconv"
 	"testing"
 	"time"
@@ -39,8 +40,9 @@ func TestCompile(t *testing.T) {
 }
 
 type tstate struct {
-	t   *testing.T
-	srv *srv.ProtSrv
+	t    *testing.T
+	srv  *srv.ProtSrv
+	nfid sp.Tfid
 }
 
 func setupNamed(pe *proc.ProcEnv) fs.Dir {
@@ -83,18 +85,26 @@ func newTstate(t *testing.T) *tstate {
 	aaf := srv.AttachAllowAllToAll
 	srv := srv.NewProtSrv(pe, pps, sp.NoPrincipal(), 0, grf, aaf)
 	srv.NewRootFid(0, ctx, root, "")
-	return &tstate{t, srv}
+	return &tstate{t, srv, sp.Tfid(2)}
 }
 
-func (ts *tstate) walkPath(fid, nfid sp.Tfid, path path.Tpathname) {
-	args := sp.NewTwalk(fid, nfid, path)
+func (ts *tstate) newFid() (fid sp.Tfid) {
+	fid = ts.nfid
+	ts.nfid += 1
+	return
+}
+
+func (ts *tstate) walkPath(pn string) sp.Tfid {
+	nfid := ts.newFid()
+	args := sp.NewTwalk(0, nfid, path.Split(pn))
 	rets := sp.Rwalk{}
 	rerr := ts.srv.Walk(args, &rets)
 	assert.Nil(ts.t, rerr, "rerror %v", rerr)
+	return nfid
 }
 
-func (ts *tstate) walk(fid, nfid sp.Tfid) {
-	ts.walkPath(fid, nfid, path.Tpathname{})
+func (ts *tstate) walk() sp.Tfid {
+	return ts.walkPath("")
 }
 
 func (ts *tstate) clunk(fid sp.Tfid) {
@@ -104,11 +114,22 @@ func (ts *tstate) clunk(fid sp.Tfid) {
 	assert.Nil(ts.t, rerr, "rerror %v", rerr)
 }
 
-func (ts *tstate) create(fid sp.Tfid, n string) {
-	args := sp.NewTcreate(fid, n, 0777, sp.ORDWR, sp.NoLeaseId, sp.NullFence())
+func (ts *tstate) create(n string) sp.Tfid {
+	nfid := ts.walkPath(filepath.Dir(n))
+	args := sp.NewTcreate(nfid, filepath.Base(n), 0777, sp.ORDWR, sp.NoLeaseId, sp.NullFence())
 	rets := sp.Rcreate{}
 	rerr := ts.srv.Create(args, &rets)
 	assert.Nil(ts.t, rerr, "rerror %v", rerr)
+	return nfid
+}
+
+func (ts *tstate) mkdir(n string) {
+	nfid := ts.walk()
+	args := sp.NewTcreate(nfid, n, 0777|sp.DMDIR, sp.ORDWR, sp.NoLeaseId, sp.NullFence())
+	rets := sp.Rcreate{}
+	rerr := ts.srv.Create(args, &rets)
+	assert.Nil(ts.t, rerr, "rerror %v", rerr)
+	ts.clunk(nfid)
 }
 
 func (ts *tstate) remove(fid sp.Tfid) {
@@ -126,34 +147,27 @@ func (ts *tstate) stat(fid sp.Tfid) *sp.Tstat {
 	return &sp.Tstat{rets.Stat}
 }
 
-func (ts *tstate) mkDir() {
+func (ts *tstate) rename(dsrc, src, ddst, dst string) {
+	d0fid := ts.walkPath(dsrc)
+	d1fid := ts.walkPath(ddst)
+	args := sp.NewTrenameat(d0fid, src, d1fid, dst, sp.NullFence())
+	rets := sp.Rrenameat{}
+	rerr := ts.srv.Renameat(args, &rets)
+	assert.Nil(ts.t, rerr, "rerror %v", rerr)
+	ts.clunk(d0fid)
+	ts.clunk(d1fid)
+}
+
+func (ts *tstate) setupDir() {
 	s := time.Now()
-	for i := 1; i < D+1; i++ {
-		ts.walk(0, sp.Tfid(i))
-		ts.create(sp.Tfid(i), "ggg"+strconv.Itoa(i))
-		ts.clunk(sp.Tfid(i))
+	for i := 0; i < D; i++ {
+		fid := ts.create("ggg" + strconv.Itoa(i))
+		ts.clunk(fid)
 	}
 	t := time.Since(s)
 	if D > 0 {
-		db.DPrintf(db.TEST, "mkDir: %d creates %v us/op %f", D, t, float64(t.Microseconds())/float64(D))
-		db.DPrintf(db.TEST, "mkDir: len freelist %d", ts.srv.Stats())
-	}
-}
-
-func TestCreateRemove(t *testing.T) {
-	ns := []int{N}
-	for _, n := range ns {
-		ts := newTstate(t)
-		ts.mkDir()
-		s := time.Now()
-		for i := 1; i < n+1; i++ {
-			ts.walk(0, sp.Tfid(i))
-			ts.create(sp.Tfid(i), "fff"+strconv.Itoa(i))
-			ts.remove(sp.Tfid(i))
-		}
-		t := time.Since(s)
-		db.DPrintf(db.TEST, "%d create+remove %v us/op %f", n, t, float64(t.Microseconds())/float64(n))
-		db.DPrintf(db.TEST, "len freelist %d", ts.srv.Stats())
+		db.DPrintf(db.TEST, "setupDir: %d creates %v us/op %f", D, t, float64(t.Microseconds())/float64(D))
+		db.DPrintf(db.TEST, "setupDir: len freelist %d", ts.srv.Stats())
 	}
 }
 
@@ -162,9 +176,28 @@ func TestWalkClunk(t *testing.T) {
 	for _, n := range ns {
 		ts := newTstate(t)
 		s := time.Now()
-		for i := 1; i < n+1; i++ {
-			ts.walk(0, sp.Tfid(i))
-			ts.clunk(sp.Tfid(i))
+		for i := 0; i < n; i++ {
+			fid := ts.walk()
+			ts.clunk(fid)
+		}
+		t := time.Since(s)
+		db.DPrintf(db.TEST, "%d walk+clunk %v us/op %f", n, t, float64(t.Microseconds())/float64(n))
+		db.DPrintf(db.TEST, "len freelist %v", ts.srv.Stats())
+	}
+}
+
+func TestWalkPathClunk(t *testing.T) {
+	ns := []int{N}
+	for _, n := range ns {
+		ts := newTstate(t)
+		ts.setupDir()
+		ts.mkdir("d0")
+		fid := ts.create("d0/fff0")
+		ts.clunk(fid)
+		s := time.Now()
+		for i := 0; i < n; i++ {
+			fid := ts.walkPath("d0/fff0")
+			ts.clunk(fid)
 		}
 		t := time.Since(s)
 		db.DPrintf(db.TEST, "%d walk+clunk %v us/op %f", n, t, float64(t.Microseconds())/float64(n))
@@ -176,17 +209,52 @@ func TestWalkStat(t *testing.T) {
 	ns := []int{N}
 	for _, n := range ns {
 		ts := newTstate(t)
-		ts.mkDir()
+		ts.setupDir()
 		s := time.Now()
-		pn := path.Split("ggg1")
-		for i := 1; i < n+1; i++ {
-			nfid := sp.Tfid(1)
-			ts.walkPath(0, nfid, pn)
+		for i := 0; i < n+1; i++ {
+			nfid := ts.walkPath("ggg0")
 			ts.stat(nfid)
 			ts.clunk(nfid)
 		}
 		t := time.Since(s)
 		db.DPrintf(db.TEST, "%d walk+stat %v us/op %f", n, t, float64(t.Microseconds())/float64(n))
 		db.DPrintf(db.TEST, "len freelist %v", ts.srv.Stats())
+	}
+}
+
+func TestCreateRemove(t *testing.T) {
+	ns := []int{N}
+	for _, n := range ns {
+		ts := newTstate(t)
+		ts.setupDir()
+		s := time.Now()
+		for i := 0; i < n; i++ {
+			fid := ts.create("fff" + strconv.Itoa(i))
+			ts.remove(fid)
+		}
+		t := time.Since(s)
+		db.DPrintf(db.TEST, "%d create+remove %v us/op %f", n, t, float64(t.Microseconds())/float64(n))
+		db.DPrintf(db.TEST, "len freelist %d", ts.srv.Stats())
+	}
+}
+func TestCreateRenameat(t *testing.T) {
+	ns := []int{N}
+	for _, n := range ns {
+		ts := newTstate(t)
+		ts.mkdir("d0")
+		ts.mkdir("d1")
+		s := time.Now()
+		for i := 0; i < n; i++ {
+			fid := ts.create("d0/fff1")
+			ts.clunk(fid)
+
+			ts.rename("d0", "fff1", "d1", "fff2")
+
+			//nfid := ts.walkPath("d1/ggg2")
+			//ts.remove(nfid)
+		}
+		t := time.Since(s)
+		db.DPrintf(db.TEST, "%d create+renameat+remove %v us/op %f", n, t, float64(t.Microseconds())/float64(n))
+		db.DPrintf(db.TEST, "len freelist %d", ts.srv.Stats())
 	}
 }

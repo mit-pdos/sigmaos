5b0f01edbd1a7de11ec8e57c0a338c64c689000e
Ryan Chang <rychang@mit.edu>
2025-01-23 21:49:31 +0000

    Add small perf program to fttask tests

--- ft/task/fttask_test.go
+++ ft/task/fttask_test.go
@@ -2,6 +2,7 @@ package task_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 
@@ -92,6 +93,7 @@ func TestStats(t *testing.T) {
 	assert.Nil(t, err)
 
 	ft, err := fttask.NewFtTasks(ts.FsLib, TASKS, ts.job)
+	assert.Nil(t, err)
 	_, err = ft.RecoverTasks()
 	assert.Nil(t, err)
 
@@ -102,6 +104,58 @@ func TestStats(t *testing.T) {
 	assert.Nil(t, err)
 	sts = ft.GetStats()
 	assert.Equal(t, 2, sts.Ntask)
+
+	ts.shutdown()
+}
+
+func TestPerf(t *testing.T) {
+	nTasks := 1000
+
+	ts, err1 := newTstate(t)
+	if !assert.Nil(t, err1, "Error New Tstate: %v", err1) {
+		return
+	}
+
+	bin := make(mr.Bin, 1)
+	bin[0].File = "hello"
+
+	start := time.Now()
+	for i := 0; i < nTasks; i++ {
+		err := ts.ft.SubmitTask(0, bin)
+		assert.Nil(t, err)
+	}
+	db.DPrintf(db.ALWAYS, "Submitting tasks took %v (%v per task)", time.Since(start), time.Since(start) / time.Duration(nTasks))
+
+	start = time.Now()
+	tns, err := ts.ft.AcquireTasks()
+	db.DPrintf(db.ALWAYS, "Acquired tasks in %v (%v per task)", time.Since(start), time.Since(start) / time.Duration(nTasks))
+	assert.Nil(t, err)
+	assert.Equal(t, nTasks, len(tns))
+
+	start = time.Now()
+	for _, tn := range tns {
+		var b mr.Bin
+		err = ts.ft.ReadTask(tn, &b)
+		assert.Nil(t, err)
+		assert.Equal(t, bin, b)
+	}
+	db.DPrintf(db.ALWAYS, "Read tasks in %v (%v per task)", time.Since(start), time.Since(start) / time.Duration(nTasks))
+
+	start = time.Now()
+	for _, tn := range tns {
+		err = ts.ft.MarkDoneOutput(tn, "bye")
+		assert.Nil(t, err)
+	}
+	db.DPrintf(db.ALWAYS, "Marked all tasks done in %v (%v per task)", time.Since(start), time.Since(start) / time.Duration(nTasks))
+
+	start = time.Now()
+	for _, tn := range tns {
+		var output string
+		err = ts.ft.ReadTaskOutput(tn, &output)
+		assert.Nil(t, err)
+		assert.Equal(t, "bye", output)
+	}
+	db.DPrintf(db.ALWAYS, "Read all outputs in %v (%v per task)", time.Since(start), time.Since(start) / time.Duration(nTasks))
 
 	ts.shutdown()
 }

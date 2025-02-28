5e10486bb198734febb6bb0023cd99172fe0a84e
Ariel Szekely <arielck@mit.edu>
2025-02-28 06:57:37 -0500

    Use sp.Tsigmapath in top-level API spec

--- api/sigmaos/sigmaos.go
+++ api/sigmaos/sigmaos.go
@@ -22,16 +22,16 @@ type FileAPI interface {
 	// Core interface
 
 	CloseFd(fd int) error
-	Stat(path string) (*sp.Tstat, error)
-	Create(path string, p sp.Tperm, m sp.Tmode) (int, error)
+	Stat(path sp.Tsigmapath) (*sp.Tstat, error)
+	Create(path sp.Tsigmapath, p sp.Tperm, m sp.Tmode) (int, error)
 
 	// If w, then wait until path exists before opening it
-	Open(path string, m sp.Tmode, w Twait) (int, error)
+	Open(path sp.Tsigmapath, m sp.Tmode, w Twait) (int, error)
 
-	Rename(srcpath string, dstpath string) error
-	Remove(path string) error
-	GetFile(path string) ([]byte, error)
-	PutFile(path string, p sp.Tperm, m sp.Tmode, d []byte, o sp.Toffset, l sp.TleaseId) (sp.Tsize, error)
+	Rename(srcpath sp.Tsigmapath, dstpath sp.Tsigmapath) error
+	Remove(path sp.Tsigmapath) error
+	GetFile(path sp.Tsigmapath) ([]byte, error)
+	PutFile(path sp.Tsigmapath, p sp.Tperm, m sp.Tmode, d []byte, o sp.Toffset, l sp.TleaseId) (sp.Tsize, error)
 	Read(fd int, b []byte) (sp.Tsize, error)
 	Write(fd int, d []byte) (sp.Tsize, error)
 	Pread(fd int, b []byte, o sp.Toffset) (sp.Tsize, error)
@@ -39,11 +39,11 @@ type FileAPI interface {
 	Seek(fd int, o sp.Toffset) error
 
 	// Leases
-	CreateLeased(path string, p sp.Tperm, m sp.Tmode, l sp.TleaseId, f sp.Tfence) (int, error)
+	CreateLeased(path sp.Tsigmapath, p sp.Tperm, m sp.Tmode, l sp.TleaseId, f sp.Tfence) (int, error)
 	ClntId() sp.TclntId
 
 	// Fences
-	FenceDir(path string, f sp.Tfence) error
+	FenceDir(path sp.Tsigmapath, f sp.Tfence) error
 	WriteFence(fd int, d []byte, f sp.Tfence) (sp.Tsize, error)
 
 	// RPC
@@ -53,14 +53,14 @@ type FileAPI interface {
 	DirWatch(fd int) (int, error)
 
 	// Mounting
-	MountTree(ep *sp.Tendpoint, tree, mount string) error
+	MountTree(ep *sp.Tendpoint, tree, mount sp.Tsigmapath) error
 	IsLocalMount(ep *sp.Tendpoint) (bool, error)
-	PathLastMount(path string) (path.Tpathname, path.Tpathname, error)
+	PathLastMount(path sp.Tsigmapath) (path.Tpathname, path.Tpathname, error)
 	GetNamedEndpoint() (*sp.Tendpoint, error)
 	GetNamedEndpointRealm(realm sp.Trealm) (*sp.Tendpoint, error)
 	InvalidateNamedEndpointCacheEntryRealm(realm sp.Trealm) error
-	NewRootMount(path string, epname string) error
-	MountPathClnt(mnt string, pc PathClntAPI) error
+	NewRootMount(path sp.Tsigmapath, epname sp.Tsigmapath) error
+	MountPathClnt(mnt sp.Tsigmapath, pc PathClntAPI) error
 
 	// Done using SigmaOS, which detaches from any mounted servers and
 	// may close the session with those servers.
@@ -68,15 +68,15 @@ type FileAPI interface {
 
 	// Debugging
 	SetLocalMount(ep *sp.Tendpoint, port sp.Tport)
-	Mounts() []string
-	Detach(path string) error
-	Disconnect(path string) error
+	Mounts() []sp.Tsigmapath
+	Detach(path sp.Tsigmapath) error
+	Disconnect(path sp.Tsigmapath) error
 	Disconnected() bool
 }
 
 type PathClntAPI interface {
-	Open(pn string, principal *sp.Tprincipal, mode sp.Tmode, w Watch) (sp.Tfid, error)
-	Create(p string, principal *sp.Tprincipal, perm sp.Tperm, mode sp.Tmode, lid sp.TleaseId, f *sp.Tfence) (sp.Tfid, error)
+	Open(pn sp.Tsigmapath, principal *sp.Tprincipal, mode sp.Tmode, w Watch) (sp.Tfid, error)
+	Create(p sp.Tsigmapath, principal *sp.Tprincipal, perm sp.Tperm, mode sp.Tmode, lid sp.TleaseId, f *sp.Tfence) (sp.Tfid, error)
 	ReadF(fid sp.Tfid, off sp.Toffset, b []byte, f *sp.Tfence) (sp.Tsize, error)
 	PreadRdr(fid sp.Tfid, off sp.Toffset, len sp.Tsize) (io.ReadCloser, error)
 	WriteF(fid sp.Tfid, off sp.Toffset, data []byte, f *sp.Tfence) (sp.Tsize, error)

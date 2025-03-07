f100ed7872b40f913bc0ffc3f71ff3f9baffe506
Ariel Szekely <arielck@mit.edu>
2025-03-06 19:07:57 -0500

    Add debug selector for shard-related LB prints

--- debug/selector.go
+++ debug/selector.go
@@ -362,6 +362,7 @@ const (
 	SIM_QMGR                   = "SIM_QMGR"
 	SIM_QMGR_TIMEOUT           = "SIM_QMGR_TIMEOUT"
 	SIM_LB                     = "SIM_LB"
+	SIM_LB_SHARD               = "SIM_LB_SHARD"
 	SIM_RAW_LAT                = "SIM_RAW_LAT"
 	SIM_LAT_STATS              = "SIM_LAT_STATS"
 	SIM_UTIL_STATS             = "SIM_UTIL_STATS"

b102e6af0afbcce341b53aa7296cb991b7c5b44d
Ariel Szekely <arielck@mit.edu>
2025-04-09 20:04:28 -0400

    More debug logging for lcschedclnt

--- sched/lcsched/clnt/clnt.go
+++ sched/lcsched/clnt/clnt.go
@@ -1,6 +1,8 @@
 package lcschedclnt
 
 import (
+	"time"
+
 	db "sigmaos/debug"
 	"sigmaos/proc"
 	shardedsvcrpcclnt "sigmaos/rpc/shardedsvc/clnt"
@@ -8,6 +10,7 @@ import (
 	"sigmaos/serr"
 	"sigmaos/sigmaclnt/fslib"
 	sp "sigmaos/sigmap"
+	"sigmaos/util/perf"
 )
 
 const (
@@ -29,19 +32,24 @@ func NewLCSchedClnt(fsl *fslib.FsLib) *LCSchedClnt {
 // Enqueue a proc on the lcsched. Returns the ID of the kernel that is running
 // the proc.
 func (lcs *LCSchedClnt) Enqueue(p *proc.Proc) (string, error) {
+	start := time.Now()
 	pqID, err := lcs.rpcdc.WaitTimedRoundRobin()
 	if err != nil {
 		return NOT_ENQ, err
 	}
+	perf.LogSpawnLatency("LCSchedClnt.Enqueue.WaitTimed", p.GetPid(), p.GetSpawnTime(), start)
+	start = time.Now()
 	rpcc, err := lcs.rpcdc.GetClnt(pqID)
 	if err != nil {
 		db.DPrintf(db.ALWAYS, "Error: Can't get lcsched clnt: %v", err)
 		return NOT_ENQ, err
 	}
+	perf.LogSpawnLatency("LCSchedClnt.Enqueue.GetClnt", p.GetPid(), p.GetSpawnTime(), start)
 	req := &proto.EnqueueReq{
 		ProcProto: p.GetProto(),
 	}
 	res := &proto.EnqueueRep{}
+	start = time.Now()
 	if err := rpcc.RPC("LCSched.Enqueue", req, res); err != nil {
 		db.DPrintf(db.ALWAYS, "LCSched.Enqueue err %v", err)
 		if serr.IsErrCode(err, serr.TErrUnreachable) {
@@ -50,6 +58,7 @@ func (lcs *LCSchedClnt) Enqueue(p *proc.Proc) (string, error) {
 		}
 		return NOT_ENQ, err
 	}
+	perf.LogSpawnLatency("LCSchedClnt.Enqueue.RPC", p.GetPid(), p.GetSpawnTime(), start)
 	db.DPrintf(db.LCSCHEDCLNT, "[%v] Got Proc %v", p.GetRealm(), p)
 	return res.MSchedID, nil
 }

f561710de69a407629b52082c81e4eacf2c8d4f1
Ariel Szekely <arielck@mit.edu>
2025-04-17 19:43:47 -0400

    Invert the benchmark: start hotel first

--- benchmarks/benchmarks_test.go
+++ benchmarks/benchmarks_test.go
@@ -787,87 +787,6 @@ func TestRealmBalanceHotelRPCImgResize(t *testing.T) {
 // Start a realm with a long-running BE mr job. Then, start a realm with an LC
 // hotel job. In phases, ramp the hotel job's CPU utilization up and down, and
 // watch the realm-level software balance resource requests across realms.
-func TestRealmBalanceHotelSpinRPCImgResize(t *testing.T) {
-	done := make(chan bool)
-	mrts, err1 := test.NewMultiRealmTstate(t, []sp.Trealm{REALM2, REALM1})
-	if !assert.Nil(t, err1, "Error New Tstate: %v", err1) {
-		return
-	}
-	defer mrts.Shutdown()
-	blockers := blockMem(mrts.GetRoot(), BLOCK_MEM)
-	// Structures for imgresize
-	//	mrts.GetRealm(REALM2), err1 := test.NewRealmTstate(mrts.GetRoot(), REALM2)
-	//	if !assert.Nil(t, err1, "Error New Tstate: %v", err1) {
-	//		return
-	//	}
-	rs1 := benchmarks.NewResults(1, benchmarks.E2E)
-	p1 := newRealmPerf(mrts.GetRealm(REALM2))
-	defer p1.Done()
-	if PREWARM_REALM {
-		benchmarks.WarmupRealm(mrts.GetRealm(REALM2), []string{"imgresize", "imgresized"})
-	}
-	// Structure for hotel
-	//	mrts.GetRealm(REALM1), err1 := test.NewRealmTstate(mrts.GetRoot(), REALM1)
-	//	if !assert.Nil(t, err1, "Error New Tstate: %v", err1) {
-	//		return
-	//	}
-	rs2 := benchmarks.NewResults(1, benchmarks.E2E)
-	p2 := newRealmPerf(mrts.GetRealm(REALM1))
-	defer p2.Done()
-	// Prep ImgResize job
-	imgJobs, imgApps := newImgResizeRPCJob(mrts.GetRealm(REALM2), p1, true, IMG_RESIZE_INPUT_PATH, N_IMG_RESIZE_TASKS_PER_SECOND, IMG_RESIZE_DUR, proc.Tmcpu(IMG_RESIZE_MCPU), proc.Tmem(IMG_RESIZE_MEM_MB), IMG_RESIZE_N_ROUNDS, proc.Tmcpu(1000))
-	// Prep Hotel job
-	hotelJobs, ji := newHotelJobs(mrts.GetRealm(REALM1), p2, true, HOTEL_DURS, HOTEL_MAX_RPS, HOTEL_NCACHE, CACHE_TYPE, proc.Tmcpu(HOTEL_CACHE_MCPU), MANUALLY_SCALE_CACHES, SCALE_CACHE_DELAY, N_CACHES_TO_ADD, HOTEL_NGEO, MANUALLY_SCALE_GEO, SCALE_GEO_DELAY, N_GEO_TO_ADD, HOTEL_NGEO_IDX, HOTEL_GEO_SEARCH_RADIUS, HOTEL_GEO_NRESULTS, func(wc *hotel.WebClnt, r *rand.Rand) {
-		//		hotel.RunDSB(mrts.GetRealm(REALM1).T, 1, wc, r)
-		_, err := hotel.SpinReq(wc, HOTEL_N_SPIN)
-		assert.Nil(t, err, "SpinReq %v", err)
-	})
-	// Monitor cores assigned to ImgResize.
-	monitorCPUUtil(mrts.GetRealm(REALM2), p1)
-	// Monitor cores assigned to Hotel.
-	monitorCPUUtil(mrts.GetRealm(REALM1), p2)
-	// Run Hotel job
-	go func() {
-		runOps(mrts.GetRealm(REALM1), ji, runHotel, rs2)
-		done <- true
-	}()
-	// Wait for hotel jobs to set up.
-	<-hotelJobs[0].ready
-	db.DPrintf(db.TEST, "Hotel setup done.")
-	// Run ImgResize job
-	go func() {
-		runOps(mrts.GetRealm(REALM2), imgApps, runImgResizeRPC, rs1)
-		done <- true
-	}()
-	// Wait for imgResize jobs to set up.
-	<-imgJobs[0].ready
-	db.DPrintf(db.TEST, "Imgresize setup done.")
-	db.DPrintf(db.TEST, "Setup phase done.")
-	if N_CLNT > 1 {
-		// Wait for hotel clients to start up on other machines.
-		db.DPrintf(db.ALWAYS, "Leader waiting for %v clnts", N_CLNT)
-		waitForClnts(mrts.GetRoot(), N_CLNT)
-		db.DPrintf(db.ALWAYS, "Leader done waiting for clnts")
-	}
-	db.DPrintf(db.TEST, "Done waiting for hotel clnts.")
-	// Kick off ImgResize jobs.
-	imgJobs[0].ready <- true
-	// Sleep for a bit
-	time.Sleep(SLEEP)
-	// Kick off hotel jobs
-	hotelJobs[0].ready <- true
-	// Wait for both jobs to finish.
-	<-done
-	<-done
-	db.DPrintf(db.TEST, "Hotel and ImgResize done.")
-	printResultSummary(rs1)
-	time.Sleep(20 * time.Second)
-	evictMemBlockers(mrts.GetRoot(), blockers)
-}
-
-// Start a realm with a long-running BE mr job. Then, start a realm with an LC
-// hotel job. In phases, ramp the hotel job's CPU utilization up and down, and
-// watch the realm-level software balance resource requests across realms.
 func TestRealmBalanceHotelImgResize(t *testing.T) {
 	done := make(chan bool)
 	mrts, err1 := test.NewMultiRealmTstate(t, []sp.Trealm{REALM2, REALM1})
@@ -1348,38 +1267,6 @@ func TestHotelSigmaosJustCliGeo(t *testing.T) {
 	jobs, ji := newHotelJobsCli(mrts.GetRealm(REALM1), true, HOTEL_DURS, HOTEL_MAX_RPS, HOTEL_NCACHE, CACHE_TYPE, proc.Tmcpu(HOTEL_CACHE_MCPU), MANUALLY_SCALE_CACHES, SCALE_CACHE_DELAY, N_CACHES_TO_ADD, HOTEL_NGEO, MANUALLY_SCALE_GEO, SCALE_GEO_DELAY, N_GEO_TO_ADD, HOTEL_NGEO_IDX, HOTEL_GEO_SEARCH_RADIUS, HOTEL_GEO_NRESULTS, func(wc *hotel.WebClnt, r *rand.Rand) {
 		_, err := hotel.GeoReq(wc)
 		assert.Nil(t, err, "Error geo req: %v", err)
-	})
-	go func() {
-		for _, j := range jobs {
-			// Wait until ready
-			<-j.ready
-			// Ack to allow the job to proceed.
-			j.ready <- true
-		}
-	}()
-	runOps(mrts.GetRealm(REALM1), ji, runHotel, rs)
-	//	printResultSummary(rs)
-	//	jobs[0].requestK8sStats()
-}
-
-func TestHotelSigmaosJustCliSpin(t *testing.T) {
-	mrts, err1 := test.NewMultiRealmTstate(t, []sp.Trealm{})
-	if !assert.Nil(t, err1, "Error New Tstate: %v", err1) {
-		return
-	}
-	if err1 := waitForRealmCreation(mrts.GetRoot(), REALM1); !assert.Nil(t, err1, "Error waitRealmCreation: %v") {
-		return
-	}
-	if err1 := mrts.AddRealmClnt(REALM1); !assert.Nil(t, err1, "Error New Tstate: %v", err1) {
-		return
-	}
-	rs := benchmarks.NewResults(1, benchmarks.E2E)
-	clientReady(mrts.GetRoot())
-	// Sleep for a bit
-	time.Sleep(SLEEP)
-	jobs, ji := newHotelJobsCli(mrts.GetRealm(REALM1), true, HOTEL_DURS, HOTEL_MAX_RPS, HOTEL_NCACHE, CACHE_TYPE, proc.Tmcpu(HOTEL_CACHE_MCPU), MANUALLY_SCALE_CACHES, SCALE_CACHE_DELAY, N_CACHES_TO_ADD, HOTEL_NGEO, MANUALLY_SCALE_GEO, SCALE_GEO_DELAY, N_GEO_TO_ADD, HOTEL_NGEO_IDX, HOTEL_GEO_SEARCH_RADIUS, HOTEL_GEO_NRESULTS, func(wc *hotel.WebClnt, r *rand.Rand) {
-		_, err := hotel.SpinReq(wc, HOTEL_N_SPIN)
-		assert.Nil(t, err, "Error search req: %v", err)
 	})
 	go func() {
 		for _, j := range jobs {
@@ -1921,4 +1808,117 @@ func TestK8sSocialNetworkImgResize(t *testing.T) {
 	evictProcs(mrts.GetRealm(sp.ROOTREALM), ps)
 	time.Sleep(10 * time.Second)
 	evictMemBlockers(mrts.GetRoot(), blockers)
+}
+
+// Start a realm with a long-running BE mr job. Then, start a realm with an LC
+// hotel job. In phases, ramp the hotel job's CPU utilization up and down, and
+// watch the realm-level software balance resource requests across realms.
+func TestRealmBalanceHotelSpinRPCImgResize(t *testing.T) {
+	done := make(chan bool)
+	mrts, err1 := test.NewMultiRealmTstate(t, []sp.Trealm{REALM2, REALM1})
+	if !assert.Nil(t, err1, "Error New Tstate: %v", err1) {
+		return
+	}
+	defer mrts.Shutdown()
+	blockers := blockMem(mrts.GetRoot(), BLOCK_MEM)
+	// Structures for imgresize
+	//	mrts.GetRealm(REALM2), err1 := test.NewRealmTstate(mrts.GetRoot(), REALM2)
+	//	if !assert.Nil(t, err1, "Error New Tstate: %v", err1) {
+	//		return
+	//	}
+	rs1 := benchmarks.NewResults(1, benchmarks.E2E)
+	p1 := newRealmPerf(mrts.GetRealm(REALM2))
+	defer p1.Done()
+	if PREWARM_REALM {
+		benchmarks.WarmupRealm(mrts.GetRealm(REALM2), []string{"imgresize", "imgresized"})
+	}
+	// Structure for hotel
+	//	mrts.GetRealm(REALM1), err1 := test.NewRealmTstate(mrts.GetRoot(), REALM1)
+	//	if !assert.Nil(t, err1, "Error New Tstate: %v", err1) {
+	//		return
+	//	}
+	rs2 := benchmarks.NewResults(1, benchmarks.E2E)
+	p2 := newRealmPerf(mrts.GetRealm(REALM1))
+	defer p2.Done()
+	// Prep ImgResize job
+	imgJobs, imgApps := newImgResizeRPCJob(mrts.GetRealm(REALM2), p1, true, IMG_RESIZE_INPUT_PATH, N_IMG_RESIZE_TASKS_PER_SECOND, IMG_RESIZE_DUR, proc.Tmcpu(IMG_RESIZE_MCPU), proc.Tmem(IMG_RESIZE_MEM_MB), IMG_RESIZE_N_ROUNDS, proc.Tmcpu(1000))
+	// Prep Hotel job
+	hotelJobs, ji := newHotelJobs(mrts.GetRealm(REALM1), p2, true, HOTEL_DURS, HOTEL_MAX_RPS, HOTEL_NCACHE, CACHE_TYPE, proc.Tmcpu(HOTEL_CACHE_MCPU), MANUALLY_SCALE_CACHES, SCALE_CACHE_DELAY, N_CACHES_TO_ADD, HOTEL_NGEO, MANUALLY_SCALE_GEO, SCALE_GEO_DELAY, N_GEO_TO_ADD, HOTEL_NGEO_IDX, HOTEL_GEO_SEARCH_RADIUS, HOTEL_GEO_NRESULTS, func(wc *hotel.WebClnt, r *rand.Rand) {
+		//		hotel.RunDSB(mrts.GetRealm(REALM1).T, 1, wc, r)
+		_, err := hotel.SpinReq(wc, HOTEL_N_SPIN)
+		assert.Nil(t, err, "SpinReq %v", err)
+	})
+	// Monitor cores assigned to ImgResize.
+	monitorCPUUtil(mrts.GetRealm(REALM2), p1)
+	// Monitor cores assigned to Hotel.
+	monitorCPUUtil(mrts.GetRealm(REALM1), p2)
+	// Run Hotel job
+	go func() {
+		runOps(mrts.GetRealm(REALM1), ji, runHotel, rs2)
+		done <- true
+	}()
+	// Wait for hotel jobs to set up.
+	<-hotelJobs[0].ready
+	db.DPrintf(db.TEST, "Hotel setup done.")
+	// Run ImgResize job
+	go func() {
+		runOps(mrts.GetRealm(REALM2), imgApps, runImgResizeRPC, rs1)
+		done <- true
+	}()
+	// Wait for imgResize jobs to set up.
+	<-imgJobs[0].ready
+	db.DPrintf(db.TEST, "Imgresize setup done.")
+	db.DPrintf(db.TEST, "Setup phase done.")
+	if N_CLNT > 1 {
+		// Wait for hotel clients to start up on other machines.
+		db.DPrintf(db.ALWAYS, "Leader waiting for %v clnts", N_CLNT)
+		waitForClnts(mrts.GetRoot(), N_CLNT)
+		db.DPrintf(db.ALWAYS, "Leader done waiting for clnts")
+	}
+	db.DPrintf(db.TEST, "Done waiting for hotel clnts.")
+	// Kick off hotel jobs
+	hotelJobs[0].ready <- true
+	// Sleep for a bit
+	time.Sleep(SLEEP)
+	// Kick off ImgResize jobs.
+	imgJobs[0].ready <- true
+	// Wait for both jobs to finish.
+	<-done
+	<-done
+	db.DPrintf(db.TEST, "Hotel and ImgResize done.")
+	printResultSummary(rs1)
+	time.Sleep(20 * time.Second)
+	evictMemBlockers(mrts.GetRoot(), blockers)
+}
+
+func TestHotelSigmaosJustCliSpin(t *testing.T) {
+	mrts, err1 := test.NewMultiRealmTstate(t, []sp.Trealm{})
+	if !assert.Nil(t, err1, "Error New Tstate: %v", err1) {
+		return
+	}
+	if err1 := waitForRealmCreation(mrts.GetRoot(), REALM1); !assert.Nil(t, err1, "Error waitRealmCreation: %v") {
+		return
+	}
+	if err1 := mrts.AddRealmClnt(REALM1); !assert.Nil(t, err1, "Error New Tstate: %v", err1) {
+		return
+	}
+	rs := benchmarks.NewResults(1, benchmarks.E2E)
+	clientReady(mrts.GetRoot())
+	// Sleep for a bit
+	time.Sleep(SLEEP)
+	jobs, ji := newHotelJobsCli(mrts.GetRealm(REALM1), true, HOTEL_DURS, HOTEL_MAX_RPS, HOTEL_NCACHE, CACHE_TYPE, proc.Tmcpu(HOTEL_CACHE_MCPU), MANUALLY_SCALE_CACHES, SCALE_CACHE_DELAY, N_CACHES_TO_ADD, HOTEL_NGEO, MANUALLY_SCALE_GEO, SCALE_GEO_DELAY, N_GEO_TO_ADD, HOTEL_NGEO_IDX, HOTEL_GEO_SEARCH_RADIUS, HOTEL_GEO_NRESULTS, func(wc *hotel.WebClnt, r *rand.Rand) {
+		_, err := hotel.SpinReq(wc, HOTEL_N_SPIN)
+		assert.Nil(t, err, "Error search req: %v", err)
+	})
+	go func() {
+		for _, j := range jobs {
+			// Wait until ready
+			<-j.ready
+			// Ack to allow the job to proceed.
+			j.ready <- true
+		}
+	}()
+	runOps(mrts.GetRealm(REALM1), ji, runHotel, rs)
+	//	printResultSummary(rs)
+	//	jobs[0].requestK8sStats()
 }

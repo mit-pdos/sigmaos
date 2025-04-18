2cb57a9d62d846eade5de4735c49837d970dea1e
Ariel Szekely <arielck@mit.edu>
2025-04-17 21:11:31 -0400

    x

--- cmd/user/hotel-wwwd/main.go
+++ cmd/user/hotel-wwwd/main.go
@@ -8,10 +8,10 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 3 {
+	if len(os.Args) != 3 && len(os.Args) != 4 {
 		db.DFatalf("Usage: %v jobname cache", os.Args[0])
 	}
-	if err := hotel.RunWww(os.Args[1]); err != nil {
+	if err := hotel.RunWww(os.Args[1], len(os.Args) == 4); err != nil {
 		db.DFatalf("Start %v err %v\n", os.Args[0], err)
 	}
 }

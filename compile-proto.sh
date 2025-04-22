161ba0622139aa154accc0b06edd9744740d0e66
Ariel Szekely <arielck@mit.edu>
2025-04-22 19:45:29 -0400

    Compile CPP versions of the proc and sigmap proxy protos

--- compile-proto.sh
+++ compile-proto.sh
@@ -2,14 +2,30 @@
 
 # Note: order is important.
 
+echo "=== Compile golang proto ===" 
+
 for P in sigmap proc ; do
-  echo "protoc $P"
+  echo "protoc $P golang"
   protoc -I=. --go_out=../ $P/$P.proto
 done
 
 for PP in session dialproxy rpc ft/lease kernel sched/besched sched/lcsched sched/msched sched/msched/proc realm proxy/db util/k8s util/tracing apps/cache apps/kv/repl apps/kv apps/hotel apps/socialnetwork proxy/sigmap sched/msched/proc/chunk apps/imgresize proxy/mongo example/example_echo_server spproto/srv; do
   for P in $PP/proto/*.proto ; do
-    echo "protoc $P"
+    echo "protoc $P golang"
     protoc -I=. --go_out=../ $P
+  done
+done
+
+echo "=== Compile cpp proto ===" 
+
+for P in proc ; do
+  echo "protoc $P cpp"
+  protoc -I=. --cpp_out=./cpp $P/$P.proto
+done
+
+for PP in proxy/sigmap ; do
+  for P in $PP/proto/*.proto ; do
+    echo "protoc $P cpp"
+    protoc -I=. --cpp_out=./cpp $P
   done
 done

f19ccb3b0633c3e9e77461a899e848e39ac69739
Ariel Szekely <arielck@mit.edu>
2025-04-28 13:14:33 -0400

    Move sigmap proxy clnt initialization code to the constructor

--- cpp/proxy/sigmap/sigmap.cc
+++ cpp/proxy/sigmap/sigmap.cc
@@ -7,25 +7,6 @@ namespace sigmaos {
 namespace proxy::sigmap {
 
 std::expected<int, std::string> Clnt::Test() {
-  // Initialize the sigmaproxyd connection
-  {
-    SigmaInitReq req;
-    SigmaErrRep rep;
-    // Set the proc env proto
-    req.set_allocated_procenvproto(_env->GetProto());
-    // Execute the RPC
-    auto res = _rpcc->RPC("SPProxySrvAPI.Init", req, rep);
-    if (!res.has_value()) {
-      std::cout << "Err RPC: " << res.error() << std::endl;
-      return res;
-    }
-    if (rep.err().errcode() != 0) {
-      throw std::runtime_error(std::format("init rpc error: {}", rep.err().DebugString()));
-    }
-    std::cout << "Init RPC successful!" << std::endl;
-    // Make sure to release the proc env proto pointer so it isn't destroyed
-    req.release_procenvproto();
-  }
   {
     std::cout << "Init RPC successful!" << std::endl;
     SigmaNullReq req;

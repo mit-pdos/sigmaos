bf0d746e556de53068bf2483c862fef13a0400b1
Ariel Szekely <arielck@mit.edu>
2025-04-29 08:49:07 -0400

    Implement the concurrent call map

---
+++ cpp/io/demux/callmap.cc
@@ -0,0 +1,42 @@
+#include <io/demux/callmap.h>
+
+namespace sigmaos {
+namespace io::demux::internal {
+
+std::expected<int, std::string> CallMap::Put(uint64_t seqno, std::unique_ptr<std::promise<std::expected<std::shared_ptr<sigmaos::io::transport::Call>, std::string>>> p) {
+  std::lock_guard<std::mutex> guard(_mu);
+  if (_closed) {
+    return std::unexpected("Err: demux closed");
+  }
+  _calls[seqno] = std::move(p);
+}
+
+std::optional<std::unique_ptr<std::promise<std::expected<std::shared_ptr<sigmaos::io::transport::Call>, std::string>>>> CallMap::Remove(uint64_t seqno) {
+  if (!_calls.contains(seqno)) {
+    return std::nullopt;
+  }
+  auto call = std::move(_calls.extract(seqno).mapped());
+  return call;
+}
+
+std::vector<uint64_t> CallMap::Outstanding() {
+  std::vector<uint64_t> outstanding(_calls.size());
+  int i = 0;
+  for (const auto &pair: _calls) {
+    outstanding[i] = pair.first;
+  }
+  return outstanding;
+}
+
+std::expected<int, std::string> CallMap::Close() {
+  std::lock_guard<std::mutex> guard(_mu);
+  _closed = true;
+}
+
+bool CallMap::IsClosed() {
+  std::lock_guard<std::mutex> guard(_mu);
+  return _closed;
+}
+
+};
+};
---
+++ cpp/io/demux/callmap.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+#include <iostream>
+#include <memory>
+#include <expected>
+#include <map>
+#include <future>
+#include<mutex>
+
+#include <io/transport/transport.h>
+
+namespace sigmaos {
+namespace io::demux::internal {
+
+class CallMap {
+  public:
+  CallMap() : _mu(), _closed(false), _calls() {}
+  ~CallMap() {}
+
+  std::expected<int, std::string> Put(uint64_t seqno, std::unique_ptr<std::promise<std::expected<std::shared_ptr<sigmaos::io::transport::Call>, std::string>>> future);
+  std::optional<std::unique_ptr<std::promise<std::expected<std::shared_ptr<sigmaos::io::transport::Call>, std::string>>>> Remove(uint64_t seqno);
+  std::vector<uint64_t> Outstanding();
+  std::expected<int, std::string> Close();
+  bool IsClosed();
+
+  private:
+  std::mutex _mu;
+  bool _closed;
+  std::map<uint64_t, std::unique_ptr<std::promise<std::expected<std::shared_ptr<sigmaos::io::transport::Call>, std::string>>>> _calls;
+};
+
+};
+};

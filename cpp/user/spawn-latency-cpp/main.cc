69c83051adca19542aa6472a7d55683768607bc5
Ariel Szekely <arielck@mit.edu>
2025-05-01 10:12:40 -0400

    Use log library in CPP user proc

--- cpp/user/spawn-latency-cpp/main.cc
+++ cpp/user/spawn-latency-cpp/main.cc
@@ -1,19 +1,19 @@
 #include <iostream>
 
-#include <proxy/sigmap/sigmap.h>
 #include <util/log/log.h>
+#include <proxy/sigmap/sigmap.h>
+
+const std::string CPP_USER_PROC = "CPP_USER_PROC";
 
 int main() {
-  auto sdbg_sink = std::make_shared<sigmaos::util::log::sigmadebug_sink>("TEST234");
-  auto log = std::make_shared<spdlog::logger>("main", sdbg_sink);
-  spdlog::register_logger(log);
-  spdlog::get("main")->info("test spdlog log");
-  std::cout << "Running" << std::endl;
+  sigmaos::util::log::init_logger(CPP_USER_PROC);
+  log(CPP_USER_PROC, "Running");
   auto sp_clnt = std::make_unique<sigmaos::proxy::sigmap::Clnt>();
 
   // Test connection to spproxyd
+  log(CPP_USER_PROC, "Test");
   sp_clnt->Test();
-  std::cout << "done testing" << std::endl;
+  log(CPP_USER_PROC, "Done testing");
   return 1;
   return 0;
 }

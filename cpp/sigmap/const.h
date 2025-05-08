a6b5cf44e628291d3810d90b30c3db7b1c72a35e
Ariel Szekely <arielck@mit.edu>
2025-05-07 20:59:52 -0400

    x

--- cpp/sigmap/const.h
+++ cpp/sigmap/const.h
@@ -22,15 +22,15 @@ const sigmaos::sigmap::types::TleaseID NO_LEASE_ID = ~0;
 // Qid consts
 // A Qid's type field represents the type of a file, the high 8 bits of
 // the file's permission.
-sigmaos::sigmap::types::TQversion NoV = ~0;
-sigmaos::sigmap::types::Qtype QTDIR     = 0x80;
-sigmaos::sigmap::types::Qtype QTAPPEND  = 0x40;
-sigmaos::sigmap::types::Qtype QTEXCL    = 0x20;
-sigmaos::sigmap::types::Qtype QTMOUNT   = 0x10;
-sigmaos::sigmap::types::Qtype QTAUTH    = 0x08;
-sigmaos::sigmap::types::Qtype QTTMP     = 0x04;
-sigmaos::sigmap::types::Qtype QTSYMLINK = 0x02;
-sigmaos::sigmap::types::Qtype QTFILE    = 0x00;
+const sigmaos::sigmap::types::TQversion NoV = ~0;
+const sigmaos::sigmap::types::Qtype QTDIR     = 0x80;
+const sigmaos::sigmap::types::Qtype QTAPPEND  = 0x40;
+const sigmaos::sigmap::types::Qtype QTEXCL    = 0x20;
+const sigmaos::sigmap::types::Qtype QTMOUNT   = 0x10;
+const sigmaos::sigmap::types::Qtype QTAUTH    = 0x08;
+const sigmaos::sigmap::types::Qtype QTTMP     = 0x04;
+const sigmaos::sigmap::types::Qtype QTSYMLINK = 0x02;
+const sigmaos::sigmap::types::Qtype QTFILE    = 0x00;
 
 };
 };

56b0eecc1bb07b4442706a44ee5df8be36d67409
Ariel Szekely <arielck@mit.edu>
2025-05-07 20:54:10 -0400

    Add some protocol-level constants

--- cpp/sigmap/const.h
+++ cpp/sigmap/const.h
@@ -10,15 +10,14 @@ const sigmaos::sigmap::types::Tsize KBYTE = 1 << 10;
 const sigmaos::sigmap::types::Tsize MBYTE = 1 << 20;
 const sigmaos::sigmap::types::Tsize GBYTE = 1 << 30;
 const sigmaos::sigmap::types::Tsize BUFSZ = 1 * MBYTE;
-const sigmaos::sigmap::types::Tsize MAXGETSET Tsize = MBYTE // If need more than MaxGetSet, use Open/Read/Close interface
+const sigmaos::sigmap::types::Tsize MAXGETSET = MBYTE; // If need more than MaxGetSet, use Open/Read/Close interface
 
-//// Size constants
-//const (
-//	KBYTE           = 1 << 10
-//	MBYTE           = 1 << 20
-//	GBYTE           = 1 << 30
-//	BUFSZ           = 1 * MBYTE
-//	MAXGETSET Tsize = MBYTE // If need more than MaxGetSet, use Open/Read/Close interface
-//)
+// Protocol constants
+const sigmaos::sigmap::types::Tfid NO_FID = ~0;
+const sigmaos::sigmap::types::Tpath NO_PATH = ~0;
+const sigmaos::sigmap::types::Toffset NO_OFFSET = ~0;
+const sigmaos::sigmap::types::TclntID NO_CLNT_ID = ~0;
+const sigmaos::sigmap::types::TleaseID NO_LEASE_ID = ~0;
+
 };
 };

84d4acdf8925f594a9999b5304d7272f535036c5
Ariel Szekely <arielck@mit.edu>
2025-05-07 20:58:31 -0400

    Add qid type consts

--- cpp/sigmap/const.h
+++ cpp/sigmap/const.h
@@ -19,5 +19,18 @@ const sigmaos::sigmap::types::Toffset NO_OFFSET = ~0;
 const sigmaos::sigmap::types::TclntID NO_CLNT_ID = ~0;
 const sigmaos::sigmap::types::TleaseID NO_LEASE_ID = ~0;
 
+// Qid consts
+// A Qid's type field represents the type of a file, the high 8 bits of
+// the file's permission.
+sigmaos::sigmap::types::TQversion NoV = ~0;
+sigmaos::sigmap::types::Qtype QTDIR     = 0x80;
+sigmaos::sigmap::types::Qtype QTAPPEND  = 0x40;
+sigmaos::sigmap::types::Qtype QTEXCL    = 0x20;
+sigmaos::sigmap::types::Qtype QTMOUNT   = 0x10;
+sigmaos::sigmap::types::Qtype QTAUTH    = 0x08;
+sigmaos::sigmap::types::Qtype QTTMP     = 0x04;
+sigmaos::sigmap::types::Qtype QTSYMLINK = 0x02;
+sigmaos::sigmap::types::Qtype QTFILE    = 0x00;
+
 };
 };

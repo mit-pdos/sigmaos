590c635784de4c896491290521c2cb7a22e42248
Ivy Wu <ivywu@mit.edu>
2025-05-13 03:25:46 +0000

    Initial benchmark

--- pyproc/stat_test.py
+++ pyproc/stat_test.py
@@ -1,9 +1,18 @@
 import splib
+import time
 
+ms_start = time.time() * 1000.0
 splib.Started()
+ms_end = time.time() * 1000.0
+
+ms_start_stat = time.time() * 1000.0
 stat_res = splib.Stat("name/s3/~any/ivywu/dummy_package.py")
-if stat_res:
-    print("Name:", stat_res.contents.name.decode())
-    print("Type:", stat_res.contents.type)
-    print("Length:", stat_res.contents.length)
+ms_end_stat = time.time() * 1000.0
+
+print("Started:", ms_end - ms_start, "ms")
+print("Stat:   ", ms_end_stat - ms_start_stat, "ms")
+
+ms_start_exit = time.time() * 1000.0
 splib.Exited(1, "Exited normally!")
+ms_end_exit = time.time() * 1000.0
+print("Exited: ", ms_end_exit - ms_start_exit, "ms")

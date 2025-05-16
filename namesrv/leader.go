37fc3364a542b93de285a298a2515e5e8ccca98b
Frans Kaashoek <kaashoek@mit.edu>
2025-05-16 13:06:28 -0400

    x

--- namesrv/leader.go
+++ namesrv/leader.go
@@ -50,7 +50,7 @@ func (nd *Named) startLeader() error {
 
 	fs.Fence(nd.elect.Key(), nd.elect.Rev())
 
-	db.DPrintf(db.NAMED, "leader %v %v\n", nd.realm, nd.elect.Key())
+	db.DPrintf(db.NAMED_LDR, "leader %v %v\n", nd.realm, nd.elect.Key())
 
 	return nil
 }

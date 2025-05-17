a7e7f8dad52a47a09567893bec4343b099a4ae6c
Frans Kaashoek <kaashoek@mit.edu>
2025-05-17 13:55:10 -0400

    Add test to check that Read returns EOF on server when client closes connection.

--- dialproxy/dialproxy_test.go
+++ dialproxy/dialproxy_test.go
@@ -1,6 +1,7 @@
 package dialproxy_test
 
 import (
+	"io"
 	"net"
 	"testing"
 	"time"
@@ -204,8 +205,8 @@ func TestNamedEndpoint(t *testing.T) {
 	ts.Shutdown()
 }
 
-// Make sure client sees server failure works
-func TestServerFail(t *testing.T) {
+// Make sure client receives EOF when server closes connection
+func TestServerClose(t *testing.T) {
 	ts, err1 := test.NewTstate(t)
 	if !assert.Nil(t, err1, "Error New Tstate: %v", err1) {
 		return
@@ -237,7 +238,49 @@ func TestServerFail(t *testing.T) {
 		b := make([]byte, len(TEST_MSG))
 		_, err := conn.Read(b)
 		assert.NotNil(t, err)
+		assert.Equal(t, io.EOF, err)
 		assert.True(t, time.Since(s).Seconds() < 2)
 	}
+	ts.Shutdown()
+}
+
+// Make sure server sees client close
+func TestClientClose(t *testing.T) {
+	ts, err1 := test.NewTstate(t)
+	if !assert.Nil(t, err1, "Error New Tstate: %v", err1) {
+		return
+	}
+	addr := sp.NewTaddr(IP, PORT)
+	npc := ts.GetDialProxyClnt()
+	// Create a listener via dialproxy
+	ep, l, err := npc.Listen(sp.INTERNAL_EP, addr)
+	assert.Nil(t, err, "Err Listen: %v", err)
+	c := make(chan bool)
+	go func(l net.Listener, ch chan bool) {
+		b := make([]byte, len(TEST_MSG))
+		// Accept connections
+		conn, err := l.Accept()
+		assert.Nil(t, err, "Err accept: %v", err)
+		n, err := conn.Read(b)
+		assert.Nil(t, err, "Err read: %v", err)
+		assert.Equal(t, len(b), n, "Err read nbyte: %v != %v", len(b), n)
+		// Wait for Read() to receive EOF
+		s := time.Now()
+		_, err = conn.Read(b)
+		assert.NotNil(t, err)
+		assert.True(t, time.Since(s).Seconds() < 2)
+		ch <- true
+		conn.Close()
+		l.Close()
+	}(l, c)
+	// Dial the listener
+	conn, err := npc.Dial(ep)
+	assert.Nil(t, err, "Err Dial: %v", err)
+	n, err := conn.Write([]byte(TEST_MSG))
+	if assert.Nil(t, err, "Err Write: %v", err) {
+		assert.Equal(t, len(TEST_MSG), n, "Err Write nbyte: %v != %v", len(TEST_MSG), n)
+	}
+	conn.Close()
+	<-c
 	ts.Shutdown()
 }

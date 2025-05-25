bb34f46b9dabf5493c9469f3a135060ba2316720
Ariel Szekely <arielck@mit.edu>
2025-05-25 11:47:48 -0400

    Use std::chrono::high_resolution_clock to get timestamps in C++ code

--- cpp/user/spin-srv-cpp/main.cc
+++ cpp/user/spin-srv-cpp/main.cc
@@ -14,7 +14,6 @@
 
 int main(int argc, char *argv[]) {
   auto pe = sigmaos::proc::GetProcEnv();
-  log(ALWAYS, "Spawn time: {}, Current time: {} CT nanos: {}", pe->GetSpawnTime().ShortDebugString(), google::protobuf::util::TimeUtil::GetCurrentTime().ShortDebugString(), google::protobuf::util::TimeUtil::GetCurrentTime().nanos());
   LogSpawnLatency(pe->GetPID(), pe->GetSpawnTime(), google::protobuf::util::TimeUtil::GetEpoch(), "E2e spawn time since spawn until main");
   LogSpawnLatency(pe->GetPID(), google::protobuf::util::TimeUtil::GetEpoch(), sigmaos::proc::GetExecTime(), "proc.exec_proc");
   sigmaos::util::log::init_logger(sigmaos::apps::spin::SPINSRV);
--- cpp/util/perf/perf.cc
+++ cpp/util/perf/perf.cc
@@ -1,5 +1,7 @@
 #include <util/perf/perf.h>
 
+#include <chrono>
+
 #include <google/protobuf/util/time_util.h>
 
 #include <util/log/log.h>
@@ -7,16 +9,17 @@
 google::protobuf::Timestamp epoch = google::protobuf::util::TimeUtil::GetEpoch();
 
 void LogSpawnLatency(sigmaos::sigmap::types::Tpid pid, google::protobuf::Timestamp spawn_time, google::protobuf::Timestamp op_start, std::string msg) {
-  auto t = google::protobuf::util::TimeUtil::GetCurrentTime();
+  // Calculate current unix time in msec
+  auto current_unix_time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
   double st_lat = 0;
   double op_lat = 0;
   // If spawn time was set, calculate latency
   if (spawn_time.seconds() != epoch.seconds() || spawn_time.nanos() != epoch.nanos()) {
-    st_lat = (double) (t.seconds() - spawn_time.seconds()) + ((double) t.nanos() - spawn_time.nanos()) / (1E9);
+    st_lat = current_unix_time - ((double) spawn_time.seconds() * 1000 + (double) spawn_time.nanos() / (1E6));
   }
   // If op time was set, calculate latency
   if (op_start.seconds() != epoch.seconds() || op_start.nanos() != epoch.nanos()) {
-    op_lat = (double) (t.seconds() - op_start.seconds()) + ((double) t.nanos() - op_start.nanos()) / (1E9);
+    op_lat = current_unix_time - ((double) op_start.seconds() * 1000 + (double) op_start.nanos() / (1E6));
   }
-  log(SPAWN_LAT, "[{}] {} op:{} sinceSpawn:{}", pid, msg, op_lat, st_lat);
+  log(SPAWN_LAT, "[{}] {} op:{}ms sinceSpawn:{}ms", pid, msg, op_lat, st_lat);
 }

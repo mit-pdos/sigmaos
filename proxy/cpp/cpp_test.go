a95a86581d5ff8b4315ce4331f3c5f1d43111115
Ariel Szekely <arielck@mit.edu>
2025-05-27 08:58:49 -0400

    x

--- proxy/cpp/cpp_test.go
+++ proxy/cpp/cpp_test.go
@@ -311,34 +311,24 @@ func TestSpinServerSpawnLatency(t *testing.T) {
 }
 
 func TestSpinServerExec(t *testing.T) {
-	//	mrts, err1 := test.NewMultiRealmTstate(t, []sp.Trealm{test.REALM1})
-	//	if !assert.Nil(t, err1, "Error New Tstate: %v", err1) {
-	//		return
-	//	}
-	//	defer mrts.Shutdown()
-	//
-	//	// Make union dir
-	//	if err := mrts.GetRealm(test.REALM1).MkDir(SRV_UNION_DIR, 0777); !assert.Nil(mrts.T, err, "Err mkunion") {
-	//		return
-	//	}
-	//
-	//	if err := mrts.GetRealm(test.REALM1).BootNode(N_NODE); !assert.Nil(t, err, "Err boot: %v", err) {
-	//		return
-	//	}
-
-	p := proc.NewProc("spin-srv-cpp", nil)
-	p.SetSpawnTime(time.Now())
-	homedir, err := os.UserHomeDir()
-	if !assert.Nil(t, err, "Err homedir: %v", err) {
-		return
+	const (
+		N_TRIALS = 10
+	)
+	for i := 0; i < N_TRIALS; i++ {
+		p := proc.NewProc("spin-srv-cpp", nil)
+		p.SetSpawnTime(time.Now())
+		homedir, err := os.UserHomeDir()
+		if !assert.Nil(t, err, "Err homedir: %v", err) {
+			return
+		}
+		p.FinalizeEnv("xxx", "yyy", "zzz")
+		cmd := exec.Command(filepath.Join(homedir, "sigmaos/bin/user", p.GetVersionedProgram()))
+		cmd.Env = append(cmd.Env, p.GetEnv()...)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Start(); !assert.Nil(t, err, "Err command start: %v", err) {
+			return
+		}
+		cmd.Wait()
 	}
-	p.FinalizeEnv("xxx", "yyy", "zzz")
-	cmd := exec.Command(filepath.Join(homedir, "sigmaos/bin/user", p.GetVersionedProgram()))
-	cmd.Env = append(cmd.Env, p.GetEnv()...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	if err := cmd.Start(); !assert.Nil(t, err, "Err command start: %v", err) {
-		return
-	}
-	cmd.Wait()
 }

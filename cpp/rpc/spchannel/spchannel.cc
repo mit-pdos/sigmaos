7a8e24390634b145e31812e47523e3d456ce7e8a
Ariel Szekely <arielck@mit.edu>
2025-05-31 14:59:30 -0400

    Start to implement a C++ RPC client for sigmap-exposed services

---
+++ cpp/rpc/spchannel/spchannel.cc
@@ -0,0 +1,65 @@
+#include <rpc/spchannel/spchannel.h>
+
+namespace sigmaos {
+namespace rpc::spchannel {
+
+bool Channel::_l = sigmaos::util::log::init_logger(SPCHAN);
+bool Channel::_l_e = sigmaos::util::log::init_logger(SPCHAN_ERR);
+
+// Initialize the channel & connect to the server
+std::expected<int, sigmaos::serr::Error> Channel::Init() {
+  // TODO: use consts
+  std::string clone_pn = _srv_pn + "/rpc/clone";
+  {
+    std::string msg = std::format("Hello world! I'm a CPP proc!");
+    log(TEST, "PutFile message \"{}\" length {}", msg, msg.size());
+    auto res = _sp_clnt->PutFile("name/hello-cpp-1", 0777, 0, &msg, 0, 0);
+    if (!res.has_value()) {
+      log(ALWAYS, "Err RPC: {}", res.error());
+      return std::unexpected(res.error());
+    }
+  }
+  {
+    auto res = _sp_clnt->GetFile(clone_pn);
+    if (!res.has_value()) {
+      log(SPCHAN_ERR, "Error GetFile clone file({}): {}", clone_pn, res.error().String());
+      return std::unexpected(res.error());
+    }
+    _sid = *res.value();
+  }
+  {
+    // TODO: use consts
+    std::string sess_dev_pn = _srv_pn + "/rpc/" + _sid + "/data";
+    auto res = _sp_clnt->Open(sess_dev_pn, sigmaos::sigmap::constants::ORDWR, false);
+    if (!res.has_value()) {
+      log(SPCHAN_ERR, "Error Open data file({}): {}", sess_dev_pn, res.error().String());
+      return std::unexpected(res.error());
+    }
+    _fd = res.value();
+  }
+  log(SPCHAN, "Successfully initialized sigmap-based RPC channel to {}", _srv_pn);
+  return 0;
+}
+
+std::expected<std::shared_ptr<sigmaos::io::transport::Call>, sigmaos::serr::Error> Channel::SendReceive(std::shared_ptr<sigmaos::io::transport::Call> call) {
+  {
+    auto res = _sp_clnt->WriteRead(_fd, call->GetInIOVec(), call->GetOutIOVec());
+    if (!res.has_value()) {
+      log(SPCHAN_ERR, "Error WriteRead({} -> {}): {}", _fd, _srv_pn, res.error().String());
+      return std::unexpected(res.error());
+    }
+  }
+  return call;
+}
+
+std::expected<int, sigmaos::serr::Error> Channel::Close() {
+  _closed = true;
+  return _sp_clnt->CloseFD(_fd);
+}
+
+bool Channel::IsClosed() {
+  return _closed;
+}
+
+};
+};
---
+++ cpp/rpc/spchannel/spchannel.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <expected>
+#include <atomic>
+
+#include <google/protobuf/message.h>
+
+#include <util/log/log.h>
+#include <serr/serr.h>
+#include <io/demux/clnt.h>
+#include <io/iovec/iovec.h>
+#include <rpc/proto/rpc.pb.h>
+#include <proxy/sigmap/sigmap.h>
+
+namespace sigmaos {
+namespace rpc::spchannel {
+
+const std::string SPCHAN = "SPCHAN";
+const std::string SPCHAN_ERR = SPCHAN + sigmaos::util::log::ERR;
+
+// A channel/connection over which to make RPCs
+class Channel {
+  public:
+// TODO: make from endpoint
+  Channel(std::string srv_pn, std::shared_ptr<sigmaos::proxy::sigmap::Clnt> sp_clnt) : _srv_pn(srv_pn), _sp_clnt(sp_clnt), _closed(false) {}
+  ~Channel() {}
+  // Initialize the channel
+  std::expected<int, sigmaos::serr::Error> Init();
+  std::expected<std::shared_ptr<sigmaos::io::transport::Call>, sigmaos::serr::Error> SendReceive(std::shared_ptr<sigmaos::io::transport::Call> call);
+  std::expected<int, sigmaos::serr::Error> Close();
+  bool IsClosed();
+  private:
+  std::string _srv_pn;
+  std::string _sid;
+  int _fd;
+  std::shared_ptr<sigmaos::proxy::sigmap::Clnt> _sp_clnt;
+  bool _closed;
+  // Used for logger initialization
+  static bool _l;
+  static bool _l_e;
+};
+
+};
+};

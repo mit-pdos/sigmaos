5aa5499d4d7afa68f6aa20963bb4c4475f9dc1c8
Ariel Szekely <arielck@mit.edu>
2025-06-01 15:13:45 -0400

    Implement key2shard in cpp cacheclnt lib

--- cpp/apps/cache/cache.h
+++ cpp/apps/cache/cache.h
@@ -6,7 +6,14 @@ namespace sigmaos {
 namespace apps::cache {
 
 uint32_t key2shard(std::string key) {
-  fatal("unimplemented");
+  // fnv32a hash inspired by https://cs.opensource.google/go/go/+/refs/tags/go1.24.3:src/hash/fnv/fnv.go;l=51
+  uint32_t s = 2166136261;
+  uint32_t prime32 = 16777619;
+  for (int i = 0; i < key.size(); i++) {
+    s ^= (uint32_t) key[i];
+    s *= prime32;
+  }
+  return s;
 }
 
 };

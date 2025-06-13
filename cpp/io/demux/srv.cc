72ae6a0b93620500e73b0f0bca532c7af338f379
Ariel Szekely <arielck@mit.edu>
2025-06-13 13:51:38 -0400

    Add some performance logging to CPP demuxsrv

--- cpp/io/demux/srv.cc
+++ cpp/io/demux/srv.cc
@@ -9,37 +9,39 @@ bool Srv::_l_e = sigmaos::util::log::init_logger(DEMUXSRV_ERR);
 void Srv::read_requests() {
   while (true) {
     if (IsClosed()) {
-      log(DEMUXSRV, "demuxsrv closed, reader thread exiting");
+      log(DEMUXSRV, "closed, reader thread exiting");
       break;
     }
     auto res = _trans->ReadCall();
     if (!res.has_value()) {
-      log(DEMUXSRV_ERR, "demuxsrv read call error: {}", res.error().String());
+      log(DEMUXSRV_ERR, "read call error: {}", res.error().String());
       // TODO: report error
       break;
     }
-    log(DEMUXSRV, "demuxsrv done reading call");
     auto req = res.value();
+    log(DEMUXSRV, "done reading call seqno:{}", req->GetSeqno());
     // Swap input/output IOV, because transport package is written from the
     // client perspective.
     req->SwapIOVecs();
-    log(DEMUXSRV, "demuxsrv done reading call {}, handle request", req->GetSeqno());
+    log(DEMUXSRV, "done reading call {}, handle request", req->GetSeqno());
     _thread_pool.Run(std::bind(&Srv::handle_request, this, res.value()));
   }
 }
 
 void Srv::handle_request(std::shared_ptr<sigmaos::io::transport::Call> req) {
-  log(DEMUXSRV, "demuxsrv handle request seqno: {}", req->GetSeqno());
+  log(DEMUXSRV, "handle_request({})", req->GetSeqno());
   std::shared_ptr<sigmaos::io::transport::Call> rep;
   {
     // Serve the request
+    auto start = GetCurrentTime();
     auto res = _serve_request(req);
     if (!res.has_value()) {
-      log(DEMUXSRV_ERR, "demuxsrv serve_request error: {}", res.error().String());
+      log(DEMUXSRV_ERR, "demuxsrv serve_request({}) error: {}", req->GetSeqno(), res.error().String());
       fatal("demuxsrv serve_request unimplemented: {}", res.error().String());
       // TODO: should we return here?
       return;
     }
+    log(DEMUXSRV, "serve_request({}) latency={:0.3f}ms", req->GetSeqno(), LatencyMS(start));
     rep = res.value();
   }
   {
@@ -48,10 +50,12 @@ void Srv::handle_request(std::shared_ptr<sigmaos::io::transport::Call> req) {
     rep->SwapIOVecs();
     // Write the reply
     std::lock_guard<std::mutex> guard(_mu);
+    auto start = GetCurrentTime();
     auto res = _trans->WriteCall(rep);
     if (!res.has_value()) {
       log(DEMUXSRV_ERR, "demuxsrv WriteCall error: {}", res.error().String());
     }
+    log(DEMUXSRV, "WriteReply({}) latency={:0.3f}ms", req->GetSeqno(), LatencyMS(start));
   }
 }
 

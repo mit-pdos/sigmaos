f315f8663bcf3972a7c9913a6e03f27590233e1e
Ariel Szekely <arielck@mit.edu>
2025-06-12 21:53:18 -0400

    x

--- apps/cossim/clnt/clnt.go
+++ apps/cossim/clnt/clnt.go
@@ -55,7 +55,7 @@ func NewCosSimClnt(fsl *fslib.FsLib, epcc *epcacheclnt.EndpointCacheClnt, srvID 
 // Register a service's endpoint
 func (clnt *CosSimClnt) CosSim(v []float64, ranges []*proto.VecRange) (uint64, float64, error) {
 	reqID := clnt.reqCntr.Add(1)
-	db.DPrintf(db.COSSIMCLNT, "CosSim: %v ranges:%v", len(v), ranges)
+	db.DPrintf(db.COSSIMCLNT, "CosSim(%v): %v ranges:%v", reqID, len(v), ranges)
 	start := time.Now()
 	var res proto.CosSimRep
 	req := &proto.CosSimReq{
@@ -70,6 +70,6 @@ func (clnt *CosSimClnt) CosSim(v []float64, ranges []*proto.VecRange) (uint64, f
 		db.DPrintf(db.COSSIMCLNT_ERR, "Err CosSim: %v", err)
 		return 0, 0.0, err
 	}
-	db.DPrintf(db.COSSIMCLNT, "CosSim ok: %v %v -> id:%v val:%v lat:%v", len(v), ranges, res.ID, res.Val, time.Since(start))
+	db.DPrintf(db.COSSIMCLNT, "CosSim(%v) ok: %v %v -> id:%v val:%v lat:%v", reqID, len(v), ranges, res.ID, res.Val, time.Since(start))
 	return res.ID, res.Val, nil
 }

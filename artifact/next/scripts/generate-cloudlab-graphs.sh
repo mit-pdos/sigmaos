9a8c2a3aa3a086d0efe1f5923b9a4e43dd1e2af7
Ariel Szekely <arielck@mit.edu>
2025-06-17 09:04:15 -0400

    x

--- artifact/next/scripts/generate-cloudlab-graphs.sh
+++ artifact/next/scripts/generate-cloudlab-graphs.sh
@@ -15,7 +15,7 @@ $GRAPH_SCRIPTS_DIR/aggregate-tpt-talk.py \
   --measurement_dir_k8s $RES_OUT_DIR/cos_sim_tail_latency_eager_scale_cossim_add_1 \
   --out $GRAPH_OUT_DIR/cossim_tail_latency_eager_scale_vs_noscale.pdf \
   --be_realm "" --hotel_realm benchrealm1 \
-  --units "Req/sec,2-srv,Scale 1->2 srv" \
+  --units "Req/sec,2-srv,Scale 1→2 srv" \
   --title "x" --total_ncore 32 --prefix "imgresize-" \
   --xmin 10000 --xmax 65000 #--legend_on_right 
 echo "Done generating eager cossim graph..."
@@ -28,7 +28,7 @@ $GRAPH_SCRIPTS_DIR/aggregate-tpt-talk.py \
   --measurement_dir_k8s $RES_OUT_DIR/cos_sim_tail_latency_scale_cossim_add_1 \
   --out $GRAPH_OUT_DIR/cossim_tail_latency_lazy_scale_vs_noscale.pdf \
   --be_realm "" --hotel_realm benchrealm1 \
-  --units "Req/sec,2-srv,Scale 1->2 srv" \
+  --units "Req/sec,2-srv,Scale 1→2 srv" \
   --title "x" --total_ncore 32 --prefix "imgresize-" \
   --xmin 10000 --xmax 65000 #--legend_on_right 
 echo "Done generating lazy cossim graph..."

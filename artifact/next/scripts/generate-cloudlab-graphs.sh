87603046108c1799d738a2ae7fa4b40e5a73ad97
Ariel Szekely <arielck@mit.edu>
2025-06-17 08:43:18 -0400

    x

--- artifact/next/scripts/generate-cloudlab-graphs.sh
+++ artifact/next/scripts/generate-cloudlab-graphs.sh
@@ -9,5 +9,5 @@ GRAPH_OUT_DIR=$ROOT_DIR/benchmarks/results/graphs
 
 # CosSim scaling
 echo "Generating cossim graph..."
-$GRAPH_SCRIPTS_DIR/aggregate-tpt-talk.py --input_load_label "cossim-srv" --measurement_dir_sigmaos $RES_OUT_DIR/cos_sim_tail_latency_eager_no_scale_cossim_nsrv_2 --measurement_dir_k8s $RES_OUT_DIR/cos_sim_tail_latency_eager_scale_cossim_add_1 --out $GRAPH_OUT_DIR/cossim_tail_latency_scale_vs_noscale.pdf --be_realm "" --hotel_realm benchrealm1 --units "Req/sec,2-srv,Scale 1->2 srv" --title "x" --total_ncore 32 --prefix "imgresize-" #--xmin 20000 --xmax 35000 #--legend_on_right 
+$GRAPH_SCRIPTS_DIR/aggregate-tpt-talk.py --input_load_label "cossim-srv" --measurement_dir_sigmaos $RES_OUT_DIR/cos_sim_tail_latency_eager_no_scale_cossim_nsrv_2 --measurement_dir_k8s $RES_OUT_DIR/cos_sim_tail_latency_eager_scale_cossim_add_1 --out $GRAPH_OUT_DIR/cossim_tail_latency_scale_vs_noscale.pdf --be_realm "" --hotel_realm benchrealm1 --units "Req/sec,2-srv,Scale 1->2 srv" --title "x" --total_ncore 32 --prefix "imgresize-" --xmin 10000 --xmax 65000 #--legend_on_right 
 echo "Done generating cossim graph..."

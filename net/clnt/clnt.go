92dd18ec9a66e88a7b306224632abdcaeba1f7bb
Frans Kaashoek <kaashoek@mit.edu>
2025-06-25 17:24:12 -0400

    Bubble up the last error on connect failure

--- net/clnt/clnt.go
+++ net/clnt/clnt.go
@@ -34,6 +34,8 @@ func NewNetClnt(pe *proc.ProcEnv, npc *dialproxyclnt.DialProxyClnt, ep *sp.Tendp
 
 func (nc *NetClnt) connect(ep *sp.Tendpoint) (net.Conn, *serr.Err) {
 	db.DPrintf(db.NETCLNT, "NetClnt connect to any of %v, starting w. %v\n", ep, ep.Addrs()[0])
+
+	var r error
 	for i, addr := range ep.Addrs() {
 		if i > 0 {
 			ep.Addr = append(ep.Addr[1:], ep.Addr[0])
@@ -41,11 +43,12 @@ func (nc *NetClnt) connect(ep *sp.Tendpoint) (net.Conn, *serr.Err) {
 		c, err := nc.npc.Dial(ep)
 		if err != nil {
 			db.DPrintf(db.NETCLNT_ERR, "Dial %v err %v", addr.IPPort(), err)
+			r = err
 			continue
 		}
 		db.DPrintf(db.NETCLNT, "NetClnt connected %v -> %v", c.LocalAddr(), addr)
 		return c, nil
 	}
 	db.DPrintf(db.NETCLNT_ERR, "NetClnt unable to connect to any of %v", ep)
-	return nil, serr.NewErr(serr.TErrUnreachable, "no connection")
+	return nil, serr.NewErr(serr.TErrUnreachable, r)
 }

f76d90f7a73c545bf4aa1dcbf820d404361dab3f
Ariel Szekely <arielck@mit.edu>
2025-07-18 17:10:40 -0400

    Wrap rust script imports in a module to avoid having to put unsafe blocks everywhere, and allow name mangling by declaring export names for callable functions in wasm scripts

--- rs/wasm/hello-wasm/src/lib.rs
+++ rs/wasm/hello-wasm/src/lib.rs
@@ -1,18 +1,26 @@
-#[link(wasm_import_module = "sigmaos_host")]
-extern "C" {
-    pub fn log(i: i32);
+mod sigmaos {
+    mod sigmaos_host {
+        #[link(wasm_import_module = "sigmaos_host")]
+        extern "C" {
+            pub fn log(i: i32);
+        }
+    }
+
+    pub fn log(i: i32) {
+        unsafe {
+            sigmaos_host::log(i);
+        }
+    }
 }
 
-#[no_mangle]
+#[export_name = "add"]
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
-#[no_mangle]
+#[export_name = "add_and_log"]
 pub fn add_and_log(left: usize, right: usize) -> usize {
     let n: i32 = 1234543;
-    unsafe {
-        log(n);
-    }
+    sigmaos::log(n);
     left + right
 }

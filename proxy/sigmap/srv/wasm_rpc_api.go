a83b37ed7161990efc925d217d8e70c3daea2f86
Ariel Szekely <arielck@mit.edu>
2025-07-23 09:19:57 -0400

    Implement WASM RPC proxy API for sigmasrv

--- proxy/sigmap/srv/wasm_rpc_api.go
+++ proxy/sigmap/srv/wasm_rpc_api.go
@@ -1,11 +1,18 @@
 package srv
 
 import (
+	"fmt"
+
+	"google.golang.org/protobuf/proto"
+
 	db "sigmaos/debug"
 	"sigmaos/proc"
 	rpcclnt "sigmaos/rpc/clnt"
+	rpcproto "sigmaos/rpc/proto"
+	"sigmaos/serr"
 	sessp "sigmaos/session/proto"
 	sigmaclnt "sigmaos/sigmaclnt"
+	sp "sigmaos/sigmap"
 	// wasmrt "sigmaos/proxy/wasm/rpc/wasmer"
 )
 
@@ -27,7 +34,7 @@ func (wp *WASMRPCProxy) Send(rpcIdx uint64, pn string, method string, b []byte, 
 	// Wrap the marshaled RPC byte slice in an RPC wrapper
 	iniov, err := rpcclnt.WrapMarshaledRPCRequest("CacheSrv.MultiGet", sessp.IoVec{b})
 	if err != nil {
-		db.DPrintf(db.SPPROXYSRV_ERR, "Error wrap & marshal WASM-proxied RPC request: %v", err)
+		db.DPrintf(db.SPPROXYSRV_ERR, "[%v] Error wrap & marshal WASM-proxied RPC request: %v", wp.p.GetPid(), err)
 		return err
 	}
 	// Run the delegated RPC asynchronously
@@ -36,6 +43,23 @@ func (wp *WASMRPCProxy) Send(rpcIdx uint64, pn string, method string, b []byte, 
 }
 
 func (wp *WASMRPCProxy) Recv(rpcIdx uint64) ([]byte, error) {
-	db.DFatalf("Unimplemented")
-	return nil, nil
+	outiov, err := wp.spp.psm.GetReply(wp.p.GetPid(), rpcIdx)
+	if err != nil {
+		db.DPrintf(db.SPPROXYSRV_ERR, "[%v] Err GetReply(%v) in WasmRPCRecv: %v", wp.p.GetPid(), err)
+		return nil, err
+	}
+	rep := &rpcproto.Rep{}
+	if err := proto.Unmarshal(outiov[0], rep); err != nil {
+		db.DPrintf(db.SPPROXYSRV_ERR, "[%v] Err Unmarshal(%v) in WasmRPCRecv: %v", wp.p.GetPid(), err)
+		return nil, serr.NewErrError(err)
+	}
+	if rep.Err.ErrCode != 0 {
+		db.DPrintf(db.SPPROXYSRV_ERR, "[%v] Err ErrCode(%v) in WasmRPCRecv: %v", wp.p.GetPid(), rep.Err.ErrCode)
+		return nil, sp.NewErr(rep.Err)
+	}
+	if len(outiov) != 2 {
+		db.DPrintf(db.SPPROXYSRV_ERR, "[%v] Err RPC(%v) unexpected outiov len: %v", wp.p.GetPid(), len(outiov))
+		return nil, serr.NewErrError(fmt.Errorf("Err unexpected outiov len: %v", len(outiov)))
+	}
+	return outiov[1], nil
 }

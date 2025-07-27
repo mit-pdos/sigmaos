a0c94b7fe8119f43de6f8288702e33ad80433be4
Ariel Szekely <arielck@mit.edu>
2025-07-27 13:57:55 -0400

    Fix error in WASM boot script which led to out-of-bounds vec access

--- rs/wasm/cached_backup_hot_shard_boot/src/lib.rs
+++ rs/wasm/cached_backup_hot_shard_boot/src/lib.rs
@@ -36,7 +36,7 @@ pub fn boot(b: *mut c_char, buf_sz: usize) {
     // Initial buffer contents are the 4-byte n_srv and the 8-byte n_keys
     for i in 0..shard_req_rpcs.len() {
         let rpc_idx = i + 1;
-        let rpc_bytes = shard_req_rpcs[rpc_idx].write_to_bytes().unwrap();
+        let rpc_bytes = shard_req_rpcs[i].write_to_bytes().unwrap();
         sigmaos::send_rpc(
             buf,
             rpc_idx.try_into().unwrap(),

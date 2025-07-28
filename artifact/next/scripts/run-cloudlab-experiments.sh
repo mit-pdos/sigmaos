4b0fa421c4a6bc2da22209e1cd172c0a88ed08ed
Ariel Szekely <arielck@mit.edu>
2025-07-28 09:36:49 -0400

    x

--- artifact/next/scripts/run-cloudlab-experiments.sh
+++ artifact/next/scripts/run-cloudlab-experiments.sh
@@ -57,12 +57,22 @@ AWS_VPC_LARGE=vpc-0affa7f07bd923811
 
 mkdir -p $LOG_DIR
 
+#if [ $EXP == "all" ] || [ $EXP == "cossim" ]; then
+##  if [ $RERUN == "true" ]; then
+##    echo "Clearing any cached CosSim data..."
+##    rm -rf benchmarks/results/$VERSION/cos_sim_tail_latency_*
+##  fi
+#  echo "Generating CosSim data..."
+#  go clean -testcache; go test -v -timeout 0 sigmaos/benchmarks/remote --run TestScaleCosSim --parallelize --platform cloudlab --vpc none --tag $TAG --no-shutdown --version $VERSION --branch cpp 2>&1 | tee $LOG_DIR/cossim.out
+#  echo "Done generating CosSim data..."
+#fi
+
 if [ $EXP == "all" ] || [ $EXP == "cossim" ]; then
 #  if [ $RERUN == "true" ]; then
 #    echo "Clearing any cached CosSim data..."
 #    rm -rf benchmarks/results/$VERSION/cos_sim_tail_latency_*
 #  fi
   echo "Generating CosSim data..."
-  go clean -testcache; go test -v -timeout 0 sigmaos/benchmarks/remote --run TestScaleCosSim --parallelize --platform cloudlab --vpc none --tag $TAG --no-shutdown --version $VERSION --branch cpp 2>&1 | tee $LOG_DIR/cossim.out
+  go clean -testcache; go test -v -timeout 0 sigmaos/benchmarks/remote --run TestScaleCachedBackup --parallelize --platform cloudlab --vpc none --tag $TAG --no-shutdown --version $VERSION --branch cpp 2>&1 | tee $LOG_DIR/cache-backup.out
   echo "Done generating CosSim data..."
 fi

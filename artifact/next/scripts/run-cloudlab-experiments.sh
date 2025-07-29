7257a619a996b16ddc3587ed41f2106d1acf3a31
Ariel Szekely <arielck@mit.edu>
2025-07-29 16:30:34 -0400

    x

--- artifact/next/scripts/run-cloudlab-experiments.sh
+++ artifact/next/scripts/run-cloudlab-experiments.sh
@@ -40,7 +40,7 @@ if [ $# -gt 0 ]; then
     exit 1
 fi
 
-if [ $EXP != "all" ] && [ $EXP != "cossim" ]; then
+if [ $EXP != "all" ] && [ $EXP != "cossim" ] && [ $EXP != "cached" ]; then
   echo "Unkown experiment $EXP"
   usage
   exit 1
@@ -57,17 +57,17 @@ AWS_VPC_LARGE=vpc-0affa7f07bd923811
 
 mkdir -p $LOG_DIR
 
-#if [ $EXP == "all" ] || [ $EXP == "cossim" ]; then
-##  if [ $RERUN == "true" ]; then
-##    echo "Clearing any cached CosSim data..."
-##    rm -rf benchmarks/results/$VERSION/cos_sim_tail_latency_*
-##  fi
-#  echo "Generating CosSim data..."
-#  go clean -testcache; go test -v -timeout 0 sigmaos/benchmarks/remote --run TestScaleCosSim --parallelize --platform cloudlab --vpc none --tag $TAG --no-shutdown --version $VERSION --branch cpp 2>&1 | tee $LOG_DIR/cossim.out
-#  echo "Done generating CosSim data..."
-#fi
-
 if [ $EXP == "all" ] || [ $EXP == "cossim" ]; then
+#  if [ $RERUN == "true" ]; then
+#    echo "Clearing any cached CosSim data..."
+#    rm -rf benchmarks/results/$VERSION/cos_sim_tail_latency_*
+#  fi
+  echo "Generating CosSim data..."
+  go clean -testcache; go test -v -timeout 0 sigmaos/benchmarks/remote --run TestScaleCosSim --parallelize --platform cloudlab --vpc none --tag $TAG --no-shutdown --version $VERSION --branch cpp 2>&1 | tee $LOG_DIR/cossim.out
+  echo "Done generating CosSim data..."
+fi
+
+if [ $EXP == "all" ] || [ $EXP == "cached" ]; then
 #  if [ $RERUN == "true" ]; then
 #    echo "Clearing any cached CosSim data..."
 #    rm -rf benchmarks/results/$VERSION/cos_sim_tail_latency_*

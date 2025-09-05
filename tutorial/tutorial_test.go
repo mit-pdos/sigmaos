3129b82ccebd9786a07c46c839ba75e1844e5469
Ariel Szekely <arielck@mit.edu>
2025-09-05 10:31:12 -0400

    Fix tutorial test

--- tutorial/tutorial_test.go
+++ tutorial/tutorial_test.go
@@ -3,14 +3,16 @@ package tutorial_test
 import (
 	"testing"
 
-	//	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 
 	db "sigmaos/debug"
 	"sigmaos/test"
 )
 
 func TestStartStop(t *testing.T) {
-	ts := test.NewTstateAll(t)
-	db.DPrintf(db.TEST, "Successfully started SigmaOS")
+	ts, err := test.NewTstateAll(t)
+	if assert.Nil(t, err) {
+		db.DPrintf(db.TEST, "Successfully started SigmaOS")
+	}
 	ts.Shutdown()
 }

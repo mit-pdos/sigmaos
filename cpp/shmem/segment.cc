693b8e452c2bb4a7047232a7bc8f19e3e3bd4b06
Ariel Szekely <arielck@mit.edu>
2025-09-16 19:06:34 -0400

    Increase shmem segment size

--- cpp/shmem/segment.cc
+++ cpp/shmem/segment.cc
@@ -1,8 +1,8 @@
 #include <fcntl.h>
 #include <shmem/segment.h>
+#include <sys/shm.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#include <sys/shm.h>
 
 #include <cstring>
 
@@ -17,13 +17,12 @@ std::expected<int, sigmaos::serr::Error> Segment::Init() {
   if (_id == -1) {
     return std::unexpected(
         sigmaos::serr::Error(sigmaos::serr::Terror::TErrError,
-                             std::format("err shmget key {}", (uint64_t) _key)));
+                             std::format("err shmget key {}", (uint64_t)_key)));
   }
   _buf = shmat(_id, nullptr, 0666);
   if (_buf == (void *)-1) {
-    return std::unexpected(
-        sigmaos::serr::Error(sigmaos::serr::Terror::TErrError,
-                             std::format("err shmat")));
+    return std::unexpected(sigmaos::serr::Error(
+        sigmaos::serr::Terror::TErrError, std::format("err shmat")));
   }
   return 0;
 }
@@ -31,15 +30,13 @@ std::expected<int, sigmaos::serr::Error> Segment::Init() {
 std::expected<int, sigmaos::serr::Error> Segment::Destroy() {
   int res = shmdt(_buf);
   if (res != 0) {
-    return std::unexpected(
-        sigmaos::serr::Error(sigmaos::serr::Terror::TErrError,
-                             std::format("err shmdt")));
+    return std::unexpected(sigmaos::serr::Error(
+        sigmaos::serr::Terror::TErrError, std::format("err shmdt")));
   }
   res = shmctl(_id, IPC_RMID, nullptr);
   if (res != 0) {
-    return std::unexpected(
-        sigmaos::serr::Error(sigmaos::serr::Terror::TErrError,
-                             std::format("err rmshmem")));
+    return std::unexpected(sigmaos::serr::Error(
+        sigmaos::serr::Terror::TErrError, std::format("err rmshmem")));
   }
   return 0;
 }
--- cpp/shmem/segment.h
+++ cpp/shmem/segment.h
@@ -2,9 +2,9 @@
 
 #include <serr/serr.h>
 #include <shmem/shmem.h>
+#include <sigmap/const.h>
 #include <sys/types.h>
 #include <util/log/log.h>
-#include <sigmap/const.h>
 
 #include <cstddef>
 #include <expected>
@@ -14,7 +14,7 @@
 namespace sigmaos {
 namespace shmem {
 
-const size_t SEGMENT_SZ = 10 * sigmaos::sigmap::constants::MBYTE;
+const size_t SEGMENT_SZ = 40 * sigmaos::sigmap::constants::MBYTE;
 
 const std::string SHMEM = "SHMEM";
 const std::string SHMEM_ERR = SHMEM + sigmaos::util::log::ERR;

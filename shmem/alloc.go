17de19205233d00b4a6dc082b8ae915df6194607
Ariel Szekely <arielck@mit.edu>
2025-09-20 14:03:02 -0400

    Add sanity check to shmem allocator

--- shmem/alloc.go
+++ shmem/alloc.go
@@ -3,6 +3,7 @@ package shmem
 import (
 	"sync/atomic"
 
+	db "sigmaos/debug"
 	"sigmaos/malloc"
 )
 
@@ -20,8 +21,13 @@ func NewAllocator(s *Segment) malloc.Allocator {
 func (a *Allocator) Alloc(b *[]byte, sz int) {
 	// Atomically claim the next sz bytes
 	endOff := a.off.Add(uint64(sz))
+	shmemBuf := a.s.GetBuf()
+	// Sanity check
+	if int(endOff) >= len(shmemBuf) {
+		db.DFatalf("Err shmem delegated RPC reply too long: %v >= %v", endOff, len(shmemBuf))
+	}
 	// Calculate offset to the start of the buffer
 	startOff := int(endOff) - sz
 	// Set the buffer to point into the shared memory segment
-	*b = a.s.GetBuf()[startOff:endOff]
+	*b = shmemBuf[startOff:endOff]
 }

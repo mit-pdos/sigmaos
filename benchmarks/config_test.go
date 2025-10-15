b19036eaf4a0c68166250e7d3df0e6a660cf075b
Ariel Szekely <arielck@mit.edu>
2025-10-15 17:13:22 -0400

    Add a TestMain function to parse config flags

--- benchmarks/config_test.go
+++ benchmarks/config_test.go
@@ -1,29 +1,30 @@
 package benchmarks_test
 
 import (
-	//	"encoding/json"
-	//	"flag"
+	"encoding/json"
+	"flag"
+	"testing"
 
 	"sigmaos/benchmarks"
-	// db "sigmaos/debug"
-	// sp "sigmaos/sigmap"
+	db "sigmaos/debug"
+	sp "sigmaos/sigmap"
 )
 
 var CosSimBenchConfig *benchmarks.CosSimBenchConfig
 
 var cossimBenchCfgStr string
 
-//func init() {
-//	flag.StringVar(&cossimBenchCfgStr, "cossim_bench_cfg", sp.NOT_SET, "JSON string for CosSimBenchConfig")
-//}
-//
-//func init() {
-//	flag.Parse()
-//	if cossimBenchCfgStr != "" {
-//		err := json.Unmarshal([]byte(cossimBenchCfgStr), &CosSimBenchConfig)
-//		if err != nil {
-//			db.DFatalf("Error unmarshaling cossim_bench_cfg: %v", err)
-//		}
-//		db.DPrintf(db.ALWAYS, "Loaded CosSimBenchConfig: %v", CosSimBenchConfig)
-//	}
-//}
+func init() {
+	flag.StringVar(&cossimBenchCfgStr, "cossim_bench_cfg", sp.NOT_SET, "JSON string for CosSimBenchConfig")
+}
+
+func TestMain(t *testing.M) {
+	flag.Parse()
+	if cossimBenchCfgStr != "" {
+		err := json.Unmarshal([]byte(cossimBenchCfgStr), &CosSimBenchConfig)
+		if err != nil {
+			db.DFatalf("Error unmarshaling cossim_bench_cfg: %v", err)
+		}
+		db.DPrintf(db.ALWAYS, "Loaded CosSimBenchConfig: %v", CosSimBenchConfig)
+	}
+}

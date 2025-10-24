bb4b6a14787fe36b3369398d2a4658eae2dede62
Ariel Szekely <arielck@mit.edu>
2025-10-24 14:34:54 -0400

    x

--- apps/cossim/srv/autoscale.go
+++ apps/cossim/srv/autoscale.go
@@ -3,21 +3,38 @@ package cossim
 import (
 	"fmt"
 
+	"sigmaos/apps/cossim/clnt"
 	"sigmaos/autoscale"
+	db "sigmaos/debug"
 )
 
 type RequestsInFlightMetric struct {
-	rif float64
+	rif  float64
+	cssc *clnt.CosSimShardClnt
 }
 
-func NewRequestsInFlightMetric() autoscale.Metric {
+func NewRequestsInFlightMetric(cssc *clnt.CosSimShardClnt) autoscale.Metric {
 	return &RequestsInFlightMetric{
-		rif: 0.0,
+		rif:  0.0,
+		cssc: cssc,
 	}
 }
 
 func (rif *RequestsInFlightMetric) update() {
-	// TODO
+	metrics, err := rif.cssc.GetAllServerMetrics()
+	if err != nil {
+		db.DPrintf(db.AUTOSCALER_ERR, "Err GetAllServerMetrics: %v", err)
+		return
+	}
+	if len(metrics) == 0 {
+		rif.rif = 0.0
+		return
+	}
+	var total uint64
+	for _, m := range metrics {
+		total += m.RIF
+	}
+	rif.rif = float64(total) / float64(len(metrics))
 }
 
 func (rif *RequestsInFlightMetric) GetValue() float64 {

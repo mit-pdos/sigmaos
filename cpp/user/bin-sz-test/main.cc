166661eea0639d8c82cc9c9cf5f60e45e171d830
Ariel Szekely <arielck@mit.edu>
2025-10-28 20:49:46 -0400

    x

--- cpp/user/bin-sz-test/main.cc
+++ cpp/user/bin-sz-test/main.cc
@@ -1,8 +1,11 @@
 #include <iostream>
-#include <util/log/log.h>
+#include <format>
+#include <string>
+//#include <util/log/log.h>
 
-int main() {
+int main(int argc, char **argv) {
   printf("Hi\n");
-  log(ALWAYS, "Hello!");
+  std::string f = std::format("num args %d", argc);
+  printf("Hi 2: %s\n", f.c_str());
   return 0;
 }

e560bf64995d4883b236bfc3f252b151eb0fdb50
Nicolas Camenisch <garnele007@gmail.com>
2026-04-18 16:08:06 +0000

    Fix import benchmarks timing

--- pyproc/benchmarks/import/is_even/main.py
+++ pyproc/benchmarks/import/is_even/main.py
@@ -1,10 +1,12 @@
 import time
-from splib.utils import log_spawn_latency
 
 start_import_ns = time.time_ns()
 import is_even
-log_spawn_latency("Python ImportModules", op_start=start_import_ns)
+end_import_ns = time.time_ns()
 
 import splib
+from splib.utils import log_spawn_latency
+log_spawn_latency("Python ImportModules", op_start=start_import_ns, now_ns=end_import_ns)
+
 splib.started()
 splib.exited(splib.Status.Ok, "Exited normally!")

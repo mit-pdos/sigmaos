ce5d0d7f7b3b8d3fcf82cc0669647efc560ec43c
Ariel Szekely <arielck@mit.edu>
2026-05-06 20:10:46 -0400

    Add some blink tests

--- blink/blink_test.go
+++ blink/blink_test.go
@@ -11,7 +11,9 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	imgrec_py "sigmaos/apps/imgrec/py"
+	imgrectestutil "sigmaos/apps/imgrec/testutil"
 	db "sigmaos/debug"
+	"sigmaos/proc"
 	sp "sigmaos/sigmap"
 	"sigmaos/test"
 )
@@ -21,29 +23,35 @@ const (
 	imgKey      = "img-save/8.jpg"
 	modelBucket = "9ps3"
 	modelKey    = "mobilenetv2-12.onnx"
-	kid = "~local"
+	kid         = "~local"
 )
 
-func TestImgrecBlink(t *testing.T) {
+func startBlinkd(t *testing.T) *exec.Cmd {
 	_, testFile, _, _ := runtime.Caller(0)
 	repoRoot := filepath.Join(filepath.Dir(testFile), "..")
 	blinkdBin := filepath.Join(repoRoot, "bin", "kernel", "blinkd")
 	logFile, err := os.Create("/tmp/blinkd.out")
 	if !assert.Nil(t, err, "blinkd log create: %v", err) {
-		return
+		return nil
 	}
-	defer logFile.Close()
 	cmd := exec.Command(blinkdBin, "test")
 	cmd.Stdout = logFile
 	cmd.Stderr = logFile
 	if err := cmd.Start(); err != nil {
 		assert.Nil(t, err, "blinkd Start: %v", err)
+		return nil
+	}
+	// Give blinkd time to finish Caladan setup and start listening.
+	time.Sleep(2 * time.Second)
+	return cmd
+}
+
+func TestImgrecBlink(t *testing.T) {
+	cmd := startBlinkd(t)
+	if cmd == nil {
 		return
 	}
 	defer cmd.Process.Kill()
-
-	// Give blinkd time to finish Caladan setup and start listening.
-	time.Sleep(2 * time.Second)
 
 	mrts, err := test.NewMultiRealmTstate(t, []sp.Trealm{test.REALM1})
 	if !assert.Nil(t, err, "Error New Tstate: %v", err) {
@@ -52,12 +60,72 @@ func TestImgrecBlink(t *testing.T) {
 	defer mrts.Shutdown()
 
 	rts := mrts.GetRealm(test.REALM1)
+	ref := imgrectestutil.GetReferenceOutput(t, rts.FsLib, imgBucket, imgKey, modelBucket, modelKey, kid)
 
 	conf := imgrec_py.NewImgrecPyJobConfig(imgBucket, imgKey, modelBucket, modelKey, kid, false, false, 0, 0)
 	job, err := imgrec_py.NewImgrecBlinkJob(conf, rts.SigmaClnt)
-	assert.Nil(t, err, "NewImgrecBlinkJob: %v", err)
+	if !assert.Nil(t, err, "NewImgrecBlinkJob: %v", err) {
+		return
+	}
 
 	msg, err := job.Run(sp.NOT_SET)
 	assert.Nil(t, err, "Run: %v", err)
 	db.DPrintf(db.TEST, "imgrec blink pred: %v", msg)
+	imgrectestutil.AssertMatchesReference(t, msg, ref)
+}
+
+func TestImgrecBlinkCoSandbox(t *testing.T) {
+	cmd := startBlinkd(t)
+	if cmd == nil {
+		return
+	}
+	defer cmd.Process.Kill()
+
+	mrts, err := test.NewMultiRealmTstate(t, []sp.Trealm{test.REALM1})
+	if !assert.Nil(t, err, "Error New Tstate: %v", err) {
+		return
+	}
+	defer mrts.Shutdown()
+
+	rts := mrts.GetRealm(test.REALM1)
+	ref := imgrectestutil.GetReferenceOutput(t, rts.FsLib, imgBucket, imgKey, modelBucket, modelKey, kid)
+
+	conf := imgrec_py.NewImgrecPyJobConfig(imgBucket, imgKey, modelBucket, modelKey, kid, true, false, 0, 0)
+	job, err := imgrec_py.NewImgrecBlinkJob(conf, rts.SigmaClnt)
+	if !assert.Nil(t, err, "NewImgrecBlinkJob: %v", err) {
+		return
+	}
+
+	msg, err := job.Run(sp.NOT_SET)
+	assert.Nil(t, err, "Run: %v", err)
+	db.DPrintf(db.TEST, "imgrec blink cosandbox pred: %v", msg)
+	imgrectestutil.AssertMatchesReference(t, msg, ref)
+}
+
+func TestImgrecBlinkShmem(t *testing.T) {
+	cmd := startBlinkd(t)
+	if cmd == nil {
+		return
+	}
+	defer cmd.Process.Kill()
+
+	mrts, err := test.NewMultiRealmTstate(t, []sp.Trealm{test.REALM1})
+	if !assert.Nil(t, err, "Error New Tstate: %v", err) {
+		return
+	}
+	defer mrts.Shutdown()
+
+	rts := mrts.GetRealm(test.REALM1)
+	ref := imgrectestutil.GetReferenceOutput(t, rts.FsLib, imgBucket, imgKey, modelBucket, modelKey, kid)
+
+	conf := imgrec_py.NewImgrecPyJobConfig(imgBucket, imgKey, modelBucket, modelKey, kid, true, false, proc.Tmem(256), 0)
+	job, err := imgrec_py.NewImgrecBlinkJob(conf, rts.SigmaClnt)
+	if !assert.Nil(t, err, "NewImgrecBlinkJob: %v", err) {
+		return
+	}
+
+	msg, err := job.Run(sp.NOT_SET)
+	assert.Nil(t, err, "Run: %v", err)
+	db.DPrintf(db.TEST, "imgrec blink shmem pred: %v", msg)
+	imgrectestutil.AssertMatchesReference(t, msg, ref)
 }

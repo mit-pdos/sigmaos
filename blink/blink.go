a899bd042f3be523f3c9b13fcc534e453631bcd7
Ariel Szekely <arielszekely@gmail.com>
2026-05-08 09:23:35 -0400

    Have blink read from an NFS-mounted chroot

--- blink/blink.go
+++ blink/blink.go
@@ -1,14 +1,14 @@
 package blink
 
 const (
-	BLINK_PORT  = 50055
-	DTAP0_ADDR  = "192.168.120.1"
+	BLINK_PORT = 50055
+	DTAP0_ADDR = "192.168.120.1"
 
 	BLINK_RESULTS = "/home/arielck/blink-ae/results/blink.recent"
 
 	JUNCTION_RUN    = "/home/arielck/blink-ae/junction/build/junction/junction_run"
 	JUNCTION_CONFIG = BLINK_RESULTS + "/junction.config"
-	JUNCTION_CHROOT = "/home/arielck/blink-ae/chroot"
+	JUNCTION_CHROOT = "/tmp/blink-nfs-mounts/chroot"
 
 	CHROOT_MOUNT_SCRIPT  = "/home/arielck/blink-ae/scripts/chroot_mount.sh"
 	CALADAN_SETUP_SCRIPT = "/home/arielck/blink-ae/junction/lib/caladan/scripts/setup_machine.sh"
